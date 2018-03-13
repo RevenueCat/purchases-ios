477134d3a5933d6c792184a62314ef86488e96fb
Jacob Eiting <jacob.eiting@gmail.com>
2018-03-13 13:16:43 -0700

    Version 0.10.0

--- Purchases/Classes/Public/RCPurchases.m
+++ Purchases/Classes/Public/RCPurchases.m
@@ -54,7 +54,7 @@ NSString * RCAppUserDefaultsKey = @"com.revenuecat.userdefaults.appUserID";
                       userDefaults:userDefaults];
 }
 + (NSString *)frameworkVersion {
-    return @"0.10.0-SNAPSHOT";
+    return @"0.10.0";
 }
 
 - (instancetype _Nullable)initWithAppUserID:(NSString *)appUserID
