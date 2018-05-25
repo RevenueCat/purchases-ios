57383669ccdda5f1eb797f54006d4db58c50576f
Jacob Eiting <jacob.eiting@gmail.com>
2018-05-25 10:00:18 -0700

    Getting tests to run.

--- PurchasesTests/PurchasesTests-Bridging-Header.h
+++ PurchasesTests/PurchasesTests-Bridging-Header.h
@@ -2,8 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include <Purchases/RCHTTPClient.h>
 #include <Purchases/RCPurchases.h>
+#include <Purchases/RCHTTPClient.h>
 #include <Purchases/RCPurchases+Protected.h>
 #include <Purchases/RCStoreKitRequestFetcher.h>
 #include <Purchases/RCStoreKitWrapper.h>
