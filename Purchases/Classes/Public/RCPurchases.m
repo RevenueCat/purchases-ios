6700272b8dc04db1b1cd022010770ca9d5fae14a
Jacob Eiting <jacob.eiting@gmail.com>
2018-07-05 16:15:09 -0700

    Prepare for next version.

--- Purchases/Classes/Public/RCPurchases.m
+++ Purchases/Classes/Public/RCPurchases.m
@@ -74,7 +74,7 @@ NSString * RCPurchaserInfoAppUserDefaultsKeyBase = @"com.revenuecat.userdefaults
 }
 
 + (NSString *)frameworkVersion {
-    return @"1.0.3";
+    return @"1.1.0-SNAPSHOT";
 }
 
 - (instancetype _Nullable)initWithAppUserID:(NSString *)appUserID
