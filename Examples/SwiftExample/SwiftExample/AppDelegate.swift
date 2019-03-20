8bd326c4ab7e0b2756c57ca104efbdcf481725c5
Cesar de la Vega <cesarvegaro@gmail.com>
2019-03-20 16:24:11 -0700

    revert changes to appdelegate

--- Examples/SwiftExample/SwiftExample/AppDelegate.swift
+++ Examples/SwiftExample/SwiftExample/AppDelegate.swift
@@ -10,30 +10,17 @@ import UIKit
 import Purchases
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, PurchasesDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var deferment: RCDeferredPromotionalPurchaseBlock? = nil
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         Purchases.debugLogsEnabled = true
         Purchases.configure(withAPIKey: "AahhcxDdDwYNehVmpLnaKbzhEnwErcZm", appUserID: nil)
-        Purchases.shared.delegate = self
+        
         return true
     }
-    
-    func purchases(_ purchases: Purchases,
-                            shouldPurchasePromoProduct product: SKProduct,
-                            defermentBlock makeDeferredPurchase: @escaping RCDeferredPromotionalPurchaseBlock) {
-        makeDeferredPurchase { (transaction, purchaserInfo, error, userCancelled) in
-            if let e = error {
-                print("PURCHASE ERROR: - \(e.localizedDescription)")
-
-            } else if purchaserInfo?.activeEntitlements.contains("pro_cat") ?? false {
-                print("Purchased Pro Cats 🎉")
-            }
-        }
-    }
-
 }
 
