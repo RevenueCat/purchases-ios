af314346938897a66281196fbc07d45491367e5b
Cesar de la Vega <cesarvegaro@gmail.com>
2019-05-05 22:17:11 -0700

    Get class using NSClassFromString

--- Purchases/RCAttributionFetcher.m
+++ Purchases/RCAttributionFetcher.m
@@ -10,17 +10,37 @@
 #import <AdSupport/AdSupport.h>
 #if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
-#import <iAd/iAd.h>
+//#import <iAd/iAd.h>
 #endif
+
+@protocol FakeAdClient <NSObject>
+
+- (instancetype)sharedClient;
+- (void)requestAttributionDetailsWithBlock:(void (^)(NSDictionary<NSString *, NSObject *> * _Nullable attributionDetails, NSError * _Nullable error))completionHandler;
+
+@end
+
+@protocol FakeASIdentifierManager <NSObject>
+
+@property (nonnull, nonatomic, readonly) NSUUID *advertisingIdentifier;
+
+- (instancetype)sharedManager;
+
+@end
 
 @implementation RCAttributionFetcher : NSObject
 
 - (NSString * _Nullable)advertisingIdentifier
 {
     if (@available(iOS 6.0, macOS 10.14, *)) {
-        if ([ASIdentifierManager class]) {
-            return ASIdentifierManager.sharedManager.advertisingIdentifier.UUIDString;
+        id<FakeASIdentifierManager> asIdentifierManagerClass = (id<FakeASIdentifierManager>)NSClassFromString(@"ASIdentifierManager");
+        if (asIdentifierManagerClass) {
+            return asIdentifierManagerClass.sharedManager.advertisingIdentifier.UUIDString;
         }
+        
+//        if ([ASIdentifierManager class]) {
+//            return ASIdentifierManager.sharedManager.advertisingIdentifier.UUIDString;
+//        }
     }
     return nil;
 }
@@ -39,8 +59,13 @@
 - (void)adClientAttributionDetailsWithBlock:(void (^)(NSDictionary<NSString *, NSObject *> * _Nullable attributionDetails, NSError * _Nullable error))completionHandler
 {
 #if TARGET_OS_IPHONE
-    if ([ADClient class]) {
-        [ADClient.sharedClient requestAttributionDetailsWithBlock:completionHandler];
+//    if ([ADClient class]) {
+//        [ADClient.sharedClient requestAttributionDetailsWithBlock:completionHandler];
+//    }
+    id<FakeAdClient> adClientClass = (id<FakeAdClient>)NSClassFromString(@"ADClient");
+    
+    if (adClientClass) {
+        [[adClientClass sharedClient] requestAttributionDetailsWithBlock:completionHandler];
     }
 #endif
 }
