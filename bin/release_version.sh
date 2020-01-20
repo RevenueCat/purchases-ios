11435a96a90e79efffb4dfc97db209d3298f0138
aboedo <andresboedo@gmail.com>
2020-01-20 16:40:57 -0300

    [final] Automate iOS releases (#162)
    * added automation of releases from tagging to zipping the files
    
    * added support for passing in named parameters for current and next version
    
    * uncommented code
    
    * bundle updates
    
    * WIP updating version number
    
    * added incrementing number in rcPurchases to lane
    
    * updated gitignore, added updating release version in the release script
    
    * deleted unused update_version_number.sh, added creating a new branch and committing the next version
    
    * fixed bug where the version number would be bumped before checking for the previous number
    
    * fixed versions being accidentally overwritten
    
    * cleanup on release script, updated Releasing.md
    
    * updates from PR comments
    
    * removed the zipping source code step since it's done automatically by GitHub

---
+++ bin/release_version.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# release a version
+# usage:
+# release_version.sh -c x.y.z -n a.b.c, where c = current version and n = next release
+# example: release_version.sh -c 3.0.2 -n 3.1.0
+
+while getopts ":c:n:" opt; do
+  case $opt in
+    c) CURRENT_VERSION="$OPTARG"
+    ;;
+    n) NEXT_VERSION="$OPTARG-SNAPSHOT"
+    ;;
+    \?) echo "Invalid option -$OPTARG. Usage: release_version.sh -c <current> -n <next>" >&2
+    ;;
+  esac
+done
+
+printf "current version is $CURRENT_VERSION"
+printf "next version is $NEXT_VERSION"
+
+echo "Creating a tag and pushing"
+git tag -a $CURRENT_VERSION -m "Version $CURRENT_VERSION"
+git push --tags
+
+CARTHAGE_BUILDS_PATH=Carthage/Build
+CARTHAGE_IOS_PATH=$CARTHAGE_BUILDS_PATH/iOS
+CARTHAGE_MAC_PATH=$CARTHAGE_BUILDS_PATH/Mac
+CARTHAGE_UPLOADS_PATH=CarthageUploads
+
+cd ..
+
+echo "Releasing version $CURRENT_VERSION"
+
+echo "Creating RVM gemset (if needed) and activating..."
+rvm gemset use --create purchases-ios
+
+echo "Installing dependencies if needed..."
+bundle install
+
+echo "Pushing release to Cocoapods..."
+pod trunk push Purchases.podspec
+
+COCOAPODS_RESULT=$
+
+if [ $COCOAPODS_RESULT == 0 ]; then
+  echo "Successfully pushed v$CURRENT_VERSION to Cocoapods!"
+else
+  echo "Error pushing to Cocoapods, aborting"
+  exit $COCOAPODS_RESULT
+fi
+
+echo "Preparing Carthage release"
+echo "building..."
+carthage build --no-skip-current
+
+echo "creating uploads folder if needed"
+mkdir $CARTHAGE_UPLOADS_PATH
+
+echo "zipping carthage files"
+
+FRAMEWORK_NAME=Purchases.framework
+IOS_DSYM_NAME=$FRAMEWORK_NAME.dSYM
+
+zip -r $CARTHAGE_UPLOADS_PATH/$FRAMEWORK_NAME.zip $CARTHAGE_IOS_PATH/$FRAMEWORK_NAME
+zip -r $CARTHAGE_UPLOADS_PATH/$IOS_DSYM_NAME.zip $CARTHAGE_IOS_PATH/$IOS_DSYM_NAME
+zip -r $CARTHAGE_UPLOADS_PATH/$FRAMEWORK_NAME.mac.zip $CARTHAGE_MAC_PATH/$FRAMEWORK_NAME
+zip -r $CARTHAGE_UPLOADS_PATH/$IOS_DSYM_NAME.mac.zip $CARTHAGE_MAC_PATH/$IOS_DSYM_NAME
+
+echo "Don't forget to create a release in GitHub and upload them!"
+
+echo "Preparing next version"
+
+BRANCH_NAME=bump/$NEXT_VERSION
+echo "Creating branch $BRANCH_NAME"
+git checkout -b $BRANCH_NAME
+
+echo "bumping next version"
+fastlane bump version:$NEXT_VERSION
+
+echo "committing and pushing"
+git commit -am "Preparing for next version"
+git push origin $BRANCH_NAME
+
+echo "All set! Don't forget to create the new release in GitHub and upload the files in $CARTHAGE_UPLOADS_PATH!"
