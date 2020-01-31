6c34568748473795e2fbdb275c39f9efa5fee78c
Andy Boedo <andresboedo@gmail.com>
2020-01-31 19:26:53 -0300

    fixed call to the wrong method

--- Purchases/Public/RCPurchases.m
+++ Purchases/Public/RCPurchases.m
@@ -646,7 +646,7 @@ static BOOL _automaticAppleSearchAdsAttributionCollection = NO;
 
 - (void)invalidatePurchaserInfoCache {
     RCDebugLog(@"Purchaser info cache is invalidated");
-    [self.deviceCache clearOfferingsCacheTimestamp];
+    [self.deviceCache clearCachesTimestamp];
 }
 
 #pragma mark - Private Methods
