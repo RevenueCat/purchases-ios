22e06d6ead67d6c1686ec8d56b42866a5c3e77fe
Andy Boedo <andresboedo@gmail.com>
2020-01-31 20:54:04 -0300

    fixed inconsistent line spacing

--- Purchases/Caching/RCInMemoryCachedObject.h
+++ Purchases/Caching/RCInMemoryCachedObject.h
@@ -12,11 +12,16 @@ NS_ASSUME_NONNULL_BEGIN
 @interface RCInMemoryCachedObject<ObjectType> : NSObject
 
 - (BOOL)isCacheStale;
+
 - (void)clearCacheTimestamp;
+
 - (void)clearCache;
+
 - (void)updateCacheTimestampWithDate:(NSDate *)date;
+
 - (void)cacheInstance:(ObjectType)instance
                  date:(NSDate *)date;
+
 - (ObjectType _Nullable)cachedInstance;
 
 - (instancetype)initWithCacheDurationInSeconds:(int)cacheDurationInSeconds;
