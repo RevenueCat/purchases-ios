cfa7741c2f1783cf745f144c82f4d5528b32620a
Andy Boedo <andresboedo@gmail.com>
2020-02-11 11:25:14 -0300

    more fixes for APIs that are unavailable on watchOS

--- Purchases/RCCrossPlatformSupport.h
+++ Purchases/RCCrossPlatformSupport.h
@@ -6,21 +6,26 @@
 //  Copyright © 2019 RevenueCat. All rights reserved.
 //
 
-#if TARGET_OS_IPHONE
-#import <UIKit/UIKit.h>
+#if TARGET_OS_IOS || TARGET_OS_TV
 #define APP_DID_BECOME_ACTIVE_NOTIFICATION_NAME UIApplicationDidBecomeActiveNotification
 #elif TARGET_OS_OSX
-#import <AppKit/AppKit.h>
 #define APP_DID_BECOME_ACTIVE_NOTIFICATION_NAME NSApplicationDidBecomeActiveNotification
 #endif
+
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#elif TARGET_OS_OSX
+#import <AppKit/AppKit.h>
+#endif
+
 #if TARGET_OS_MACCATALYST
 #define PLATFORM_HEADER @"uikitformac"
 #elif TARGET_OS_IOS
 #define PLATFORM_HEADER @"iOS"
 #elif TARGET_OS_OSX
 #define PLATFORM_HEADER @"macOS"
-#elif TARGET_OS_WATCHOS
+#elif TARGET_OS_WATCH
 #define PLATFORM_HEADER @"watchOS"
-#elif TARGET_OS_TVOS
+#elif TARGET_OS_TV
 #define PLATFORM_HEADER @"tvOS"
 #endif
