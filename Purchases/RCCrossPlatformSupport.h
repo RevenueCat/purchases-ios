8129a479de8dfbbc202da6e51f50b5fbd267c769
Andy Boedo <andresboedo@gmail.com>
2020-02-11 10:00:21 -0300

    fixed compilation issues related to target platforms for watch os

--- Purchases/RCCrossPlatformSupport.h
+++ Purchases/RCCrossPlatformSupport.h
@@ -9,14 +9,18 @@
 #if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
 #define APP_DID_BECOME_ACTIVE_NOTIFICATION_NAME UIApplicationDidBecomeActiveNotification
-#elif TARGET_OS_MAC
+#elif TARGET_OS_OSX
 #import <AppKit/AppKit.h>
 #define APP_DID_BECOME_ACTIVE_NOTIFICATION_NAME NSApplicationDidBecomeActiveNotification
 #endif
 #if TARGET_OS_MACCATALYST
 #define PLATFORM_HEADER @"uikitformac"
-#elif TARGET_OS_IPHONE
+#elif TARGET_OS_IOS
 #define PLATFORM_HEADER @"iOS"
-#elif TARGET_OS_MAC
+#elif TARGET_OS_OSX
 #define PLATFORM_HEADER @"macOS"
+#elif TARGET_OS_WATCHOS
+#define PLATFORM_HEADER @"watchOS"
+#elif TARGET_OS_TVOS
+#define PLATFORM_HEADER @"tvOS"
 #endif
