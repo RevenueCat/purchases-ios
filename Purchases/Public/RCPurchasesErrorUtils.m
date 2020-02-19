4698be0453fe25696387ae0019946b996b0dc637
Andy Boedo <andresboedo@gmail.com>
2020-02-19 14:24:46 -0300

    checked availability for different apis and updated the macros accordingly

--- Purchases/Public/RCPurchasesErrorUtils.m
+++ Purchases/Public/RCPurchasesErrorUtils.m
@@ -150,7 +150,7 @@ static RCPurchasesErrorCode RCPurchasesErrorCodeFromRCBackendErrorCode(RCBackend
     return RCUnknownError;
 }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IPHONE
     #define CODE_IF_TARGET_IPHONE(code, value) code
 #else
     #define CODE_IF_TARGET_IPHONE(code, value) ((SKErrorCode) value)
