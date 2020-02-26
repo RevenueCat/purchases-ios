1b10a28ae97b1bc82c30d170bc36c1a872b69905
Andy Boedo <andresboedo@gmail.com>
2020-02-26 15:09:11 -0300

    added public methods for subscriber attributes in RCPurchases, subscribed to nsNotifications

---
+++ Purchases/SubscriberAttributes/RCPurchases+SubscriberAttributes.h
@@ -0,0 +1,21 @@
+//
+// Created by Andrés Boedo on 2/21/20.
+// Copyright (c) 2020 Purchases. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCPurchases.h"
+#import "RCSubscriberAttribute.h"
+
+@class RCSubscriberAttribute, RCSubscriberAttributesManager;
+
+@interface RCPurchases ()
+
+@property (nonatomic) RCSubscriberAttributesManager *subscriberAttributesManager;
+
+- (void)configureSubscriberAttributesManager;
+- (RCSubscriberAttributeDict)unsyncedAttributesByKey;
+- (void)markAttributesAsSynced:(RCSubscriberAttributeDict)syncedAttributes
+                     appUserID:(NSString *)appUserID;
+
+@end
