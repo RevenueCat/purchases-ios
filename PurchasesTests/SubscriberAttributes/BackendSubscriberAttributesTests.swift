d1490c9f49c1d306c459de2070704fdececdb3de
Andy Boedo <andresboedo@gmail.com>
2020-02-27 15:59:17 -0300

    added test to check that the correct params are sent to post attributes

--- PurchasesTests/SubscriberAttributes/BackendSubscriberAttributesTests.swift
+++ PurchasesTests/SubscriberAttributes/BackendSubscriberAttributesTests.swift
@@ -10,16 +10,33 @@ import Nimble
 import Purchases
 
 class BackendSubscriberAttributesTests: XCTestCase {
+    let appUserID = "abc123"
+    let now = Date()
+    var dateProvider: MockDateProvider!
+    var subscriberAttribute1: RCSubscriberAttribute!
+    var subscriberAttribute2: RCSubscriberAttribute!
+
+    override func setUp() {
+        dateProvider = MockDateProvider(stubbedNow: now)
+        subscriberAttribute1 = RCSubscriberAttribute(key: "a key",
+                                                     value: "a value",
+                                                     appUserID: appUserID,
+                                                     dateProvider: dateProvider)
+
+        subscriberAttribute2 = RCSubscriberAttribute(key: "another key",
+                                                     value: "another value",
+                                                     appUserID: appUserID,
+                                                     dateProvider: dateProvider)
+    }
 
     func testPostSubscriberAttributesSendsRightParameters() {
         let mockHTTPClient = MockHTTPClient(platformFlavor: "iPhone")
         guard let backend = RCBackend(httpClient: mockHTTPClient, apiKey: "key") else { fatalError() }
-        let appUserID = "abc123"
-        let subscriberAttribute = RCSubscriberAttribute(key: "a key",
-                                                        value: "a value",
-                                                        appUserID: appUserID)
 
-        backend.postSubscriberAttributes(["a key": subscriberAttribute], 
+        backend.postSubscriberAttributes([
+                                             subscriberAttribute1.key: subscriberAttribute1,
+                                             subscriberAttribute2.key: subscriberAttribute2
+                                         ],
                                          appUserID: appUserID,
                                          completion: { (error: Error!) in })
 
@@ -32,7 +49,21 @@ class BackendSubscriberAttributesTests: XCTestCase {
 
         expect(receivedParameters.HTTPMethod) == "POST"
         expect(receivedParameters.path) == "/subscribers/abc123/attributes"
-        expect(receivedParameters.requestBody).to(beNil())
+
+        let expectedBody: [String: [String: NSObject]] = [
+            "attributes": [
+                subscriberAttribute1.key: [
+                    "updated_at": subscriberAttribute1.setTime.timeIntervalSince1970,
+                    "value": subscriberAttribute1.value
+                ] as NSObject,
+                subscriberAttribute2.key: [
+                    "updated_at": subscriberAttribute2.setTime.timeIntervalSince1970,
+                    "value": subscriberAttribute2.value
+                ] as NSObject,
+            ]
+        ]
+
+        expect(receivedParameters.requestBody as? [String: [String: NSObject]]).to(equal(expectedBody))
         expect(receivedParameters.headers) == ["Authorization": "Bearer key"]
     }
 
