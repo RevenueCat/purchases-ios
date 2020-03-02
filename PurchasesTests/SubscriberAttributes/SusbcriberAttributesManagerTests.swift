1f43b4cfcbfa13fd0c55b1647fe8b4582ef260bc
Andy Boedo <andresboedo@gmail.com>
2020-03-02 12:36:22 -0300

    added test cases for syncAttributesIfNeeded

--- PurchasesTests/SubscriberAttributes/SusbcriberAttributesManagerTests.swift
+++ PurchasesTests/SubscriberAttributes/SusbcriberAttributesManagerTests.swift
@@ -13,6 +13,9 @@ class SubscriberAttributesManagerTests: XCTestCase {
     var mockBackend: MockBackend!
     var mockDeviceCache: MockDeviceCache!
     var subscriberAttributesManager: RCSubscriberAttributesManager!
+    var subscriberAttributeHeight: RCSubscriberAttribute!
+    var subscriberAttributeWeight: RCSubscriberAttribute!
+    var mockAttributes: [String: RCSubscriberAttribute]!
 
     override func setUp() {
         super.setUp()
@@ -20,6 +23,16 @@ class SubscriberAttributesManagerTests: XCTestCase {
         self.mockBackend = MockBackend()
         self.subscriberAttributesManager = RCSubscriberAttributesManager(backend: mockBackend,
                                                                          deviceCache: mockDeviceCache)
+        self.subscriberAttributeHeight = RCSubscriberAttribute(key: "height",
+                                                               value: "183",
+                                                               appUserID: "waldo")
+        self.subscriberAttributeWeight = RCSubscriberAttribute(key: "weight",
+                                                               value: "160",
+                                                               appUserID: "waldo")
+        self.mockAttributes = [
+            subscriberAttributeHeight.key: subscriberAttributeHeight,
+            subscriberAttributeWeight.key: subscriberAttributeWeight
+        ]
     }
 
     // MARK: setting attributes
@@ -46,7 +59,6 @@ class SubscriberAttributesManagerTests: XCTestCase {
         expect(attributesByKey["instrument"]?.isSynced) == false
         expect(attributesByKey["instrument"]?.appUserID) == "Stevie Ray Vaughan"
     }
-
 
     func testSetAttributesSkipsIfSameValue() {
         self.mockDeviceCache.stubbedSubscriberAttributeResult = RCSubscriberAttribute(key: "genre",
@@ -252,4 +264,126 @@ class SubscriberAttributesManagerTests: XCTestCase {
     }
 
     // MARK: syncing
+
+    func testSyncIfNeededWithAppUserIDSkipsIfNoUnsyncedAttributes() {
+        mockDeviceCache.stubbedNumberOfUnsyncedAttributesResult = 0
+        subscriberAttributesManager.syncIfNeeded(withAppUserID: "whoever", completion: { _ in })
+
+        expect(self.mockDeviceCache.invokedStore).toEventually(beFalse())
+        expect(self.mockBackend.invokedPostSubscriberAttributes).toEventually(beFalse())
+    }
+
+    func testSyncIfNeededWithAppUserIDCallsCompletionIfNoUnsyncedAttributes() {
+        mockDeviceCache.stubbedNumberOfUnsyncedAttributesResult = 0
+        var invokedCompletionCount = 0
+        var invokedError: Error? = nil
+        subscriberAttributesManager.syncIfNeeded(withAppUserID: "whoever", completion: { error in
+            invokedCompletionCount += 1
+            invokedError = error
+        })
+
+        expect(invokedError).toEventually(beNil())
+        expect(invokedCompletionCount).toEventually(equal(1))
+    }
+
+    func testSyncIfNeededWithAppUserMakesRightCallsToBackend() {
+        mockDeviceCache.stubbedNumberOfUnsyncedAttributesResult = UInt(mockAttributes.count)
+        mockDeviceCache.stubbedUnsyncedAttributesByKeyResult = mockAttributes
+
+        subscriberAttributesManager.syncIfNeeded(withAppUserID: "Rick Sanchez", completion: { _ in })
+
+        expect(self.mockDeviceCache.invokedNumberOfUnsyncedAttributesCount) == 1
+        expect(self.mockDeviceCache.invokedNumberOfUnsyncedAttributesCount) == 1
+        expect(self.mockBackend.invokedPostSubscriberAttributesCount) == 1
+
+        expect(self.mockBackend.invokedPostSubscriberAttributesParameters?.appUserID) == "Rick Sanchez"
+        expect(self.mockBackend.invokedPostSubscriberAttributesParameters?.subscriberAttributes) == mockAttributes
+    }
+
+    func testSyncIfNeededCallsCompletionAfterSyncing() {
+        mockDeviceCache.stubbedNumberOfUnsyncedAttributesResult = UInt(mockAttributes.count)
+        mockDeviceCache.stubbedUnsyncedAttributesByKeyResult = mockAttributes
+        mockBackend.stubbedPostSubscriberAttributesCompletionResult = (nil, ())
+
+        var receivedError: Error? = nil
+        var invokedCompletionCount = 0
+        subscriberAttributesManager.syncIfNeeded(withAppUserID: "Rick Sanchez", completion: { error in
+            receivedError = error
+            invokedCompletionCount += 1
+        })
+
+        expect(receivedError).toEventually(beNil())
+        expect(invokedCompletionCount).toEventually(equal(1))
+    }
+
+    func testSyncIfNeededWithAppUserIDMarksAttributesAsSyncedIfNoError() {
+        mockDeviceCache.stubbedNumberOfUnsyncedAttributesResult = UInt(mockAttributes.count)
+        mockDeviceCache.stubbedUnsyncedAttributesByKeyResult = mockAttributes
+        mockBackend.stubbedPostSubscriberAttributesCompletionResult = (nil, ())
+
+        subscriberAttributesManager.syncIfNeeded(withAppUserID: "Rick Sanchez", completion: { _ in })
+        assertMockAttributesSynced()
+    }
+
+    func testSyncIfNeededWithAppUserIDMarksSyncedIfMarkSyncedKeyPresent() {
+        mockDeviceCache.stubbedNumberOfUnsyncedAttributesResult = UInt(mockAttributes.count)
+        mockDeviceCache.stubbedUnsyncedAttributesByKeyResult = mockAttributes
+        let errorCode = Purchases.ErrorCode.unknownBackendError.rawValue
+        let mockError = NSError(domain: "error", code: errorCode, userInfo: ["shouldMarkSynced": "true"])
+        mockBackend.stubbedPostSubscriberAttributesCompletionResult = (mockError, ())
+
+        var receivedError: Error? = nil
+        subscriberAttributesManager.syncIfNeeded(withAppUserID: "Rick Sanchez", completion: { error in
+            receivedError = error
+        })
+
+        expect(receivedError as NSError?).toEventually(equal(mockError))
+        assertMockAttributesSynced()
+    }
+
+    func testSyncIfNeededWithAppUserIDDoesntMarkSyncedIfShouldMarkSyncedKeyNotPresent() {
+        mockDeviceCache.stubbedNumberOfUnsyncedAttributesResult = UInt(mockAttributes.count)
+        mockDeviceCache.stubbedUnsyncedAttributesByKeyResult = mockAttributes
+        let errorCode = Purchases.ErrorCode.networkError.rawValue
+        let mockError = NSError(domain: Purchases.ErrorDomain, code: errorCode, userInfo: [:])
+        mockBackend.stubbedPostSubscriberAttributesCompletionResult = (mockError, ())
+
+        var receivedError: Error? = nil
+        subscriberAttributesManager.syncIfNeeded(withAppUserID: "Rick Sanchez", completion: { error in
+            receivedError = error
+        })
+
+        expect(receivedError as NSError?).toEventually(equal(mockError))
+        expect(self.mockDeviceCache.invokedStoreSubscriberAttributesCount).toEventually(equal(0))
+    }
+}
+
+private extension SubscriberAttributesManagerTests {
+    func assertMockAttributesSynced() {
+        expect(self.mockDeviceCache.invokedStoreSubscriberAttributesCount).toEventually(equal(1))
+
+        guard let invokedParams = self.mockDeviceCache.invokedStoreSubscriberAttributesParameters else {
+            fatalError("no parameters for storeSubscriberAttributes found")
+        }
+        expect(invokedParams.attributesByKey).toNot(beEmpty())
+        let attributesByKey = invokedParams.attributesByKey
+
+        expect(attributesByKey[self.subscriberAttributeHeight.key]?.key)
+            .toEventually(equal(subscriberAttributeHeight.key))
+        expect(attributesByKey[self.subscriberAttributeHeight.key]?.value)
+            .toEventually(equal(subscriberAttributeHeight.value))
+        expect(attributesByKey[self.subscriberAttributeHeight.key]?.isSynced)
+            .toEventually(equal(true))
+        expect(attributesByKey[self.subscriberAttributeHeight.key]?.appUserID)
+            .toEventually(equal(subscriberAttributeHeight.appUserID))
+
+        expect(attributesByKey[self.subscriberAttributeWeight.key]?.key)
+            .toEventually(equal(subscriberAttributeWeight.key))
+        expect(attributesByKey[self.subscriberAttributeWeight.key]?.value)
+            .toEventually(equal(subscriberAttributeWeight.value))
+        expect(attributesByKey[self.subscriberAttributeWeight.key]?.isSynced)
+            .toEventually(equal(true))
+        expect(attributesByKey[self.subscriberAttributeWeight.key]?.appUserID)
+            .toEventually(equal(subscriberAttributeWeight.appUserID))
+    }
 }
