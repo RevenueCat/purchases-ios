924fc7af44ff3bbf62fa20b69f421647f5e11301
aboedo <andresboedo@gmail.com>
2020-03-03 14:10:25 -0300

    [Final] subscriber attributes part 1 (#187)
    * added subscriber attributes class and special subscriber attributes
    
    * added subscriber attributes to podspec and package.swift
    
    * added subscriber attributes to bridging header
    
    * moved new classes to private instead of project
    
    * added tests for RCSubscriberAttribute
    
    * added RCDateProvider and mockDateProvider
    
    * reduced precision for the check against now to account for floating point precision
    
    * slight improvement on date check
    
    * removed appUserID from subscriberAttribute

---
+++ Purchases/SubscriberAttributes/RCSpecialSubscriberAttributes.h
@@ -0,0 +1,15 @@
+//
+// Created by RevenueCat on 2/7/20.
+// Copyright (c) 2020 Purchases. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define SPECIAL_ATTRIBUTE_EMAIL @"$email"
+#define SPECIAL_ATTRIBUTE_PHONE_NUMBER @"$phoneNumber"
+#define SPECIAL_ATTRIBUTE_DISPLAY_NAME @"$displayName"
+#define SPECIAL_ATTRIBUTE_PUSH_TOKEN @"$apnsTokens"
+
+NS_ASSUME_NONNULL_END
