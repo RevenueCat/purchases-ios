01c0b85dab6bc0804ac7148a35bc31de94cbbb87
Andy Boedo <andresboedo@gmail.com>
2020-03-05 16:52:04 -0300

    added explicit exclusion of Info.plist file in the target to get rid of xcode 11.4 beta 3 warning

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@ func resolveTargets() -> [Target] {
         .target(name: "Purchases",
                 dependencies: [],
                 path: ".",
+                exclude: ["Purchases/Info.plist"],
                 sources: ["Purchases", "Purchases/Public"],
                 publicHeadersPath: "Purchases/Public",
                 cSettings: [
