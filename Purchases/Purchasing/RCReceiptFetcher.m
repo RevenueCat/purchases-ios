9729494b399f89fccb2a7b70f9771668308f3073
aboedo <andresboedo@gmail.com>
2020-06-20 15:12:53 -0300

    Feature/watch os app (#263)
    * added watch app
    
    added assets
    
    * added workaround for the watchOS bug where the sandbox receipt is stored in the production location.
    
    * ensured that the patch is only used in sandbox
    
    * podfile update

--- Purchases/Purchasing/RCReceiptFetcher.m
+++ Purchases/Purchasing/RCReceiptFetcher.m
@@ -8,12 +8,27 @@
 
 #import "RCReceiptFetcher.h"
 #import "RCLogUtils.h"
+#import "RCSystemInfo.h"
 
 @implementation RCReceiptFetcher : NSObject
 
-- (NSData *)receiptData
-{
+- (NSData *)receiptData {
     NSURL *receiptURL = [[NSBundle mainBundle] appStoreReceiptURL];
+    
+#if TARGET_OS_WATCH
+    // as of watchOS 6.2.8, there's a bug where the receipt is stored in the sandbox receipt location,
+    // but the appStoreReceiptURL method returns the URL for the production receipt.
+    // This code replaces "sandboxReceipt" with "receipt" as the last component of the receiptURL so that we get the
+    // correct receipt.
+    // This has been filed as radar FB7699277. More info in https://github.com/RevenueCat/purchases-ios/issues/207.
+    
+    if (RCSystemInfo.isSandbox) {
+        NSString *receiptURLFolder = [[receiptURL absoluteString] stringByDeletingLastPathComponent];
+        NSURL *productionReceiptURL = [NSURL URLWithString:[receiptURLFolder stringByAppendingPathComponent:@"receipt"]];
+        receiptURL = productionReceiptURL;
+    }
+#endif
+    
     NSData *data = [NSData dataWithContentsOfURL:receiptURL];
     RCDebugLog(@"Loaded receipt from %@", receiptURL);
     return data;
