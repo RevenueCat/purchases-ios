d237965426a2ef5a5de14e74047ae4980d80c9f2
Andy Boedo <andresboedo@gmail.com>
2020-07-01 14:02:23 -0300

    updated type of checkTRialOrIntroductoyPriceEligibility, since Dictionary -> NSDictionary conversion will automatically wrap Ints into NSNumbers. updated order of introElegibilityStatus enum.

--- Purchases/SwiftSources/LocalReceiptParsing/LocalReceiptParser.swift
+++ Purchases/SwiftSources/LocalReceiptParsing/LocalReceiptParser.swift
@@ -14,15 +14,15 @@ import Foundation
 
 private enum IntroEligibilityStatus: Int {
     case unknown,
-         eligible,
-         ineligible
+         ineligible,
+         eligible
 }
 
 @objc public class LocalReceiptParser: NSObject {
     
     @objc public func checkTrialOrIntroductoryPriceEligibility(withData data: Data,
                                                                productIdentifiers: [String],
-                                                               completion: ([String : NSNumber], Error?) -> Void) {
+                                                               completion: ([String : Int], Error?) -> Void) {
         completion([:], NSError(domain: "This method hasn't been implemented yet",
                                 code: LocalReceiptParserErrorCode.UnknownError.rawValue,
                                 userInfo: nil))
