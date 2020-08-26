e8f4bc534caa4e13f2968c2ecee175a554d7c7d0
Andy Boedo <andresboedo@gmail.com>
2020-08-26 12:53:16 -0300

    added a comment to explain the need for Purchases.swift

--- Purchases.swift
+++ Purchases.swift
@@ -5,5 +5,7 @@
 //  Created by Andrés Boedo on 8/24/20.
 //  Copyright © 2020 Purchases. All rights reserved.
 //
+// This file is mostly empty, but is needed so that Xcode can correctly
+// link Swift dependencies for the frameowrk.
 
 import Foundation
