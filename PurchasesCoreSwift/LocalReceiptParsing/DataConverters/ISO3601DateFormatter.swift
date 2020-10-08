8978fb503da7a53a5d5c02bcf44725c39160f153
aboedo <andresboedo@gmail.com>
2020-10-08 19:14:19 -0300

    fix parsing of dates in receipts with milliseconds  (#367)
    * fixed parsing of apple receipts when dates have millisecond resolution
    
    * escaped the dot so that it works correctly with international formats
    
    * learned to spell milliseconds

--- PurchasesCoreSwift/LocalReceiptParsing/DataConverters/ISO3601DateFormatter.swift
+++ PurchasesCoreSwift/LocalReceiptParsing/DataConverters/ISO3601DateFormatter.swift
@@ -8,16 +8,17 @@ import Foundation
 struct ISO3601DateFormatter {
     static let shared = ISO3601DateFormatter()
 
-    private let dateFormatter = DateFormatter()
+    private let secondsDateFormatter = DateFormatter()
+    private let milisecondsDateFormatter = DateFormatter()
 
     private init() {
-        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZ"
+        secondsDateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZ"
+        milisecondsDateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSSZ"
     }
 
     func date(fromBytes bytes: ArraySlice<UInt8>) -> Date? {
-        if let dateString = String(bytes: Array(bytes), encoding: .ascii) {
-            return dateFormatter.date(from: dateString)
-        }
-        return nil
+        guard let dateString = String(bytes: Array(bytes), encoding: .ascii) else { return nil }
+        return (secondsDateFormatter.date(from: dateString)
+            ?? milisecondsDateFormatter.date(from: dateString))
     }
 }
