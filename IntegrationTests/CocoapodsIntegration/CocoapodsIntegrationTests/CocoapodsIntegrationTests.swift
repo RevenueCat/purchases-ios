abe00d3938839b7187a7de92e9a13f928b5b2d83
Andy Boedo <andresboedo@gmail.com>
2020-10-27 19:12:53 -0300

    added unit tests

--- IntegrationTests/CocoapodsIntegration/CocoapodsIntegrationTests/CocoapodsIntegrationTests.swift
+++ IntegrationTests/CocoapodsIntegration/CocoapodsIntegrationTests/CocoapodsIntegrationTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Pods_CocoapodsIntegration
 
 class CocoapodsIntegrationTests: XCTestCase {
 
@@ -18,13 +19,12 @@ class CocoapodsIntegrationTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        RCIntegrationRunner().start()
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
