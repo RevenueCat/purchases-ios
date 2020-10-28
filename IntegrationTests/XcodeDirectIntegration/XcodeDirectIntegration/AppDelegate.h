7163a5a8e6be4698c24898496323a765ca4b5efe
Andy Boedo <andresboedo@gmail.com>
2020-10-28 17:28:46 -0300

    updated carthage bootstrap step

---
+++ IntegrationTests/XcodeDirectIntegration/XcodeDirectIntegration/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  XcodeDirectIntegration
+//
+//  Created by Andrés Boedo on 10/28/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ IntegrationTests/XcodeDirectIntegration/XcodeDirectIntegration/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  XcodeDirectIntegration
+//
+//  Created by Andrés Boedo on 10/28/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
