513cb14bbc8db0bca66dca481e40abea029445f7
Andy Boedo <andresboedo@gmail.com>
2020-10-28 17:57:42 -0300

    added xcode direct integration, fixed carthage build by duplicating carthage.sh script

---
+++ IntegrationTests/XcodeDirectIntegration/XcodeDirectIntegrationTests/XcodeDirectIntegrationTests.swift
@@ -0,0 +1,32 @@
+//
+//  XcodeDirectIntegrationTests.swift
+//  XcodeDirectIntegrationTests
+//
+//  Created by Andrés Boedo on 10/28/20.
+//
+
+import XCTest
+import XcodeDirectIntegration
+
+class XcodeDirectIntegrationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        RCIntegrationRunner().start()
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
