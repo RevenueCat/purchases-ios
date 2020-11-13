6dc4d294571ee362d2894e1840a06bf9de32243e
Andy Boedo <andresboedo@gmail.com>
2020-11-13 17:00:10 -0300

    replaced individual, repeated integrationTests files with a single, unified file, using a flag to determine how to import the framework correctly.

--- IntegrationTests/CommonFiles/IntegrationTests.swift
+++ IntegrationTests/CommonFiles/IntegrationTests.swift
@@ -1,15 +1,25 @@
 //
-//  CarthageIntegrationTests.swift
-//  CarthageIntegrationTests
+//  IntegrationTests.swift
+//  IntegrationTests
 //
 //  Created by Andrés Boedo on 10/28/20.
 //
 
 import XCTest
-import CarthageIntegration
+
+#if SPM_INTEGRATION
+@testable import SPMIntegration
+#elseif CARTHAGE_INTEGRATION
+@testable import CarthageIntegration
+#elseif COCOAPODS_INTEGRATION
+@testable import CocoapodsIntegration
+#elseif XCODE_INTEGRATION
+@testable import XcodeDirectIntegration
+#endif
+
 import Purchases
 
-class CarthageIntegrationTests: XCTestCase {
+class SPMIntegrationTests: XCTestCase {
 
     func testCanConfigureTheSDK() throws {
         RCIntegrationRunner().start()
