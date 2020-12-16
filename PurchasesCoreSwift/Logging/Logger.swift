186ce3052d93984d51f48a8880b076d2ffe8e4e2
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-12-15 17:34:22 -0800

    Logger extension + LogIntent

--- PurchasesCoreSwift/Logging/Logger.swift
+++ PurchasesCoreSwift/Logging/Logger.swift
@@ -29,20 +29,59 @@ import Foundation
         guard level != .debug || shouldShowDebugLogs else { return }
         NSLog("[\(frameworkDescription)] - \(level.description()): \(message)")
     }
+    
+    @objc public static func log(level: LogLevel, intent: LogIntent, message: String) {
+        #if V2_LOGS_ENABLED
+            let messageWithPrefix = "\(intent.suffix) \(message)"
+            Logger.log(level: level, message: messageWithPrefix)
+        #else
+            Logger.log(level: level, message: message)
+        #endif
+    }
 
     static func debug(_ message: String) {
-        log(level: .debug, message: message)
+        log(level: .debug, intent: .info, message: message)
     }
 
     static func info(_ message: String) {
-        log(level: .debug, message: message)
+        log(level: .info, intent: .info, message: message)
     }
 
     static func warn(_ message: String) {
-        log(level: .debug, message: message)
+        log(level: .warn, intent: .warning, message: message)
     }
 
     static func error(_ message: String) {
-        log(level: .debug, message: message)
+        log(level: .error, message: message)
+    }
+}
+
+extension Logger {
+    @objc public static func appleError(_ message: String) {
+        log(level: .error, intent: .appleError, message: message)
+    }
+    
+    @objc public static func appleWarning(_ message: String) {
+        log(level: .warn, intent: .appleError, message: message)
+    }
+    
+    @objc public static func purchase(_ message: String) {
+        log(level: .debug, intent: .purchase, message: message)
+    }
+    
+    @objc public static func rcError(_ message: String) {
+        log(level: .error, intent: .rcError, message: message)
+    }
+    
+    @objc public static func rcPurchaseSuccess(_ message: String) {
+        log(level: .info, intent: .rcPurchaseSuccess, message: message)
+    }
+    
+    @objc public static func rcSuccess(_ message: String) {
+        log(level: .debug, intent: .rcSuccess, message: message)
+    }
+    
+    @objc public static func user(_ message: String) {
+        log(level: .debug, intent: .user, message: message)
     }
 }
--- PurchasesCoreSwift/Logging/Strings/Emoji.swift
+++ PurchasesCoreSwift/Logging/Strings/Emoji.swift
@@ -1,5 +1,5 @@
 //
-//  Emojis.swift
+//  Emoji.swift
 //  PurchasesCoreSwift
 //
 //  Created by Tina Nguyen on 12/8/20.
@@ -8,13 +8,13 @@
 
 import Foundation
 
-enum Emojis: String {
-    case appleError = "\u{1F34E}\u{FE0F}" // apple emoji
+enum Emoji: String {
+    case apple = "\u{1F34E}\u{FE0F}" // apple emoji
     case doubleExclamation = "\u{203C}\u{FE0F}" // red double exclamation mark emoji
-    case rcSuccess = "\u{1F63B}\u{FE0F}" // heart cat eyes emoji
+    case heartCatEyes = "\u{1F63B}\u{FE0F}" // heart cat eyes emoji
     case info = "\u{2139}\u{FE0F}" // info emoji
-    case purchase = "\u{1F4B0}\u{FE0F}" // money bag emoji
-    case rcError = "\u{1F63F}\u{FE0F}" // sad cat eyes emoji
-    case appUserID = "\u{1F464}\u{FE0F}" // user emoji
+    case moneyBag = "\u{1F4B0}\u{FE0F}" // money bag emoji
+    case sadCatEyes = "\u{1F63F}\u{FE0F}" // sad cat eyes emoji
+    case person = "\u{1F464}\u{FE0F}" // user emoji
     case warning = "\u{26A0}\u{FE0F}"  // yellow warning emoji
 }
