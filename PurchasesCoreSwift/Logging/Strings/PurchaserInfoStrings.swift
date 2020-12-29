2cb7acebc0168fae0d2527270f9b624676ed2b50
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-12-28 16:00:23 -0800

    removes comments from PurchaserInfoStrings

--- PurchasesCoreSwift/Logging/Strings/PurchaserInfoStrings.swift
+++ PurchasesCoreSwift/Logging/Strings/PurchaserInfoStrings.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 @objc(RCPurchaserInfoStrings) public class PurchaserInfoStrings: NSObject {
-    @objc public var checking_intro_eligibility_locally_error: String { "Couldn't check intro eligibility locally, error: %@" } //error, og checking_intro_eligibility_error - 
-    @objc public var checking_intro_eligibility_locally_result: String { "Local intro eligibility computed locally. Result: %@" } //debug, og checking_intro_eligibility_result -
-    @objc public var checking_intro_eligibility_locally: String { "Attempting to check intro eligibility locally" } //debug, og checking_intro_eligibility -
-    @objc public var invalidating_purchaserinfo_cache: String { "Invalidating PurchaserInfo cache." } //debug - 
-    @objc public var no_cached_purchaserinfo: String { "No cached PurchaserInfo, fetching from network." } //debug -
-    @objc public var purchaserinfo_stale_updating_in_background: String { "PurchaserInfo cache is stale, updating from network in background." } //debug -
-    @objc public var purchaserinfo_stale_updating_in_foreground: String { "PurchaserInfo cache is stale, updating from network in foreground." } //debug - 
-    @objc public var purchaserinfo_updated_from_network: String { "PurchaserInfo updated from network." } //rcSuccess
-    @objc public var sending_latest_purchaserinfo_to_delegate: String { "Sending latest PurchaserInfo to delegate." } //debug -
-    @objc public var sending_updated_purchaserinfo_to_delegate: String { "Sending updated PurchaserInfo to delegate." } //debug -
-    @objc public var vending_cache: String { "Vending PurchaserInfo from cache." } //debug - 
+    @objc public var checking_intro_eligibility_locally_error: String { "Couldn't check intro eligibility locally, error: %@" }
+    @objc public var checking_intro_eligibility_locally_result: String { "Local intro eligibility computed locally. Result: %@" }
+    @objc public var checking_intro_eligibility_locally: String { "Attempting to check intro eligibility locally" }
+    @objc public var invalidating_purchaserinfo_cache: String { "Invalidating PurchaserInfo cache." }
+    @objc public var no_cached_purchaserinfo: String { "No cached PurchaserInfo, fetching from network." }
+    @objc public var purchaserinfo_stale_updating_in_background: String { "PurchaserInfo cache is stale, updating from network in background." }
+    @objc public var purchaserinfo_stale_updating_in_foreground: String { "PurchaserInfo cache is stale, updating from network in foreground." }
+    @objc public var purchaserinfo_updated_from_network: String { "PurchaserInfo updated from network." }
+    @objc public var sending_latest_purchaserinfo_to_delegate: String { "Sending latest PurchaserInfo to delegate." }
+    @objc public var sending_updated_purchaserinfo_to_delegate: String { "Sending updated PurchaserInfo to delegate." }
+    @objc public var vending_cache: String { "Vending PurchaserInfo from cache." }
 }
