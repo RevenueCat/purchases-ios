1fa90684f280fcc64df144478af97c0c13e8d4b4
Andy Boedo <andresboedo@gmail.com>
2021-02-09 16:15:18 -0300

    created copy of swiftExample called PurchaseTests

---
+++ IntegrationTests/PurchaseTests/SwiftExample/AppDelegate.swift
@@ -0,0 +1,65 @@
+//
+//  AppDelegate.swift
+//  SwiftExample
+//
+//  Created by Ryan Kotzebue on 1/9/19.
+//  Copyright © 2019 RevenueCat. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import Purchases
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        Purchases.debugLogsEnabled = true
+        Purchases.configure(withAPIKey: <your_public_sdk_key_here>)
+
+        // set attributes to store additional, structured information for a user in RevenueCat.
+        // More info: https://docs.revenuecat.com/docs/user-attributes
+        Purchases.shared.setAttributes(["favorite_cat" : "garfield"])
+
+        // we're requesting push notifications on app start only to showcase how to set the push token in RevenueCat
+        requestPushNotificationsPermissions()
+        
+        Purchases.shared.checkTrialOrIntroductoryPriceEligibility(["com.revenuecat.monthly_4.99.1_week_intro"]) { introEligibilityDict in
+            print(introEligibilityDict)
+        }
+
+        return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Purchases.shared.setPushToken(deviceToken)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+
+    private func requestPushNotificationsPermissions() {
+        let userNotificationCenter = UNUserNotificationCenter.current()
+        userNotificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, error in
+            print("Permission granted: \(granted)")
+            if granted {
+                self?.getNotificationSettings()
+            }
+        }
+    }
+
+    private func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+}
---
+++ IntegrationTests/PurchaseTests/SwiftExample/InitialViewController.swift
@@ -0,0 +1,51 @@
+//
+//  InitialViewController.swift
+//  SwiftExample
+//
+//  Created by Ryan Kotzebue on 1/9/19.
+//  Copyright © 2019 RevenueCat. All rights reserved.
+//
+
+import UIKit
+import Purchases
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Get the latest purchaserInfo to see if we have a pro cat user or not
+        Purchases.shared.purchaserInfo { (purchaserInfo, error) in
+            if let e = error {
+                print(e.localizedDescription)
+            }
+            
+            // Route the view depending if we have a premium cat user or not
+            if purchaserInfo?.entitlements["pro_cat"]?.isActive == true {
+                
+                // if we have a pro_cat subscriber, send them to the cat screen
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let controller = storyboard.instantiateViewController(withIdentifier: "cats")
+                controller.modalPresentationStyle = .fullScreen
+                self.present(controller, animated: true, completion: nil)
+                
+            } else {
+                // if we don't have a pro subscriber, send them to the upsell screen
+                let controller = SwiftPaywall(
+                    termsOfServiceUrlString: "https://www.revenuecat.com/terms",
+                    privacyPolicyUrlString: "https://www.revenuecat.com/terms")
+                
+                controller.titleLabel.text = "Upsell Screen"
+                controller.subtitleLabel.text = "New cats, unlimited cats, personal cat insights and more!"
+                controller.modalPresentationStyle = .fullScreen
+                self.present(controller, animated: true, completion: nil)
+            }
+        }
+        
+    }
+}
