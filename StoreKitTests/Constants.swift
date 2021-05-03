5d013277f0c0e3ccbc36a0d57d37e092074bf377
Andy Boedo <andresboedo@gmail.com>
2021-05-03 18:16:42 -0300

    added storekitTest target and app

---
+++ StoreKitTests/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  StoreKitTests
+//
+//  Created by Andrés Boedo on 5/4/21.
+//  Copyright © 2021 Purchases. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "REVENUECAT_API_KEY"
+    static let userDefaultsSuiteName = "StoreKitTests"
+    static let storeKitConfigFileName = "Configuration"
+}
