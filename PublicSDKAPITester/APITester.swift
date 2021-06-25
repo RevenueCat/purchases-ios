9dc6062ce8972ed8439e3e05f8c60e453f414726
Joshua Liebowitz <taquitos@users.noreply.github.com>
2021-06-24 18:39:23 -0700

    Add a swift file
    Because apparently this is a thing.

---
+++ PublicSDKAPITester/APITester.swift
@@ -0,0 +1,11 @@
+//
+//  APITester.swift
+//  APITester
+//
+//  Created by Joshua Liebowitz on 6/24/21.
+//  Copyright © 2021 Purchases. All rights reserved.
+//
+
+import Foundation
+
+// This is fine 🔥
