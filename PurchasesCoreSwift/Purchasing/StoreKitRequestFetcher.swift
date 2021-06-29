02cf35d6f1dbe39fe1a02955d168f34d7b4af58c
Andy Boedo <andresboedo@gmail.com>
2021-06-29 16:22:06 -0300

    added StoreKitRequestFetcher.swift

---
+++ PurchasesCoreSwift/Purchasing/StoreKitRequestFetcher.swift
@@ -0,0 +1,81 @@
+//
+//  StoreKitRequestFetcher.swift
+//  PurchasesCoreSwift
+//
+//  Created by Andrés Boedo on 6/29/21.
+//  Copyright © 2021 Purchases. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+typealias RCFetchProductsCompletionHandler = ([SKProduct]?) -> Void
+typealias RCFetchReceiptCompletionHandler = () -> Void
+
+@objc(RCReceiptRefreshRequestFactory) public class ReceiptRefreshRequestFactory: NSObject {
+    func receiptRefreshRequest() -> SKReceiptRefreshRequest? {
+        return SKReceiptRefreshRequest()
+    }
+}
+
+@objc(RCStoreKitRequestFetcher) public class StoreKitRequestFetcher: NSObject {
+    private let requestFactory: ReceiptRefreshRequestFactory?
+    private var receiptRefreshRequest: SKRequest?
+    private var receiptRefreshCompletionHandlers: [RCFetchReceiptCompletionHandler]
+    private let queue = DispatchQueue(label: "StoreKitRequestFetcher")
+
+    init(requestFactory: ReceiptRefreshRequestFactory) {
+        self.requestFactory = requestFactory
+        receiptRefreshRequest = nil
+        receiptRefreshCompletionHandlers = []
+    }
+
+    @objc public func fetchReceiptData(_ completion: @escaping () -> Void) {
+        queue.sync {
+            self.receiptRefreshCompletionHandlers.append(completion)
+
+            if self.receiptRefreshRequest == nil {
+                self.receiptRefreshRequest = self.requestFactory?.receiptRefreshRequest()
+                self.receiptRefreshRequest?.delegate = self
+                self.receiptRefreshRequest?.start()
+            }
+        }
+    }
+
+}
+
+extension StoreKitRequestFetcher: SKRequestDelegate {
+
+    public func requestDidFinish(_ request: SKRequest) {
+        if request is SKReceiptRefreshRequest {
+            let receiptHandlers = finishReceiptRequest(request)
+            for receiptHandler in receiptHandlers ?? [] {
+                receiptHandler()
+            }
+        }
+        request.cancel()
+    }
+
+    public func request(_ request: SKRequest, didFailWithError error: Error) {
+        Logger.appleError(String(format: Strings.offering.sk_request_failed, error.localizedDescription))
+        if request is SKReceiptRefreshRequest {
+            let receiptHandlers = finishReceiptRequest(request)
+            for receiptHandler in receiptHandlers ?? [] {
+                receiptHandler()
+            }
+        }
+        request.cancel()
+    }
+}
+
+private extension StoreKitRequestFetcher {
+
+    func finishReceiptRequest(_ request: SKRequest?) -> [RCFetchReceiptCompletionHandler]? {
+        queue.sync {
+            receiptRefreshRequest = nil
+            let handlers = receiptRefreshCompletionHandlers
+            receiptRefreshCompletionHandlers = []
+            return handlers
+        }
+    }
+}
