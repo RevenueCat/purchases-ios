e92740ac2d108348123dfb1762ade94c07feea96
Joshua Liebowitz <taquitos@users.noreply.github.com>
2021-07-06 16:50:50 -0700

    Add RCIntroEligibilityAPI to tester project

---
+++ PublicSDKAPITester/RCIntroEligibilityAPI.h
@@ -0,0 +1,20 @@
+//
+//  RCIntroEligibilityAPI.h
+//  APITester
+//
+//  Created by Joshua Liebowitz on 7/6/21.
+//  Copyright © 2021 Purchases. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCIntroEligibilityAPI : NSObject
+
++ (void)checkAPI;
++ (void)checkEnums;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PublicSDKAPITester/RCIntroEligibilityAPI.m
@@ -0,0 +1,27 @@
+//
+//  RCIntroEligibilityAPI.m
+//  APITester
+//
+//  Created by Joshua Liebowitz on 7/6/21.
+//  Copyright © 2021 Purchases. All rights reserved.
+//
+
+@import Purchases;
+#import "RCIntroEligibilityAPI.h"
+
+@implementation RCIntroEligibilityAPI
+
++ (void)checkAPI {
+    RCIntroEligibility *ie = [[RCIntroEligibility alloc] init];
+    RCIntroEligibilityStatus status = [ie status];
+    NSLog(@"%zd", status);
+
+}
+
++ (void)checkEnums {
+    RCIntroEligibilityStatus s = RCIntroEligibilityStatusUnknown;
+    s = RCIntroEligibilityStatusIneligible;
+    s = RCIntroEligibilityStatusEligible;
+}
+
+@end
