1454a2bf3187f2933f11a97b5b610694e07afd46
Juanpe Catalán <juanpecm@gmail.com>
2021-07-08 21:05:04 +0200

    remove RCHTTPRequest

--- PurchasesCoreSwift/Networking/HTTPRequest.swift
+++ PurchasesCoreSwift/Networking/HTTPRequest.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+public typealias HTTPClientResponseHandler = (Int, [String: Any]?, NSError?) -> Void
+
+// TODO(post migration): Change back to internal
+@objc(RCHTTPRequest)
+public class HTTPRequest: NSObject, NSCopying {
+    @objc public var httpMethod: String
+    @objc public var path: String
+    @objc public var requestBody: [String: Any]?
+    @objc public var headers: [String: String]?
+    @objc public var completionHandler: HTTPClientResponseHandler?
+
+    @objc(initWithHTTPMethod:path:body:headers:completionHandler:)
+    public init(httpMethod: String, path: String, requestBody: [String: Any]?, headers: [String: String]?, completionHandler: HTTPClientResponseHandler?) {
+        self.httpMethod = httpMethod
+        self.path = path
+        self.requestBody = requestBody
+        self.headers = headers
+        self.completionHandler = completionHandler
+    }
+
+    public func copy(with zone: NSZone? = nil) -> Any {
+        let copy = HTTPRequest(
+            httpMethod: httpMethod,
+            path: path,
+            requestBody: requestBody,
+            headers: headers,
+            completionHandler: completionHandler
+        )
+
+        return copy
+    }
+
+    public override var description: String {
+        """
+        <\(type(of: self))
+        httpMethod=\(httpMethod)
+        path=\(path)
+        requestBody=\(requestBody?.description ?? "")
+        headers=\(headers?.description ?? "")
+        >
+        """
+    }
+}
