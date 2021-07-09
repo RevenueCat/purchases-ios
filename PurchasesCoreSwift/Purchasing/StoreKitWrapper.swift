d1806528115c63eddfd7bf94562c120d5a8334fa
Andy Boedo <andresboedo@gmail.com>
2021-07-09 20:12:28 -0300

    added storekitwrapper.swift

---
+++ PurchasesCoreSwift/Purchasing/StoreKitWrapper.swift
@@ -0,0 +1,150 @@
+//  RCStoreKitWrapper.m
+//  Purchases
+//
+//  Created by RevenueCat.
+//  Copyright © 2019 RevenueCat. All rights reserved.
+//
+import StoreKit
+
+@objc(RCStoreKitWrapperDelegate) public protocol StoreKitWrapperDelegate: AnyObject {
+    @objc func storeKitWrapper(_ storeKitWrapper: StoreKitWrapper,
+                               updatedTransaction transaction: SKPaymentTransaction)
+
+    @objc func storeKitWrapper(_ storeKitWrapper: StoreKitWrapper,
+                               removedTransaction transaction: SKPaymentTransaction)
+
+    @objc func storeKitWrapper(_ storeKitWrapper: StoreKitWrapper,
+                               shouldAddStore payment: SKPayment,
+                               for product: SKProduct) -> Bool
+
+    @objc func storeKitWrapper(_ storeKitWrapper: StoreKitWrapper,
+                               didRevokeEntitlementsForProductIdentifiers productIdentifiers: [String])
+}
+
+@objc(RCStoreKitWrapper) public class StoreKitWrapper: NSObject, SKPaymentTransactionObserver {
+    public weak var delegate: StoreKitWrapperDelegate?
+
+    public private(set) static var _simulatesAskToBuyInSandbox = false
+    private var paymentQueue: SKPaymentQueue
+
+    init(paymentQueue: SKPaymentQueue) {
+        self.paymentQueue = paymentQueue
+        super.init()
+    }
+
+    deinit {
+        paymentQueue.remove(self)
+    }
+
+    class func simulatesAskToBuyInSandbox() -> Bool {
+        return _simulatesAskToBuyInSandbox
+    }
+
+    class func setSimulatesAskToBuyInSandbox(_ simulatesAskToBuyInSandbox: Bool) {
+        _simulatesAskToBuyInSandbox = simulatesAskToBuyInSandbox
+    }
+
+    func add(_ payment: SKPayment?) {
+        if let payment = payment {
+            paymentQueue.add(payment)
+        }
+    }
+
+    func finish(_ transaction: SKPaymentTransaction?) {
+        //        Logger.purchase(String(format:
+        //                                Strings.purchase.finishing_transaction,
+        //                               transaction?.payment.productIdentifier,
+        //                               transaction?.transactionIdentifier,
+        //                               transaction?.original?.transactionIdentifier))
+
+        if let transaction = transaction {
+            paymentQueue.finishTransaction(transaction)
+        }
+    }
+
+    func presentCodeRedemptionSheet() {
+        if #available(iOS 14.0, *) {
+            paymentQueue.presentCodeRedemptionSheet()
+        } else {
+            Logger.info(String(format: "%@", Strings.purchase.presenting_code_redemption_sheet_unavailable))
+        }
+    }
+
+    func payment(with product: SKProduct) -> SKMutablePayment {
+        let payment = SKMutablePayment(product: product)
+        // todo: check that it's fine to omit tvOS and iOS since the relevant methods exist in targets lower than ours
+        if #available(macOS 10.14, watchOS 6.2, macCatalyst 13.0, *) {
+            payment.simulatesAskToBuyInSandbox = StoreKitWrapper.simulatesAskToBuyInSandbox()
+        }
+        return payment
+    }
+
+    @available(iOS 12.2, macOS 10.14.4, watchOS 6.2, macCatalyst 13.0, tvOS 12.2, *)
+    func payment(with product: SKProduct, discount: SKPaymentDiscount) -> SKMutablePayment {
+        let payment = self.payment(with: product)
+        payment.paymentDiscount = discount
+        return payment
+    }
+
+}
+
+extension StoreKitWrapper: SKPaymentQueueDelegate {
+
+    public func paymentQueue(
+        _ queue: SKPaymentQueue,
+        updatedTransactions transactions: [SKPaymentTransaction]
+    ) {
+        for transaction in transactions {
+            //            Logger.debug(String(format:
+            //                Strings.purchase.paymentqueue_updatedtransaction,
+            //                transaction.payment.productIdentifier,
+            //                transaction.transactionIdentifier ?? "",
+            //                transaction.error ?? "",
+            //                transaction.original?.transactionIdentifier ?? "",
+            //                transaction.transactionState.rawValue))
+            delegate?.storeKitWrapper(self, updatedTransaction: transaction)
+        }
+    }
+
+    // Sent when transactions are removed from the queue (via finishTransaction:).
+    public func paymentQueue(
+        _ queue: SKPaymentQueue,
+        removedTransactions transactions: [SKPaymentTransaction]
+    ) {
+        for transaction in transactions {
+            //            Logger.debug(String(format:
+            //                                    Strings.purchase.paymentqueue_removedtransaction,
+            //                                transaction.payment.productIdentifier,
+            //                                transaction.transactionIdentifier,
+            //                                transaction.original?.transactionIdentifier,
+            //                                transaction.error,
+            //                                (transaction.error as NSError?)?.userInfo,
+            //                                transaction.transactionState.rawValue))
+            delegate?.storeKitWrapper(self, removedTransaction: transaction)
+        }
+    }
+
+    // Should match available platforms in
+    // https://developer.apple.com/documentation/storekit/skpaymenttransactionobserver/2877502-paymentqueuelanguage=objc
+    #if os(tvOS) || os(iOS) && !targetEnvironment(macCatalyst)
+
+    public func paymentQueue(
+        _ queue: SKPaymentQueue,
+        shouldAddStorePayment payment: SKPayment,
+        for product: SKProduct
+    ) -> Bool {
+        return delegate?.storeKitWrapper(self, shouldAddStore: payment, for: product) ?? false
+    }
+
+    #endif
+
+    // Sent when access to a family shared subscription is revoked from a family member or canceled the subscription
+    @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+    public func paymentQueue(
+        _ queue: SKPaymentQueue,
+        didRevokeEntitlementsForProductIdentifiers productIdentifiers: [String]
+    ) {
+        Logger.debug(String(format: Strings.purchase.paymentqueue_revoked_entitlements_for_product_identifiers, productIdentifiers))
+        delegate?.storeKitWrapper(self, didRevokeEntitlementsForProductIdentifiers: productIdentifiers)
+    }
+}
