ee32bc0d84ba716c005beecb9da977c4d267b225
Maddie Beyl <maddie@revenuecat.com>
2021-07-14 19:49:40 -0700

    First pass migrating initializer -- it builds but needs optimization

--- PurchasesCoreSwift/Public/PurchaserInfo.swift
+++ PurchasesCoreSwift/Public/PurchaserInfo.swift
@@ -9,38 +9,30 @@
 import Foundation
 
 @objc public class PurchaserInfo: NSObject {
-    
+//    typealias TransactionsByProductId =  NSDictionary
+
     // Entitlements attached to this purchaser info
     var entitlements: EntitlementInfos?
-    
+
     // All *subscription* product identifiers with expiration dates in the future.
-    var activeSubscriptions: Set<String>? {
-        get {
-            // TODO implement
-            return nil
-        }
-    }
-    
+    // TODO iplement
+    var activeSubscriptions: Set<String>?
+
     // All product identifiers purchases by the user regardless of expiration.
     let allPurchasedProductIdentifiers: Set<String> = Set()
-    
+
     // Returns the latest expiration date of all products, nil if there are none
-    var latestExpirationDate: Date? {
-        get {
-            // TODO implement
-            return nil
-        }
-    }
+    // TODO implement
+    var latestExpirationDate: Date?
 
     // Returns all product IDs of the non-subscription purchases a user has made.
     // TODO add deprecation message:  DEPRECATED_MSG_ATTRIBUTE("use nonSubscriptionTransactions");
     var nonConsumablePurchases: Set<String> = Set()
-    
-    
+
     // Returns all the non-subscription purchases a user has made.
     // The purchases are ordered by purchase date in ascending order.
-    var nonSubscriptionTransactions: Array<Transaction> = Array()
-    
+    var nonSubscriptionTransactions: [Transaction] = []
+
     /**
     Returns the build number (in iOS) or the marketing version (in macOS) for the version of the application when the user bought the app.
     This corresponds to the value of CFBundleVersion (in iOS) or CFBundleShortVersionString (in macOS) in the Info.plist file when the purchase was originally made.
@@ -50,7 +42,7 @@ import Foundation
      @note This can be nil, see -[RCPurchases restoreTransactionsForAppStore:]
      */
     var originalApplicationVersion: String?
-    
+
     /**
     Returns the purchase date for the version of the application when the user bought the app.
     Use this for grandfathering users when migrating to subscriptions.
@@ -58,139 +50,103 @@ import Foundation
     @note This can be nil, see -[RCPurchases restoreTransactionsForAppStore:]
      */
     var originalPurchaseDate: Date?
-    
+
     /**
      Returns the fetch date of this Purchaser info.
      @note Can be nil if was cached before we added this
      */
     let requestDate: Date?
-    
-    ///The date this user was first seen in RevenueCat.
+
+    // The date this user was first seen in RevenueCat.
     var firstSeen: Date?
-    
+
     // The original App User Id recorded for this user.
     var originalAppUserId: String?
-    
+
     // URL to manage the active subscription of the user.
     // If this user has an active iOS subscription, this will point to the App Store,
     // if the user has an active Play Store subscription it will point there.
     // If there are no active subscriptions it will be null.
     // If there are multiple for different platforms, it will point to the App Store
     var managementURL: URL?
-    
+
     // TODO figure out if these should really be private?
-    private var expirationDatesByProduct: Dictionary<String, Date>?
-    private var purchaseDatesByProduct: Dictionary<String, Date>?
-    private let originalData: [String : Any]?
+    private var expirationDatesByProduct: [String: Date]?
+    private var purchaseDatesByProduct: [String: Date]?
+    private let originalData: NSDictionary
     private let schemaVersion: String?
-    
-    //TODO is this equivalent to dispatch_once_t
-    private static var dateFormatter: DateFormatter {
-        get {
-            let formatter = DateFormatter()
-            formatter.timeZone = TimeZone.init(secondsFromGMT: 0)
-            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-            formatter.locale = Locale(identifier: "en_US_POSIX")
-            return formatter
-        }
-    }
-    
-    @objc public init?(data: [String : Any]) {
+
+    // TODO is this equivalent to dispatch_once_t
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+
+    @objc public init?(data: NSDictionary) {
         // TODO avoid force casting
-        if let subscriberData: [String : Any] = data["subscriber"] as! [String : Any]? {
-            if let subscriptions = subscriberData["subscriptions"] as! [String : [String : Any]]? {
-        //        setUpDateFormatter()
-                // TODO use method instead
-                
+        if let subscriberObject = data["subscriber"] as? [String: Any] {
+            if let subscriberData = SubscriberData.init(subscriberData: subscriberObject) {
                 self.originalData = data
-                
-                self.schemaVersion = data["schema_version"] as! String?
-                self.requestDate = PurchaserInfo.dateFormatter.date(from: ((data["request_date"] ?? "") as! String))
-                
-                super.init()
-                self.configureWithSubscriberData(subscriberData: subscriberData, subscriptions: subscriptions)
+                self.schemaVersion = data["schema_version"] as? String
+
+                guard let requestDateString = data["request_date"] as? String else {
+                    return nil
+                }
+                self.requestDate = PurchaserInfo.dateFormatter.date(from: requestDateString)
+
+                self.originalPurchaseDate = subscriberData.originalPurchaseDate
+                self.firstSeen = subscriberData.firstSeen
+                self.originalAppUserId = subscriberData.originalAppUserId
+                self.managementURL = subscriberData.managementURL
+
+                guard let nonSubscriptionProductIds = subscriberData.nonSubscriptions.allKeys as? [String] else {
+                    return nil
+                }
+                self.nonConsumablePurchases = Set(nonSubscriptionProductIds)
+
+                self.entitlements = EntitlementInfos.init(entitlementsData: subscriberData.entitlements, purchasesData: subscriberData.allPurchases, dateFormatter: PurchaserInfo.dateFormatter, requestDate: requestDate)
             } else {
                 return nil
             }
         } else {
             return nil
         }
-
-    }
-    
-    func configureWithSubscriberData(subscriberData: [String : Any], subscriptions: [String : [String : Any]]) {
-        self.initializePurchasesAndEntitlementsWithSubscriberDataSubscriptions(subscriberData: subscriberData, subscriptions: subscriptions)
-        self.initializeMetadataWithSubscriberData(subscriberData: subscriberData)
-    }
-    
-    func initializeMetadataWithSubscriberData(subscriberData: [String : Any]) {
-        self.originalApplicationVersion = subscriberData["original_application_version"] as! String?
-
-        self.originalPurchaseDate = parseDate(dateString: subscriberData["original_purchase_date"]!, withDateFormatter: PurchaserInfo.dateFormatter)
-
-        self.firstSeen = parseDate(dateString: subscriberData["first_seen"], withDateFormatter: PurchaserInfo.dateFormatter)!
-
-        self.originalAppUserId = subscriberData["original_app_user_id"] as! String
-        self.managementURL = parseURL(urlString: subscriberData["management_url"])
     }
 
-    func initializePurchasesAndEntitlementsWithSubscriberDataSubscriptions(subscriberData: [String : Any], subscriptions: [String : [String : Any]]) {
-        let nonSubscriptionsData = subscriberData["non_subscriptions"] as! [String: [[String: Any]]]
-        nonConsumablePurchases = Set.init(nonSubscriptionsData.keys)
-
-        let transactionsFactory = TransactionsFactory.init()
-        nonSubscriptionTransactions = transactionsFactory.nonSubscriptionTransactions(withSubscriptionsData: nonSubscriptionsData, dateFormatter: PurchaserInfo.dateFormatter)
-
-        var nonSubscriptionsLatestPurchases = Dictionary<String, Dictionary<String, Any>>()
-        for (productId, _) in nonSubscriptionsData {
-            // TODO is there a swifty way to do this
-            let arrayOfPurchases = nonSubscriptionsData[productId] as! [[String: Any]]
-            if (!arrayOfPurchases.isEmpty) {
-                nonSubscriptionsLatestPurchases[productId] = arrayOfPurchases[arrayOfPurchases.count - 1]
-            }
-        }
-
-        var allPurchases = Dictionary<String, Dictionary<String, Any>>()
-        allPurchases.merge(nonSubscriptionsLatestPurchases, uniquingKeysWith: { (_, last) in last} )
-        allPurchases.merge(subscriptions ?? [:], uniquingKeysWith: { (_, last) in last} )
-        let entitlements = subscriberData["entitlements"] as! [String: Any]?
-        self.entitlements = EntitlementInfos.init(entitlementsData: entitlements, purchasesData: allPurchases, dateFormatter: PurchaserInfo.dateFormatter, requestDate: self.requestDate)
-
-        self.expirationDatesByProduct = self.parseExpirationDate(expirationDates: subscriptions)
-        self.purchaseDatesByProduct = self.parsePurchaseDate(purchaseDates: allPurchases)
-    }
-    
-    func parseURL(urlString: Any) -> URL? {
-        if (urlString is String) {
-            return URL.init(fileURLWithPath: urlString as! String)
+    class func parseURL(url: Any) -> URL? {
+        if let urlString = url as? String {
+            return URL.init(fileURLWithPath: urlString)
         }
         return nil
     }
-    
-    func parseDate(dateString: Any, withDateFormatter: DateFormatter) -> Date? {
-        if (dateString is String) {
-            return PurchaserInfo.dateFormatter.date(from: dateString as! String)
+
+    class func parseDate(date: Any, withDateFormatter: DateFormatter) -> Date? {
+        if let dateString = date as? String {
+            return PurchaserInfo.dateFormatter.date(from: dateString)
         }
         return nil
     }
-    
-    func parseExpirationDate(expirationDates: Dictionary<String, Dictionary<String, Any>>) -> Dictionary<String, Date> {
+
+    class func parseExpirationDate(expirationDates: [String: Any]) -> NSDictionary {
         return parseDatesIn(dates: expirationDates, label: "expires_date")
     }
-    
-    func parsePurchaseDate(purchaseDates: Dictionary<String, Dictionary<String, Any>>) -> Dictionary<String, Date> {
+
+    class func parsePurchaseDate(purchaseDates: [String: Any]) -> NSDictionary {
         return parseDatesIn(dates: purchaseDates, label: "purchase_date")
     }
-    
-    func parseDatesIn(dates: Dictionary<String, Dictionary<String, Any>>, label: String) -> Dictionary<String, Date> {
-        var parsedDates = Dictionary<String, Date>()
-        
+
+    class func parseDatesIn(dates: [String: Any], label: String) -> NSDictionary {
+        let parsedDates = NSMutableDictionary()
+
         for (identifier, _) in dates {
-            let dateString = dates[identifier]?[label]
-            
-            if (dateString is String) {
-                let date = PurchaserInfo.dateFormatter.date(from: dateString as! String)
-                if (date != nil) {
+            let date = (dates[identifier] as? NSDictionary ?? NSDictionary())[label]
+
+            if let dateString = date as? String {
+                let date = PurchaserInfo.dateFormatter.date(from: dateString)
+                if date != nil {
                     parsedDates[identifier] = date
                 }
             } else {
@@ -199,42 +155,118 @@ import Foundation
         }
         return parsedDates
     }
+
+    struct SubscriberData {
+        let originalAppUserId: String
+        let managementURL: URL?
+        let originalApplicationVersion: String?
+        let originalPurchaseDate: Date?
+        let firstSeen: Date
+        let subscriptions: [String: Any]
+        let nonSubscriptions: NSDictionary
+        let entitlements: [String: Any]
+        let nonConsumablePurchases: Set<String>
+        let nonSubscriptionLatestTransactions: NSDictionary
+        let nonSubscriptionTransactions: [Transaction]
+        let allPurchases: [String: Any]
+        let expirationDatesByProduct: NSDictionary
+        let purchaseDatesByProduct: NSDictionary
+
+        init?(subscriberData: [String: Any]) {
+            guard let subscriptionsDictionary = subscriberData["subscriptions"] as? [String: Any] else {
+                return nil
+            }
+            self.subscriptions = subscriptionsDictionary
+
+            guard let originalApplicationVersionString = subscriberData["original_application_version"] as? String else {
+                return nil
+            }
+            self.originalApplicationVersion = originalApplicationVersionString
+
+            if let originalPurchaseDateString = subscriberData["original_purchase_date"] as? String {
+                self.originalPurchaseDate = PurchaserInfo.parseDate(date: originalPurchaseDateString, withDateFormatter: PurchaserInfo.dateFormatter)
+            } else {
+                self.originalPurchaseDate = nil
+            }
+
+            guard let firstSeenDateString = subscriberData["first_seen"] as? String else {
+                return nil
+            }
+
+            self.firstSeen = PurchaserInfo.parseDate(date: firstSeenDateString, withDateFormatter: PurchaserInfo.dateFormatter)!
+
+            guard let originalAppUserIdString = subscriberData["original_app_user_id"] as? String else {
+                return nil
+            }
+            self.originalAppUserId = originalAppUserIdString
+
+            if let managementUrlString = subscriberData["management_url"] as? String {
+                self.managementURL = parseURL(url: managementUrlString)
+            } else {
+                self.managementURL = nil
+            }
+
+            self.nonSubscriptions = subscriberData["non_subscriptions"] as? NSDictionary ?? NSDictionary()
+            self.entitlements = subscriberData["entitlements"] as? [String: Any] ?? [String: Any]()
+
+            self.nonConsumablePurchases = Set(nonSubscriptions.allKeys as? [String] ?? [String]())
+
+            self.nonSubscriptionTransactions = TransactionsFactory().nonSubscriptionTransactions(withSubscriptionsData: nonSubscriptions as? [String: [[String: Any]]] ?? [String: [[String: Any]]](), dateFormatter: PurchaserInfo.dateFormatter)
+
+            // nonSubscriptionLatestTransactions
+            let productIdToLatestTransaction = NSMutableDictionary()
+            for productId in nonSubscriptions.allKeys {
+                let purchasesArray = nonSubscriptions[productId] as? [Any] ?? [Any]()
+                if let latestTransaction = purchasesArray.last {
+                    productIdToLatestTransaction[productId] = latestTransaction
+                }
+            }
+            self.nonSubscriptionLatestTransactions = productIdToLatestTransaction
+
+            // all purchases
+            guard let nonSubsLatest = nonSubscriptionLatestTransactions as? [String: Any] else {
+                return nil
+            }
+            self.allPurchases = nonSubsLatest.merging(subscriptions) { (current, _) in current }
+
+            self.expirationDatesByProduct = PurchaserInfo.parseExpirationDate(expirationDates: subscriptions)
+            self.purchaseDatesByProduct = PurchaserInfo.parsePurchaseDate(purchaseDates: allPurchases)
+        }
+    }
+
 }
 
-
-
-///**
+/// **
 // Get the expiration date for a given product identifier. You should use Entitlements though!
 //
 // @param productIdentifier Product identifier for product
 //
 // @return The expiration date for `productIdentifier`, `nil` if product never purchased
 // */
-//- (nullable NSDate *)expirationDateForProductIdentifier:(NSString *)productIdentifier;
+// - (nullable NSDate *)expirationDateForProductIdentifier:(NSString *)productIdentifier;
 //
-///**
+/// **
 // Get the latest purchase or renewal date for a given product identifier. You should use Entitlements though!
 //
 // @param productIdentifier Product identifier for subscription product
 //
 // @return The purchase date for `productIdentifier`, `nil` if product never purchased
 // */
-//- (nullable NSDate *)purchaseDateForProductIdentifier:(NSString *)productIdentifier;
+// - (nullable NSDate *)purchaseDateForProductIdentifier:(NSString *)productIdentifier;
 //
-///** Get the expiration date for a given entitlement.
+/// ** Get the expiration date for a given entitlement.
 //
 // @param entitlementId The id of the entitlement.
 //
 // @return The expiration date for the passed in `entitlement`, can be `nil`
 // */
-//- (nullable NSDate *)expirationDateForEntitlement:(NSString *)entitlementId;
+// - (nullable NSDate *)expirationDateForEntitlement:(NSString *)entitlementId;
 //
-///**
+/// **
 // Get the latest purchase or renewal date for a given entitlement identifier.
 //
 // @param entitlementId Entitlement identifier for entitlement
 //
 // @return The purchase date for `entitlementId`, `nil` if product never purchased
 // */
-//- (nullable NSDate *)purchaseDateForEntitlement:(NSString *)entitlementId;
-
+// - (nullable NSDate *)purchaseDateForEntitlement:(NSString *)entitlementId;
