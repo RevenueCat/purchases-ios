39c704c6ed40c6b2f3fdcb570d74375537cec9e6
vegaro <cesarvegaro@gmail.com>
2021-07-21 14:43:42 +0200

    remove unnecessary space

--- Purchases/Attribution/RCAttributionFetcher.h
+++ Purchases/Attribution/RCAttributionFetcher.h
@@ -31,7 +31,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)adClientAttributionDetailsWithCompletionBlock:(RCAttributionDetailsBlock)completionHandler;
 
-- (BOOL) isAuthorizedToPostSearchAds;
+- (BOOL)isAuthorizedToPostSearchAds;
 
 @end
 
