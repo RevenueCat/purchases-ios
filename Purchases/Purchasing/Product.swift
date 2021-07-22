3eddc28ddbbdc9a5755893a0bc4fe94479f780ba
Andy Boedo <andresboedo@gmail.com>
2021-07-22 16:32:39 -0300

    added Product protocol and separated wrappers for sk1 and sk2

--- Purchases/Purchasing/Product.swift
+++ Purchases/Purchasing/Product.swift
@@ -9,20 +9,106 @@
 import Foundation
 import StoreKit
 
-public struct Product {
-    public let underlyingSK1Product: SKProduct?
+public protocol Product {
 
-    @available(iOS 15.0, tvOS 15.0, watchOS 7.0, macOS 12.0, *)
-    public var underlyingSK2Product: StoreKit.Product? {
-        return nil
+    var localizedDescription: String { get }
+
+    //    var localizedTitle: String { get }
+    //
+    //    var price: NSDecimalNumber { get }
+    //
+    //    var priceLocale: Locale { get }
+    //
+    //    var productIdentifier: String { get }
+    //
+    //    // YES if this product has content downloadable using SKDownload
+    //    var isDownloadable: Bool { get }
+    //
+    //    // YES if this product allows for sharing among family members
+    //    @available(iOS 14.0, *)
+    //    var isFamilyShareable: Bool { get }
+    //
+    //
+    //    var downloadContentLengths: [NSNumber] { get }
+    //
+    //    // Version of the downloadable content
+    //    var contentVersion: String { get }
+    //
+    //    var downloadContentVersion: String { get }
+    //
+    //    @available(iOS 11.2, *)
+    //    var subscriptionPeriod: SKProductSubscriptionPeriod? { get }
+    //
+    //    @available(iOS 11.2, *)
+    //    var introductoryPrice: SKProductDiscount? { get }
+    //
+    //    @available(iOS 12.0, *)
+    //    var subscriptionGroupIdentifier: String? { get }
+    //
+    //    @available(iOS 12.2, *)
+    //    var discounts: [SKProductDiscount] { get }
+}
+
+@available(iOS 15.0, tvOS 15.0, watchOS 7.0, macOS 12.0, *)
+public struct SK2ProductWrapper: Product {
+    public let underlyingSK2Product: StoreKit.Product
+
+    public var localizedDescription: String {
+        return underlyingSK2Product.description
     }
+
+    init(sk2Product: StoreKit.Product) {
+        self.underlyingSK2Product = sk2Product
+    }
+}
+
+public struct SK1ProductWrapper: Product {
+    public let underlyingSK1Product: SKProduct
+    public var localizedDescription: String {
+        return underlyingSK1Product.localizedDescription
+    }
+    //
+    //    public var localizedTitle: String {
+    //        return underlyingSK1Product.localizedTitle
+    //    }
+    //
+    //    public var price: NSDecimalNumber {
+    //        return underlyingSK1Product.price
+    //    }
+    //
+    //    public var priceLocale: Locale {
+    //        return underlyingSK1Product.priceLocale
+    //    }
+    //
+    //    public var productIdentifier: String {
+    //        return underlyingSK1Product.productIdentifier
+    //    }
+
+    // YES if this product has content downloadable using SKDownload
+    //    public var isDownloadable: Bool
+    //    public var downloadContentLengths: [NSNumber]
+    //    public var downloadContentVersion: String
+
+    // YES if this product allows for sharing among family members
+    //    @available(iOS 14.0, *)
+    //    public private(set) var isFamilyShareable: Bool
+
+    //    @available(iOS 11.2, *)
+    //    open var subscriptionPeriod: SKProductSubscriptionPeriod?
+
+    //    @available(iOS 11.2, *)
+    //    open var introductoryPrice: SKProductDiscount?
+
+    //    @available(iOS 12.0, *)
+    //    public private(set) var subscriptionGroupIdentifier: String?
+
+    //    @available(iOS 12.2, *)
+    //    open var discounts: [SKProductDiscount]
+
+    // Version of the downloadable content
+    //    public private(set) var contentVersion: String
 
     init(sk1Product: SKProduct) {
         self.underlyingSK1Product = sk1Product
-    }
-
-    @available(iOS 15.0, tvOS 15.0, watchOS 7.0, macOS 12.0, *)
-    init(sk2Product: StoreKit.Product) {
-        self.underlyingSK1Product = nil
     }
 }
