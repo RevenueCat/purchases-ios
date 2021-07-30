ea676212cff57231717488dde0f33fd08ed1154f
Andy Boedo <andresboedo@gmail.com>
2021-07-30 17:40:18 -0300

    updates from PR comments: extracted methods & addressed a few other comments

--- PurchasesCoreSwift/Networking/HTTPClient.swift
+++ PurchasesCoreSwift/Networking/HTTPClient.swift
@@ -89,72 +89,13 @@ private extension HTTPClient {
                         retried: Bool = false,
                         completionHandler maybeCompletionHandler: ((Int, [AnyHashable: Any]?, Error?) -> Void)?) {
         operationDispatcher.dispatchOnHTTPSerialQueue { [self] in
-            do {
-                try self.assertIsValidRequest(httpMethod: httpMethod, requestBody: maybeRequestBody)
-            } catch let error {
-                if let completionHandler = maybeCompletionHandler {
-                    self.operationDispatcher.dispatchOnMainThread {
-                        completionHandler(-1, nil, error)
-                    }
-                    return
-                }
-            }
-
-            let requestHeaders = self.defaultHeaders.merging(authHeaders, uniquingKeysWith: { (_, last) in last })
-
-            let maybeURLRequest = self.createRequest(httpMethod: httpMethod, path: path, requestBody: maybeRequestBody,
-                                                     headers: requestHeaders, refreshETag: retried)
-
-            guard let urlRequest = maybeURLRequest else {
-                if let requestBody = maybeRequestBody {
-                    Logger.error("Could not create request to \(path) with body \(requestBody)")
-                } else {
-                    Logger.error("Could not create request to \(path) without body")
-                }
-                if let completionHandler = maybeCompletionHandler {
-                    self.operationDispatcher.dispatchOnMainThread {
-                        completionHandler(-1, nil, ErrorUtils.networkError(withUnderlyingError: ErrorUtils.unknownError()))
-                    }
-                }
-                return
-            }
-
-            let request = HTTPRequest(httpMethod: httpMethod,
-                                      path: path,
-                                      requestBody: maybeRequestBody,
-                                      authHeaders: authHeaders,
-                                      retried: retried,
-                                      urlRequest: urlRequest,
-                                      completionHandler: maybeCompletionHandler)
-
-            if performSerially && !retried {
-                if self.currentSerialRequest != nil {
-                    let logMessage =
-                        String(format: Strings.network.serial_request_queued, self.queuedRequests.count, httpMethod, path)
-                    Logger.debug(logMessage)
-                    self.queuedRequests.append(request)
-                    return
-                } else {
-                    Logger.debug(String(format: Strings.network.starting_request, httpMethod, path))
-                    self.currentSerialRequest = request
-                }
-            }
-
-            let logMessage = String(format: Strings.network.api_request_started,
-                                    urlRequest.httpMethod ?? "",
-                                    urlRequest.url?.path ?? "")
-            Logger.debug(logMessage)
-
-            let task = self.session.dataTask(with: urlRequest) { (data, urlResponse, error) -> Void in
-                self.handleResponse(urlResponse: urlResponse,
-                                    request: request,
-                                    data: data,
-                                    error: error,
-                                    completionHandler: maybeCompletionHandler,
-                                    beginNextRequestWhenFinished: performSerially,
-                                    retried: retried)
-            }
-            task.resume()
+            self.threadUnsafePerformRequest(httpMethod,
+                                            performSerially: performSerially,
+                                            path: path,
+                                            requestBody: maybeRequestBody,
+                                            authHeaders: authHeaders,
+                                            retried: retried,
+                                            completionHandler: maybeCompletionHandler)
         }
     }
 
@@ -167,76 +108,171 @@ private extension HTTPClient {
                         beginNextRequestWhenFinished: Bool,
                         retried: Bool) {
         operationDispatcher.dispatchOnHTTPSerialQueue { [self] in
-            var shouldBeginNextRequestWhenFinished = beginNextRequestWhenFinished
-            var statusCode = HTTPStatusCodes.networkConnectTimeoutError.rawValue
-            var jsonObject: [String: Any]?
-            var maybeHTTPResponse: HTTPResponse? = HTTPResponse(statusCode: statusCode, jsonObject: jsonObject)
-            var maybeJSONError: Error?
+            self.threadUnsafeHandleResponse(urlResponse: maybeURLResponse,
+                                            request: request,
+                                            data: maybeData,
+                                            error: maybeNetworkError,
+                                            completionHandler: maybeCompletionHandler,
+                                            beginNextRequestWhenFinished: beginNextRequestWhenFinished,
+                                            retried: retried)
+        }
+    }
 
-            if maybeNetworkError == nil {
-                if let httpURLResponse = maybeURLResponse as? HTTPURLResponse {
-                    statusCode = httpURLResponse.statusCode
-                    let logMessage = String(format: Strings.network.api_request_completed,
-                                            request.httpMethod, request.urlRequest.url?.path ?? "", statusCode)
-                    Logger.debug(logMessage)
-
-                    if statusCode == HTTPStatusCodes.notModifiedResponseCode.rawValue || maybeData == nil {
-                        jsonObject = [:]
-                    } else if let data = maybeData {
-                        do {
-                            jsonObject = try JSONSerialization.jsonObject(with: data,
-                                                                          options: .mutableContainers) as? [String: Any]
-                        } catch let jsonError {
-                            Logger.error(String(format: Strings.network.parsing_json_error, jsonError.localizedDescription))
-
-                            let dataAsString = String(data: maybeData ?? Data(), encoding: .utf8) ?? ""
-                            Logger.error(String(format: Strings.network.json_data_received, dataAsString))
-
-                            maybeJSONError = jsonError
-                        }
-                    }
-
-                    maybeHTTPResponse = self.eTagManager.httpResultFromCacheOrBackend(with: httpURLResponse,
-                                                                                      jsonObject: jsonObject,
-                                                                                      error: maybeJSONError,
-                                                                                      request: request.urlRequest,
-                                                                                      retried: retried)
-                    if maybeHTTPResponse == nil {
-                        let message = String(format: Strings.network.retrying_request, request.httpMethod,
-                                             request.path)
-                        Logger.debug(message)
-                        let retriedRequest = HTTPRequest(byCopyingRequest: request, retried: true)
-                        self.queuedRequests.insert(retriedRequest, at: 0)
-                        shouldBeginNextRequestWhenFinished = true
-                    }
-                }
-            }
-
-            if let httpResponse = maybeHTTPResponse,
-               let completionHandler = maybeCompletionHandler {
+    func threadUnsafePerformRequest(_ httpMethod: String,
+                                    performSerially: Bool,
+                                    path: String,
+                                    requestBody maybeRequestBody: [String: Any]?,
+                                    authHeaders: [String: String],
+                                    retried: Bool,
+                                    completionHandler maybeCompletionHandler: ((Int, [AnyHashable: Any]?, Error?) -> Void)?) {
+        do {
+            try self.assertIsValidRequest(httpMethod: httpMethod, requestBody: maybeRequestBody)
+        } catch let error {
+            if let completionHandler = maybeCompletionHandler {
                 self.operationDispatcher.dispatchOnMainThread {
-                    let error = maybeJSONError ?? maybeNetworkError
-                    completionHandler(httpResponse.statusCode, httpResponse.jsonObject, error)
+                    completionHandler(-1, nil, error)
+                }
+                return
+            }
+        }
+
+        let requestHeaders = self.defaultHeaders.merging(authHeaders, uniquingKeysWith: { (_, last) in last })
+
+        let maybeURLRequest = self.createRequest(httpMethod: httpMethod,
+                                                 path: path,
+                                                 requestBody: maybeRequestBody,
+                                                 headers: requestHeaders,
+                                                 refreshETag: retried)
+
+        guard let urlRequest = maybeURLRequest else {
+            if let requestBody = maybeRequestBody {
+                Logger.error("Could not create request to \(path) with body \(requestBody)")
+            } else {
+                Logger.error("Could not create request to \(path) without body")
+            }
+            if let completionHandler = maybeCompletionHandler {
+                self.operationDispatcher.dispatchOnMainThread {
+                    completionHandler(-1, nil, ErrorUtils.networkError(withUnderlyingError: ErrorUtils.unknownError()))
                 }
             }
+            return
+        }
 
-            if shouldBeginNextRequestWhenFinished {
-                let logMessage = String(format: Strings.network.serial_request_done,
-                                        self.currentSerialRequest?.httpMethod ?? "",
-                                        self.currentSerialRequest?.path ?? "",
-                                        self.queuedRequests.count)
+        let request = HTTPRequest(httpMethod: httpMethod,
+                                  path: path,
+                                  requestBody: maybeRequestBody,
+                                  authHeaders: authHeaders,
+                                  retried: retried,
+                                  urlRequest: urlRequest,
+                                  completionHandler: maybeCompletionHandler)
+
+        if performSerially && !retried {
+            if self.currentSerialRequest != nil {
+                let logMessage =
+                    String(format: Strings.network.serial_request_queued, self.queuedRequests.count, httpMethod, path)
                 Logger.debug(logMessage)
-                self.currentSerialRequest = nil
-                if !self.queuedRequests.isEmpty {
-                    let nextRequest = self.queuedRequests.removeFirst()
-                    Logger.debug(String(format: Strings.network.starting_next_request, nextRequest))
-                    self.performRequest(nextRequest.httpMethod,
-                                        performSerially: true,
-                                        path: nextRequest.path,
-                                        requestBody: nextRequest.requestBody,
-                                        headers: nextRequest.authHeaders,
-                                        completionHandler: nextRequest.completionHandler)
+                self.queuedRequests.append(request)
+                return
+            } else {
+                Logger.debug(String(format: Strings.network.starting_request, httpMethod, path))
+                self.currentSerialRequest = request
+            }
+        }
+
+        let logMessage = String(format: Strings.network.api_request_started,
+                                urlRequest.httpMethod ?? "",
+                                urlRequest.url?.path ?? "")
+        Logger.debug(logMessage)
+
+        let task = self.session.dataTask(with: urlRequest) { (data, urlResponse, error) -> Void in
+            self.handleResponse(urlResponse: urlResponse,
+                                request: request,
+                                data: data,
+                                error: error,
+                                completionHandler: maybeCompletionHandler,
+                                beginNextRequestWhenFinished: performSerially,
+                                retried: retried)
+        }
+        task.resume()
+    }
+
+    // swiftlint:disable function_parameter_count
+    func threadUnsafeHandleResponse(urlResponse maybeURLResponse: URLResponse?,
+                                    request: HTTPRequest,
+                                    data maybeData: Data?,
+                                    error maybeNetworkError: Error?,
+                                    completionHandler maybeCompletionHandler: ((Int, [AnyHashable: Any]?, Error?) -> Void)?,
+                                    beginNextRequestWhenFinished: Bool,
+                                    retried: Bool) {
+        var shouldBeginNextRequestWhenFinished = beginNextRequestWhenFinished
+        var statusCode = HTTPStatusCodes.networkConnectTimeoutError.rawValue
+        var jsonObject: [String: Any]?
+        var maybeHTTPResponse: HTTPResponse? = HTTPResponse(statusCode: statusCode, jsonObject: jsonObject)
+        var maybeJSONError: Error?
+
+        if maybeNetworkError == nil {
+            if let httpURLResponse = maybeURLResponse as? HTTPURLResponse {
+                statusCode = httpURLResponse.statusCode
+                let logMessage = String(format: Strings.network.api_request_completed,
+                                        request.httpMethod, request.urlRequest.url?.path ?? "", statusCode)
+                Logger.debug(logMessage)
+
+                if statusCode == HTTPStatusCodes.notModifiedResponseCode.rawValue || maybeData == nil {
+                    jsonObject = [:]
+                } else if let data = maybeData {
+                    do {
+                        jsonObject = try JSONSerialization.jsonObject(with: data,
+                                                                      options: .mutableContainers) as? [String: Any]
+                    } catch let jsonError {
+                        Logger.error(String(format: Strings.network.parsing_json_error, jsonError.localizedDescription))
+
+                        let dataAsString = String(data: maybeData ?? Data(), encoding: .utf8) ?? ""
+                        Logger.error(String(format: Strings.network.json_data_received, dataAsString))
+
+                        maybeJSONError = jsonError
+                    }
                 }
+
+                maybeHTTPResponse = self.eTagManager.httpResultFromCacheOrBackend(with: httpURLResponse,
+                                                                                  jsonObject: jsonObject,
+                                                                                  error: maybeJSONError,
+                                                                                  request: request.urlRequest,
+                                                                                  retried: retried)
+                if maybeHTTPResponse == nil {
+                    let message = String(format: Strings.network.retrying_request, request.httpMethod,
+                                         request.path)
+                    Logger.debug(message)
+                    let retriedRequest = HTTPRequest(byCopyingRequest: request, retried: true)
+                    self.queuedRequests.insert(retriedRequest, at: 0)
+                    shouldBeginNextRequestWhenFinished = true
+                }
+            }
+        }
+
+        if let httpResponse = maybeHTTPResponse,
+            let completionHandler = maybeCompletionHandler {
+            self.operationDispatcher.dispatchOnMainThread {
+                let error = maybeJSONError ?? maybeNetworkError
+                completionHandler(httpResponse.statusCode, httpResponse.jsonObject, error)
+            }
+        }
+
+        if shouldBeginNextRequestWhenFinished {
+            let logMessage = String(format: Strings.network.serial_request_done,
+                                    self.currentSerialRequest?.httpMethod ?? "",
+                                    self.currentSerialRequest?.path ?? "",
+                                    self.queuedRequests.count)
+            Logger.debug(logMessage)
+            self.currentSerialRequest = nil
+            if !self.queuedRequests.isEmpty {
+                let nextRequest = self.queuedRequests.removeFirst()
+                Logger.debug(String(format: Strings.network.starting_next_request, nextRequest))
+                self.performRequest(nextRequest.httpMethod,
+                                    performSerially: true,
+                                    path: nextRequest.path,
+                                    requestBody: nextRequest.requestBody,
+                                    headers: nextRequest.authHeaders,
+                                    completionHandler: nextRequest.completionHandler)
             }
         }
     }
