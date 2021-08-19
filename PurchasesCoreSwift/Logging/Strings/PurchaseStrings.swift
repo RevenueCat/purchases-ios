fc849a8eae4dfc8732c64b4debf2ccd212878206
vegaro <cesarvegaro@gmail.com>
2021-08-19 15:26:03 +0200

    Strings

--- PurchasesCoreSwift/Logging/Strings/PurchaseStrings.swift
+++ PurchasesCoreSwift/Logging/Strings/PurchaseStrings.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 // swiftlint:disable identifier_name
-@objc(RCPurchaseStrings) public class PurchaseStrings: NSObject {
+class PurchaseStrings {
 
-    @objc public var cannot_purchase_product_appstore_configuration_error: String { "Could not purchase SKProduct. " +
+    var cannot_purchase_product_appstore_configuration_error: String { "Could not purchase SKProduct. " +
         "There is a problem with your configuration in App Store Connect. " +
         "More info here: https://errors.rev.cat/configuring-products" }
-    @objc public var entitlements_revoked_syncing_purchases: String { "Entitlements revoked for product " +
+    var entitlements_revoked_syncing_purchases: String { "Entitlements revoked for product " +
         "identifiers: %@. \nsyncing purchases" }
     var finishing_transaction: String { "Finishing transaction %@ %@ (%@)" }
-    @objc public var purchasing_with_observer_mode_and_finish_transactions_false_warning: String { "Observer mode is " +
+    var purchasing_with_observer_mode_and_finish_transactions_false_warning: String { "Observer mode is " +
         "active (finishTransactions is set to false) and purchase has been initiated. RevenueCat will not finish the " +
         "transaction, are you sure you want to do this?" }
     var paymentqueue_removedtransaction: String {
@@ -27,14 +27,14 @@ import Foundation
         "didRevokeEntitlementsForProductIdentifiers: %@" }
     var paymentqueue_updatedtransaction: String { "PaymentQueue updatedTransaction: %@ %@ (%@) %@ - %d" }
     var presenting_code_redemption_sheet: String { "Presenting code redemption sheet." }
-    @objc public var purchases_synced: String { "Purchases synced." }
-    @objc public var purchasing_product_from_package: String { "Purchasing product from package  - %@ in Offering %@" }
-    @objc public var purchasing_product: String { "Purchasing product - %@" }
+    var purchases_synced: String { "Purchases synced." }
+    var purchasing_product_from_package: String { "Purchasing product from package  - %@ in Offering %@" }
+    var purchasing_product: String { "Purchasing product - %@" }
     var skpayment_missing_from_skpaymenttransaction: String { "There is a problem with the " +
         "SKPaymentTransaction missing an SKPayment - this is an issue with the App Store." }
     var skpayment_missing_product_identifier: String { "There is a problem with the SKPayment missing " +
         "a product identifier - this is an issue with the App Store." }
-    @objc public var could_not_purchase_product_id_not_found: String { "makePurchase - Could not purchase SKProduct. " +
+    var could_not_purchase_product_id_not_found: String { "makePurchase - Could not purchase SKProduct. " +
         "Couldn't find its product identifier. This is possibly an App Store quirk." }
     var product_identifier_nil: String {
         "Apple returned a product where the productIdentifier is nil, this is possibly an App Store quirk"
