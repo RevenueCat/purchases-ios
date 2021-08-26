c8f1581d2bc5fc30d6100cd4f2d5c5f793d716a4
Joshua Liebowitz <taquitos@users.noreply.github.com>
2021-08-26 10:09:36 -0700

    Clean up references to PurchasesCoreSwift things

--- Package.swift
+++ Package.swift
@@ -4,6 +4,36 @@
 import PackageDescription
 import class Foundation.ProcessInfo
 
+func resolveTargets() -> [Target] {
+    let objcSources = ["Purchases/Info.plist",
+                       "Purchases/Attribution",
+                       "Purchases/Caching",
+                       "Purchases/FoundationExtensions",
+                       "Purchases/Identity",
+                       "Purchases/LocalReceiptParsing",
+                       "Purchases/Logging",
+                       "Purchases/Misc",
+                       "Purchases/Networking",
+                       "Purchases/Public",
+                       "Purchases/Purchasing",
+                       "Purchases/StoreKitExtensions",
+                       "Purchases/SubscriberAttributes",
+    ]
+
+    let infoPlist = "Purchases/Info.plist"
+
+    let baseTargets: [Target] = [
+        .target(name: "Purchases",
+                path: ".",
+                exclude: [infoPlist],
+                sources: ["Purchases"],
+                publicHeadersPath: "Purchases/Public",
+                cSettings: objcSources.map { CSetting.headerSearchPath($0) }
+        )
+    ]
+
+    return baseTargets
+}
 
 let package = Package(
     name: "Purchases",
@@ -12,10 +42,8 @@ let package = Package(
     ],
     products: [
         .library(name: "Purchases",
-                 targets: ["Purchases"])
+                 targets: ["Purchases"]),
     ],
     dependencies: [],
-    targets: [
-        .target(name: "Purchases", path: ".", sources: ["Purchases"])
-    ]
+    targets: resolveTargets()
 )
--- Purchases.podspec
+++ Purchases.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
   s.name             = "Purchases"
   s.version          = "3.13.0-SNAPSHOT"
-  s.summary          = "Subscription and in-app-purchase backend service."
+  s.summary          = "Swift portion of RevenueCat's Subscription and in-app-purchase backend service."
 
   s.description      = <<-DESC
                        Save yourself the hastle of implementing a subscriptions backend. Use RevenueCat instead https://www.revenuecat.com/
@@ -22,12 +22,7 @@ Pod::Spec.new do |s|
   s.tvos.deployment_target = '9.0'
 
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
-  s.dependency 'PurchasesCoreSwift', '3.13.0-SNAPSHOT'
 
-
-  s.source_files = ['Purchases/**/*.{h,m}']
-  s.public_header_files = [
-    'Purchases/Public/*.h'
-  ]
-
+  s.source_files = 'Purchases/**/*.{h,swift}'
+  
 end
