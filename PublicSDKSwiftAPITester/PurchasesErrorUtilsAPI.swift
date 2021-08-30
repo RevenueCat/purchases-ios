e67893ca2da87c86589b8560e94d7f865fff6c00
Maddie Beyl <maddie@revenuecat.com>
2021-08-30 14:22:17 -0700

    Add PurchasesErrorUtilsAPI, attempt to fix main.swift issue

--- PublicSDKSwiftAPITester/PurchasesErrorUtilsAPI.swift
+++ PublicSDKSwiftAPITester/PurchasesErrorUtilsAPI.swift
@@ -13,10 +13,24 @@
 
 import Foundation
 import Purchases
+import StoreKit
 
 func checkPurchasesErrorUtilsAPI() {
+    let underlyingError = NSError(domain: "NetworkErrorDomain", code: 28, userInfo: [String: Any]())
+    var error: Error = Purchases.ErrorUtils.networkError(withUnderlyingError: underlyingError)
+    error = Purchases.ErrorUtils.backendError(withBackendCode: 12345, backendMessage: nil)
+    error = Purchases.ErrorUtils.backendError(withBackendCode: 12345, backendMessage: "message")
+    error = Purchases.ErrorUtils.unexpectedBackendResponseError()
+    error = Purchases.ErrorUtils.missingReceiptFileError()
+    error = Purchases.ErrorUtils.missingAppUserIDError()
+    error = Purchases.ErrorUtils.logOutAnonymousUserError()
+    error = Purchases.ErrorUtils.paymentDeferredError()
+    error = Purchases.ErrorUtils.unknownError()
+    // TODO we had this in objc, do we not in swift?
+//    error = Purchases.ErrorUtils.unknownError(message: )
 
-    // todo wat
-//    let error: Error = RCPurchasesErrorUtils.init()
+    let underlyingSKError = NSError(domain: SKErrorDomain, code: SKError.unknown.rawValue, userInfo: nil)
+    error = Purchases.ErrorUtils.purchasesError(withSKError: underlyingSKError)
 
+    print(error, error.localizedDescription)
 }
---
+++ PublicSDKSwiftAPITester/main.swift
@@ -0,0 +1,40 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.checkOfferingAPI
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  main.swift
+//
+//  Created by Madeline Beyl on 8/25/21.
+
+import Foundation
+
+checkRCAttributionNetworkEnums()
+
+checkEntitlementInfoAPI()
+checkEntitlementInfoEnums()
+
+checkEntitlementInfosAPI()
+
+checkIntroEligibilityAPI()
+checkIntroEligibilityEnums()
+
+checkOfferingAPI()
+
+checkOfferingsAPI()
+
+checkPurchaserInfoAPI()
+
+checkPurchasesAPI()
+checkPurchasesEnums()
+
+checkPurchasesErrorUtilsAPI()
+
+checkPackageAPI()
+checkPackageEnums()
+
+checkTransactionAPI()
