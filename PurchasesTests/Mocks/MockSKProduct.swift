fabef4063d495bf2304456ab2d2817c0862d1e46
Cesar de la Vega <cesarvegaro@gmail.com>
2021-09-08 14:33:44 -0700

    Adds missing changes from #679 (bugfix on check intro eligibility) (#825)

--- PurchasesTests/Mocks/MockSKProduct.swift
+++ PurchasesTests/Mocks/MockSKProduct.swift
@@ -32,23 +32,26 @@ class MockSKProduct: SKProduct {
         return mockPrice ?? 2.99 as NSDecimalNumber
     }
 
-    @available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *)
+    @available(iOS 11.2, macCatalyst 13.0, tvOS 11.2, macOS 10.13.2, *)
     override var introductoryPrice: SKProductDiscount? {
-        mockDiscount ?? MockDiscount()
+        if #available(iOS 12.2, *) {
+            return mockDiscount
+        }
+        return nil
     }
 
-    @available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *)
+    @available(iOS 11.2, macCatalyst 13.0, tvOS 11.2, macOS 10.13.2, *)
     lazy var mockDiscount: SKProductDiscount? = nil
 
-    @available(iOS 12.2, tvOS 12.2, macOS 10.13.2, *)
+    @available(iOS 12.2, macCatalyst 13.0, tvOS 12.2, macOS 10.13.2, *)
     override var discounts: [SKProductDiscount] {
         return (mockDiscount != nil) ? [mockDiscount!] : []
     }
 
-    @available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *)
+    @available(iOS 11.2, macCatalyst 13.0, tvOS 11.2, macOS 10.13.2, *)
     lazy var mockSubscriptionPeriod: SKProductSubscriptionPeriod? = nil
 
-    @available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *)
+    @available(iOS 11.2, macCatalyst 13.0, tvOS 11.2, macOS 10.13.2, *)
     override var subscriptionPeriod: SKProductSubscriptionPeriod {
         return mockSubscriptionPeriod ?? SKProductSubscriptionPeriod(numberOfUnits: 1, unit:.month)
     }
