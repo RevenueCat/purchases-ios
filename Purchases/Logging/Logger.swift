c60d10bcf728be84d4d95ef628dc9d6d21df3f66
aboedo <andresboedo@gmail.com>
2021-09-13 12:08:02 -0300

    remove unused Logging methods (#842)
    * removed unused methods in Logger.swift, now that the format has been updated
    
    * updates `LogLevel` to use the same `CustomStringConvertible` pattern used for String files

--- Purchases/Logging/Logger.swift
+++ Purchases/Logging/Logger.swift
@@ -14,11 +14,11 @@
 
 import Foundation
 
-@objc(RCLogLevel) public enum LogLevel: Int {
+@objc(RCLogLevel) public enum LogLevel: Int, CustomStringConvertible {
 
     case debug, info, warn, error
 
-    func description() -> String {
+    public var description: String {
         switch self {
         case .debug: return "DEBUG"
         case .info: return "INFO"
@@ -26,43 +26,28 @@ import Foundation
         case .error: return "ERROR"
         }
     }
+
 }
 
 class Logger {
 
     static var logLevel: LogLevel = .info
     static var logHandler: (LogLevel, String) -> Void = { level, message in
-        NSLog("[\(frameworkDescription)] - \(level.description()): \(message)")
+        NSLog("[\(frameworkDescription)] - \(level.description): \(message)")
     }
 
     private static let frameworkDescription = "Purchases"
 
-    static func debug(_ message: String) {
-        log(level: .debug, intent: .info, message: message)
-    }
-
     static func debug(_ message: CustomStringConvertible) {
         log(level: .debug, intent: .info, message: message.description)
-    }
-
-    static func info(_ message: String) {
-        log(level: .info, intent: .info, message: message)
     }
 
     static func info(_ message: CustomStringConvertible) {
         log(level: .info, intent: .info, message: message.description)
     }
 
-    static func warn(_ message: String) {
-        log(level: .warn, intent: .warning, message: message)
-    }
-
     static func warn(_ message: CustomStringConvertible) {
         log(level: .warn, intent: .warning, message: message.description)
-    }
-
-    static func error(_ message: String) {
-        log(level: .error, intent: .rcError, message: message)
     }
 
     static func error(_ message: CustomStringConvertible) {
@@ -73,48 +58,24 @@ class Logger {
 
 extension Logger {
 
-    static func appleError(_ message: String) {
-        log(level: .error, intent: .appleError, message: message)
-    }
-
     static func appleError(_ message: CustomStringConvertible) {
         log(level: .error, intent: .appleError, message: message.description)
-    }
-
-    static func appleWarning(_ message: String) {
-        log(level: .warn, intent: .appleError, message: message)
     }
 
     static func appleWarning(_ message: CustomStringConvertible) {
         log(level: .warn, intent: .appleError, message: message.description)
     }
 
-    static func purchase(_ message: String) {
-        log(level: .debug, intent: .purchase, message: message)
-    }
-
     static func purchase(_ message: CustomStringConvertible) {
         log(level: .debug, intent: .purchase, message: message.description)
-    }
-
-    static func rcPurchaseSuccess(_ message: String) {
-        log(level: .info, intent: .rcPurchaseSuccess, message: message)
     }
 
     static func rcPurchaseSuccess(_ message: CustomStringConvertible) {
         log(level: .info, intent: .rcPurchaseSuccess, message: message.description)
     }
 
-    static func rcSuccess(_ message: String) {
-        log(level: .debug, intent: .rcSuccess, message: message)
-    }
-
     static func rcSuccess(_ message: CustomStringConvertible) {
         log(level: .debug, intent: .rcSuccess, message: message.description)
-    }
-
-    static func user(_ message: String) {
-        log(level: .debug, intent: .user, message: message)
     }
 
     static func user(_ message: CustomStringConvertible) {
