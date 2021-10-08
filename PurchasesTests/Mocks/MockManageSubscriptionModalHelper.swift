776e276560c79ca778e2f12b1a669b7c0dfa7998
vegaro <cesarvegaro@gmail.com>
2021-10-08 13:49:54 -0700

    why do you break!?

---
+++ PurchasesTests/Mocks/MockManageSubscriptionModalHelper.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  MockManageSubsModalHelper.swift
+//
+//  Created by César de la Vega on 10/8/21.
+
+import Foundation
+@testable import RevenueCat
+
+class MockManageSubscriptionModalHelper: ManageSubscriptionModalHelper {
+
+    
+
+}
