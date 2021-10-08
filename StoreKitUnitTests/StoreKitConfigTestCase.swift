c7460879263dafe839b45ebbbf53198ac52030c2
aboedo <andresboedo@gmail.com>
2021-10-08 18:24:56 -0300

    improve pre-warming and fix flaky tests (#897)
    * replaced manual pre-warming with orb
    
    * removed appleTV from prewarming list
    
    * removed duplicate source file
    
    * added test timeouts
    
    * added artificial wait before running storekit tests
    
    * made the artificial wait run only once
    
    * removed unnecessary param
    
    * moved the `Self.hasWaited = true` line above, removed unnecessary parenthesis
    
    * doubled the wait time just in case, made it a static let.
    
    * doubled initial wait time again
    
    * removed another instance of `wait_for_simulator:true`

--- StoreKitUnitTests/StoreKitConfigTestCase.swift
+++ StoreKitUnitTests/StoreKitConfigTestCase.swift
@@ -20,6 +20,10 @@ import XCTest
 @available(iOS 14.0, tvOS 14.0, macOS 11.0, watchOS 6.2, *)
 class StoreKitConfigTestCase: XCTestCase {
 
+    private static var hasWaited = false
+    private static let waitLock = NSLock()
+    private static let waitTimeInSeconds: Double = 20
+
     var testSession: SKTestSession!
     var userDefaults: UserDefaults!
 
@@ -31,9 +35,27 @@ class StoreKitConfigTestCase: XCTestCase {
         testSession.resetToDefaultState()
         testSession.disableDialogs = true
         testSession.clearTransactions()
+
+        self.waitForStoreKitTestIfNeeded()
+
         let suiteName = "StoreKitConfigTests"
         userDefaults = UserDefaults(suiteName: suiteName)
         userDefaults?.removePersistentDomain(forName: suiteName)
+    }
+
+}
+
+private extension StoreKitConfigTestCase {
+
+    func waitForStoreKitTestIfNeeded() {
+        // StoreKitTest seems to take a few seconds to initialize, and running tests before that
+        // might result in failure. So we give it a few seconds to load before testing.
+        Self.waitLock.lock()
+        if !Self.hasWaited {
+            Self.hasWaited = true
+            Thread.sleep(forTimeInterval: Self.waitTimeInSeconds)
+        }
+        Self.waitLock.unlock()
     }
 
 }
