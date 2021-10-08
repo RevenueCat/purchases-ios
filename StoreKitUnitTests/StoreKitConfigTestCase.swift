eb8adac27c117b3f7145308d5e5b0c24f28d7859
aboedo <andresboedo@gmail.com>
2021-10-08 15:16:51 -0300

    Sk2 purchase tests (#873)
    * added tests skeleton, cleaned up handling of purchase sk2 package
    
    * code clean up, added mocks to sk unit tests target, clean up tests code
    
    * added mockStoreKit2Listener, fixed purchasesOrchestator test
    
    * implemented missing test cases
    
    * casted SK2Product to any for storing to prevent linking issues
    
    * fixed more issues with linking + moved PurchasesOrchestratorTests to storekit tests
    
    * linting
    
    * fixed duplicate files in build phase
    
    * fixed more issues with linking on older OS
    
    * fixed rebase issues
    
    * fix one test in SwiftAPITester
    
    * updated xcode version for swift api tester
    
    * updated api testers to use an existing bundle id to prevent code signing issues
    
    * remove development team + undo bundle id updates
    
    * added fastlane match readonly step
    
    * re-add dev team
    
    * re-add dev team to release builds for objc api tester ios
    
    * updated certs to use fastlane match
    
    * specified missing params for match
    
    * fixed copy/paste issue
    
    * `MockStoreKit2Listener` -> `MockStoreKit2TransactionListener`
    
    * added clearing of user defaults before every test run
    
    * added crude handling of the user cancelled case for SK2 purchases.
    
    * fixed indentation
    
    * updated test name to reflect what it's actually testing
    
    * reset the value of `failTransactionsEnabled` before every test
    
    * moved to using `resetToDefaultState`

---
+++ StoreKitUnitTests/StoreKitConfigTestCase.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  StoreKitConfigTestCase.swift
+//
+//  Created by Andrés Boedo on 23/9/21.
+
+import Foundation
+import Nimble
+@testable import RevenueCat
+import StoreKitTest
+import XCTest
+
+@available(iOS 14.0, tvOS 14.0, macOS 11.0, watchOS 6.2, *)
+class StoreKitConfigTestCase: XCTestCase {
+
+    var testSession: SKTestSession!
+    var userDefaults: UserDefaults!
+
+    override func setUpWithError() throws {
+        guard #available(iOS 14.0, tvOS 14.0, macOS 11.0, watchOS 6.2, *) else {
+            throw XCTSkip("Required API is not available for this test.")
+        }
+        testSession = try SKTestSession(configurationFileNamed: "UnitTestsConfiguration")
+        testSession.resetToDefaultState()
+        testSession.disableDialogs = true
+        testSession.clearTransactions()
+        let suiteName = "StoreKitConfigTests"
+        userDefaults = UserDefaults(suiteName: suiteName)
+        userDefaults?.removePersistentDomain(forName: suiteName)
+    }
+
+}
