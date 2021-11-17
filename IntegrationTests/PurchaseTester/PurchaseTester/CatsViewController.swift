0ebe66ce6ddcc28299d9b460796ab26d32d14075
Maddie Beyl <maddie@revenuecat.com>
2021-11-16 16:08:13 -0800

    Add buttons for manage sub and begin refund

--- IntegrationTests/PurchaseTester/PurchaseTester/CatsViewController.swift
+++ IntegrationTests/PurchaseTester/PurchaseTester/CatsViewController.swift
@@ -12,6 +12,8 @@ import RevenueCat
 class CatsViewController: UIViewController {
     
     @IBOutlet weak var goPremiumButton: UIButton!
+    @IBOutlet weak var manageSubButton: UIButton!
+    @IBOutlet weak var beginRefundButton: UIButton!
     @IBOutlet weak var restorePurchasesButton: UIButton!
     @IBOutlet weak var catContentLabel: UILabel!
     @IBOutlet weak var expirationDateLabel: UILabel!
@@ -21,6 +23,8 @@ class CatsViewController: UIViewController {
         super.viewDidLoad()
         
         goPremiumButton.addTarget(self, action: #selector(goPremiumButtonTapped), for: .touchUpInside)
+        manageSubButton.addTarget(self, action: #selector(manageSubButtonTapped), for: .touchUpInside)
+        beginRefundButton.addTarget(self, action: #selector(beginRefundButtonTapped), for: .touchUpInside)
         restorePurchasesButton.addTarget(self, action: #selector(restorePurchasesButtonTapped), for: .touchUpInside)
         
         Purchases.shared.getCustomerInfo{ (maybeCustomerInfo, error) in
@@ -49,12 +53,13 @@ class CatsViewController: UIViewController {
                 }
                 if let expirationDate = customerInfo.expirationDate(forEntitlement: "pro_cat") {
                     self.expirationDateLabel.text = "Expiration Date: \(dateFormatter.string(from: expirationDate))"
-                    
                 }
+
                 
             } else {
                 print("Happy cats are only for premium members 😿")
                 self.catContentLabel.text = "😿"
+                self.beginRefundButton.isHidden = true
             }
         }
     }
@@ -62,6 +67,27 @@ class CatsViewController: UIViewController {
     
     @objc func goPremiumButtonTapped() {
         navigationController?.popViewController(animated: true)
+    }
+
+    @objc func manageSubButtonTapped() {
+        Purchases.shared.showManageSubscriptionModal { error in
+            print("Error opening Manage Sub Modal")
+        }
+    }
+
+    @objc func beginRefundButtonTapped() {
+        if #available(iOS 15.0, *) {
+            Purchases.shared.beginRefundRequest(for: "com.revenuecat.purchaseTester.annual_39.99.2_week_intro")
+            { status, error in
+                switch status {
+                case .success: print("Refund request submitted!")
+                case .userCancelled: print("Refund request cancelled")
+                case .error: print("Issue submitting refund request: \(error?.localizedDescription ?? "")")
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
     @objc func restorePurchasesButtonTapped() {
