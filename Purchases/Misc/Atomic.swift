e526e7ceea519c6d87c6c31e05299c104a8cac83
NachoSoto <ignaciosoto90@gmail.com>
2021-11-25 10:17:55 -0800

    Improved Atomic documentation

--- Purchases/Misc/Atomic.swift
+++ Purchases/Misc/Atomic.swift
@@ -13,13 +13,38 @@
 
 import Foundation
 
-/// An atomic variable.
+/**
+ * A generic object that performs all write and read operations atomically.
+ * Use it to prevent data races when accessing an object.
+ *
+ * Usage:
+ * ```swift
+ * let foo = Atomic<MyClass>
+ *
+ * // read values
+ * foo.withValue {
+ *     let currentBar = $0.bar
+ *     let currentX = $0.x
+ * }
+ *
+ * // write value
+ * foo.modify {
+ *     $0.bar = 2
+ *     $0.x = "new X"
+ * }
+ * ```
+ *
+ * Or for single-line read/writes:
+ * ```swift
+ * let currentX = foo.value.x
+ * foo.value = MyClass()
+ * ```
+ **/
 internal final class Atomic<T> {
 
     private let lock: Lock
     private var _value: T
 
-    /// Atomically get or set the value of the variable.
     var value: T {
         get { withValue { $0 } }
         set { modify { $0 = newValue } }
@@ -30,7 +55,6 @@ internal final class Atomic<T> {
         lock = Lock()
     }
 
-    /// Atomically modifies the variable.
     @discardableResult
     func modify<Result>(_ action: (inout T) throws -> Result) rethrows -> Result {
         return try lock.perform {
@@ -38,7 +62,6 @@ internal final class Atomic<T> {
         }
     }
 
-    /// Atomically perform an action using the current value.
     @discardableResult
     func withValue<Result>(_ action: (T) throws -> Result) rethrows -> Result {
         return try lock.perform {
