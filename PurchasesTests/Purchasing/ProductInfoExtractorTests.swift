f8291ef3b30bf4e8bb3189900ef41cb2fc81f95d
NachoSoto <ignaciosoto90@gmail.com>
2021-12-06 16:50:52 -0800

    Refactored ProductInfoExtractorTests: moved duplicated code into `setUp`

--- PurchasesTests/Purchasing/ProductInfoExtractorTests.swift
+++ PurchasesTests/Purchasing/ProductInfoExtractorTests.swift
@@ -5,189 +5,155 @@ import StoreKit
 
 class ProductInfoExtractorTests: XCTestCase {
 
+    private var product: MockSK1Product!
+
+    private static let productID = "cool_product"
+
+    override func setUp() {
+        self.product = MockSK1Product(mockProductIdentifier: Self.productID)
+    }
+
+    private func extract() -> ProductInfo {
+        return ProductInfoExtractor.extractInfo(from: self.product)
+    }
+
     func testExtractInfoFromProductExtractsProductIdentifier() {
-        let productID = "cool_product"
-        let product = MockSK1Product(mockProductIdentifier: productID)
-        let productInfoExtractor = ProductInfoExtractor()
+        let receivedProductInfo = self.extract()
 
-        let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
-
-        expect(receivedProductInfo.productIdentifier) == productID
+        expect(receivedProductInfo.productIdentifier) == Self.productID
     }
 
     func testExtractInfoFromProductExtractsPrice() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
         let price: NSDecimalNumber = 10.99
         product.mockPrice = price
-        let productInfoExtractor = ProductInfoExtractor()
 
-        let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+        let receivedProductInfo = self.extract()
 
         expect(receivedProductInfo.price) == price
     }
 
     func testExtractInfoFromProductExtractsCurrencyCode() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
         product.mockPriceLocale = Locale(identifier: "es_UY")
-        let productInfoExtractor = ProductInfoExtractor()
 
-        var receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+        var receivedProductInfo = self.extract()
 
         expect(receivedProductInfo.currencyCode) == "UYU"
 
         product.mockPriceLocale = Locale(identifier: "en_US")
-        receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+        receivedProductInfo = self.extract()
         expect(receivedProductInfo.currencyCode) == "USD"
     }
 
     func testExtractInfoFromProductExtractsPaymentMode() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
-
         if #available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *) {
             let mockDiscount = MockDiscount()
             mockDiscount.mockPaymentMode = .freeTrial
 
             product.mockDiscount = mockDiscount
-            let productInfoExtractor = ProductInfoExtractor()
 
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.paymentMode.rawValue) == ProductInfo.PaymentMode.freeTrial.rawValue
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.paymentMode) == ProductInfo.PaymentMode.none
         }
     }
 
     func testExtractInfoFromProductExtractsIntroPrice() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
-
         if #available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *) {
             let mockDiscount = MockDiscount()
             mockDiscount.mockPrice = 10.99
 
             product.mockDiscount = mockDiscount
-            let productInfoExtractor = ProductInfoExtractor()
 
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.introPrice) == 10.99
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.introPrice).to(beNil())
         }
     }
 
     func testExtractInfoFromProductExtractsNormalDuration() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
-
         if #available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *) {
             product.mockSubscriptionPeriod = SKProductSubscriptionPeriod(numberOfUnits: 2, unit: .month)
-            let productInfoExtractor = ProductInfoExtractor()
 
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.normalDuration) == "P2M"
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.normalDuration).to(beNil())
         }
     }
 
     func testExtractInfoFromProductDoesNotExtractNormalDurationIfSubscriptionPeriodIsZero() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
-
         if #available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *) {
             product.mockSubscriptionPeriod = SKProductSubscriptionPeriod(numberOfUnits: 0, unit: .month)
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.normalDuration).to(beNil())
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.normalDuration).to(beNil())
         }
     }
 
     func testExtractInfoFromProductExtractsIntroDuration() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
-
         if #available(iOS 11.2, tvOS 11.2, macOS 10.13.2, *) {
             let mockDiscount = MockDiscount()
             mockDiscount.mockSubscriptionPeriod = SKProductSubscriptionPeriod(numberOfUnits: 3, unit: .year)
 
             product.mockDiscount = mockDiscount
-            let productInfoExtractor = ProductInfoExtractor()
 
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.introDuration) == "P3Y"
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.introDuration).to(beNil())
         }
     }
 
     func testExtractInfoFromProductExtractsIntroDurationType() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
-
         if #available(iOS 11.2, macOS 10.14.4, tvOS 11.2, *) {
             let mockDiscount = MockDiscount()
             mockDiscount.mockPaymentMode = .freeTrial
 
             product.mockDiscount = mockDiscount
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.introDurationType) == .freeTrial
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.introDurationType) == IntroDurationType.none
         }
     }
 
     func testExtractInfoFromProductExtractsSubscriptionGroup() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
         if #available(iOS 12.0, macCatalyst 13.0, macOS 10.14, tvOS 12.0, watchOS 6.2, *) {
             let group = "mock_group"
             product.mockSubscriptionGroupIdentifier = group
-            let productInfoExtractor = ProductInfoExtractor()
 
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.subscriptionGroup) == group
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.subscriptionGroup).to(beNil())
         }
     }
 
     func testExtractInfoFromProductExtractsDiscounts() {
-        let product = MockSK1Product(mockProductIdentifier: "cool_product")
-
         if #available(iOS 12.2, tvOS 12.2, macOS 10.13.2, *) {
             let mockDiscount = MockDiscount()
             let paymentMode: SKProductDiscount.PaymentMode = .freeTrial
@@ -198,9 +164,7 @@ class ProductInfoExtractorTests: XCTestCase {
             mockDiscount.mockIdentifier = discountID
 
             product.mockDiscount = mockDiscount
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.discounts?.count) == 1
             let receivedPromotionalOffer = receivedProductInfo.discounts?[0]
@@ -208,9 +172,7 @@ class ProductInfoExtractorTests: XCTestCase {
             expect(receivedPromotionalOffer?.price) == price
             expect(receivedPromotionalOffer?.paymentMode.rawValue) == Int(paymentMode.rawValue)
         } else {
-            let productInfoExtractor = ProductInfoExtractor()
-
-            let receivedProductInfo = productInfoExtractor.extractInfo(from: product)
+            let receivedProductInfo = self.extract()
 
             expect(receivedProductInfo.discounts).to(beNil())
         }
