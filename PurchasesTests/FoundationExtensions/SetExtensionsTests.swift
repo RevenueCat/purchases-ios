5c7c778ffb9c705ad45cea67bb6289fc9555961c
NachoSoto <NachoSoto@users.noreply.github.com>
2021-12-16 09:27:18 -0800

    Introduced `Set.dictionaryWithValues`, `Sequence.dictionaryWithKeys`, and `Sequence.dictionaryAllowingDuplicateKeys` (#1090)
    This makes creating dictionaries simpler than using `reduce(into:)`.
    Note that this doesn't change any behaviors. The existing implementations that allowed duplicates still do, and vice-versa.

---
+++ PurchasesTests/FoundationExtensions/SetExtensionsTests.swift
@@ -0,0 +1,43 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  SetExtensionsTests.swift
+//
+//  Created by Nacho Soto on 12/15/21.
+
+import XCTest
+import Nimble
+
+@testable import RevenueCat
+
+class SetExtensionsTests: XCTestCase {
+
+    func testCreatingDictionaryWithEmptySet() {
+        expect(Set<String>().dictionaryWithValues { $0 }) == [:]
+    }
+
+    func testCreatingDictionaryWithOneItem() {
+        let keys: Set<String> = ["1"]
+
+        expect(keys.dictionaryWithValues { Int($0)! }) == [
+            "1": 1
+        ]
+    }
+
+    func testCreatingDictionaryWithMultipleValues() {
+        let keys: Set<String> = ["1", "2", "3"]
+
+        expect(keys.dictionaryWithValues { Int($0)! + 1 }) == [
+            "1": 2,
+            "2": 3,
+            "3": 4
+        ]
+    }
+    
+}
