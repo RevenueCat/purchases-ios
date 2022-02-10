20df7193684d6d1cff779e4649550f888e7ab0b4
aboedo <andresboedo@gmail.com>
2022-02-10 16:48:34 -0300

    fixed bad references

--- Purchases/Purchasing/Purchases.swift
+++ Purchases/Purchasing/Purchases.swift
@@ -730,7 +730,8 @@ public extension Purchases {
      * This function will log in the current user with an ``appUserID``.
      *
      * - Parameter appUserID: The ``appUserID`` that should be linked to the current user.
-     * - returns: The completion block will be called with the latest ``CustomerInfo`` and a `Boolean` specifying
+     *
+     * The `completion` block will be called with the latest ``CustomerInfo`` and a `Boolean` specifying
      * whether the user was created for the first time in the RevenueCat backend.
      *
      * RevenueCat provides a source of truth for a subscriber's status across different platforms.
@@ -774,7 +775,7 @@ public extension Purchases {
      * This function will log in the current user with an ``appUserID``.
      *
      * - Parameter appUserID: The ``appUserID`` that should be linked to the current user.
-     * - returns: The completion block will be called with the latest ``CustomerInfo`` and a `Boolean` specifying
+     * - returns: A tuple of: the latest ``CustomerInfo`` and a `Boolean` specifying
      * whether the user was created for the first time in the RevenueCat backend.
      *
      * RevenueCat provides a source of truth for a subscriber's status across different platforms.
