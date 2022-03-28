6f553811aedd453305abf492e54008e1a58e9535
aboedo <andresboedo@gmail.com>
2022-03-28 16:46:30 -0300

    added comment to empty file

--- Sources/DocCDocumentation/EmptyFile.swift
+++ Sources/DocCDocumentation/EmptyFile.swift
@@ -4,5 +4,7 @@
 //
 //  Created by Andrés Boedo on 3/28/22.
 //
-
-import Foundation
+// This is an empty source file used to make RevenueCat a valid
+// documentation build target.
+//
+// see Apple's example at https://github.com/apple/swift-docc-plugin/
