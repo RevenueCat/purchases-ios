683f703974b5579a564b5433124720e94932b096
NachoSoto <NachoSoto@users.noreply.github.com>
2022-04-01 13:34:53 -0700

    `BackendIntegrationTests`: extracted base class to allow future test classes (#1440)

---
+++ Tests/BackendIntegrationTests/BaseBackendIntegrationTests.swift
@@ -0,0 +1,69 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  BaseBackendIntegrationTests.swift
+//
+//  Created by Nacho Soto on 4/1/22.
+
+import RevenueCat
+import XCTest
+
+final class TestPurchaseDelegate: NSObject, PurchasesDelegate {
+
+    var customerInfo: CustomerInfo?
+    var customerInfoUpdateCount = 0
+
+    func purchases(_ purchases: Purchases, receivedUpdated customerInfo: CustomerInfo) {
+        self.customerInfo = customerInfo
+        customerInfoUpdateCount += 1
+    }
+
+}
+
+class BaseBackendIntegrationTests: XCTestCase {
+
+    private var userDefaults: UserDefaults!
+    // swiftlint:disable:next weak_delegate
+    private(set) var purchasesDelegate: TestPurchaseDelegate!
+
+    class var sk2Enabled: Bool { return false }
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+        guard Constants.apiKey != "REVENUECAT_API_KEY", Constants.proxyURL != "REVENUECAT_PROXY_URL" else {
+            XCTFail("Must set configuration in `Constants.swift`")
+            throw ErrorCode.configurationError
+        }
+
+        userDefaults = UserDefaults(suiteName: Constants.userDefaultsSuiteName)
+        userDefaults?.removePersistentDomain(forName: Constants.userDefaultsSuiteName)
+        if !Constants.proxyURL.isEmpty {
+            Purchases.proxyURL = URL(string: Constants.proxyURL)
+        }
+
+        configurePurchases()
+    }
+
+}
+
+private extension BaseBackendIntegrationTests {
+
+    func configurePurchases() {
+        purchasesDelegate = TestPurchaseDelegate()
+        Purchases.configure(withAPIKey: Constants.apiKey,
+                            appUserID: nil,
+                            observerMode: false,
+                            userDefaults: userDefaults,
+                            useStoreKit2IfAvailable: Self.sk2Enabled)
+        Purchases.logLevel = .debug
+        Purchases.shared.delegate = purchasesDelegate
+    }
+
+}
