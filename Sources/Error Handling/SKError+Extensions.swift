02e5e3aca3c830acaa36d05ec8977a476550ac3f
NachoSoto <ignaciosoto90@gmail.com>
2022-04-05 13:17:14 -0700

    Refactored undocumented error codes

--- Sources/Error Handling/SKError+Extensions.swift
+++ Sources/Error Handling/SKError+Extensions.swift
@@ -49,10 +49,14 @@ extension SKError {
             return ErrorUtils.invalidPromotionalOfferError(error: self)
         case .unknown:
             if let error = self.userInfo[NSUnderlyingErrorKey] as? NSError {
-                switch (error.domain, error.code) {
-                case ("ASDServerErrorDomain", 3532): // "You’re currently subscribed to this"
-                    // See https://github.com/RevenueCat/purchases-ios/issues/392
-                    return ErrorUtils.productAlreadyPurchasedError(error: self)
+                switch error.domain {
+                case ASDServerError.domain:
+                    switch ASDServerError.Code(rawValue: error.code) {
+                    case .currentlySubscribed:
+                        return ErrorUtils.productAlreadyPurchasedError(error: self)
+
+                    default: break
+                    }
 
                 default: break
                 }
@@ -61,13 +65,19 @@ extension SKError {
             return ErrorUtils.storeProblemError(error: self)
 
         @unknown default:
-            switch self.code.rawValue {
-            case 907: // "Unhandled exception"
+            switch SKError.UndocumentedCode(rawValue: self.code.rawValue) {
+            case .unhandledException:
                 if let error = self.userInfo[NSUnderlyingErrorKey] as? NSError {
-                    switch (error.domain, error.code) {
-                    case ("AMSErrorDomain", 6): // "Payment Sheet Failed"
-                        // See https://github.com/RevenueCat/purchases-ios/issues/1445
-                        return .purchaseCancelledError
+                    switch error.domain {
+                    case AMSError.domain:
+                        switch AMSError.Code(rawValue: error.code) {
+                            // See https://github.com/RevenueCat/purchases-ios/issues/1445
+                            // Cancellations sometimes show up as undocumented errors instead of regular cancellations
+                        case .paymentSheetFailed:
+                            return ErrorUtils.purchaseCancelledError(error: self)
+
+                        default: break
+                        }
 
                     default: break
                     }
@@ -78,6 +88,43 @@ extension SKError {
 
             return ErrorUtils.unknownError(error: self)
         }
+    }
+
+}
+
+private extension SKError {
+
+    enum UndocumentedCode: Int {
+
+        // See https://github.com/RevenueCat/purchases-ios/issues/1445
+        case unhandledException = 907
+
+    }
+
+}
+
+private enum ASDServerError {
+
+    static let domain = "ASDServerErrorDomain"
+
+    enum Code: Int {
+
+        // See https://github.com/RevenueCat/purchases-ios/issues/392
+        case currentlySubscribed = 3532
+
+    }
+
+}
+
+private enum AMSError {
+
+    static let domain = "AMSErrorDomain"
+
+    enum Code: Int {
+
+        // See https://github.com/RevenueCat/purchases-ios/issues/1445
+        case paymentSheetFailed = 6
+
     }
 
 }
