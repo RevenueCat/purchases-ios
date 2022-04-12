0f64ac4b8d977587d330912570fb08cf89b2ed68
Joshua Liebowitz <josh@revenuecat.com>
2022-04-12 15:28:25 -0700

    Pass storefront into ProductRequestData for receipt posting
    [CF-434]

--- Sources/Purchasing/ProductRequestData.swift
+++ Sources/Purchasing/ProductRequestData.swift
@@ -23,6 +23,7 @@ struct ProductRequestData {
     let productIdentifier: String
     let paymentMode: StoreProductDiscount.PaymentMode?
     let currencyCode: String?
+    let storefront: String?
     let price: Decimal
     let normalDuration: String?
     let introDuration: String?
@@ -32,9 +33,11 @@ struct ProductRequestData {
     let discounts: [StoreProductDiscount]?
 
     var cacheKey: String {
-        var key = """
-        \(productIdentifier)-\(price)-\(currencyCode ?? "")-\(paymentMode?.rawValue ?? -1)-\(introPrice ?? 0)-\
-        \(subscriptionGroup ?? "")-\(normalDuration ?? "")-\(introDuration ?? "")-\(introDurationType?.rawValue ?? -1)
+        var key =
+        """
+        \(productIdentifier)-\(price)-\(currencyCode ?? "")-\(storefront ?? "")-\(paymentMode?.rawValue ?? -1)-\
+        \(introPrice ?? 0)-\(subscriptionGroup ?? "")-\(normalDuration ?? "")-\(introDuration ?? "")-\
+        \(introDurationType?.rawValue ?? -1)
         """
 
         guard let discounts = discounts else {
@@ -56,6 +59,7 @@ extension ProductRequestData: Encodable {
         case productIdentifier = "product_id"
         case paymentMode = "payment_mode"
         case currencyCode = "currency"
+        case storefront
         case price
         case normalDuration = "normal_duration"
         case introDuration = "intro_duration"
@@ -75,6 +79,7 @@ extension ProductRequestData: Encodable {
 
         try container.encodeIfPresent(self.paymentMode, forKey: .paymentMode)
         try container.encode(self.currencyCode, forKey: .currencyCode)
+        try container.encode(self.storefront, forKey: .storefront)
         try container.encode((self.price as NSDecimalNumber).description, forKey: .price)
         try container.encodeIfPresent(self.subscriptionGroup, forKey: .subscriptionGroup)
         try container.encodeIfPresent(self.discounts, forKey: .discounts)
--- Sources/Purchasing/StoreKit1/StoreKitWrapper.swift
+++ Sources/Purchasing/StoreKit1/StoreKitWrapper.swift
@@ -39,6 +39,14 @@ class StoreKitWrapper: NSObject, SKPaymentTransactionObserver {
     @available(iOS 8.0, macOS 10.14, watchOS 6.2, macCatalyst 13.0, *)
     static var simulatesAskToBuyInSandbox = false
 
+    var currentStorefront: String? {
+        guard #available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.2, *) else {
+            return nil
+        }
+
+        return self.paymentQueue.storefront?.countryCode
+    }
+
     weak var delegate: StoreKitWrapperDelegate? {
         didSet {
             if delegate != nil {
