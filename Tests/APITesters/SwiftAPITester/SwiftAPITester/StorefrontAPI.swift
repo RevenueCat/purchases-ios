34104c6df3b731e2868ffa95340efe7309562292
NachoSoto <ignaciosoto90@gmail.com>
2022-04-22 14:47:10 -0700

    Added `Storefront.currentStorefront` to `SwiftAPITester`

--- Tests/APITesters/SwiftAPITester/SwiftAPITester/StorefrontAPI.swift
+++ Tests/APITesters/SwiftAPITester/SwiftAPITester/StorefrontAPI.swift
@@ -17,6 +17,10 @@ func checkStorefrontAPI() {
     let sk1Storefront: SKStorefront? = storefront.sk1Storefront
     let sk2Storefront: StoreKit.Storefront? = storefront.sk2Storefront
 
+    _ = Task {
+        let _: RevenueCat.Storefront? = await Storefront.currentStorefront
+    }
+
     print(identifier,
           countryCode,
           sk1Storefront!,
