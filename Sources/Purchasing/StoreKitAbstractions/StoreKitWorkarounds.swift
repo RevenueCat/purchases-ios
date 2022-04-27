2dd572ca5b8a4e678d1ed3b61064c3c549650a3f
NachoSoto <ignaciosoto90@gmail.com>
2022-04-27 12:23:19 -0700

    Moved another extension to `StoreKitWorkarounds`

--- Sources/Purchasing/StoreKitAbstractions/StoreKitWorkarounds.swift
+++ Sources/Purchasing/StoreKitAbstractions/StoreKitWorkarounds.swift
@@ -45,3 +45,33 @@ extension SKPaymentTransaction {
     }
 
 }
+
+extension SK1Product {
+
+    /// Attempts to find a non-nil `productIdentifier`.
+    ///
+    /// Although both `SK1Product.productIdentifier` and `SKPayment.productIdentifier`
+    /// are supposed to be non-nil, we've seen instances where this is not true.
+    /// so we cast into optionals in order to check nullability, and try to fall back if possible.
+    func extractProductIdentifier(withPayment payment: SKPayment,
+                                  fileName: String = #fileID,
+                                  functionName: String = #function,
+                                  line: UInt = #line) -> String? {
+        if let identifierFromProduct = self.productIdentifier as String?,
+           !identifierFromProduct.trimmingWhitespacesAndNewLines.isEmpty {
+            return identifierFromProduct
+        }
+        Logger.appleWarning(Strings.purchase.product_identifier_nil,
+                            fileName: fileName, functionName: functionName, line: line)
+
+        if let identifierFromPayment = payment.productIdentifier as String?,
+           !identifierFromPayment.trimmingWhitespacesAndNewLines.isEmpty {
+            return identifierFromPayment
+        }
+        Logger.appleWarning(Strings.purchase.payment_identifier_nil,
+                            fileName: fileName, functionName: functionName, line: line)
+
+        return nil
+    }
+
+}
