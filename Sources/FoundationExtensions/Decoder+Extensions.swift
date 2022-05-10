3bda51b677082bea8780c94988d7908554861478
NachoSoto <NachoSoto@users.noreply.github.com>
2022-05-10 10:26:13 -0700

    Created `DefaultValue` and `DefaultDecodable` (#1537)
    A future PR will make use of these: #1540.
    The purpose of this is to be able to define `Decodable` types with default values without needing to implement the whole `init(from decoder: Decoder)` from scratch. If any one property has a default value, that's the only way to provide it without these.
    
    For example, `EntitlementInfo.ProductData` implements this:
    ```swift
    init(from decoder: Decoder) throws {
        let container = try decoder.container(keyedBy: CodingKeys.self)
    
        self.isSandbox = try container.decodeIfPresent(Bool.self, forKey: .isSandbox) ?? false
        self.originalPurchaseDate = try container.decodeIfPresent(Date.self, forKey: .originalPurchaseDate)
        self.expiresDate = try container.decodeIfPresent(Date.self, forKey: .expiresDate)
        self.unsubscribeDetectedAt = try container.decodeIfPresent(Date.self, forKey: .unsubscribeDetectedAt)
        self.billingIssuesDetectedAt = try container.decodeIfPresent(Date.self, forKey: .billingIssuesDetectedAt)
        self.periodType = container.decode(PeriodType.self, forKey: .periodType, defaultValue: .normal)
        self.store = container.decode(Store.self, forKey: .store, defaultValue: .unknownStore)
        self.ownershipType = container.decode(PurchaseOwnershipType.self,
                                              forKey: .ownershipType,
                                              defaultValue: .purchased)
    }
    ```
    
    With these new types, that whole implementation is unnecessary and can be data-driven instead, which is more maintanable and less error-prone:
    ```swift
    struct ProductData: Decodable {
    
        @DefaultValue<PeriodType> var periodType: PeriodType
        var originalPurchaseDate: Date?
        var expiresDate: Date?
        @DefaultValue<Store> var store: Store
        @DefaultDecodable.False var isSandbox: Bool
        var unsubscribeDetectedAt: Date?
        var billingIssuesDetectedAt: Date?
        @DefaultValue<PurchaseOwnershipType> var ownershipType: PurchaseOwnershipType
    
    }
    ```

--- Sources/FoundationExtensions/Decoder+Extensions.swift
+++ Sources/FoundationExtensions/Decoder+Extensions.swift
@@ -13,6 +13,8 @@
 
 import Foundation
 
+// swiftlint:disable nesting
+
 extension Decoder {
 
     func throwValueNotFoundError(expectedType: Any.Type, message: String) -> CodableError {
@@ -21,5 +23,125 @@ extension Decoder {
                                             underlyingError: nil)
         return CodableError.valueNotFound(value: expectedType, context: context)
     }
+
+}
+
+// MARK: - DefaultValueProvider / DefaultValue
+
+/// A type that can provide a default value.
+protocol DefaultValueProvider {
+
+    associatedtype Value
+
+    static var defaultValue: Value { get }
+
+}
+
+/// A property wrapper for providing a default value to properties that conform to `DefaultValueProvider`.
+/// - Important: the value will also because `E.defaultValue` if there is a decoding error.
+/// - Example:
+/// ```
+/// struct Data {
+///     @DefaultValue<E> var e: E
+/// }
+/// ```
+@propertyWrapper
+struct DefaultValue<Source: DefaultValueProvider> {
+
+    typealias Value = Source.Value
+
+    var wrappedValue = Source.defaultValue
+
+}
+
+extension DefaultValue: Equatable where Value: Equatable {}
+extension DefaultValue: Hashable where Value: Hashable {}
+
+extension DefaultValue: Decodable where Value: Decodable {
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        self.wrappedValue = try container.decode(Value.self)
+    }
+
+}
+
+extension DefaultValue: Encodable where Value: Encodable {
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.wrappedValue)
+    }
+
+}
+
+// MARK: - DefaultDecodable
+
+// Inspired by https://swiftbysundell.com/tips/default-decoding-values/
+
+extension KeyedDecodingContainer {
+
+    func decode<T>(_ type: DefaultValue<T>.Type, forKey key: Key) -> DefaultValue<T> where T.Value: Decodable {
+        do {
+            return try self.decodeIfPresent(type, forKey: key) ?? .init()
+        } catch {
+            return .init()
+        }
+    }
+
+}
+
+/// Empty namespace for default decodable wrappers.
+enum DefaultDecodable {
+
+    typealias List = Decodable & ExpressibleByArrayLiteral
+    typealias Map = Decodable & ExpressibleByDictionaryLiteral
+
+    enum Sources {
+
+        enum True: DefaultValueProvider {
+            static var defaultValue: Bool { true }
+        }
+
+        enum False: DefaultValueProvider {
+            static var defaultValue: Bool { false }
+        }
+
+        enum EmptyString: DefaultValueProvider {
+            static var defaultValue: String { "" }
+        }
+
+        enum EmptyArray<T: List>: DefaultValueProvider {
+            static var defaultValue: T { [] }
+        }
+
+        enum EmptyDictionary<T: Map>: DefaultValueProvider {
+            static var defaultValue: T { [:] }
+        }
+
+    }
+
+}
+
+/**
+ * Property wrappers to allow providing default values to properties in `Decodable` types.
+ * Example usage:
+ * ```
+ * struct Data {
+ *   @DefaultDecodable.True var bool1: Bool
+ *   @DefaultDecodable.False var bool2: Bool
+ *   @DefaultDecodable.EmptyString var identifier: String
+ *   @DefaultDecodable.EmptyArray var values: [String]
+ *   @DefaultDecodable.EmptyDictionary var dictionary: [String: Int]
+ * }
+ * ```
+ */
+extension DefaultDecodable {
+
+    typealias True = DefaultValue<Sources.True>
+    typealias False = DefaultValue<Sources.False>
+    typealias EmptyString = DefaultValue<Sources.EmptyString>
+    typealias EmptyArray<T: List> = DefaultValue<Sources.EmptyArray<T>>
+    typealias EmptyDictionary<T: Map> = DefaultValue<Sources.EmptyDictionary<T>>
 
 }
---
+++ Tests/UnitTests/FoundationExtensions/DecoderExtensionTests.swift
@@ -0,0 +1,144 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  DecoderExtensionTests.swift
+//
+//  Created by Nacho Soto on 4/26/22.
+
+import Nimble
+import XCTest
+
+@testable import RevenueCat
+
+// swiftlint:disable type_name identifier_name nesting
+
+class DecoderExtensionsDefaultValueTests: XCTestCase {
+
+    private struct Data: Codable, Equatable {
+        enum E: String, DefaultValueProvider, Codable, Equatable {
+            case e1
+            case e2
+
+            static let defaultValue: Self = .e2
+        }
+
+        @DefaultValue<E> var e: E
+
+        init(e: E) {
+            self.e = e
+        }
+    }
+
+    private struct Data2: Codable, Equatable {
+        enum DefaultString: DefaultValueProvider {
+            static var defaultValue: String = "default"
+        }
+
+        @DefaultValue<DefaultString> var string: String
+
+        init(string: String) {
+            self.string = string
+        }
+    }
+
+    func testDecodesActualValue() throws {
+        let data = Data(e: .e1)
+        let decodedData = try data.encodeAndDecode()
+
+        expect(decodedData) == data
+    }
+
+    func testDecodesDefaultValueIfMissing() throws {
+        expect(try Data.decodeEmptyData().e) == Data.E.defaultValue
+    }
+
+    func testDecodesDefaultValueForInvalidValue() throws {
+        let json = "{\"e\": \"e3\"}".data(using: .utf8)!
+        let data: Data = try JSONDecoder.default.decode(jsonData: json)
+
+        expect(data.e) == Data.E.defaultValue
+    }
+
+    func testDecodesDefaultValueFromAnotherSource() throws {
+        expect(try Data2.decodeEmptyData().string) == Data2.DefaultString.defaultValue
+    }
+
+}
+
+class DecoderExtensionsDefaultDecodableTests: XCTestCase {
+
+    private struct Data: Codable, Equatable {
+        @DefaultDecodable.True var bool1: Bool
+        @DefaultDecodable.False var bool2: Bool
+        @DefaultDecodable.EmptyString var string: String
+        @DefaultDecodable.EmptyArray var array: [String]
+        @DefaultDecodable.EmptyDictionary var dictionary: [String: Int]
+
+        init(
+            bool1: Bool,
+            bool2: Bool,
+            string: String,
+            array: [String],
+            dictionary: [String: Int]
+        ) {
+            self.bool1 = bool1
+            self.bool2 = bool2
+            self.string = string
+            self.array = array
+            self.dictionary = dictionary
+        }
+    }
+
+    func testDecodesActualValues() throws {
+        let data = Data(bool1: false, bool2: true, string: "test", array: ["a", "b"], dictionary: ["a": 1])
+        let decodedData = try data.encodeAndDecode()
+
+        expect(decodedData) == data
+    }
+
+    func testDecodesDefaultTrue() throws {
+        expect(try Data.decodeEmptyData().bool1) == true
+    }
+
+    func testDecodesDefaultFalse() throws {
+        expect(try Data.decodeEmptyData().bool2) == false
+    }
+
+    func testDecodesDefaultString() throws {
+        expect(try Data.decodeEmptyData().string) == ""
+    }
+
+    func testDecodesDefaultArray() throws {
+        expect(try Data.decodeEmptyData().array) == []
+    }
+
+    func testDecodesDefaultDictionary() throws {
+        expect(try Data.decodeEmptyData().dictionary) == [:]
+    }
+
+}
+
+private extension Decodable where Self: Encodable {
+
+    func encodeAndDecode() throws -> Self {
+        return try JSONDecoder.default.decode(
+            jsonData: JSONEncoder.default.encode(self)
+        )
+    }
+
+}
+
+private extension Decodable {
+
+    static func decodeEmptyData() throws -> Self {
+        let json = "{}".data(using: .utf8)!
+        return try JSONDecoder.default.decode(jsonData: json)
+    }
+
+}
