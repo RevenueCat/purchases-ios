1d7d54a49af50a7b8471ad93d5f478e19a5041e3
NachoSoto <NachoSoto@users.noreply.github.com>
2022-05-10 10:29:05 -0700

    Simplified `EntitlementInfo` decoding using new `@DefaultValue` property wrappers (#1540)
    Follow up to #1537.
    `EntitlementInfo.ProductData` and related types will be moved out as part of #1496, to become part of the `CustomerInfo` response. But this is an intermediate change to simplify the decoding of this part of the data.

--- Sources/FoundationExtensions/JSONDecoder+Extensions.swift
+++ Sources/FoundationExtensions/JSONDecoder+Extensions.swift
@@ -82,25 +82,6 @@ extension JSONDecoder {
 
 }
 
-extension KeyedDecodingContainer {
-
-    /// Decodes a value of the given type for the given key.
-    /// - Parameters:
-    ///   - type: The type of value to decode.
-    ///   - key: The key that the decoded value is associated with.
-    ///   - defaultValue: The default value returned if decoding fails.
-    /// - Returns: A value of the requested type, or the given default value
-    /// if decoding fails.
-    func decode<T: Decodable>(_ type: T.Type, forKey key: Self.Key, defaultValue: T) -> T {
-        do {
-            return try decode(type, forKey: key)
-        } catch {
-            return defaultValue
-        }
-    }
-
-}
-
 extension JSONSerialization {
 
     static func dictionary(withData data: Data) throws -> [String: Any] {
