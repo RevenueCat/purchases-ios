f509e60c4702db333d0f1e0e7aa3ce56a805e934
NachoSoto <ignaciosoto90@gmail.com>
2022-05-23 19:23:01 -0700

    `PurchasesOrchestrator.purchase(sk2Product:promotionalOffer:)`: simplified implementation with new operator
    Both `return` statements where the same, the `else` was just providing a default value to `customerInfoIfSynced`.
    Because `??` doesn't support an `async` function, we couldn't use that. By using this new operator we can simplify the implementation to a single `return` statement.

---
+++ Sources/FoundationExtensions/Operators+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  Operators+Extensions.swift
+//
+//  Created by Nacho Soto on 5/23/22.
+
+infix operator ???
+
+/// Equivalent to `??` but allows an `async` default value.
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
+func ??? <T>(value: T?, defaultValue: @autoclosure () async throws -> T) async rethrows -> T {
+    if let value = value {
+        return value
+    } else {
+        return try await defaultValue()
+    }
+}
