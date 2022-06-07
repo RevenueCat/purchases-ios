9000b5f1dec7ffd7f1f890c10c043b5006f8c987
NachoSoto <NachoSoto@users.noreply.github.com>
2022-06-07 15:08:59 -0700

    Removed `LossyCollections` (#1657)
    See #1651.
    
    Since that regression fix, we're no longer using it because it's not possible to make it decode dictionary keys without changing their case.
    Therefore we can't ensure data isn't changed in the process.

--- Tests/UnitTests/Purchasing/TransactionsFactoryTests.swift
+++ Tests/UnitTests/Purchasing/TransactionsFactoryTests.swift
@@ -40,42 +40,6 @@ class TransactionsFactoryTests: TestCase {
         expect(list).to(beEmpty())
     }
 
-    func testBuildsCorrectlyEvenIfSomeTransactionsCantBeBuilt() throws {
-        let identifier = "lifetime_access"
-        let transactionIdentifier = "d6c097ba74"
-        let date = "2018-07-11T18:36:20Z"
-        let subscriptionsData = [
-            identifier: [
-                [
-                    "id": transactionIdentifier,
-                    "is_sandbox": true,
-                    "original_purchase_date": "2018-07-11T18:36:20Z",
-                    "purchase_date": date,
-                    "store": "app_store"
-                ]
-            ],
-            "invalid_non_transaction": [
-                [
-                    "ioasgioaew": 0832
-                ]
-            ]
-        ]
-
-        let nonSubscriptionTransactions = try TransactionsFactory.nonSubscriptionTransactions(
-            withSubscriptionsData: subscriptionsData
-        )
-        expect(nonSubscriptionTransactions.count) == 1
-
-        let transaction = try XCTUnwrap(nonSubscriptionTransactions.first)
-
-        expect(transaction.productIdentifier) == identifier
-        expect(transaction.purchaseDate.timeIntervalSinceReferenceDate).to(beCloseTo(
-            ISO8601DateFormatter.default.date(from: date)!.timeIntervalSinceReferenceDate
-        ))
-        expect(transaction.transactionIdentifier) == transactionIdentifier
-        expect(transaction.quantity) == 1
-    }
-
 }
 
 private extension TransactionsFactoryTests {
@@ -127,11 +91,11 @@ private extension TransactionsFactory {
         withSubscriptionsData data: [String: Any]
     ) throws -> [StoreTransaction] {
         let data = try JSONSerialization.data(withJSONObject: data)
-        let transactions: LossyArrayDictionary<CustomerInfoResponse.Transaction> = try JSONDecoder.default.decode(
+        let transactions: [String: [CustomerInfoResponse.Transaction]] = try JSONDecoder.default.decode(
             jsonData: data
         )
 
-        return Self.nonSubscriptionTransactions(withSubscriptionsData: transactions.wrappedValue)
+        return Self.nonSubscriptionTransactions(withSubscriptionsData: transactions)
     }
 
 }
