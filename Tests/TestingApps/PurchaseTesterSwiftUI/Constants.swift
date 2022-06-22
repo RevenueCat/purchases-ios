ea206d4b07ffdacf0165fe934c9ebd90eca04a8e
Josh Holtz <me@joshholtz.com>
2022-06-22 15:02:46 -0500

    Undoing things here because whoops

--- Tests/TestingApps/PurchaseTesterSwiftUI/Constants.swift
+++ Tests/TestingApps/PurchaseTesterSwiftUI/Constants.swift
@@ -16,6 +16,6 @@ struct Constants {
     /*
      The API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
      */
-//    #error("Modify this property to reflect your app's API key, then comment this line out.")
+    #error("Modify this property to reflect your app's API key, then comment this line out.")
     static let apiKey = "api_key"
 }
