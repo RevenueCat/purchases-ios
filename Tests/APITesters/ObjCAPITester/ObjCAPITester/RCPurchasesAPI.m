a511a742bb87cc905067265c9da727e9397acc24
Maddie Beyl <maddie@revenuecat.com>
2022-06-28 16:10:22 -0700

    fix deprecation messages to clarify method now called on shared instance

--- Tests/APITesters/ObjCAPITester/ObjCAPITester/RCPurchasesAPI.m
+++ Tests/APITesters/ObjCAPITester/ObjCAPITester/RCPurchasesAPI.m
@@ -76,7 +76,8 @@ BOOL isAnonymous;
     [RCPurchases addAttributionData:@{} fromNetwork:RCAttributionNetworkBranch forNetworkUserId:nil];
 
     // should have deprecation warning:
-    // 'automaticAppleSearchAdsAttributionCollection' is deprecated: Use Purchases.attribution.enableAdServicesAttributionTokenCollection instead
+    // 'automaticAppleSearchAdsAttributionCollection' is deprecated: Use
+    // Purchases.shared.attribution.enableAdServicesAttributionTokenCollection instead
     automaticAppleSearchAdsAttributionCollection = [RCPurchases automaticAppleSearchAdsAttributionCollection];
 
     // should have deprecation warning 'debugLogsEnabled' is deprecated: use logLevel instead
