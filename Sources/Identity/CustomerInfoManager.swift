ca2cc1c411bb325bb646b4f59a95e5bbae9c2f8e
Josh Holtz <me@joshholtz.com>
2022-08-15 06:54:48 -0500

    Force call delegate with customer info when being set

--- Sources/Identity/CustomerInfoManager.swift
+++ Sources/Identity/CustomerInfoManager.swift
@@ -220,6 +220,8 @@ class CustomerInfoManager {
             .map { $0 + 1 } // Next index
             ?? 0 // Or default to 0
 
+        print("nextIdentifier: \(nextIdentifier)")
+
         self.customerInfoObserversByIdentifier[nextIdentifier] = changes
 
         return { [weak self] in
