41ce4b6ec428756f8b7611ffeb242d20cdbebbe1
NachoSoto <ignaciosoto90@gmail.com>
2022-11-29 13:42:33 -0800

    Fixed `LogLevel` ordering and added tests
    A last minute change in #2080 made `LogLevel.verbose` have a higher `rawValue` to maintain backwards compatibility.
    Unfortunately, we we re relying on these to implement ordering.
    
    This PR makes that explicit by making `LogLevel` conform to `Comparable`, and added tests to verify this behavior in `Logger`.

---
+++ Tests/UnitTests/Misc/LoggerTests.swift
@@ -0,0 +1,79 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  LoggerTests.swift
+//
+//  Created by Nacho Soto on 11/29/22.
+
+import Nimble
+import XCTest
+
+@testable import RevenueCat
+
+class LoggerTests: TestCase {
+
+    private var logger: TestLogHandler!
+
+    override func setUp() {
+        super.setUp()
+
+        self.logger = .init()
+    }
+
+    func testLogLevelOrderContainsAllLevels() {
+        expect(Set(LogLevel.order.keys)) == Set(LogLevel.allCases)
+    }
+
+    func testLogLevelOrdering() {
+        let levels: [LogLevel] = LogLevel.allCases.shuffled()
+
+        expect(levels.sorted()) == [
+            .verbose,
+            .debug,
+            .info,
+            .warn,
+            .error
+        ]
+    }
+
+    func testLoggerLogsMessagesWithHigherLevel() {
+        Logger.logLevel = .info
+        Logger.warn(Self.logMessage)
+
+        self.logger.verifyMessageWasLogged(Self.logMessage, level: .warn)
+    }
+
+    func testLoggerLogsMessagesWithHigherLevelThanVerbose() {
+        Logger.logLevel = .verbose
+        Logger.info(Self.logMessage)
+
+        self.logger.verifyMessageWasLogged(Self.logMessage, level: .info)
+    }
+
+    func testLoggerLogsMessagesWithSameLevel() {
+        Logger.logLevel = .info
+        Logger.info(Self.logMessage)
+
+        self.logger.verifyMessageWasLogged(Self.logMessage, level: .info)
+    }
+
+    func testLoggerDoesNotLogMessagesWithLowerLevel() {
+        Logger.logLevel = .info
+        Logger.debug(Self.logMessage)
+
+        self.logger.verifyMessageWasNotLogged(Self.logMessage)
+    }
+
+}
+
+private extension LoggerTests {
+
+    static let logMessage = "Log message"
+
+}
