27b1e9585026da38adb55c3157799c92e6bedd67
NachoSoto <ignaciosoto90@gmail.com>
2022-12-22 12:46:37 -0800

    Added `Dictionary.merge`
    This simplifies the `+=` implementation by avoiding an intermediate dictionary, and allows a simpler way of merging dictionaries.
    
    Will be used in #2174.

--- Sources/FoundationExtensions/Dictionary+Extensions.swift
+++ Sources/FoundationExtensions/Dictionary+Extensions.swift
@@ -60,7 +60,18 @@ extension Dictionary {
     /// - Returns: A new dictionary with the combined keys and values of this
     ///   dictionary and `other`.
     func merging(_ other: [Key: Value], strategy: MergeStrategy<Value> = .overwriteValue) -> [Key: Value] {
-        merging(other, uniquingKeysWith: strategy.combine)
+        return self.merging(other, uniquingKeysWith: strategy.combine)
+    }
+
+    /// Merges the given dictionary into this dictionary,
+    /// using a merge strategy to determine the value for duplicate keys.
+    ///
+    /// - Parameters:
+    ///   - other:  A dictionary to merge.
+    ///   - strategy: The merge strategy to use for any duplicate keys. The strategy provides a
+    ///   closure that returns the desired value for the final dictionary. The default is `overwriteValue`.
+    mutating func merge(_ other: [Key: Value], strategy: MergeStrategy<Value> = .overwriteValue) {
+        self.merge(other, uniquingKeysWith: strategy.combine)
     }
 
     /// Merge the keys/values of two dictionaries.
@@ -72,7 +83,7 @@ extension Dictionary {
     ///   - rhs: Another dictionary to merge.
     /// - Returns: An dictionary with keys and values from both.
     static func + (lhs: [Key: Value], rhs: [Key: Value]) -> [Key: Value] {
-        lhs.merging(rhs)
+        return lhs.merging(rhs)
     }
 
     /// Adds values from rhs to lhs dictionary
@@ -84,7 +95,7 @@ extension Dictionary {
     ///   - rhs: Another dictionary to merge.
     /// - Returns: An dictionary with keys and values from both.
     static func += (lhs: inout [Key: Value], rhs: [Key: Value]) {
-        lhs = lhs.merging(rhs)
+        lhs.merge(rhs)
     }
 
 }
