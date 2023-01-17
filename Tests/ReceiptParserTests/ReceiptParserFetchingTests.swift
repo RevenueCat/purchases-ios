627995c977b3b38044190c3720fb732bbc156007
NachoSoto <NachoSoto@users.noreply.github.com>
2023-01-17 05:52:02 -0800

    Added `ReceiptParserTests` (#2203)

---
+++ Tests/ReceiptParserTests/ReceiptParserFetchingTests.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  ReceiptParserFetchingTests.swift
+//
+//  Created by Nacho Soto on 1/10/23.
+
+import Nimble
+@testable import ReceiptParser
+import XCTest
+
+class ReceiptParserFetchingTests: XCTestCase {
+
+}
