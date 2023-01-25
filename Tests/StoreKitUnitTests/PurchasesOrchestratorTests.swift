06994c7f07b0690fd588d6ea501914b4b73c80a6
NachoSoto <ignaciosoto90@gmail.com>
2023-01-24 16:59:35 -0800

    Added test to verify ordering

--- Tests/StoreKitUnitTests/PurchasesOrchestratorTests.swift
+++ Tests/StoreKitUnitTests/PurchasesOrchestratorTests.swift
@@ -625,6 +625,37 @@ class PurchasesOrchestratorTests: StoreKitConfigTestCase {
     }
 
     @available(iOS 15.0, tvOS 15.0, watchOS 8.0, macOS 12.0, *)
+    func testStoreKit2TransactionListenerOnlyFinishesTransactionsAfterPostingReceipt() async throws {
+        try AvailabilityChecks.iOS15APIAvailableOrSkipTest()
+
+        self.setUpStoreKit2Listener()
+
+        enum Operation {
+            case receiptPost
+            case finishTransaction
+        }
+
+        var operations: [Operation] = []
+
+        self.customerInfoManager.stubbedCachedCustomerInfoResult = self.mockCustomerInfo
+        self.backend.stubbedPostReceiptResult = .success(self.mockCustomerInfo)
+        self.backend.onPostReceipt = { operations.append(.receiptPost) }
+
+        let transaction = MockStoreTransaction()
+        transaction.onFinishInvoked = { operations.append(.finishTransaction) }
+
+        try await self.orchestrator.storeKit2TransactionListener(
+            self.mockStoreKit2TransactionListener!,
+            updatedTransaction: transaction
+        )
+
+        expect(operations) == [
+            .receiptPost,
+            .finishTransaction
+        ]
+    }
+
+    @available(iOS 15.0, tvOS 15.0, watchOS 8.0, macOS 12.0, *)
     func testStoreKit2TransactionListenerDelegateWithObserverMode() async throws {
         try AvailabilityChecks.iOS15APIAvailableOrSkipTest()
 
