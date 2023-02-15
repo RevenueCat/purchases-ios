d86312d6e99da115b6c9350ddbff88ef4bfb44ad
NachoSoto <ignaciosoto90@gmail.com>
2023-02-15 13:49:29 -0800

    Formatting

--- Tests/UnitTests/Mocks/MockHTTPClient.swift
+++ Tests/UnitTests/Mocks/MockHTTPClient.swift
@@ -31,13 +31,14 @@ class MockHTTPClient: HTTPClient {
             // swiftlint:disable:next force_try
             let data = try! JSONSerialization.data(withJSONObject: response)
 
-            self.init(response: .success(.init(
+            let response = HTTPResponse(
                 statusCode: statusCode,
                 responseHeaders: [:],
                 body: data,
                 validationResult: .notRequested
-            )),
-                      delay: delay)
+            )
+
+            self.init(response: .success(response), delay: delay)
         }
 
         init(error: NetworkError, delay: DispatchTimeInterval = .never) {
