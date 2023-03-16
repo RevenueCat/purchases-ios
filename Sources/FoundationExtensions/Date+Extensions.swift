8fee3ade754662ca8d4b9e2d41a2a12f75230c42
NachoSoto <ignaciosoto90@gmail.com>
2023-03-16 12:39:35 -0700

    Fixed precision loss

--- Sources/FoundationExtensions/Date+Extensions.swift
+++ Sources/FoundationExtensions/Date+Extensions.swift
@@ -22,7 +22,7 @@ extension Date {
 
     /// - Important: this needs to be 64 bits because `Int` is 32 bits in watchOS
     var millisecondsSince1970: UInt64 {
-        return UInt64(self.timeIntervalSince1970) * 1000
+        return UInt64(self.timeIntervalSince1970 * 1000)
     }
 
 }
