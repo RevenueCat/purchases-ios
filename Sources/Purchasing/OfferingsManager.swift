RevenueCat/purchases-ios

commit 298e250f6ceebe4ddb3d49c92c0fc2704f04ac6e

`OfferingsManager`: moved log to common method to remove hardcoded string (#2363)

Sources/Purchasing/OfferingsManager.swift
@@ -58,16 +58,11 @@ class OfferingsManager {
 
         systemInfo.isApplicationBackgrounded { isAppBackgrounded in
             if self.deviceCache.isOfferingsCacheStale(isAppBackgrounded: isAppBackgrounded) {
-                Logger.debug(isAppBackgrounded
-                             ? Strings.offering.offerings_stale_updating_in_background
-                             : Strings.offering.offerings_stale_updating_in_foreground)
-
                 self.updateOfferingsCache(appUserID: appUserID,
                                           isAppBackgrounded: isAppBackgrounded,
-                                          fetchPolicy: fetchPolicy,
-                                          completion: nil)
-
-                Logger.rcSuccess(Strings.offering.offerings_stale_updated_from_network)
+                                          fetchPolicy: fetchPolicy) { _ in
+                    Logger.rcSuccess(Strings.offering.offerings_stale_updated_from_network)
+                }
             }
         }
     }
@@ -78,6 +73,10 @@ class OfferingsManager {
         fetchPolicy: FetchPolicy = .default,
         completion: (@MainActor @Sendable (Result<Offerings, Error>) -> Void)?
     ) {
+        Logger.debug(isAppBackgrounded
+                     ? Strings.offering.offerings_stale_updating_in_background
+                     : Strings.offering.offerings_stale_updating_in_foreground)
+
         self.backend.offerings.getOfferings(appUserID: appUserID, withRandomDelay: isAppBackgrounded) { result in
             switch result {
             case let .success(response):

Sources/Purchasing/Purchases/Purchases.swift
@@ -1297,7 +1297,6 @@ private extension Purchases {
                 return
             }
 
-            Logger.debug("Offerings cache is stale, updating caches")
             self.offeringsManager.updateOfferingsCache(appUserID: self.appUserID,
                                                        isAppBackgrounded: isAppBackgrounded,
                                                        completion: nil)

commit b15607188c85331b0c8e0adf37e6f69e1575fe4d

`OfferingsManager`: fixed Xcode 13.x build
This was broken in #2363.

Sources/Purchasing/OfferingsManager.swift
@@ -60,9 +60,8 @@ class OfferingsManager {
             if self.deviceCache.isOfferingsCacheStale(isAppBackgrounded: isAppBackgrounded) {
                 self.updateOfferingsCache(appUserID: appUserID,
                                           isAppBackgrounded: isAppBackgrounded,
-                                          fetchPolicy: fetchPolicy) { _ in
-                    Logger.rcSuccess(Strings.offering.offerings_stale_updated_from_network)
-                }
+                                          fetchPolicy: fetchPolicy,
+                                          completion: nil)
             }
         }
     }
@@ -159,6 +158,8 @@ private extension OfferingsManager {
             }
 
             if let createdOfferings = self.offeringsFactory.createOfferings(from: productsByID, data: response) {
+                Logger.rcSuccess(Strings.offering.offerings_stale_updated_from_network)
+
                 self.deviceCache.cache(offerings: createdOfferings)
                 self.dispatchCompletionOnMainThreadIfPossible(completion, result: .success(createdOfferings))
             } else {