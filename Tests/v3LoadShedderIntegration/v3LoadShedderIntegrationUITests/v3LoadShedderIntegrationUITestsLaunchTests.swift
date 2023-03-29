5d66a5da07f5e5ab0c183ec0ee7f0e9cf6d6620c
Andy Boedo <andresboedo@gmail.com>
2023-03-29 17:25:29 -0300

    addresses SDK-3045. Adds a separate target for v3 integration tests

---
+++ Tests/v3LoadShedderIntegration/v3LoadShedderIntegrationUITests/v3LoadShedderIntegrationUITestsLaunchTests.swift
@@ -0,0 +1,38 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  v3LoadShedderIntegrationUITestsLaunchTests.swift
+//
+//  Created by Andrés Boedo on 3/29/23.
+
+import XCTest
+
+final class v3LoadShedderIntegrationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
