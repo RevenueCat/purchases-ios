785c36260b6302e26127895044353d273e8740c7
Andy Boedo <andresboedo@gmail.com>
2023-03-29 17:52:51 -0300

    added basic storekit config file setup

--- Tests/v3LoadShedderIntegration/v3LoadShedderIntegrationTests/v3LoadShedderIntegrationTests.swift
+++ Tests/v3LoadShedderIntegration/v3LoadShedderIntegrationTests/v3LoadShedderIntegrationTests.swift
@@ -20,18 +20,46 @@ import StoreKitTest
 final class v3LoadShedderIntegrationTests: XCTestCase {
     let apiKey = "API_KEY"
 
+    let skConfigFileName = "v3LoadShedderIntegrationTestsConfiguration"
+
+    var testSession: SKTestSession!
+
     override func setUpWithError() throws {
         Purchases.logLevel = .debug
         let userDefaultsSuite = "v3LoadShedderIntegrationTests"
         let userDefaults = UserDefaults(suiteName: userDefaultsSuite)!
         userDefaults.removePersistentDomain(forName: userDefaultsSuite)
+        try configureTestSession()
+
         Purchases.configure(withAPIKey: self.apiKey,
                             appUserID: nil,
                             observerMode: false,
                             userDefaults: userDefaults)
+        clearReceiptIfExists()
 
 
+        func configureTestSession() throws {
+            assert(self.testSession == nil, "Attempted to configure session multiple times")
 
+            self.testSession = try SKTestSession(configurationFileNamed: skConfigFileName)
+            self.testSession.resetToDefaultState()
+            self.testSession.disableDialogs = true
+            self.testSession.clearTransactions()
+        }
+
+    }
+
+    func clearReceiptIfExists() {
+        let manager = FileManager.default
+
+        guard let url = Bundle.main.appStoreReceiptURL, manager.fileExists(atPath: url.path) else { return }
+
+        do {
+            print("Removing receipt from url: \(url)")
+            try manager.removeItem(at: url)
+        } catch {
+            print("Error attempting to remove receipt URL '\(url)': \(error)")
+        }
     }
 
     override func tearDownWithError() throws {
