049fd9e16c4c8822b9f34584da77ea30490459f3
Andy Boedo <andresboedo@gmail.com>
2023-03-30 10:29:04 -0300

    iOS v3 load shedder integration tests (#2387)
    Added a new test suite for integration tests for the load shedder using
    iOS v3.
    
    The way this is set up is a new project under Tests. 
    This new project as a Swift Package Manager dependency on the latest iOS
    v3.
    
    It has some code that's duplicated from the main integration tests, but
    honestly it's very minimal, so I don't think it should be an issue.
    
    The new project includes a StoreKit Configuration file, and a couple of
    simple unit tests: one for testing getOfferings, one for purchasing the
    first available package. Dead simple.
    
    In a follow-up PR I'll actually set this up to run automatically on a
    schedule. But as it stands right now, this can be used. Just replace the
    API key with the one for the load shedder app and it works.
    
    I tested on my machine and it runs successfully. 
    
    The actual setup:
    | Project | StoreKit Config file | Running it |
    | :-: | :-: | :-: |
    | <img width="408" alt="image"
    src="https://user-images.githubusercontent.com/3922667/228669788-a3dee73a-705a-47ea-aead-51408e478bed.png">
    | <img width="857" alt="image"
    src="https://user-images.githubusercontent.com/3922667/228669935-64fe16cb-615c-47f7-a4d0-be98dc5096ba.png">
    | <img width="297" alt="image"
    src="https://user-images.githubusercontent.com/3922667/228669986-8ae3a719-4165-4b3c-965c-2f4b1c8075d3.png">
    |

---
+++ Tests/v3LoadShedderIntegration/v3LoadShedderIntegrationTests/V3LoadShedderIntegrationTests.swift
@@ -0,0 +1,94 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  v3LoadShedderIntegrationTests.swift
+//
+//  Created by Andrés Boedo on 3/29/23.
+
+@testable import Purchases
+import StoreKit
+import StoreKitTest
+@testable import v3LoadShedderIntegration
+import XCTest
+
+// swiftlint:disable xctestcase_superclass
+final class V3LoadShedderIntegrationTests: XCTestCase {
+    let apiKey = "API_KEY"
+
+    let skConfigFileName = "V3LoadShedderIntegrationTestsConfiguration"
+    let entitlementIdentifier = "premium"
+
+    var testSession: SKTestSession!
+
+    override func setUpWithError() throws {
+        Purchases.logLevel = .debug
+        let userDefaultsSuite = "v3LoadShedderIntegrationTests"
+        let userDefaults = UserDefaults(suiteName: userDefaultsSuite)!
+        userDefaults.removePersistentDomain(forName: userDefaultsSuite)
+        try configureTestSession()
+
+        Purchases.configure(withAPIKey: self.apiKey,
+                            appUserID: nil,
+                            observerMode: false,
+                            userDefaults: userDefaults)
+        clearReceiptIfExists()
+    }
+
+    override func tearDownWithError() throws {
+        self.testSession = nil
+        try super.tearDownWithError()
+    }
+
+    func configureTestSession() throws {
+        assert(self.testSession == nil, "Attempted to configure session multiple times")
+
+        self.testSession = try SKTestSession(configurationFileNamed: skConfigFileName)
+        self.testSession.resetToDefaultState()
+        self.testSession.disableDialogs = true
+        self.testSession.clearTransactions()
+    }
+
+    func clearReceiptIfExists() {
+        let manager = FileManager.default
+
+        guard let url = Bundle.main.appStoreReceiptURL, manager.fileExists(atPath: url.path) else { return }
+
+        do {
+            print("Removing receipt from url: \(url)")
+            try manager.removeItem(at: url)
+        } catch {
+            print("Error attempting to remove receipt URL '\(url)': \(error)")
+        }
+    }
+
+    func testGetOfferings() async throws {
+        let offerings = try await Purchases.shared.offerings()
+        let offering = try XCTUnwrap(offerings.current)
+        XCTAssert(offering.availablePackages.count > 0)
+        let package = try XCTUnwrap(offering.availablePackages[0])
+        XCTAssert(package.product.productIdentifier == "com.revenuecat.loadShedder.monthly")
+        XCTAssert(offering.identifier == "default")
+    }
+
+    func testPurchasePackage() async throws {
+        let offerings = try await Purchases.shared.offerings()
+        let offering = try XCTUnwrap(offerings.current)
+        XCTAssert(offering.availablePackages.count > 0)
+        let package = try XCTUnwrap(offering.availablePackages[0])
+
+        let (_, nullablePurchaserInfo, userCancelled) = try await Purchases.shared.purchasePackage(package)
+
+        XCTAssert(!userCancelled)
+
+        let purchaserInfo = try XCTUnwrap(nullablePurchaserInfo)
+        let activeEntitlements = purchaserInfo.entitlements.active
+        XCTAssert(activeEntitlements[self.entitlementIdentifier] != nil)
+    }
+
+}
