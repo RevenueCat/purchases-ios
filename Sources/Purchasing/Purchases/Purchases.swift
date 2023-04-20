7e32c993ec9a2e00709a11da8c066a933b0bea21
Andy Boedo <andresboedo@gmail.com>
2023-04-20 19:02:57 -0300

    remove unnecessary line break

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -500,7 +500,6 @@ public typealias StartPurchaseBlock = (@escaping PurchaseCompletedBlock) -> Void
             guard let self = self else { return }
             self.handleCustomerInfoChanged(customerInfo)
         }
-
     }
 
     deinit {
