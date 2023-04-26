0733fb53567dc7d341e9746b489b600d7fa35a27
NachoSoto <ignaciosoto90@gmail.com>
2023-04-26 11:38:28 -0700

    Custom Entitlement Computation: API testers
    ### Changes:
    - Created `CustomEntitlementComputationSwiftAPITester`
    - Cleaned up checks for `ENABLE_CUSTOM_ENTITLEMENT_COMPUTATION` in existing API testers
    - Running new API tester in CI as part of the Custom Entitlement Computation SPM build

---
+++ Tests/APITesters/CustomEntitlementComputationSwiftAPITester/SwiftAPITester/PackageAPI.swift
@@ -0,0 +1,45 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  PackageAPI.swift
+//
+//  Created by Madeline Beyl on 8/26/21.
+
+import Foundation
+import RevenueCat_CustomEntitlementComputation
+import StoreKit
+
+var pack: Package!
+func checkPackageAPI() {
+    let ident: String = pack.identifier
+    let pType: PackageType = pack.packageType
+    let prod: StoreProduct = pack.storeProduct
+    let lps: String = pack.localizedPriceString
+    let lips: String? = pack.localizedIntroductoryPriceString
+
+    print(pack!, ident, pType, prod, lps, lips!)
+}
+
+var packageType: PackageType!
+func checkPackageEnums() {
+    switch packageType! {
+    case .custom,
+         .lifetime,
+         .annual,
+         .sixMonth,
+         .threeMonth,
+         .twoMonth,
+         .monthly,
+         .weekly,
+         .unknown:
+        print(packageType!)
+    @unknown default:
+        fatalError()
+    }
+}
---
+++ Tests/APITesters/CustomEntitlementComputationSwiftAPITester/SwiftAPITester/TransactionAPI.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  TransactionAPI.swift
+//
+//  Created by Madeline Beyl on 8/26/21.
+
+import Foundation
+import RevenueCat_CustomEntitlementComputation
+
+var trans: StoreTransaction!
+func checkTransactionAPI() {
+
+    let rci: String = trans.transactionIdentifier
+    let pid: String = trans.productIdentifier
+    let date: Date = trans.purchaseDate
+
+    print(trans!, rci, pid, date)
+}
