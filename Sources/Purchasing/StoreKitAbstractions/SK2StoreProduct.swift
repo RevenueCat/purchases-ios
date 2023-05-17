278283788d05a4e84f786582f5d5148dd30764f0
NachoSoto <ignaciosoto90@gmail.com>
2023-05-16 17:44:42 -0700

    Fixed iOS 15 again

--- Sources/Purchasing/StoreKitAbstractions/SK2StoreProduct.swift
+++ Sources/Purchasing/StoreKitAbstractions/SK2StoreProduct.swift
@@ -95,14 +95,16 @@ private extension SK2StoreProduct {
 
     var _currencyCode: String? {
         #if swift(>=5.7)
-        return self.currencyCodeFromPriceFormat
-        #else
+        if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
+            return self.currencyCodeFromPriceFormat
+        }
+        #endif
+
         // note: if we ever need more information from the jsonRepresentation object, we
         // should use Codable or another decoding method to clean up this code.
         let attributes = jsonDict["attributes"] as? [String: Any]
         let offers = attributes?["offers"] as? [[String: Any]]
         return offers?.first?["currencyCode"] as? String
-        #endif
     }
 
     private var jsonDict: [String: Any] {
@@ -111,8 +113,9 @@ private extension SK2StoreProduct {
     }
 
     // This is marked as `@_backDeploy`, but it's only visible when compiling with Xcode 14.x
+    // and for some reason only returning a non-empty string on iOS 16+.
     #if swift(>=5.7)
-    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
     private var currencyCodeFromPriceFormat: String {
         return self.underlyingSK2Product.priceFormatStyle.currencyCode
     }
