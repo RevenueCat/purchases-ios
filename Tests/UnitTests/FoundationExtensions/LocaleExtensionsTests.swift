8def349d09eea157e634b33595faf8bc272c9181
NachoSoto <ignaciosoto90@gmail.com>
2023-06-21 14:29:48 -0700

    `Xcode 15`: handle `Locale.currencyCode` deprecation

---
+++ Tests/UnitTests/FoundationExtensions/LocaleExtensionsTests.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  LocaleExtensionsTests.swift
+//
+//  Created by Nacho Soto on 6/21/23.
+
+import Nimble
+import XCTest
+
+@testable import RevenueCat
+
+class LocaleExtensionsTests: TestCase {
+
+    func testCurrencyCode() {
+        let locale = Locale(identifier: "en_US")
+        expect(locale.rc_currencyCode) == "USD"
+    }
+
+}
