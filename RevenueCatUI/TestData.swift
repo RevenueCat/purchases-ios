155222c1277d072445841d1770ff06cc69e5df3d
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-11 13:04:24 -0700

    `Paywalls`: initial `RevenueCatUI` target setup (#2776)
    ### Changes:
    
    - Added `RevenueCatUI` to `Package.json`
    - Set up tests for `RevenueCatUI` using `snapshot-testing` (not in CI
    during initial development)
    - Added Schemes to allow easily building packages while working on the
    `Package.swift`
    - Updated `SimpleApp` to use new package

---
+++ RevenueCatUI/TestData.swift
@@ -0,0 +1,72 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nacho Soto on 7/6/23.
+//
+
+import Foundation
+import RevenueCat
+
+#if DEBUG
+
+internal enum TestData {
+
+    static let product1 = TestStoreProduct(
+        localizedTitle: "PRO monthly",
+        price: 3.99,
+        localizedPriceString: "$3.99",
+        productIdentifier: "com.revenuecat.product",
+        productType: .autoRenewableSubscription,
+        localizedDescription: "PRO monthly",
+        subscriptionGroupIdentifier: "group",
+        subscriptionPeriod: .init(value: 1, unit: .month),
+        introductoryDiscount: .init(
+            identifier: "intro",
+            price: 0,
+            localizedPriceString: "$0.00",
+            paymentMode: .freeTrial,
+            subscriptionPeriod: .init(value: 1, unit: .week),
+            numberOfPeriods: 1,
+            type: .introductory
+        ),
+        discounts: []
+    )
+    static let product2 = TestStoreProduct(
+        localizedTitle: "PRO annual",
+        price: 34.99,
+        localizedPriceString: "$34.99",
+        productIdentifier: "com.revenuecat.product",
+        productType: .autoRenewableSubscription,
+        localizedDescription: "PRO annual",
+        subscriptionGroupIdentifier: "group",
+        subscriptionPeriod: .init(value: 1, unit: .year),
+        introductoryDiscount: nil,
+        discounts: []
+    )
+
+    static let offering = Offering(
+        identifier: Self.offeringIdentifier,
+        serverDescription: "Main offering",
+        metadata: [:],
+        availablePackages: [
+            .init(
+                identifier: "monthly",
+                packageType: .monthly,
+                storeProduct: product1.toStoreProduct(),
+                offeringIdentifier: Self.offeringIdentifier
+            ),
+            .init(
+                identifier: "annual",
+                packageType: .annual,
+                storeProduct: product2.toStoreProduct(),
+                offeringIdentifier: Self.offeringIdentifier
+            )
+        ]
+    )
+
+    private static let offeringIdentifier = "offering"
+
+}
+
+#endif
