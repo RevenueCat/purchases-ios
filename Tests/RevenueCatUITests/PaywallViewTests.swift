3da83df2ec401bed14e87deda131854da2002498
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-11 13:04:24 -0700

    `Paywalls`: initial `RevenueCatUI` target setup (#2776)
    ### Changes:
    
    - Added `RevenueCatUI` to `Package.json`
    - Set up tests for `RevenueCatUI` using `snapshot-testing` (not in CI
    during initial development)
    - Added Schemes to allow easily building packages while working on the
    `Package.swift`
    - Updated `SimpleApp` to use new package

---
+++ Tests/RevenueCatUITests/PaywallViewTests.swift
@@ -0,0 +1,16 @@
+import Nimble
+@testable import RevenueCatUI
+import SnapshotTesting
+import XCTest
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+class PaywallViewTests: TestCase {
+
+    func testOne() {
+        let view = PaywallView(offering: TestData.offering)
+            .frame(width: 300, height: 400)
+
+        expect(view).to(haveValidSnapshot(as: .image))
+    }
+
+}
