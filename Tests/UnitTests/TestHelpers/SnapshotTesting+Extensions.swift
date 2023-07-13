c35c7ccac281e4ebc5d5e0f1e09d2e6460afb8eb
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-13 10:16:16 -0700

    `Paywalls`: added header image to configuration (#2800)
    - Added `PaywallData.assetBaseURL` and
    `PaywallData.Configuration.headerImageName`
    - Changed template to fetch image from network
    - Updated snapshot testing to support asynchronous checking, necessary
    for `AsyncImageView`
    - Added support for overriding images for snapshot tests
    - Extracted `DebugErrorView`

--- Tests/UnitTests/TestHelpers/SnapshotTesting+Extensions.swift
+++ Tests/UnitTests/TestHelpers/SnapshotTesting+Extensions.swift
@@ -12,7 +12,9 @@
 //  Created by Nacho Soto on 3/4/22.
 
 import Foundation
+import Nimble
 import SnapshotTesting
+import SwiftUI
 
 @testable import RevenueCat
 
@@ -40,6 +42,54 @@ extension Snapshotting where Value == Encodable, Format == String {
     }
 
 }
+
+// MARK: - Image Snapshoting
+
+#if !os(watchOS) && swift(>=5.8)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+extension SwiftUI.View {
+
+    func snapshot(
+        size: CGSize,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        // Add test view to the hierarchy to make sure async rendering takes place.
+        // The tested view is `controller.view` instead of `self` to keep it in memory
+        // while rendering happens
+        let controller = UIViewController()
+        let window = UIWindow()
+        window.rootViewController = controller
+
+        controller.view.addSubview(
+            self
+                .frame(width: size.width, height: size.height)
+                .asUIView(container: controller, size: size)
+        )
+        controller.view.backgroundColor = .white
+
+        expect(
+            file: file, line: line,
+            controller.view
+        ).toEventually(
+            haveValidSnapshot(
+                as: .image(size: size),
+                named: "1", // Force each retry to end in `.1.png`
+                file: file,
+                line: line
+            ),
+            timeout: timeout,
+            pollInterval: pollInterval
+        )
+    }
+
+}
+#endif
+
+private let timeout: DispatchTimeInterval = .seconds(1)
+private let pollInterval: DispatchTimeInterval = .milliseconds(100)
+
+// MARK: - Private
 
 private extension Encodable {
 
@@ -76,3 +126,47 @@ private let outputFormatting: JSONEncoder.OutputFormatting = {
 
     return result
 }()
+
+// MARK: - SwiftUIContainerView
+
+#if !os(watchOS)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+@available(watchOS, unavailable)
+private final class SwiftUIContainerView<V: SwiftUI.View>: UIView {
+
+    private let controller: UIHostingController<V>
+
+    init(container: UIViewController, view: V, size: CGSize) {
+        self.controller = UIHostingController(rootView: view)
+        self.controller.view.backgroundColor = nil
+
+        super.init(frame: .init(origin: .zero, size: size))
+
+        container.addChild(self.controller)
+        self.addSubview(self.controller.view)
+        self.controller.didMove(toParent: container)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        self.controller.view.frame = self.bounds
+    }
+
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
+private extension SwiftUI.View {
+
+    func asUIView(container: UIViewController, size: CGSize) -> SwiftUIContainerView<Self> {
+        return SwiftUIContainerView(container: container, view: self, size: size)
+    }
+
+}
+
+#endif
