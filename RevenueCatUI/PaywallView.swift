3cf5fb831fb9e81b0fe1b440ecfb7bea98c8fd24
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-13 14:03:00 -0700

    `Paywalls`: determine intro eligibility (#2808)
    Follow up to #2796.
    This also makes `PaywallData.Configuration`'s intro strings optional.
    
    ### Other changes:
    - Injected `TrialOrIntroEligibilityChecker`
    - Created mock `TrialOrIntroEligibilityChecker` for previews
    - Handling state and transitions for loading eligibility
    - Snapshot tests for the different new cases
    - Improved `SnapshotTesting` delay management
    - Expanded `DebugErrorView` to customize release behavior

--- RevenueCatUI/PaywallView.swift
+++ RevenueCatUI/PaywallView.swift
@@ -1,21 +1,40 @@
 import RevenueCat
 import SwiftUI
 
-// swiftlint:disable missing_docs
-
+/// A full-screen SwiftUI view for displaying a `PaywallData` for an `Offering`.
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
 public struct PaywallView: View {
 
     private let offering: Offering
     private let paywall: PaywallData
+    private let introEligibility: TrialOrIntroEligibilityChecker?
 
+    /// Create a view for the given offering and paywal.
+    /// - Warning: `Purchases` must have been configured prior to displaying it.
     public init(offering: Offering, paywall: PaywallData) {
-        self.offering = offering
-        self.paywall = paywall
+        self.init(
+            offering: offering,
+            paywall: paywall,
+            introEligibility: Purchases.isConfigured ? .init() : nil
+        )
     }
 
+    init(offering: Offering, paywall: PaywallData, introEligibility: TrialOrIntroEligibilityChecker?) {
+        self.offering = offering
+        self.paywall = paywall
+        self.introEligibility = introEligibility
+    }
+
+    // swiftlint:disable:next missing_docs
     public var body: some View {
-        self.paywall.createView(for: self.offering)
+        if let checker = self.introEligibility {
+            self.paywall
+                .createView(for: self.offering)
+                .environmentObject(checker)
+        } else {
+            DebugErrorView("Purchases has not been configured.",
+                           releaseBehavior: .fatalError)
+        }
     }
 
 }
@@ -26,8 +45,19 @@ public struct PaywallView: View {
 struct PaywallView_Previews: PreviewProvider {
 
     static var previews: some View {
-        let offering = TestData.offeringWithIntroOffer
-        PaywallView(offering: offering, paywall: offering.paywall!)
+        let offering = TestData.offeringWithNoIntroOffer
+
+        if let paywall = offering.paywall {
+            PaywallView(
+                offering: offering,
+                paywall: paywall,
+                introEligibility: TrialOrIntroEligibilityChecker
+                    .producing(eligibility: .eligible)
+                    .with(delay: .seconds(1))
+            )
+        } else {
+            Text("Preview not correctly setup, offering has no paywall!")
+        }
     }
 
 }
