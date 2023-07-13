2aa97e700b4c296010fd00f6e6662d2c284e084a
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-13 14:03:00 -0700

    `Paywalls`: determine intro eligibility (#2808)
    Follow up to #2796.
    This also makes `PaywallData.Configuration`'s intro strings optional.
    
    ### Other changes:
    - Injected `TrialOrIntroEligibilityChecker`
    - Created mock `TrialOrIntroEligibilityChecker` for previews
    - Handling state and transitions for loading eligibility
    - Snapshot tests for the different new cases
    - Improved `SnapshotTesting` delay management
    - Expanded `DebugErrorView` to customize release behavior

---
+++ RevenueCatUI/Modifiers/ViewExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  ViewExtensions.swift
+//  
+//
+//  Created by Nacho Soto on 7/13/23.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.2, *)
+extension View {
+
+    @ViewBuilder
+    func hidden(if condition: Bool) -> some View {
+        if condition {
+            self.hidden()
+        } else {
+            self
+        }
+    }
+
+}
