4b62ba4f3e0b22bc1e402e7b32f1789b6bc4fccf
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-13 14:03:00 -0700

    `Paywalls`: determine intro eligibility (#2808)
    Follow up to #2796.
    This also makes `PaywallData.Configuration`'s intro strings optional.
    
    ### Other changes:
    - Injected `TrialOrIntroEligibilityChecker`
    - Created mock `TrialOrIntroEligibilityChecker` for previews
    - Handling state and transitions for loading eligibility
    - Snapshot tests for the different new cases
    - Improved `SnapshotTesting` delay management
    - Expanded `DebugErrorView` to customize release behavior

--- Tests/RevenueCatUITests/PaywallViewTests.swift
+++ Tests/RevenueCatUITests/PaywallViewTests.swift
@@ -10,24 +10,64 @@ class PaywallViewTests: TestCase {
     override class func setUp() {
         super.setUp()
 
-        // isRecording = true
+         // isRecording = true
     }
 
     func testSamplePaywall() {
         let offering = TestData.offeringWithNoIntroOffer
 
-        let view = PaywallView(offering: offering, paywall: offering.paywall!.withLocalImage)
+        let view = PaywallView(offering: offering,
+                               paywall: offering.paywallWithLocalImage,
+                               introEligibility: Self.eligibleChecker)
+
         view.snapshot(size: Self.size)
     }
 
     func testSamplePaywallWithIntroOffer() {
         let offering = TestData.offeringWithIntroOffer
 
-        let view = PaywallView(offering: offering, paywall: offering.paywall!.withLocalImage)
+        let view = PaywallView(offering: offering,
+                               paywall: offering.paywallWithLocalImage,
+                               introEligibility: Self.eligibleChecker)
+
         view.snapshot(size: Self.size)
     }
 
+    func testSamplePaywallWithIneligibleIntroOffer() {
+        let offering = TestData.offeringWithIntroOffer
+
+        let view = PaywallView(offering: offering,
+                               paywall: offering.paywallWithLocalImage,
+                               introEligibility: Self.ineligibleChecker)
+
+        view.snapshot(size: Self.size)
+    }
+
+    func testSamplePaywallWithLoadingEligibility() {
+        let offering = TestData.offeringWithIntroOffer
+
+        let view = PaywallView(
+            offering: offering,
+            paywall: offering.paywallWithLocalImage,
+            introEligibility: Self.ineligibleChecker
+                .with(delay: .seconds(30))
+        )
+
+        view.snapshot(size: Self.size)
+    }
+
+    private static let eligibleChecker: TrialOrIntroEligibilityChecker = .producing(eligibility: .eligible)
+    private static let ineligibleChecker: TrialOrIntroEligibilityChecker = .producing(eligibility: .ineligible)
+
     private static let size: CGSize = .init(width: 460, height: 950)
+
+}
+
+private extension Offering {
+
+    var paywallWithLocalImage: PaywallData {
+        return self.paywall!.withLocalImage
+    }
 
 }
 
