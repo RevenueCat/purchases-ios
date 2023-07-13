b84a2bec132d31a6254cf7536d3ab838b30e1ad4
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-12 21:27:57 -0700

    `Paywalls`: initial configuration types (#2780)
    - Added `Offering.paywall`
    - Decoding `PaywallData` in `OfferingsResponse`, using
    `IgnoreDecodeErrors` (this will be better after #2778)
    - New `PaywallData` `struct`
    - Added new APIs to testers
    - Testing paywall deserialization from `Offerings`
    - Testing paywall deserialization separately to check edge cases

---
+++ Tests/UnitTests/Networking/Responses/PaywallDataTests.swift
@@ -0,0 +1,76 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  PaywallDataTests.swift
+//
+//  Created by Nacho Soto on 7/11/23.
+
+import Nimble
+@testable import RevenueCat
+import XCTest
+
+class PaywallDataTests: BaseHTTPResponseTest {
+
+    override func setUp() {
+        super.setUp()
+
+        expect(Locale.current.identifier).to(
+            equal(Self.defaultLocale),
+            description: "Tests require this"
+        )
+    }
+
+    func testSample1() throws {
+        let paywall: PaywallData = try self.decodeFixture("PaywallData-Sample1")
+
+        expect(paywall.template) == .example1
+        expect(paywall.defaultLocale) == Locale(identifier: Self.defaultLocale)
+
+        let enConfig = try XCTUnwrap(paywall.config(for: Locale(identifier: "en_US")))
+        expect(enConfig.callToAction) == "Purchase now"
+        expect(enConfig.title) == "Paywall"
+
+        let esConfig = try XCTUnwrap(paywall.config(for: Locale(identifier: "es_ES")))
+        expect(esConfig.callToAction) == "Comprar"
+        expect(esConfig.title) == "Tienda"
+
+        expect(paywall.localizedConfiguration) == paywall.config(for: Locale.current)
+
+        expect(paywall.config(for: Locale(identifier: "gl_ES"))).to(beNil())
+    }
+
+    func testMissingCurrentLocaleLoadsDefault() throws {
+        let paywall: PaywallData = try self.decodeFixture("PaywallData-missing_current_locale")
+
+        expect(paywall.defaultLocale.identifier) == "es_ES"
+
+        let localization = paywall.localizedConfiguration
+        expect(localization.callToAction) == "Purchase now"
+        expect(localization.title) == "Paywall"
+    }
+
+    #if !os(watchOS)
+    func testMissingCurrentAndDefaultFails() throws {
+        let paywall: PaywallData = try self.decodeFixture("PaywallData-missing_current_and_default_locale")
+
+        expect(paywall.defaultLocale.identifier) == "es_ES"
+
+        expect {
+            let _: PaywallData.LocalizedConfiguration = paywall.localizedConfiguration
+        }.to(throwAssertion())
+    }
+    #endif
+
+}
+
+private extension PaywallDataTests {
+
+    static let defaultLocale = "en_US"
+
+}
