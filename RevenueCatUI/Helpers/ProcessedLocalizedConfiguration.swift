1e90ddbafecbf472eb781f4f6255a948c8a78b18
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-13 10:08:22 -0700

    `Paywalls`: add support for displaying `StoreProductDiscount`s (#2796)
    ![image](https://github.com/RevenueCat/purchases-ios/assets/685609/2e009460-5faf-42af-8dd0-8ff416e51335)

--- RevenueCatUI/Helpers/ProcessedLocalizedConfiguration.swift
+++ RevenueCatUI/Helpers/ProcessedLocalizedConfiguration.swift
@@ -6,7 +6,9 @@ struct ProcessedLocalizedConfiguration: PaywallLocalizedConfiguration {
     var title: String
     var subtitle: String
     var callToAction: String
+    var callToActionWithIntroOffer: String
     var offerDetails: String
+    var offerDetailsWithIntroOffer: String
 
     init(
         _ configuration: PaywallData.LocalizedConfiguration,
@@ -16,15 +18,26 @@ struct ProcessedLocalizedConfiguration: PaywallLocalizedConfiguration {
             title: configuration.title.processed(with: dataProvider),
             subtitle: configuration.subtitle.processed(with: dataProvider),
             callToAction: configuration.callToAction.processed(with: dataProvider),
-            offerDetails: configuration.offerDetails.processed(with: dataProvider)
+            callToActionWithIntroOffer: configuration.callToActionWithIntroOffer.processed(with: dataProvider),
+            offerDetails: configuration.offerDetails.processed(with: dataProvider),
+            offerDetailsWithIntroOffer: configuration.offerDetailsWithIntroOffer.processed(with: dataProvider)
         )
     }
 
-    private init(title: String, subtitle: String, callToAction: String, offerDetails: String) {
+    private init(
+        title: String,
+        subtitle: String,
+        callToAction: String,
+        callToActionWithIntroOffer: String,
+        offerDetails: String,
+        offerDetailsWithIntroOffer: String
+    ) {
         self.title = title
         self.subtitle = subtitle
         self.callToAction = callToAction
+        self.callToActionWithIntroOffer = callToActionWithIntroOffer
         self.offerDetails = offerDetails
+        self.offerDetailsWithIntroOffer = offerDetailsWithIntroOffer
     }
 
 }
