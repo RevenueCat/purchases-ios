9779e744a87e4108dd8218edeea32ecf2da96560
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-12 21:35:35 -0700

    `Paywalls`: added support for variables (#2793)
    ### Other changes:
    - Added `subtitle` and `offerDetails` to `LocalizedConfiguration`
    - Improved sample paywall to include these with test variables
    - Updated snapshot test to display the current progress
    - Added initial implementation of `VariableHandler` with tests

--- RevenueCatUI/Templates/Example1Template.swift
+++ RevenueCatUI/Templates/Example1Template.swift
@@ -4,8 +4,8 @@ import SwiftUI
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
 struct Example1Template: TemplateViewType {
 
-    private let packages: [Package]
-    private let localization: PaywallData.LocalizedConfiguration
+    private let package: Package
+    private let localization: ProcessedLocalizedConfiguration
     private let configuration: PaywallData.Configuration
 
     init(
@@ -13,39 +13,78 @@ struct Example1Template: TemplateViewType {
         localization: PaywallData.LocalizedConfiguration,
         configuration: PaywallData.Configuration
     ) {
-        self.packages = packages
-        self.localization = localization
+        // The RC SDK ensures this when constructing Offerings
+        precondition(!packages.isEmpty)
+
+        self.package = packages[0]
+        self.localization = localization.processVariables(with: self.package)
         self.configuration = configuration
+
+        precondition(
+            self.package.storeProduct.productCategory == .subscription,
+            "Unexpected product type for this template: \(self.package.storeProduct.productType)"
+        )
     }
 
     var body: some View {
-        VStack {
-            Text(verbatim: self.localization.title)
-                .font(.title)
-
-            List {
-                ForEach(self.packages, id: \.identifier) { package in
-                    self.label(for: package)
-                        .listRowBackground(
-                            Rectangle()
-                                .foregroundStyle(.thinMaterial)
-                        )
-                }
-            }
-            .scrollContentBackground(.hidden)
-
-            Button {
-
-            } label: {
-                Text(self.localization.callToAction)
-            }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
-            .tint(Color.indigo.gradient)
-            .buttonBorderShape(.roundedRectangle)
-            .controlSize(.large)
+        ZStack {
+            self.content
         }
-        .background(.blue.gradient)
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        VStack {
+            Image("image", bundle: .module)
+                .resizable()
+                .frame(maxWidth: .infinity)
+                .aspectRatio(1, contentMode: .fill)
+                .edgesIgnoringSafeArea(.top)
+                .padding(.bottom)
+                .mask(alignment: .top) {
+                    Circle()
+                        .offset(y: -160)
+                        .scale(2.5)
+                }
+
+            Spacer()
+
+            VStack {
+                Text(verbatim: self.localization.title)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .padding(.bottom)
+
+                Text(verbatim: self.localization.subtitle)
+                    .font(.subheadline)
+                    .padding(.horizontal)
+
+                Spacer()
+
+                Text(verbatim: self.localization.offerDetails)
+                    .font(.callout)
+
+                self.button
+            }
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+        }
+    }
+
+    @ViewBuilder
+    private var button: some View {
+        Button {
+
+        } label: {
+            Text(self.localization.callToAction)
+                .frame(maxWidth: .infinity)
+        }
+        .font(.title2)
+        .fontWeight(.semibold)
+        .tint(Color.green.gradient.opacity(0.8))
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
     }
 
 }
