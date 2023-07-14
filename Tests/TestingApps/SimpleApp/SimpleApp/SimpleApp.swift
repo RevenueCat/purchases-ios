8c67d2f71cf6465e0da4238ad4440318afd30f21
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-14 14:45:25 -0700

    `Paywalls`: added support for purchasing (#2812)
    This adds a new `PurchaseHandler` type, which can be instantiated with
    `Purchases` or with a mock implementation for previews. That gets
    injected, and templates can now purchase and dismiss themselves.
    
    I've improved the `SimpleApp` setup to better work with the lifetime of
    purchases. Now it only displays the paywall if the user doesn't have an
    entitlement.
    
    I've also added some basic error handling to the new `AsyncButton`:
    ![Simulator Screenshot - iPhone 14 Pro - 2023-07-13 at 15 32
    06](https://github.com/RevenueCat/purchases-ios/assets/685609/e8c2e6e3-f1e0-411f-9ca2-8a3cc3823a42)

--- Tests/TestingApps/SimpleApp/SimpleApp/SimpleApp.swift
+++ Tests/TestingApps/SimpleApp/SimpleApp/SimpleApp.swift
@@ -9,59 +9,85 @@ import RevenueCat
 import RevenueCatUI
 import SwiftUI
 
-#warning("This needs to be configured.")
-private let apiKey = ""
-// Note: you can leave this empty to use the production server, or point to your own instance.
-private let proxyURL = ""
-
 @main
 struct SimpleApp: App {
+
     init() {
         Purchases.logLevel = .verbose
-        Purchases.proxyURL = proxyURL.isEmpty
+        Purchases.proxyURL = Configuration.proxyURL.isEmpty
             ? nil
-            : URL(string: proxyURL)!
+            : URL(string: Configuration.proxyURL)!
 
         Purchases.configure(
-            with: .init(withAPIKey: apiKey)
+            with: .init(withAPIKey: Configuration.apiKey)
                 .with(usesStoreKit2IfAvailable: true)
         )
     }
 
     @State
-    private var offering: Result<Offering, Error>?
+    private var offering: Result<Offering, NSError>?
+
+    @State
+    private var customerInfo: CustomerInfo?
 
     var body: some Scene {
         WindowGroup {
-            Group {
-                if let offering = self.offering {
-                    switch offering {
-                    case let .success(offering):
-                        if let paywall = offering.paywall {
-                            PaywallView(offering: offering, paywall: paywall)
-                        } else {
-                            Text(
-                                "Didn't find a paywall associated to the current offering.\n" +
-                                "Check the logs for any potential errors.")
-                        }
-
-                    case let .failure(error):
-                        Text("Error loading offerings: \(error.localizedDescription)")
+            AppContentView(customerInfo: self.customerInfo)
+                .overlay {
+                    if let info = self.customerInfo, !info.hasPro {
+                        self.paywallView
+                            .animation(.default, value: self.offering)
                     }
                 }
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .overlay {
-                DebugView()
-                    .frame(maxHeight: .infinity, alignment: .bottom)
-            }
-            .task {
-                do {
-                    self.offering = .success(try await Purchases.shared.offerings().current!)
-                } catch {
-                    self.offering = .failure(error)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay {
+                    DebugView()
+                        .frame(maxHeight: .infinity, alignment: .bottom)
                 }
-            }
+                .task {
+                    for await info in Purchases.shared.customerInfoStream {
+                        self.customerInfo = info
+                    }
+                }
         }
     }
+
+    @ViewBuilder
+    private var paywallView: some View {
+        if let offering = self.offering {
+            switch offering {
+            case let .success(offering):
+                if let paywall = offering.paywall {
+                    PaywallScreen(offering: offering, paywall: paywall)
+                } else {
+                    Text(
+                        "Didn't find a paywall associated to the current offering.\n" +
+                        "Check the logs for any potential errors.")
+                }
+
+            case let .failure(error):
+                Text("Error loading offerings: \(error.localizedDescription)")
+            }
+        } else {
+            ProgressView()
+                .progressViewStyle(.circular)
+                .controlSize(.large)
+                .task {
+                    do {
+                        self.offering = .success(try await Purchases.shared.offerings().current!)
+                    } catch {
+                        self.offering = .failure(error as NSError)
+                    }
+                }
+        }
+    }
+
+}
+
+private extension CustomerInfo {
+
+    var hasPro: Bool {
+        return self.entitlements.active.contains { $1.identifier == Configuration.entitlement }
+    }
+
 }
