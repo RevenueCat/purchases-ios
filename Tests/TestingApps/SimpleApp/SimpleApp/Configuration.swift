d9784e1c89b98e94427d48280f82c5e5f19b41ea
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-14 14:45:25 -0700

    `Paywalls`: added support for purchasing (#2812)
    This adds a new `PurchaseHandler` type, which can be instantiated with
    `Purchases` or with a mock implementation for previews. That gets
    injected, and templates can now purchase and dismiss themselves.
    
    I've improved the `SimpleApp` setup to better work with the lifetime of
    purchases. Now it only displays the paywall if the user doesn't have an
    entitlement.
    
    I've also added some basic error handling to the new `AsyncButton`:
    ![Simulator Screenshot - iPhone 14 Pro - 2023-07-13 at 15 32
    06](https://github.com/RevenueCat/purchases-ios/assets/685609/e8c2e6e3-f1e0-411f-9ca2-8a3cc3823a42)

---
+++ Tests/TestingApps/SimpleApp/SimpleApp/Configuration.swift
@@ -0,0 +1,20 @@
+//
+//  Configuration.swift
+//  SimpleApp
+//
+//  Created by Nacho Soto on 7/13/23.
+//
+
+import Foundation
+
+enum Configuration {
+
+    #warning("This needs to be configured.")
+
+    // Note: you can leave this empty to use the production server, or point to your own instance.
+    static let proxyURL = ""
+    static let apiKey = ""
+
+    static let entitlement = "pro"
+
+}
