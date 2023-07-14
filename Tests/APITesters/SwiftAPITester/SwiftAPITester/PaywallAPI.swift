3c75ab966221d0d8850b44f853b9fdaef11a13da
NachoSoto <ignaciosoto90@gmail.com>
2023-07-14 16:47:25 -0700

    `Paywalls`: added support for decoding colors
    This adds a new `PaywallColor` that handles the cross-platform representation of colors, decoding, and even combining light and dark schemes.
    `PaywallData.Configuration` also gets the new fields containing the colors.

--- Tests/APITesters/SwiftAPITester/SwiftAPITester/PaywallAPI.swift
+++ Tests/APITesters/SwiftAPITester/SwiftAPITester/PaywallAPI.swift
@@ -8,6 +8,10 @@
 import Foundation
 import RevenueCat
 
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+
 func checkPaywallData(_ data: PaywallData) {
     let template: PaywallTemplate = data.template
     let config: PaywallData.Configuration = data.config
@@ -22,8 +26,9 @@ func checkPaywallData(_ data: PaywallData) {
                                assetBaseURL: assetBaseURL)
 }
 
-func checkPaywallConfiguration(_ config: PaywallData.Configuration) {
-    let _: PaywallData.Configuration = .init(packages: [.monthly, .annual], headerImageName: "")
+func checkPaywallConfiguration(_ config: PaywallData.Configuration,
+                               _ colors: PaywallData.Configuration.ColorInformation) {
+    let _: PaywallData.Configuration = .init(packages: [.monthly, .annual], headerImageName: "", colors: colors)
     let _: [PackageType] = config.packages
     let _: String = config.headerImageName
 }
@@ -44,6 +49,45 @@ func checkPaywallLocalizedConfig(_ config: PaywallData.LocalizedConfiguration) {
         offerDetails: offerDetails,
         offerDetailsWithIntroOffer: offerDetailsWithIntroOffer
     )
+}
+
+func checkPaywallColors(_ config: PaywallData.Configuration.Colors) {
+    let background: PaywallColor = config.background
+    let foreground: PaywallColor = config.foreground
+    let callToActionBackground: PaywallColor = config.callToActionBackground
+    let callToActionForeground: PaywallColor = config.callToActionForeground
+
+    _ = PaywallData.Configuration.Colors(
+        background: background,
+        foreground: foreground,
+        callToActionBackground: callToActionBackground,
+        callToActionForeground: callToActionForeground
+    )
+}
+
+func checkPaywallColorInformation(_ config: PaywallData.Configuration.ColorInformation) {
+    let light: PaywallData.Configuration.Colors = config.light
+    let dark: PaywallData.Configuration.Colors? = config.dark
+
+    _ = PaywallData.Configuration.ColorInformation(
+        light: light,
+        dark: dark
+    )
+}
+
+func checkPaywallColor(_ color: PaywallColor) throws {
+    _ = try PaywallColor(stringRepresentation: "")
+    if #available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *) {
+        _ = PaywallColor(light: color, dark: color)
+    }
+
+    let _: String = color.debugDescription
+    let _: String = color.stringRepresentation
+    #if canImport(SwiftUI)
+    if #available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.2, *) {
+        let _: Color = color.underlyingColor
+    }
+    #endif
 }
 
 func checkPaywallTemplate(_ template: PaywallTemplate) {
