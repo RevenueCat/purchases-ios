9606acaaae85e6de55d77ac7de7d179511c36b89
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-18 06:47:22 -0700

    `Paywalls`: improved support for dynamic type with snapshots (#2827)
    This improves the layout of the template, and adds a scroll view that
    optionally scrolls if the content is too large.
    This can serve as a the basis for future templates.
    
    ![iOS16-testAccessibility3
    1](https://github.com/RevenueCat/purchases-ios/assets/685609/40cb6929-d7f0-49d4-a9c5-7fdea9747db9)

---
+++ Tests/RevenueCatUITests/Helpers/DataExtensions.swift
@@ -0,0 +1,32 @@
+//
+//  DataExtensions.swift
+//  
+//
+//  Created by Nacho Soto on 7/17/23.
+//
+
+import Foundation
+import RevenueCat
+import RevenueCatUI
+
+// MARK: - Extensions
+
+extension Offering {
+
+    var paywallWithLocalImage: PaywallData {
+        return self.paywall!.withLocalImage
+    }
+
+}
+
+extension PaywallData {
+
+    var withLocalImage: Self {
+        var copy = self
+        copy.assetBaseURL = URL(fileURLWithPath: Bundle.module.bundlePath)
+        copy.config.headerImageName = "image.png"
+
+        return copy
+    }
+
+}
