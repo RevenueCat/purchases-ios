5b080d131d8e67812e8b2e6257f12428c7eeb7b6
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-18 06:43:54 -0700

    `Paywalls`: disable `macOS`/`macCatalyst`/`watchOS` for now (#2821)
    In the future we'll be able to polish support for those platforms.

--- RevenueCatUI/PaywallView.swift
+++ RevenueCatUI/PaywallView.swift
@@ -3,6 +3,9 @@ import SwiftUI
 
 /// A full-screen SwiftUI view for displaying a `PaywallData` for an `Offering`.
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
+@available(watchOS, unavailable, message: "RevenueCatUI does not support watchOS yet")
+@available(macOS, unavailable, message: "RevenueCatUI does not support macOS yet")
+@available(macCatalyst, unavailable, message: "RevenueCatUI does not support Catalyst yet")
 public struct PaywallView: View {
 
     private let offering: Offering
@@ -51,6 +54,9 @@ public struct PaywallView: View {
 #if DEBUG
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
+@available(watchOS, unavailable)
+@available(macOS, unavailable)
+@available(macCatalyst, unavailable)
 struct PaywallView_Previews: PreviewProvider {
 
     static var previews: some View {
