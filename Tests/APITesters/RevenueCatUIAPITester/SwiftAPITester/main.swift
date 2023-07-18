4725fb2d3173e59f47c26570fcf2b9a8bed78fb5
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-18 06:14:36 -0700

    `Paywalls`: set up CI tests and API Tester (#2816)

---
+++ Tests/APITesters/RevenueCatUIAPITester/SwiftAPITester/main.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  main.swift
+//
+//  Created by Nacho Soto on 7/14/23.
+
+import Foundation
+
+func main() -> Int {
+    return 0
+}
