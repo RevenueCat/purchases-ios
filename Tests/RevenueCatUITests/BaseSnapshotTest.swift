6a95c2eb3dea971b12cf266188542d58bd553884
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-18 06:47:22 -0700

    `Paywalls`: improved support for dynamic type with snapshots (#2827)
    This improves the layout of the template, and adds a scroll view that
    optionally scrolls if the content is too large.
    This can serve as a the basis for future templates.
    
    ![iOS16-testAccessibility3
    1](https://github.com/RevenueCat/purchases-ios/assets/685609/40cb6929-d7f0-49d4-a9c5-7fdea9747db9)

---
+++ Tests/RevenueCatUITests/BaseSnapshotTest.swift
@@ -0,0 +1,33 @@
+//
+//  BaseSnapshotTest.swift
+//  
+//
+//  Created by Nacho Soto on 7/17/23.
+//
+import Nimble
+import RevenueCat
+@testable import RevenueCatUI
+import SnapshotTesting
+import XCTest
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+class BaseSnapshotTest: TestCase {
+
+    override class func setUp() {
+        super.setUp()
+
+        // isRecording = true
+    }
+
+}
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+extension BaseSnapshotTest {
+
+    static let eligibleChecker: TrialOrIntroEligibilityChecker = .producing(eligibility: .eligible)
+    static let ineligibleChecker: TrialOrIntroEligibilityChecker = .producing(eligibility: .ineligible)
+    static let purchaseHandler: PurchaseHandler = .mock()
+
+    static let fullScreenSize: CGSize = .init(width: 460, height: 950)
+
+}
