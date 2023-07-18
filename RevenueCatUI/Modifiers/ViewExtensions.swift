a157bd2ff2d4372fc0da95966e8ffb0a23b54e86
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-18 06:47:22 -0700

    `Paywalls`: improved support for dynamic type with snapshots (#2827)
    This improves the layout of the template, and adds a scroll view that
    optionally scrolls if the content is too large.
    This can serve as a the basis for future templates.
    
    ![iOS16-testAccessibility3
    1](https://github.com/RevenueCat/purchases-ios/assets/685609/40cb6929-d7f0-49d4-a9c5-7fdea9747db9)

--- RevenueCatUI/Modifiers/ViewExtensions.swift
+++ RevenueCatUI/Modifiers/ViewExtensions.swift
@@ -20,4 +20,27 @@ extension View {
         }
     }
 
+    @ViewBuilder
+    func scrollable(
+        _ axes: Axis.Set = .vertical,
+        if condition: Bool
+    ) -> some View {
+        if condition {
+            ScrollView(axes) {
+                self
+            }
+        } else {
+            self
+        }
+    }
+
+    @ViewBuilder
+    func scrollBounceBehaviorBasedOnSize() -> some View {
+        if #available(iOS 16.4, macOS 13.3, tvOS 16.4, watchOS 9.4, *) {
+            self.scrollBounceBehavior(.basedOnSize)
+        } else {
+            self
+        }
+    }
+
 }
