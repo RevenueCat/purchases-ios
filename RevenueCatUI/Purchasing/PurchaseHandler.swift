48709f9cb7a398de3831b4ba08d08eb0aef998d0
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-21 06:51:31 +0200

    `Paywalls`: extracted `PurchaseButton` (#2839)
    Another small refactor, follow up to #2837.
    Slowly taking implementation details from `Example1Template` to make it
    easier to implement more templates.

--- RevenueCatUI/Purchasing/PurchaseHandler.swift
+++ RevenueCatUI/Purchasing/PurchaseHandler.swift
@@ -7,13 +7,17 @@
 
 import RevenueCat
 import StoreKit
+import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
 final class PurchaseHandler: ObservableObject {
 
     typealias PurchaseBlock = @Sendable (Package) async throws -> PurchaseResultData
 
-    let purchaseBlock: PurchaseBlock
+    private let purchaseBlock: PurchaseBlock
+
+    @Published
+    var purchased: Bool = false
 
     convenience init(purchases: Purchases = .shared) {
         self.init { package in
@@ -27,11 +31,24 @@ final class PurchaseHandler: ObservableObject {
 
 }
 
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
 extension PurchaseHandler {
 
     func purchase(package: Package) async throws -> PurchaseResultData {
-        return try await self.purchaseBlock(package)
+        let result = try await self.purchaseBlock(package)
+
+        if !result.userCancelled {
+            withAnimation(Constants.defaultAnimation) {
+                self.purchased = true
+            }
+        }
+
+        return result
+    }
+
+    /// Creates a copy of this `PurchaseHandler` wrapping the purchase block
+    func map(_ block: @escaping (@escaping PurchaseBlock) -> PurchaseBlock) -> Self {
+        return .init(purchase: block(self.purchaseBlock))
     }
 
 }
