921ef51a1ff149a526590fce76fef28619067e7c
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-24 15:49:26 +0200

    `Paywalls`: handle missing paywalls gracefully (#2855)
    ### Changes:
    - Improved `PaywallView` API: now there's only 2 constructors (with
    optional `Mode` parameters):
        - `PaywallView()`
        - `PaywallView(offering:)`
    - New `PaywallData.default` as a fallback when trying to present a
    paywall with missing data (either because it failed to decode, or it's
    missing)
    - Extracted error state handling to `ErrorDisplay` (used by
    `PaywallView` and `AsyncButton` now). It can optionally dismiss the
    presenting sheet.
    - Handling offering loading errors in `PaywallView`
    - Improved `DebugErrorView` to allow displaying a fallback view:
    ```swift
    DebugErrorView(
        "Offering '\(offering.identifier)' has no configured paywall.\n" +
        "The displayed paywall contains default configuration.\n" +
        "This error will be hidden in production.",
        releaseBehavior: .replacement(
            AnyView(
                LoadedOfferingPaywallView(
                    offering: offering,
                    paywall: .default,
                    mode: mode,
                    introEligibility: checker,
                    purchaseHandler: purchaseHandler
                )
            )
        )
    )
    ```
    - Added `LoadingPaywallView` as a placeholder view during loading
    - Improved `MultiPackageTemplate` and `RemoteImage` to fix some layout
    issues during transitions
    - Added transition animations between loading state and loaded paywall

---
+++ Tests/RevenueCatUITests/OtherPaywallViewTests.swift
@@ -0,0 +1,41 @@
+//
+//  DefaultPaywallViewTests.swift
+//  
+//
+//  Created by Nacho Soto on 7/20/23.
+//
+
+import Nimble
+import RevenueCat
+@testable import RevenueCatUI
+import SnapshotTesting
+
+#if !os(macOS)
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+class OtherPaywallViewTests: BaseSnapshotTest {
+
+    func testDefaultPaywall() {
+        let view = PaywallView(offering: Self.offeringWithNoPaywall,
+                               introEligibility: Self.eligibleChecker,
+                               purchaseHandler: Self.purchaseHandler)
+
+        view.snapshot(size: Self.fullScreenSize)
+    }
+
+    func testLoadingPaywallView() {
+        let view = LoadingPaywallView()
+        view.snapshot(size: Self.fullScreenSize)
+    }
+
+    private static let offeringWithNoPaywall = Offering(
+        identifier: "offering",
+        serverDescription: "Main offering",
+        metadata: [:],
+        paywall: nil,
+        availablePackages: TestData.packages
+    )
+
+}
+
+#endif
