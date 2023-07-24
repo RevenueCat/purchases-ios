af2b27089fa3f2dc96eb2ca3ca21bf8c1a5f290d
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-24 15:49:26 +0200

    `Paywalls`: handle missing paywalls gracefully (#2855)
    ### Changes:
    - Improved `PaywallView` API: now there's only 2 constructors (with
    optional `Mode` parameters):
        - `PaywallView()`
        - `PaywallView(offering:)`
    - New `PaywallData.default` as a fallback when trying to present a
    paywall with missing data (either because it failed to decode, or it's
    missing)
    - Extracted error state handling to `ErrorDisplay` (used by
    `PaywallView` and `AsyncButton` now). It can optionally dismiss the
    presenting sheet.
    - Handling offering loading errors in `PaywallView`
    - Improved `DebugErrorView` to allow displaying a fallback view:
    ```swift
    DebugErrorView(
        "Offering '\(offering.identifier)' has no configured paywall.\n" +
        "The displayed paywall contains default configuration.\n" +
        "This error will be hidden in production.",
        releaseBehavior: .replacement(
            AnyView(
                LoadedOfferingPaywallView(
                    offering: offering,
                    paywall: .default,
                    mode: mode,
                    introEligibility: checker,
                    purchaseHandler: purchaseHandler
                )
            )
        )
    )
    ```
    - Added `LoadingPaywallView` as a placeholder view during loading
    - Improved `MultiPackageTemplate` and `RemoteImage` to fix some layout
    issues during transitions
    - Added transition animations between loading state and loaded paywall

---
+++ RevenueCatUI/Helpers/PaywallData+Default.swift
@@ -0,0 +1,66 @@
+//
+//  PaywallData+Default.swift
+//  
+//
+//  Created by Nacho Soto on 7/20/23.
+//
+
+import Foundation
+import RevenueCat
+
+#if canImport(SwiftUI) && swift(>=5.7)
+
+extension PaywallData {
+
+    /// Default `PaywallData` to display when attempting to present a ``PaywallView`` with an offering
+    /// that has no paywall configuration, or when that configuration is invalid.
+    public static let `default`: Self = .init(
+        template: .multiPackage,
+        config: .init(
+            packages: [.weekly, .monthly, .annual],
+            imageNames: [
+                Self.backgroundImage
+            ],
+            colors: Self.colors,
+            blurredBackgroundImage: true,
+            displayRestorePurchases: true
+        ),
+        localization: Self.localization,
+        assetBaseURL: Self.defaultTemplateBaseURL
+    )
+
+}
+
+private extension PaywallData {
+
+    // swiftlint:disable force_try
+    static let colors: PaywallData.Configuration.ColorInformation = .init(
+        light: .init(
+            background: try! .init(stringRepresentation: "#FFFFFF"),
+            foreground: try! .init(stringRepresentation: "#000000"),
+            callToActionBackground: try! .init(stringRepresentation: "#EC807C"),
+            callToActionForeground: try! .init(stringRepresentation: "#FFFFFF")
+        ),
+        dark: .init(
+            background: try! .init(stringRepresentation: "#000000"),
+            foreground: try! .init(stringRepresentation: "#FFFFFF"),
+            callToActionBackground: try! .init(stringRepresentation: "#ACD27A"),
+            callToActionForeground: try! .init(stringRepresentation: "#000000")
+        )
+    )
+    // swiftlint:enable force_try
+
+    static let localization: PaywallData.LocalizedConfiguration = .init(
+        title: "Subscription",
+        subtitle: "Unlock access",
+        callToAction: "Purchase",
+        offerDetails: "{{ price_per_month }} per month",
+        offerDetailsWithIntroOffer: "Start your {{ intro_duration }} trial, then {{ price_per_month }} per month"
+    )
+
+    static let backgroundImage = "background.jpg"
+    static let defaultTemplateBaseURL = Bundle.module.resourceURL ?? Bundle.module.bundleURL
+
+}
+
+#endif
