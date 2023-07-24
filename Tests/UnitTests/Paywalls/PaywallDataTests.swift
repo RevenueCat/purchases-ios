ad91288103f84f0b0074cbad7e035d65e6efaebf
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-24 14:43:01 +0200

    `Paywalls`: added `FooterView` (#2850)
    This adds 3 new features: restore purchases, ToS, and privacy policy.
    All configurable.
    
    ![image](https://github.com/RevenueCat/purchases-ios/assets/685609/b38dadec-9fa1-4d48-876d-b53d0dda93d9)

--- Tests/UnitTests/Paywalls/PaywallDataTests.swift
+++ Tests/UnitTests/Paywalls/PaywallDataTests.swift
@@ -34,6 +34,9 @@ class PaywallDataTests: BaseHTTPResponseTest {
         expect(paywall.assetBaseURL) == URL(string: "https://rc-paywalls.s3.amazonaws.com")!
         expect(paywall.config.packages) == [.monthly, .annual]
         expect(paywall.config.imageNames) == ["asset_name.png"]
+        expect(paywall.config.displayRestorePurchases) == false
+        expect(paywall.config.termsOfServiceURL) == URL(string: "https://revenuecat.com/tos")!
+        expect(paywall.config.privacyURL) == URL(string: "https://revenuecat.com/privacy")!
 
         expect(paywall.config.colors.light.background.stringRepresentation) == "#FF00AA"
         expect(paywall.config.colors.light.foreground.stringRepresentation) == "#FF00AA22"
