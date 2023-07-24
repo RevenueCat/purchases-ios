6e1eb74576cf1cc0e69d3ecf78df526417ae6732
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-24 15:49:26 +0200

    `Paywalls`: handle missing paywalls gracefully (#2855)
    - Improved `PaywallView` API: now there's only 2 constructors (with
    optional `Mode` parameters):
        - `PaywallView()`
        - `PaywallView(offering:)`
    - New `PaywallData.default` as a fallback when trying to present a
    paywall with missing data (either because it failed to decode, or it's
    missing)
    - Extracted error state handling to `ErrorDisplay` (used by
    `PaywallView` and `AsyncButton` now). It can optionally dismiss the
    presenting sheet.
    - Handling offering loading errors in `PaywallView`
    - Improved `DebugErrorView` to allow displaying a fallback view:
    ```swift
    DebugErrorView(
        "Offering '\(offering.identifier)' has no configured paywall.\n" +
        "The displayed paywall contains default configuration.\n" +
        "This error will be hidden in production.",
        releaseBehavior: .replacement(
            AnyView(
                LoadedOfferingPaywallView(
                    offering: offering,
                    paywall: .default,
                    mode: mode,
                    introEligibility: checker,
                    purchaseHandler: purchaseHandler
                )
            )
        )
    )
    ```
    - Added `LoadingPaywallView` as a placeholder view during loading
    - Improved `MultiPackageTemplate` and `RemoteImage` to fix some layout
    issues during transitions
    - Added transition animations between loading state and loaded paywall

---
+++ RevenueCatUI/Views/LoadingPaywallView.swift
@@ -0,0 +1,105 @@
+//
+//  LoadingPaywallView.swift
+//  
+//
+//  Created by Nacho Soto on 7/21/23.
+//
+
+import RevenueCat
+import SwiftUI
+
+/// A `PaywallView` suitable to be displayed as a loading placeholder.
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
+struct LoadingPaywallView: View {
+
+    var body: some View {
+        LoadedOfferingPaywallView(
+            offering: .init(
+                identifier: Self.offeringIdentifier,
+                serverDescription: "",
+                metadata: [:],
+                paywall: .default,
+                availablePackages: [
+                    Self.monthlyPackage,
+                    Self.weeklyPackage,
+                    Self.annualPackage
+                ]
+            ),
+            paywall: .default,
+            mode: .fullScreen,
+            introEligibility: Self.introEligibility,
+            purchaseHandler: Self.purchaseHandler
+        )
+        .disabled(true)
+        .redacted(reason: .placeholder)
+    }
+
+}
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
+private extension LoadingPaywallView {
+
+    static let introEligibility: TrialOrIntroEligibilityChecker = .init(checker: { packages in
+        return Dictionary(
+            uniqueKeysWithValues: packages.map { ($0, .unknown) }
+        )
+    })
+    static let purchaseHandler: PurchaseHandler = .init(
+        purchase: { _ in
+            fatalError("Should not be able to purchase")
+        },
+        restorePurchases: {
+            fatalError("Should not be able to purchase")
+        }
+    )
+
+    static let offeringIdentifier = "offering"
+    static let weeklyPackage = Package(
+        identifier: "weekly",
+        packageType: .weekly,
+        storeProduct: Self.weeklyProduct.toStoreProduct(),
+        offeringIdentifier: Self.offeringIdentifier
+    )
+    static let monthlyPackage = Package(
+        identifier: "monthly",
+        packageType: .monthly,
+        storeProduct: Self.monthlyProduct.toStoreProduct(),
+        offeringIdentifier: Self.offeringIdentifier
+    )
+    static let annualPackage = Package(
+        identifier: "annual",
+        packageType: .annual,
+        storeProduct: Self.annualProduct.toStoreProduct(),
+        offeringIdentifier: Self.offeringIdentifier
+    )
+    static let weeklyProduct = TestStoreProduct(
+        localizedTitle: "Weekly",
+        price: 1.99,
+        localizedPriceString: "$1.99",
+        productIdentifier: "com.revenuecat.product_1",
+        productType: .autoRenewableSubscription,
+        localizedDescription: "PRO weekly",
+        subscriptionGroupIdentifier: "group",
+        subscriptionPeriod: .init(value: 1, unit: .week)
+    )
+    static let monthlyProduct = TestStoreProduct(
+        localizedTitle: "Monthly",
+        price: 12.99,
+        localizedPriceString: "$12.99",
+        productIdentifier: "com.revenuecat.product_2",
+        productType: .autoRenewableSubscription,
+        localizedDescription: "PRO monthly",
+        subscriptionGroupIdentifier: "group",
+        subscriptionPeriod: .init(value: 1, unit: .month)
+    )
+    static let annualProduct = TestStoreProduct(
+        localizedTitle: "Annual",
+        price: 69.49,
+        localizedPriceString: "$69.49",
+        productIdentifier: "com.revenuecat.product_3",
+        productType: .autoRenewableSubscription,
+        localizedDescription: "PRO annual",
+        subscriptionGroupIdentifier: "group",
+        subscriptionPeriod: .init(value: 1, unit: .year)
+    )
+}
