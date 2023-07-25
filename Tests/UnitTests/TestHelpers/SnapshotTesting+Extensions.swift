c892fd3112300eaf0db6b4724ec8b64c45f035eb
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-25 11:16:43 +0200

    `Paywalls`: fixed tests on CI (#2872)
    ### Changes:
    - Added JUnit test reporting for CircleCI:
    <img width="624" alt="Screenshot 2023-07-25 at 11 02 52"
    src="https://github.com/RevenueCat/purchases-ios/assets/685609/a6e73b34-6d90-4b88-aa4d-f108bab47d21">
    
    - Fixed `macOS` build
    - Fixed snapshots
    - Uploading `.xcarchive` to CircleCI to inspect failures (like
    Snapshots)
    - Simplified snapshot testing

--- Tests/UnitTests/TestHelpers/SnapshotTesting+Extensions.swift
+++ Tests/UnitTests/TestHelpers/SnapshotTesting+Extensions.swift
@@ -56,29 +56,18 @@ extension SwiftUI.View {
     ) {
         UIView.setAnimationsEnabled(false)
 
-        // Add test view to the hierarchy to make sure async rendering takes place.
         // The tested view is `controller.view` instead of `self` to keep it in memory
         // while rendering happens
-        let controller = UIViewController()
-        let window = UIWindow()
-        window.rootViewController = controller
-
-        controller.view.addSubview(
-            self
+        let controller = UIHostingController(rootView: self
                 .frame(width: size.width, height: size.height)
-                .asUIView(container: controller, size: size)
         )
-        controller.view.backgroundColor = .white
 
         expect(
             file: file, line: line,
-            controller.view
+            controller
         ).toEventually(
             haveValidSnapshot(
-                as: .wait(
-                    for: initialDelay.seconds,
-                    on: .image(size: size)
-                ),
+                as: .image(perceptualPrecision: 0.98, size: size),
                 named: "1", // Force each retry to end in `.1.png`
                 file: file,
                 line: line
@@ -92,8 +81,7 @@ extension SwiftUI.View {
 #endif
 
 private let timeout: DispatchTimeInterval = .seconds(5)
-private let pollInterval: DispatchTimeInterval = .milliseconds(300)
-private let initialDelay: DispatchTimeInterval = .milliseconds(100)
+private let pollInterval: DispatchTimeInterval = .milliseconds(100)
 
 // MARK: - Private
 
@@ -132,47 +120,3 @@ private let outputFormatting: JSONEncoder.OutputFormatting = {
 
     return result
 }()
-
-// MARK: - SwiftUIContainerView
-
-#if !os(watchOS) && !os(macOS)
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
-@available(watchOS, unavailable)
-private final class SwiftUIContainerView<V: SwiftUI.View>: UIView {
-
-    private let controller: UIHostingController<V>
-
-    init(container: UIViewController, view: V, size: CGSize) {
-        self.controller = UIHostingController(rootView: view)
-        self.controller.view.backgroundColor = nil
-
-        super.init(frame: .init(origin: .zero, size: size))
-
-        container.addChild(self.controller)
-        self.addSubview(self.controller.view)
-        self.controller.didMove(toParent: container)
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        self.controller.view.frame = self.bounds
-    }
-
-}
-
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
-private extension SwiftUI.View {
-
-    func asUIView(container: UIViewController, size: CGSize) -> SwiftUIContainerView<Self> {
-        return SwiftUIContainerView(container: container, view: self, size: size)
-    }
-
-}
-
-#endif
