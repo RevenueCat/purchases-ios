31a44f86a6a5236ead6798a41c121fdcb94b1e65
Josh Holtz <me@joshholtz.com>
2023-07-26 02:07:32 -0500

    `xrOS`: fixed `SubscriptionStoreView` for visionOS beta 2 (#2884)
    ### Motivation
    
    visionOS beta 2 had compile errors in `SubscriptionStoreView`
    
    ### Description
    
    Replaced `DefaultSubscriptionStoreMarketingContent` with
    `AutomaticSubscriptionStoreMarketingContent`
    
    ---------
    
    Co-authored-by: NachoSoto <ignaciosoto90@gmail.com>

--- Sources/Support/PaywallExtensions.swift
+++ Sources/Support/PaywallExtensions.swift
@@ -85,20 +85,6 @@ extension SubscriptionStoreView {
 
 }
 
-#if os(xrOS)
-@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
-extension SubscriptionStoreView where Content == DefaultSubscriptionStoreMarketingContent {
-
-    /// Creates a ``SubscriptionStoreView`` from an ``Offering``
-    /// that doesn't take a custom view to use for marketing content.
-    public static func forOffering(_ offering: Offering) -> some View {
-        return self
-            .init(productIDs: offering.subscriptionProductIdentifiers)
-            .handlePurchases(offering)
-    }
-
-}
-#else
 @available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
 extension SubscriptionStoreView where Content == AutomaticSubscriptionStoreMarketingContent {
 
@@ -111,7 +97,6 @@ extension SubscriptionStoreView where Content == AutomaticSubscriptionStoreMarke
     }
 
 }
-#endif
 
 // MARK: - Private
 
