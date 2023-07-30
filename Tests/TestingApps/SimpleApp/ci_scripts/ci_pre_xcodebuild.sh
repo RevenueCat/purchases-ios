301958baa124258c7505b158090dc979f69c0865
NachoSoto <ignaciosoto90@gmail.com>
2023-07-30 10:26:36 +0200

    Moved file

--- Tests/TestingApps/SimpleApp/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/SimpleApp/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash -e
 
 SIMPLE_APP_API_KEY=$REVENUECAT_XCODE_CLOUD_SIMPLE_APP_API_KEY
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -z "$SIMPLE_APP_API_KEY" ]; then
   echo "SimpleApp API key environment variable is not set."
 else
 	echo "Replacing API key on SimpleApp"
 
-	file="Tests/TestingApps/SimpleApp/SimpleApp/Configuration.swift"
+	file="$SCRIPT_DIR/../SimpleApp/Configuration.swift"
 	sed -i.bak 's/private static let apiKeyFromCI = ""/private static let apiKeyFromCI = "'$SIMPLE_APP_API_KEY'"/g' $file
 	rm $file.bak
 fi
