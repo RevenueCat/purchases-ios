40d45be54d18e810125bf59817b89f358c347ab7
NachoSoto <ignaciosoto90@gmail.com>
2023-07-31 11:31:18 +0200

    Indentation

--- Tests/TestingApps/SimpleApp/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/SimpleApp/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,11 +4,11 @@ SIMPLE_APP_API_KEY=$REVENUECAT_XCODE_CLOUD_SIMPLE_APP_API_KEY
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -z "$SIMPLE_APP_API_KEY" ]; then
-  echo "SimpleApp API key environment variable is not set."
+    echo "SimpleApp API key environment variable is not set."
 else
-	echo "Replacing API key on SimpleApp"
+    echo "Replacing API key on SimpleApp"
 
-	file="$SCRIPT_DIR/../SimpleApp/Configuration.swift"
-	sed -i.bak 's/private static let apiKeyFromCI = ""/private static let apiKeyFromCI = "'$SIMPLE_APP_API_KEY'"/g' $file
-	rm $file.bak
+    file="$SCRIPT_DIR/../SimpleApp/Configuration.swift"
+    sed -i.bak 's/private static let apiKeyFromCI = ""/private static let apiKeyFromCI = "'$SIMPLE_APP_API_KEY'"/g' $file
+    rm $file.bak
 fi
