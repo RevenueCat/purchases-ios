7113779e78c93514f07e68c5e4834ae9e7463251
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-31 20:57:38 +0200

    `Paywalls`: new `.onPurchaseCompleted` modifier (#2930)
    This allows users to present a `PaywallView` and be notified of
    purchasing events. Example:
    ```swift
    VStack {
        YourViews()
    
        if !self.didPurchase {
            PaywallView()
        }
    }
        .onPurchaseCompleted { customerInfo in
            print("Purchase completed: \(customerInfo.entitlements)")
            self.didPurchase = true
        }
    ```
    
    Additionally, an explicit optional closure is added to
    `.presentPaywallIfNecessary`:
    ```swift
    YourApp()
        .presentPaywallIfNecessary {
            !$0.entitlements.active.keys.contains("entitlement_identifier")
        } purchaseCompleted: { customerInfo in
            print("Customer info unlocked entitlement: \(customerInfo.entitlements)")
        }
    ```
    
    These are now tested through new unit tests.

--- Tests/APITesters/RevenueCatUIAPITester/SwiftAPITester/PaywallViewAPI.swift
+++ Tests/APITesters/RevenueCatUIAPITester/SwiftAPITester/PaywallViewAPI.swift
@@ -13,8 +13,16 @@ import SwiftUI
 struct App: View {
 
     private var offering: Offering
+    private var completed: PurchaseCompletedHandler = { (_: CustomerInfo) in }
 
     var body: some View {
+        self.content
+    }
+
+    // Note: `body` is implicitly `MainActor`, but this is not on purpose
+    // to ensure that these constructors can be called outside of `@MainActor`.
+    @ViewBuilder
+    var content: some View {
         PaywallView()
         PaywallView(mode: .fullScreen)
         PaywallView(offering: self.offering)
@@ -22,12 +30,26 @@ struct App: View {
     }
 
     @ViewBuilder
-    var extensions: some View {
+    var checkPresentPaywallIfNecessary: some View {
         Text("")
             .presentPaywallIfNecessary(requiredEntitlementIdentifier: "")
-            .presentPaywallIfNecessary(mode: .fullScreen, requiredEntitlementIdentifier: "")
+            .presentPaywallIfNecessary(requiredEntitlementIdentifier: "", purchaseCompleted: completed)
+            .presentPaywallIfNecessary(mode: .fullScreen,
+                                       requiredEntitlementIdentifier: "",
+                                       purchaseCompleted: completed)
             .presentPaywallIfNecessary { (_: CustomerInfo) in false }
-            .presentPaywallIfNecessary(mode: .fullScreen) { (_: CustomerInfo) in false }
+            .presentPaywallIfNecessary { (_: CustomerInfo) in false } purchaseCompleted: { completed($0) }
+            .presentPaywallIfNecessary(mode: .fullScreen) { (_: CustomerInfo) in
+                return false
+            } purchaseCompleted: {
+                completed($0)
+            }
+    }
+
+    @ViewBuilder
+    var checkOnPurchaseCompleted: some View {
+        Text("")
+            .onPurchaseCompleted(self.completed)
     }
 
     private func modes(_ mode: PaywallViewMode) {
---
+++ Tests/RevenueCatUITests/PurchaseCompletedHandlerTests.swift
@@ -0,0 +1,70 @@
+//
+//  PurchaseCompletedHandlerTests.swift
+//  
+//
+//  Created by Nacho Soto on 7/31/23.
+//
+
+import Nimble
+import RevenueCat
+@testable import RevenueCatUI
+import SwiftUI
+import XCTest
+
+#if !os(macOS)
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+@MainActor
+class PurchaseCompletedHandlerTests: TestCase {
+
+    func testOnPurchaseCompletedWithCancellation() throws {
+        let handler: PurchaseHandler = .cancelling()
+
+        var customerInfo: CustomerInfo?
+        var purchased = false
+
+        PaywallView(
+            offering: Self.offering.withLocalImages,
+            introEligibility: .producing(eligibility: .eligible),
+            purchaseHandler: handler
+        )
+            .onPurchaseCompleted {
+                customerInfo = $0
+            }
+            .addToHierarchy()
+
+        Task {
+            _ = try await Self.purchaseHandler.purchase(package: Self.package)
+            purchased = true
+        }
+
+        expect(purchased).toEventually(beTrue())
+        expect(customerInfo).to(beNil())
+    }
+
+    func testOnPurchaseCompleted() throws {
+        var customerInfo: CustomerInfo?
+
+        PaywallView(
+            offering: Self.offering.withLocalImages,
+            introEligibility: .producing(eligibility: .eligible),
+            purchaseHandler: Self.purchaseHandler
+        )
+            .onPurchaseCompleted {
+                customerInfo = $0
+            }
+            .addToHierarchy()
+
+        Task {
+            _ = try await Self.purchaseHandler.purchase(package: Self.package)
+        }
+
+        expect(customerInfo).toEventually(be(TestData.customerInfo))
+    }
+
+    private static let purchaseHandler: PurchaseHandler = .mock()
+    private static let offering = TestData.offeringWithNoIntroOffer
+    private static let package = TestData.annualPackage
+}
+
+#endif
