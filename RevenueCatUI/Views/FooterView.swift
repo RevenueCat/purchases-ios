4477f5aec5c0faf898515f29ffde5d7d59f99104
NachoSoto <NachoSoto@users.noreply.github.com>
2023-07-31 11:30:19 +0200

    `Paywalls`: improved template accessibility support (#2920)
    Just some low hanging fruit to make paywalls work better with voice
    over. There's still more we can improve but this makes them already 100%
    usable.

--- RevenueCatUI/Views/FooterView.swift
+++ RevenueCatUI/Views/FooterView.swift
@@ -50,6 +50,7 @@ struct FooterView: View {
     private var separator: some View {
         Image(systemName: "circle.fill")
             .font(.system(size: 5))
+            .accessibilityHidden(true)
     }
 
     private var hasTOS: Bool { self.configuration.termsOfServiceURL != nil }
