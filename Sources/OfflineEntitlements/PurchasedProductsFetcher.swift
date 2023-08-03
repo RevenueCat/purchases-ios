4ca4ec4fbe6a72b758394620848a8bd11155728c
Mark Villacampa <m@markvillacampa.com>
2023-08-04 00:27:08 +0200

    make PurchasedProductsFetcherType conform to Sendable

--- Sources/OfflineEntitlements/PurchasedProductsFetcher.swift
+++ Sources/OfflineEntitlements/PurchasedProductsFetcher.swift
@@ -14,7 +14,7 @@
 import Foundation
 import StoreKit
 
-protocol PurchasedProductsFetcherType {
+protocol PurchasedProductsFetcherType: Sendable {
 
     @available(iOS 15.0, tvOS 15.0, watchOS 8.0, macOS 12.0, *)
     func fetchPurchasedProducts() async throws -> [PurchasedSK2Product]
