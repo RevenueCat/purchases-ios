0903c1f5cca61cc4cc03552121af3fdfa91884a1
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-04 11:43:35 -0700

    `Paywalls`: multi-package horizontal template  (#2949)
    ![image](https://github.com/RevenueCat/purchases-ios/assets/685609/a5cc3f4b-99d0-40a2-8a85-82f243383978)

---
+++ Tests/RevenueCatUITests/Templates/MultiPackageHorizontalPaywallViewTests.swift
@@ -0,0 +1,38 @@
+import Nimble
+import RevenueCat
+@testable import RevenueCatUI
+import SnapshotTesting
+
+#if !os(macOS)
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+class MultiPackageHorizontalPaywallViewTests: BaseSnapshotTest {
+
+    func testSamplePaywall() {
+        PaywallView(offering: Self.offering.withLocalImages,
+                    introEligibility: Self.eligibleChecker,
+                    purchaseHandler: Self.purchaseHandler)
+        .snapshot(size: Self.fullScreenSize)
+    }
+
+    func testLargeDynamicType() {
+        PaywallView(offering: Self.offering.withLocalImages,
+                    introEligibility: Self.eligibleChecker,
+                    purchaseHandler: Self.purchaseHandler)
+        .environment(\.dynamicTypeSize, .xxLarge)
+        .snapshot(size: Self.fullScreenSize)
+    }
+
+    func testLargerDynamicType() {
+        PaywallView(offering: Self.offering.withLocalImages,
+                    introEligibility: Self.eligibleChecker,
+                    purchaseHandler: Self.purchaseHandler)
+        .environment(\.dynamicTypeSize, .accessibility2)
+        .snapshot(size: Self.fullScreenSize)
+    }
+
+    private static let offering = TestData.offeringWithMultiPackageHorizontalPaywall
+
+}
+
+#endif
