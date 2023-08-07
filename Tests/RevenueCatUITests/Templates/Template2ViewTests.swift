c7f08e8349a85de09a56b05bc15669ad50a3e7a1
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-07 13:19:38 -0700

    `Paywalls`: updated template names (#2971)

--- Tests/RevenueCatUITests/Templates/Template2ViewTests.swift
+++ Tests/RevenueCatUITests/Templates/Template2ViewTests.swift
@@ -6,7 +6,7 @@ import SnapshotTesting
 #if !os(macOS)
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
-class MultiPackageBoldPaywallViewTests: BaseSnapshotTest {
+class Template2ViewTests: BaseSnapshotTest {
 
     func testSamplePaywall() {
         let view = PaywallView(offering: Self.offering.withLocalImages,
