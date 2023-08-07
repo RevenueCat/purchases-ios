990b6468304b8802bdc62700a54746d0a020daa3
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-07 14:24:45 -0700

    `Paywalls`: warm-up image cache (#2978)
    This relies on `AsyncImage` using `URLSession.shared`
    
    I've tested this, and the app doesn't even make a request the second
    time around because the images are caches on disk.

--- Tests/UnitTests/Purchasing/Purchases/PurchasesGetOfferingsTests.swift
+++ Tests/UnitTests/Purchasing/Purchases/PurchasesGetOfferingsTests.swift
@@ -99,7 +99,7 @@ class PurchasesGetOfferingsTests: BasePurchasesTests {
         expect(self.deviceCache.clearOfferingsCacheTimestampCount) == 0
     }
 
-    func testWarmsUpEligibilityCache() throws {
+    func testWarmsUpPaywallsCache() throws {
         let bundle = Bundle(for: Self.self)
         let offeringsURL = try XCTUnwrap(bundle.url(forResource: "Offerings",
                                                     withExtension: "json",
@@ -126,8 +126,12 @@ class PurchasesGetOfferingsTests: BasePurchasesTests {
         self.setupPurchases()
 
         expect(self.mockOfferingsManager.invokedUpdateOfferingsCacheCount).toEventually(equal(1))
+
         expect(self.paywallCache.invokedWarmUpEligibilityCache).toEventually(beTrue())
         expect(self.paywallCache.invokedWarmUpEligibilityCacheOfferings) == offerings
+
+        expect(self.paywallCache.invokedWarmUpPaywallImagesCache).toEventually(beTrue())
+        expect(self.paywallCache.invokedWarmUpPaywallImagesCacheOfferings) == offerings
     }
 
 }
