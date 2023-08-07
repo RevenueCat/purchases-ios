e1bf737d914a7940afb6c04a5bed98ff108bc102
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-07 12:27:51 -0700

    `Paywalls`: updated variable names (#2970)
    These match the new agreed upon variables.
    
    - Improved the localized unit abbreviations
    - Refactored `VariableDataProvider` so all the logic is now in `Package`
    - Added new tests for `Package` variables

--- RevenueCatUI/Helpers/Package+VariableDataProvider.swift
+++ RevenueCatUI/Helpers/Package+VariableDataProvider.swift
@@ -8,20 +8,16 @@ extension Package: VariableDataProvider {
         return Bundle.main.applicationDisplayName
     }
 
-    var isSubscription: Bool {
-        return self.storeProduct.productCategory == .subscription
-    }
-
-    var isMonthly: Bool {
-        return self.packageType == .monthly
-    }
-
     var localizedPrice: String {
         return self.storeProduct.localizedPriceString
     }
 
     var localizedPricePerMonth: String {
         return self.priceFormatter.string(from: self.pricePerMonth) ?? ""
+    }
+
+    var localizedIntroductoryOfferPrice: String? {
+        return self.storeProduct.introductoryDiscount?.localizedPriceString
     }
 
     var productName: String {
@@ -41,12 +37,38 @@ extension Package: VariableDataProvider {
         return self.introDuration(locale)
     }
 
+    func localizedPricePerPeriod(_ locale: Locale) -> String {
+        guard let period = self.storeProduct.subscriptionPeriod else {
+            return self.localizedPrice
+        }
+
+        let unit = Localization.abbreviatedUnitLocalizedString(for: period.unit, locale: locale)
+        return "\(self.localizedPrice)/\(unit)"
+    }
+
+    func localizedPriceAndPerMonth(_ locale: Locale) -> String {
+        if !self.isSubscription || self.isMonthly {
+            return self.localizedPrice
+        } else {
+            let unit = Localization.abbreviatedUnitLocalizedString(for: .month, locale: locale)
+            return "\(self.localizedPrice) (\(self.localizedPricePerMonth)/\(unit))"
+        }
+    }
+
 }
 
 // MARK: - Private
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
 private extension Package {
+
+    var isSubscription: Bool {
+        return self.storeProduct.productCategory == .subscription
+    }
+
+    var isMonthly: Bool {
+        return self.packageType == .monthly
+    }
 
     var pricePerMonth: NSDecimalNumber {
         guard let price = self.storeProduct.pricePerMonth else {
