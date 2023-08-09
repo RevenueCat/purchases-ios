bbf63fde5fc2b87107f917ea8a2c64a126dd0df3
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-09 07:58:55 -0700

    `Paywalls`: send presented `PaywallViewMode` with purchases (#2859)
    I've moved `PaywallViewMode` from `RevenueCatUI` to `RevenueCat`. This
    uses the same approach as #2645. This method is `public` so it can be
    used from `RevenueCatUI`, but the documentation stays its purpose.
    
    The behavior is covered with integration tests and a snapshot test.
    
    Depends on https://github.com/RevenueCat/khepri/pull/6364.

--- Sources/Identity/CustomerInfoManager.swift
+++ Sources/Identity/CustomerInfoManager.swift
@@ -329,6 +329,7 @@ private extension CustomerInfoManager {
                     let transactionData = PurchasedTransactionData(
                         appUserID: appUserID,
                         presentedOfferingID: nil,
+                        presentedPaywallMode: nil,
                         unsyncedAttributes: [:],
                         storefront: await Storefront.currentStorefront,
                         source: Self.sourceForUnfinishedTransaction
