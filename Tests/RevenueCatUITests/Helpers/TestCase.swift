41a8207d46529d0600bedfb3f591b1d294bfb586
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-23 11:33:51 -0700

    `Paywalls`: add support for generating snapshots on CI (#3055)

--- Tests/RevenueCatUITests/Helpers/TestCase.swift
+++ Tests/RevenueCatUITests/Helpers/TestCase.swift
@@ -47,7 +47,7 @@ private enum SnapshotTests {
     static func updateSnapshotsIfNeeded() {
         guard !Self.environmentVariableChecked else { return }
 
-        if ProcessInfo.processInfo.environment["CIRCLECI_TESTS_GENERATE_SNAPSHOTS"] == "1" {
+        if ProcessInfo.processInfo.environment["CIRCLECI_TESTS_GENERATE_REVENUECAT_UI_SNAPSHOTS"] == "1" {
             isRecording = true
         }
     }
