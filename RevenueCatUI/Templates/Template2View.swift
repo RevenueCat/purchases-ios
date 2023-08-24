2103b047629dac12b6cd1bd556beb6ec8b68fa76
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-24 14:18:04 -0700

    `Paywalls`: added MIT license to all headers (#3084)
    `Xcode` doesn't allow customizing the header (looks like maybe it worked
    at some point but not anymore:
    https://stackoverflow.com/questions/67614785/changing-the-default-header-comment-license-in-swift-package).
    
    At least this adds the license to all the initial files in the package.

--- RevenueCatUI/Templates/Template2View.swift
+++ RevenueCatUI/Templates/Template2View.swift
@@ -1,3 +1,16 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  Template2View.swift
+//
+//  Created by Nacho Soto.
+
 import RevenueCat
 import SwiftUI
 
