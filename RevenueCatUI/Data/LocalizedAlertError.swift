6659614c2b3c469743195c8eec3b4e09cbbf3e4a
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-24 14:18:04 -0700

    `Paywalls`: added MIT license to all headers (#3084)
    `Xcode` doesn't allow customizing the header (looks like maybe it worked
    at some point but not anymore:
    https://stackoverflow.com/questions/67614785/changing-the-default-header-comment-license-in-swift-package).
    
    At least this adds the license to all the initial files in the package.

--- RevenueCatUI/Data/LocalizedAlertError.swift
+++ RevenueCatUI/Data/LocalizedAlertError.swift
@@ -1,9 +1,15 @@
 //
-//  File.swift
-//  
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  LocalizedAlertError.swift
 //
 //  Created by Nacho Soto on 7/21/23.
-//
 
 import RevenueCat
 import SwiftUI
--- RevenueCatUI/View+PresentPaywall.swift
+++ RevenueCatUI/View+PresentPaywall.swift
@@ -1,9 +1,15 @@
 //
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
 //  View+PresentPaywall.swift
-//  
 //
 //  Created by Nacho Soto on 7/24/23.
-//
 
 import RevenueCat
 import SwiftUI
