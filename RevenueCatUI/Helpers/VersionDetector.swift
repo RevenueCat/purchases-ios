30262bc16b3080514a24fd592e71b9ccd112a042
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-24 14:18:04 -0700

    `Paywalls`: added MIT license to all headers (#3084)
    `Xcode` doesn't allow customizing the header (looks like maybe it worked
    at some point but not anymore:
    https://stackoverflow.com/questions/67614785/changing-the-default-header-comment-license-in-swift-package).
    
    At least this adds the license to all the initial files in the package.

--- RevenueCatUI/Helpers/VersionDetector.swift
+++ RevenueCatUI/Helpers/VersionDetector.swift
@@ -1,9 +1,15 @@
 //
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
 //  VersionDetector.swift
-//  
 //
 //  Created by Nacho Soto on 8/17/23.
-//
 
 import Foundation
 
