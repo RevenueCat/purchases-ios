7673a7f5ada06eb222c83d19a283d8625a3fc6ad
NachoSoto <ignaciosoto90@gmail.com>
2023-08-24 13:42:34 -0700

    Fixed new tests

--- Tests/RevenueCatUITests/Templates/Template1ViewTests.swift
+++ Tests/RevenueCatUITests/Templates/Template1ViewTests.swift
@@ -15,11 +15,9 @@ class Template1ViewTests: BaseSnapshotTest {
     }
 
     func testTabletPaywall() {
-        PaywallView(offering: Self.offeringWithNoIntroOffer,
-                    introEligibility: Self.eligibleChecker,
-                    purchaseHandler: Self.purchaseHandler)
-        .environment(\.userInterfaceIdiom, .pad)
-        .snapshot(size: Self.iPadSize)
+        Self.createPaywall(offering: Self.offeringWithNoIntroOffer)
+            .environment(\.userInterfaceIdiom, .pad)
+            .snapshot(size: Self.iPadSize)
     }
 
     func testCustomFont() {
