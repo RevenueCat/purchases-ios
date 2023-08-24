68603908c13ca71cf9c269191aaef9d94c886254
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-24 14:18:04 -0700

    `Paywalls`: added MIT license to all headers (#3084)
    `Xcode` doesn't allow customizing the header (looks like maybe it worked
    at some point but not anymore:
    https://stackoverflow.com/questions/67614785/changing-the-default-header-comment-license-in-swift-package).
    
    At least this adds the license to all the initial files in the package.

--- Tests/RevenueCatUITests/BaseSnapshotTest.swift
+++ Tests/RevenueCatUITests/BaseSnapshotTest.swift
@@ -1,9 +1,16 @@
 //
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
 //  BaseSnapshotTest.swift
-//  
 //
 //  Created by Nacho Soto on 7/17/23.
-//
+
 import Nimble
 import RevenueCat
 @testable import RevenueCatUI
--- Tests/RevenueCatUITests/Templates/Template3ViewTests.swift
+++ Tests/RevenueCatUITests/Templates/Template3ViewTests.swift
@@ -1,3 +1,14 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  Template3ViewTests.swift
+
 import Nimble
 import RevenueCat
 @testable import RevenueCatUI
