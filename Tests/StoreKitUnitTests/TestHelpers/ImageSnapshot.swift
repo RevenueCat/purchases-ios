a22455c35fcf4f023890641fe3ec2dd4bd04225b
NachoSoto <NachoSoto@users.noreply.github.com>
2023-08-26 19:38:40 -0700

    `Paywalls`: fixed iOS 12 build (#3104)

--- Tests/StoreKitUnitTests/TestHelpers/ImageSnapshot.swift
+++ Tests/StoreKitUnitTests/TestHelpers/ImageSnapshot.swift
@@ -11,12 +11,12 @@
 //
 //  Created by Nacho Soto on 6/12/23.
 
+#if swift(>=5.8)
+
 import Foundation
 import Nimble
 import SnapshotTesting
 import SwiftUI
-
-#if swift(>=5.8)
 
 /// - Parameter separateOSVersions: pass `true` if you want to generate separate snapshots for each version.
 func haveValidSnapshot<Value>(
