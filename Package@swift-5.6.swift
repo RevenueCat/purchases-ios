86647042bbf943da789994623b9d76013cb3f7dd
NachoSoto <ignaciosoto90@gmail.com>
2023-08-31 14:16:57 -0700

    Fixed Xcode 13

---
+++ Package@swift-5.6.swift
@@ -0,0 +1,56 @@
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import class Foundation.ProcessInfo
+
+// Only add DocC Plugin when building docs, so that clients of this library won't
+// unnecessarily also get the DocC Plugin
+let environmentVariables = ProcessInfo.processInfo.environment
+let shouldIncludeDocCPlugin = environmentVariables["INCLUDE_DOCC_PLUGIN"] == "true"
+
+var dependencies: [Package.Dependency] = [
+    .package(url: "git@github.com:Quick/Nimble.git", from: "10.0.0")
+]
+if shouldIncludeDocCPlugin {
+    dependencies.append(.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"))
+}
+
+let package = Package(
+    name: "RevenueCat",
+    platforms: [
+        .macOS(.v10_13),
+        .watchOS("6.2"),
+        .tvOS(.v11),
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "RevenueCat",
+                 targets: ["RevenueCat"]),
+        .library(name: "RevenueCat_CustomEntitlementComputation",
+                 targets: ["RevenueCat_CustomEntitlementComputation"]),
+        .library(name: "ReceiptParser",
+                 targets: ["ReceiptParser"])
+    ],
+    dependencies: dependencies,
+    targets: [
+        .target(name: "RevenueCat",
+                path: "Sources",
+                exclude: ["Info.plist", "LocalReceiptParsing/ReceiptParser-only-files"],
+                resources: [
+                    .copy("../Sources/PrivacyInfo.xcprivacy")
+                ]),
+        .target(name: "RevenueCat_CustomEntitlementComputation",
+                path: "CustomEntitlementComputation",
+                exclude: ["Info.plist", "LocalReceiptParsing/ReceiptParser-only-files"],
+                resources: [
+                    .copy("PrivacyInfo.xcprivacy")
+                ],
+                swiftSettings: [.define("ENABLE_CUSTOM_ENTITLEMENT_COMPUTATION")]),
+        .target(name: "ReceiptParser",
+                path: "LocalReceiptParsing"),
+        .testTarget(name: "ReceiptParserTests",
+                    dependencies: ["ReceiptParser", "Nimble"],
+                    exclude: ["ReceiptParserTests-Info.plist"])
+    ]
+)
