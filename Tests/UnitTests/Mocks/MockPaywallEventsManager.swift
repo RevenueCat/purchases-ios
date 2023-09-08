bfcdf643d3ba220f41f607d0f2eea7e28f1a01a1
NachoSoto <NachoSoto@users.noreply.github.com>
2023-09-08 15:07:00 -0700

    `Paywalls`: created `PaywallEventsManager` (#3159)
    This combines the types created in prior PRs (#3157, #3158) to store and
    post events.

---
+++ Tests/UnitTests/Mocks/MockPaywallEventsManager.swift
@@ -0,0 +1,34 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  MockPaywallEventsManager.swift
+//
+//  Created by Nacho Soto on 9/6/23.
+
+import Foundation
+@testable import RevenueCat
+
+@available(iOS 15.0, tvOS 15.0, macOS 12.0, watchOS 8.0, *)
+actor MockPaywallEventsManager: PaywallEventsManagerType {
+
+    var trackedEvents: [PaywallEvent] = []
+
+    func track(paywallEvent: PaywallEvent) async {
+        self.trackedEvents.append(paywallEvent)
+    }
+
+    var invokedFlushEvents = false
+    var invokedFlushEventsCount = 0
+
+    func flushEvents(count: Int) async {
+        self.invokedFlushEvents = true
+        self.invokedFlushEventsCount += 1
+    }
+
+}
