e3468b752a7a2e3aebfe16cb30e769ec3ce61ee2
NachoSoto <NachoSoto@users.noreply.github.com>
2023-09-11 10:51:19 -0700

    `Paywalls`: don't apply dark appearance with no dark mode colors (#3184)
    Reported by @joshdholtz. If the developer doesn't configure dark colors,
    we don't want things like materials from inheriting the automatic dark
    mode configuration.

--- RevenueCatUI/Helpers/ColorInformation+MultiScheme.swift
+++ RevenueCatUI/Helpers/ColorInformation+MultiScheme.swift
@@ -77,6 +77,16 @@ extension PaywallData.Configuration.ColorInformation {
 
 #endif
 
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
+extension TemplateViewConfiguration {
+
+    /// Whether dark mode colors have been configured in this paywall.
+    var hasDarkMode: Bool {
+        return self.configuration.colors.dark != nil
+    }
+
+}
+
 #if canImport(SwiftUI)
 
 import SwiftUI
