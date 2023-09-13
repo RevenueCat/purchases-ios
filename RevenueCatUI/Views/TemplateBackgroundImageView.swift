3867aedde9817934e4af756b09cbdd4b53f0d91d
NachoSoto <NachoSoto@users.noreply.github.com>
2023-09-13 16:43:10 -0700

    `Paywalls`: fixed `TemplateBackgroundImageView` aspect ratio (#3201)

--- RevenueCatUI/Views/TemplateBackgroundImageView.swift
+++ RevenueCatUI/Views/TemplateBackgroundImageView.swift
@@ -17,11 +17,23 @@ import SwiftUI
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
 struct TemplateBackgroundImageView: View {
 
-    var configuration: TemplateViewConfiguration
+    private let url: URL?
+    private let blurred: Bool
+
+    init(configuration: TemplateViewConfiguration) {
+        self.init(url: configuration.backgroundImageURLToDisplay,
+                  blurred: configuration.configuration.blurredBackgroundImage)
+    }
+
+    init(url: URL?, blurred: Bool) {
+        self.url = url
+        self.blurred = blurred
+    }
 
     var body: some View {
-        if let url = self.configuration.backgroundImageURLToDisplay {
+        if let url = self.url {
             self.image(url)
+                .scaledToFill()
                 .unredacted()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .edgesIgnoringSafeArea(.all)
@@ -30,7 +42,7 @@ struct TemplateBackgroundImageView: View {
 
     @ViewBuilder
     private func image(_ url: URL) -> some View {
-        if self.configuration.configuration.blurredBackgroundImage {
+        if self.blurred {
             RemoteImage(url: url)
                 .blur(radius: 40)
                 .opacity(0.7)
@@ -40,3 +52,43 @@ struct TemplateBackgroundImageView: View {
     }
 
 }
+
+// MARK: -
+
+#if DEBUG
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
+@available(watchOS, unavailable)
+@available(macOS, unavailable)
+@available(tvOS, unavailable)
+struct TemplateBackgroundImageView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        TemplateBackgroundImageView(
+            url: TestData.paywallAssetBaseURL.appendingPathComponent(TestData.paywallHeaderImageName),
+            blurred: false
+        )
+        .previewDisplayName("Wrong aspect ratio not blured")
+
+        TemplateBackgroundImageView(
+            url: TestData.paywallAssetBaseURL.appendingPathComponent(TestData.paywallHeaderImageName),
+            blurred: true
+        )
+        .previewDisplayName("Wrong aspect ratio blured")
+
+        TemplateBackgroundImageView(
+            url: TestData.paywallAssetBaseURL.appendingPathComponent(TestData.paywallBackgroundImageName),
+            blurred: false
+        )
+        .previewDisplayName("Correct aspect ratio not blured")
+
+        TemplateBackgroundImageView(
+            url: TestData.paywallAssetBaseURL.appendingPathComponent(TestData.paywallBackgroundImageName),
+            blurred: true
+        )
+        .previewDisplayName("Correct aspect ratio blured")
+    }
+
+}
+
+#endif
