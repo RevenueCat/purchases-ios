RevenueCat/purchases-ios

commit 29755a24b0d2877c5fbf906b06288342d709a6ea

`Paywalls`: add support for CTA button gradients (#3121)
![Screenshot 2023-08-29 at 15 41
31](https://github.com/RevenueCat/purchases-ios/assets/685609/349d7d84-ac3b-4a81-85d8-d876d63dc4b6)

RevenueCatUI/Views/PurchaseButton.swift
@@ -83,16 +83,35 @@ struct PurchaseButton: View {
                        ? .infinity
                         : nil
                 )
+                .padding()
                 .padding(.vertical, self.userInterfaceIdiom == .pad ? 10 : 0)
         }
         .font(self.fonts.font(for: self.mode.buttonFont).weight(.semibold))
-        .tint(self.colors.callToActionBackgroundColor)
-        .buttonBorderShape(self.mode.buttonBorderShape)
-        .controlSize(self.mode.buttonSize)
-        .buttonStyle(.borderedProminent)
+        .background(self.backgroundView)
+        .tint(.clear)
         .frame(maxWidth: .infinity)
         .dynamicTypeSize(...Constants.maximumDynamicTypeSize)
         .disabled(self.package.currentlySubscribed)
+    }
+
+    @ViewBuilder
+    private var backgroundView: some View {
+        Capsule(style: .continuous)
+            .foregroundStyle(self.backgroundColor)
+    }
+
+    private var backgroundColor: some ShapeStyle {
+        let primary = self.colors.callToActionBackgroundColor
+
+        if let secondary = self.colors.callToActionSecondaryBackgroundColor {
+            return AnyShapeStyle(
+                LinearGradient(colors: [primary, secondary],
+                               startPoint: .top,
+                               endPoint: .bottom)
+            )
+        } else {
+            return AnyShapeStyle(primary)
+        }
     }
 
 }
@@ -109,24 +128,6 @@ private extension PaywallViewMode {
     var fullWidthButton: Bool {
         switch self {
         case .fullScreen, .footer, .condensedFooter: return true
-        }
-    }
-
-    @available(tvOS, unavailable)
-    var buttonSize: ControlSize {
-        switch self {
-        case .fullScreen, .footer, .condensedFooter: return .large
-        }
-    }
-
-    var buttonBorderShape: ButtonBorderShape {
-        switch self {
-        case .fullScreen, .footer, .condensedFooter:
-            #if os(macOS) || os(tvOS)
-            return .roundedRectangle
-            #else
-            return .capsule
-            #endif
         }
     }
 

RevenueCatUI/Helpers/ColorInformation+MultiScheme.swift
@@ -42,6 +42,8 @@ extension PaywallData.Configuration.Colors {
             text2: .init(light: light.text2, dark: dark.text2),
             callToActionBackground: .init(light: light.callToActionBackground, dark: dark.callToActionBackground),
             callToActionForeground: .init(light: light.callToActionForeground, dark: dark.callToActionForeground),
+            callToActionSecondaryBackground: .init(light: light.callToActionSecondaryBackground,
+                                                   dark: dark.callToActionSecondaryBackground),
             accent1: .init(light: light.accent1, dark: dark.accent1),
             accent2: .init(light: light.accent2, dark: dark.accent2)
         )
@@ -88,6 +90,7 @@ extension PaywallData.Configuration.Colors {
     var text2Color: Color { self.text2?.underlyingColor ?? self.text1.underlyingColor }
     var callToActionBackgroundColor: Color { self.callToActionBackground.underlyingColor }
     var callToActionForegroundColor: Color { self.callToActionForeground.underlyingColor }
+    var callToActionSecondaryBackgroundColor: Color? { self.callToActionSecondaryBackground?.underlyingColor }
     var accent1Color: Color { self.accent1?.underlyingColor ?? self.callToActionForegroundColor }
     var accent2Color: Color { self.accent2?.underlyingColor ?? self.accent1Color }
 

Sources/Paywalls/PaywallData.swift
@@ -297,6 +297,8 @@ extension PaywallData.Configuration {
         public var callToActionBackground: PaywallColor
         /// Foreground color of the main call to action button.
         public var callToActionForeground: PaywallColor
+        /// If present, the CTA will create a vertical gradient from ``callToActionBackground`` to this color.
+        public var callToActionSecondaryBackground: PaywallColor?
         /// Primary accent color.
         public var accent1: PaywallColor?
         /// Secondary accent color
@@ -309,6 +311,7 @@ extension PaywallData.Configuration {
             text2: PaywallColor? = nil,
             callToActionBackground: PaywallColor,
             callToActionForeground: PaywallColor,
+            callToActionSecondaryBackground: PaywallColor? = nil,
             accent1: PaywallColor? = nil,
             accent2: PaywallColor? = nil
         ) {
@@ -317,6 +320,7 @@ extension PaywallData.Configuration {
             self.text2 = text2
             self.callToActionBackground = callToActionBackground
             self.callToActionForeground = callToActionForeground
+            self.callToActionSecondaryBackground = callToActionSecondaryBackground
             self.accent1 = accent1
             self.accent2 = accent2
         }

Tests/APITesters/SwiftAPITester/SwiftAPITester/PaywallAPI.swift
@@ -101,6 +101,7 @@ func checkPaywallColors(_ config: PaywallData.Configuration.Colors) {
     let text2: PaywallColor? = config.text2
     let callToActionBackground: PaywallColor = config.callToActionBackground
     let callToActionForeground: PaywallColor = config.callToActionForeground
+    let callToActionSecondaryBackground: PaywallColor? = config.callToActionSecondaryBackground
     let accent1: PaywallColor? = config.accent1
     let accent2: PaywallColor? = config.accent2
 
@@ -110,6 +111,7 @@ func checkPaywallColors(_ config: PaywallData.Configuration.Colors) {
         text2: text2,
         callToActionBackground: callToActionBackground,
         callToActionForeground: callToActionForeground,
+        callToActionSecondaryBackground: callToActionSecondaryBackground,
         accent1: accent1,
         accent2: accent2
     )

Tests/UnitTests/Paywalls/PaywallDataTests.swift
@@ -39,6 +39,7 @@ class PaywallDataTests: BaseHTTPResponseTest {
         expect(paywall.config.colors.light.text2?.stringRepresentation) == "#FF00AA11"
         expect(paywall.config.colors.light.callToActionBackground.stringRepresentation) == "#FF00AACC"
         expect(paywall.config.colors.light.callToActionForeground.stringRepresentation) == "#FF00AA"
+        expect(paywall.config.colors.light.callToActionSecondaryBackground?.stringRepresentation) == "#FF00BB"
         expect(paywall.config.colors.light.accent1?.stringRepresentation) == "#FF0000"
         expect(paywall.config.colors.light.accent2?.stringRepresentation) == "#00FF00"
 

commit cf8c05edae0607a17aacbb3171af307a06fb502c

`Paywalls`: loading indicator for in-progress purchases
Fixes #3216.

This likely got broken with #3121 because we stopped using `.buttonStyle`.
This implements a manual style when the button is `disabled` (when `PurchaseHandler.actionInProgress` is `true`).

RevenueCatUI/Views/PurchaseButton.swift
@@ -59,9 +59,6 @@ struct PurchaseButton: View {
         self.purchaseHandler = purchaseHandler
     }
 
-    @Environment(\.userInterfaceIdiom)
-    var userInterfaceIdiom
-
     var body: some View {
         self.button
     }
@@ -69,29 +66,22 @@ struct PurchaseButton: View {
     private var button: some View {
         AsyncButton {
             guard !self.purchaseHandler.actionInProgress else { return }
+            guard !self.package.currentlySubscribed else { return }
 
             _ = try await self.purchaseHandler.purchase(package: self.package.content)
         } label: {
-            IntroEligibilityStateView(
-                textWithNoIntroOffer: self.package.localization.callToAction,
-                textWithIntroOffer: self.package.localization.callToActionWithIntroOffer,
+            PurchaseButtonLabel(
+                package: self.package,
+                colors: self.colors,
                 introEligibility: self.introEligibility,
-                foregroundColor: self.colors.callToActionForegroundColor
+                mode: self.mode
             )
-                .frame(
-                    maxWidth: self.mode.fullWidthButton
-                       ? .infinity
-                        : nil
-                )
-                .padding()
-                .padding(.vertical, self.userInterfaceIdiom == .pad ? 10 : 0)
         }
         .font(self.fonts.font(for: self.mode.buttonFont).weight(.semibold))
         .background(self.backgroundView)
         .tint(.clear)
         .frame(maxWidth: .infinity)
         .dynamicTypeSize(...Constants.maximumDynamicTypeSize)
-        .disabled(self.package.currentlySubscribed)
     }
 
     @ViewBuilder
@@ -112,6 +102,46 @@ struct PurchaseButton: View {
         } else {
             return AnyShapeStyle(primary)
         }
+    }
+
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
+private struct PurchaseButtonLabel: View {
+
+    let package: TemplateViewConfiguration.Package
+    let colors: PaywallData.Configuration.Colors
+    let introEligibility: IntroEligibilityStatus?
+    let mode: PaywallViewMode
+
+    @Environment(\.userInterfaceIdiom)
+    private var userInterfaceIdiom
+
+    @Environment(\.isEnabled)
+    private var isEnabled
+
+    var body: some View {
+        IntroEligibilityStateView(
+            textWithNoIntroOffer: self.package.localization.callToAction,
+            textWithIntroOffer: self.package.localization.callToActionWithIntroOffer,
+            introEligibility: self.introEligibility,
+            foregroundColor: self.colors.callToActionForegroundColor
+        )
+            .frame(
+                maxWidth: self.mode.fullWidthButton
+                   ? .infinity
+                    : nil
+            )
+            .padding()
+            .padding(.vertical, self.userInterfaceIdiom == .pad ? 10 : 0)
+            .hidden(if: !self.isEnabled)
+            .overlay {
+                if !self.isEnabled {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .tint(self.colors.callToActionForegroundColor)
+                }
+            }
     }
 
 }
@@ -149,6 +179,9 @@ struct PurchaseButton_Previews: PreviewProvider {
         @State
         private var eligibility: IntroEligibilityStatus?
 
+        @StateObject
+        private var purchaseHandler = PreviewHelpers.purchaseHandler
+
         var body: some View {
             PurchaseButton(
                 package: Self.package,
@@ -156,8 +189,10 @@ struct PurchaseButton_Previews: PreviewProvider {
                 fonts: DefaultPaywallFontProvider(),
                 introEligibility: self.eligibility,
                 mode: self.mode,
-                purchaseHandler: PreviewHelpers.purchaseHandler
+                purchaseHandler: self.purchaseHandler
             )
+            // This is done by PaywallView
+            .disabled(self.purchaseHandler.actionInProgress)
             .task {
                 self.eligibility = await PreviewHelpers.introEligibilityChecker.eligibility(for: Self.package.content)
             }
@@ -170,7 +205,7 @@ struct PurchaseButton_Previews: PreviewProvider {
                 context: .init(discountRelativeToMostExpensivePerMonth: nil),
                 locale: .current
             ),
-            currentlySubscribed: Bool.random(),
+            currentlySubscribed: false,
             discountRelativeToMostExpensivePerMonth: nil
         )
     }