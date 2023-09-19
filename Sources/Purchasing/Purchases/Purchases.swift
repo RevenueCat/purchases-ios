0e58d896d208a62614476f500294a15521e919fd
Mark Villacampa <m@markvillacampa.com>
2023-09-19 19:29:47 +0200

    Fix custom entitlements compilation

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -1296,7 +1296,7 @@ public extension Purchases {
         Self.configure(
             with: .builder(withAPIKey: apiKey)
                 .with(appUserID: appUserID)
-                .with(dangerousSettings: DangerousSettings(customEntitlementComputation: true))
+                .with(dangerousSettings: DangerousSettings(customEntitlementComputation: true, usesStoreKit2JWS: false))
                 .build())
     }
 
