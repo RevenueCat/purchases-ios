fbe776be8c83b860efd3c67b4d636cd4a298ee66
NachoSoto <ignaciosoto90@gmail.com>
2023-09-21 16:05:20 -0700

    `Paywalls`: renamed `SimpleApp` to `PaywallsTester`
    The name was picked before we were ready to announce RevenueCat paywalls publicly.
    This is a better name for what's become of this app now, and it will help to reference it from our docs.

---
+++ Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+PAYWALLS_TESTER_API_KEY=$REVENUECAT_XCODE_CLOUD_SIMPLE_APP_API_KEY
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ -z "$PAYWALLS_TESTER_API_KEY" ]; then
+    echo "PaywallsTester API key environment variable is not set."
+else
+    echo "Replacing API key on PaywallsTester"
+
+    file="$SCRIPT_DIR/../PaywallsTester/Configuration.swift"
+    sed -i.bak 's/private static let apiKeyFromCI = ""/private static let apiKeyFromCI = "'$SIMPLE_APP_API_KEY'"/g' $file
+    rm $file.bak
+fi
