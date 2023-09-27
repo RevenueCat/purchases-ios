a9f1734cf0449b4674da1162ed64a7d3476d1082
Andy Boedo <andresboedo@gmail.com>
2023-09-27 19:32:56 -0300

    remove old code

--- Tests/TestingApps/PaywallsTester/PaywallsTester/Views/OfferingsList.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/Views/OfferingsList.swift
@@ -61,47 +61,23 @@ struct OfferingsList: View {
         List {
             let offeringsWithPaywall = Self.offeringsWithPaywall(from: offerings)
 
-            #if targetEnvironment(macCatalyst)
             Section {
                 ForEach(offeringsWithPaywall, id: \.offering.id) { offering, paywall in
+                    #if targetEnvironment(macCatalyst)
                     NavigationLink(
                         destination: PaywallView(offering: offering),
                         tag: offering,
                         selection: self.$selectedOffering
                     ) {
-                        Button {
+                        OfferButton(offering: offering, paywall: paywall) {
                             self.selectedOffering = offering
-                        } label: {
-                            VStack(alignment: .leading) {
-                                Text(offering.serverDescription)
-                                Text(verbatim: "Template: \(paywall.templateName)")
-                            }
                         }
-                        .buttonStyle(.plain)
-                        .contentShape(Rectangle())
                     }
-                }
-            } header: {
-                Text(verbatim: "With paywall")
-            } footer: {
-                if offeringsWithPaywall.isEmpty {
-                    Text(verbatim: "No offerings with paywall")
-                }
-            }
-            #else
-            Section {
-                ForEach(offeringsWithPaywall, id: \.offering.id) { offering, paywall in
-
-                    Button {
+                    #else
+                    OfferButton(offering: offering, paywall: paywall) {
                         self.selectedOffering = offering
-                    } label: {
-                        VStack(alignment: .leading) {
-                            Text(offering.serverDescription)
-                            Text(verbatim: "Template: \(paywall.templateName)")
-                        }
                     }
-                    .buttonStyle(.plain)
-                    .contentShape(Rectangle())
+                    #endif
                 }
             } header: {
                 Text(verbatim: "With paywall")
@@ -110,13 +86,29 @@ struct OfferingsList: View {
                     Text(verbatim: "No offerings with paywall")
                 }
             }
-            #endif
 
             Section("Without paywall") {
                 ForEach(Self.offeringsWithNoPaywall(from: offerings), id: \.id) { offering in
                     Text(offering.serverDescription)
                 }
             }
+        }
+    }
+
+    private struct OfferButton: View {
+        let offering: Offering
+        let paywall: PaywallData
+        let action: () -> Void
+
+        var body: some View {
+            Button(action: action) {
+                VStack(alignment: .leading) {
+                    Text(offering.serverDescription)
+                    Text(verbatim: "Template: \(paywall.templateName)")
+                }
+            }
+            .buttonStyle(.plain)
+            .contentShape(Rectangle())
         }
     }
 
