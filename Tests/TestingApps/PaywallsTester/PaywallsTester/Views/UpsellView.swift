633476b7923df3c47a28ba538194ee31f4297320
Andy Boedo <andresboedo@gmail.com>
2023-10-02 17:44:48 -0300

    style improvements

--- Tests/TestingApps/PaywallsTester/PaywallsTester/Views/UpsellView.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/Views/UpsellView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct UpsellView: View {
+
     var body: some View {
         VStack {
             Text("""
                 This view automatically displays the default paywall if you're not subscribed.
                 
                 This is achieved by calling
-                `.presentPaywallIfNeeded(requiredEntitlementIdentifier: \(Configuration.entitlement))
+                `.presentPaywallIfNeeded(requiredEntitlementIdentifier: \(Configuration.entitlement))`
                 """)
 
         }
         .padding()
         .presentPaywallIfNeeded(requiredEntitlementIdentifier: Configuration.entitlement)
     }
+
 }
 
 struct UpsellView_Previews: PreviewProvider {
