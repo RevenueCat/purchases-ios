eea2efba31d7dc57a0d147160080fac286023ef1
Mark Villacampa <m@markvillacampa.com>
2023-10-11 16:16:31 +0200

    fix custom entitlements build

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -1304,7 +1304,7 @@ public extension Purchases {
         Self.configure(
             with: .builder(withAPIKey: apiKey)
                 .with(appUserID: appUserID)
-                .with(dangerousSettings: DangerousSettings(customEntitlementComputation: true, usesStoreKit2JWS: false))
+                .with(dangerousSettings: DangerousSettings(customEntitlementComputation: true))
                 .build())
     }
 
