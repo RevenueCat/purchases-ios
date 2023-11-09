f5eee703aa2fb1290fafc3df56ecbb177c23e5b4
NachoSoto <ignaciosoto90@gmail.com>
2023-11-09 09:09:00 -0800

    `RCSystemInfo.isSandbox`: improve implementation
    This brings #2276 and #2176 to v3.x

--- PurchasesTests/Misc/SystemInfoTests.swift
+++ PurchasesTests/Misc/SystemInfoTests.swift
@@ -71,4 +71,74 @@ class SystemInfoTests: XCTestCase {
                                   finishTransactions: finishTransactions)
         expect(systemInfo.finishTransactions) == finishTransactions
     }
+
+    func testIsSandboxInSimulator() {
+        expect(RCSystemInfo.isSandbox(with: MockBundle(.sandboxReceipt),
+                                      inSimulator: true)) == true
+    }
+
+    func testIsSandboxOnDevice() {
+        expect(RCSystemInfo.isSandbox(with: MockBundle(.sandboxReceipt),
+                                      inSimulator: false)) == true
+    }
+
+    func testIsAlwaysSandboxInSimulator() {
+        expect(RCSystemInfo.isSandbox(with: MockBundle(.productionReceipt),
+                                      inSimulator: true)) == true
+    }
+
+    func testIsNotSandboxWithRealReceipt() {
+        expect(RCSystemInfo.isSandbox(with: MockBundle(.productionReceipt),
+                                      inSimulator: false)) == false
+    }
+
+    func testMASSandbox() {
+        expect(RCSystemInfo.isSandbox(with: MockBundle(.macOSSandboxReceipt),
+                                      inSimulator: false)) == true
+    }
+
+    func testMASProduction() {
+        expect(RCSystemInfo.isSandbox(with: MockBundle(.macOSAppStoreReceipt),
+                                      inSimulator: false)) == false
+    }
+
+}
+
+// MARK: -
+
+private final class MockBundle: Bundle {
+
+    enum ReceiptURLResult {
+
+        case sandboxReceipt
+        case productionReceipt
+        case macOSAppStoreReceipt
+        case macOSSandboxReceipt
+        case nilURL
+
+    }
+
+    let receiptURLResult: ReceiptURLResult
+
+    init(_ receiptURLResult: ReceiptURLResult) {
+        self.receiptURLResult = receiptURLResult
+        super.init()
+    }
+
+    override var appStoreReceiptURL: URL? {
+        switch self.receiptURLResult {
+        case .sandboxReceipt:
+            return URL(string: "file:///private/var/mobile/Containers/Data/Application/8E17D2EF-7F56-4332-9A5B-6EABD08EFC47/StoreKit/sandboxReceipt")!
+        case .productionReceipt:
+            return URL(string: "file:///private/var/mobile/Containers/Data/Application/8E17D2EF-7F56-4332-9A5B-6EABD08EFC47/StoreKit/receipt")!
+        case .macOSSandboxReceipt:
+            // swiftlint:disable:next line_length
+            return URL(string: "/Users/nachosoto/Library/Developer/Xcode/DerivedData/PurchaseTester-coxthvoqhbhicvcmwbbwnogtdrle/Build/Products/Debug-maccatalyst/PurchaseTester.app/Contents/_MASReceipt/receipt")!
+        case .macOSAppStoreReceipt:
+            return URL(string: "/Applications/PurchaseTester.app/Contents/_MASReceipt/receipt")!
+        case .nilURL:
+            return nil
+        }
+    }
+
 }
