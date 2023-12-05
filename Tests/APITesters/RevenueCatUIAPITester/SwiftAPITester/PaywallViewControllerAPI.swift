6d1f64f33f90dabfdb3ccc2c9876e93342013780
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-12-05 13:02:49 +0100

    Add api tests

--- Tests/APITesters/RevenueCatUIAPITester/SwiftAPITester/PaywallViewControllerAPI.swift
+++ Tests/APITesters/RevenueCatUIAPITester/SwiftAPITester/PaywallViewControllerAPI.swift
@@ -20,6 +20,15 @@ func paywallViewControllerAPI(_ delegate: Delegate, _ offering: Offering?) {
 }
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
+func paywallFooterViewControllerAPI(_ delegate: FooterDelegate, _ offering: Offering?) {
+    let controller = PaywallFooterViewController()
+    controller.delegate = delegate
+
+    let _: UIViewController = PaywallFooterViewController()
+    let _: UIViewController = PaywallFooterViewController(offering: offering)
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
 final class Delegate: PaywallViewControllerDelegate {
 
     func paywallViewController(_ controller: PaywallViewController,
@@ -38,3 +47,21 @@ final class Delegate: PaywallViewControllerDelegate {
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
 final class DelegateWithNoImplementations: PaywallViewControllerDelegate {}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
+final class FooterDelegate: PaywallFooterViewControllerDelegate {
+
+    func paywallFooterViewController(_ controller: PaywallFooterViewController,
+                                     didFinishPurchasingWith customerInfo: CustomerInfo) {}
+
+    func paywallFooterViewController(_ controller: PaywallFooterViewController,
+                                     didFinishPurchasingWith customerInfo: CustomerInfo,
+                                     transaction: StoreTransaction?) {}
+
+    func paywallFooterViewController(_ controller: PaywallFooterViewController,
+                                     didFinishRestoringWith customerInfo: CustomerInfo) {}
+
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
+final class FooterDelegateWithNoImplementations: PaywallFooterViewControllerDelegate {}
