883ed6f893afeee0012a0c68fbb098a4c0b0b3c3
Mark Villacampa <m@markvillacampa.com>
2023-12-12 21:13:33 +0100

    clarify tuple values

--- Sources/FoundationExtensions/AsyncExtensions.swift
+++ Sources/FoundationExtensions/AsyncExtensions.swift
@@ -130,19 +130,19 @@ internal enum Async {
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
     static func retry<T>(
         maximumRetries: Int = 5,
-        pollInterval: DispatchTimeInterval = DispatchTimeInterval.milliseconds(300),
-        until value: @Sendable () async -> (Bool, T)
+        pollInterval: DispatchTimeInterval = .milliseconds(300),
+        until value: @Sendable () async -> (shouldRetry: Bool, result: T)
     ) async -> T {
         var lastValue: T
         var retries = 0
 
         repeat {
             retries += 1
-            let currentValue = await value()
-            if currentValue.0 {
-                return currentValue.1
+            let (shouldRetry, result) = await value()
+            if shouldRetry {
+                return result
             } else {
-                lastValue = currentValue.1
+                lastValue = result
                 try? await Task.sleep(nanoseconds: UInt64(pollInterval.nanoseconds))
             }
         } while !(retries > maximumRetries)
