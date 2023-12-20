8a8a4abb8116e2ac67e410ef5e81c0354aa7733a
Mark Villacampa <m@markvillacampa.com>
2023-12-20 11:31:21 +0100

    Set explicit int values for StoreKitVersion enum

--- Sources/Misc/StoreKitVersion.swift
+++ Sources/Misc/StoreKitVersion.swift
@@ -18,13 +18,14 @@ import Foundation
 public enum StoreKitVersion: Int {
 
     /// Always use StoreKit 1. 
-    case storeKit1
+    case storeKit1 = 1
 
     /// Always use StoreKit 2 (StoreKit 1 will be used if StoreKit 2 is not available in the current device.)
     ///
     /// Make sure you have an In-App Purchase Key configured in your app.
     /// Please see https://rev.cat/in-app-purchase-key-configuration for more info.
-    case storeKit2
+    case storeKit2 = 2
+
 }
 
 public extension StoreKitVersion {
