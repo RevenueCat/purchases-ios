236b90595543653e0e959202a6f5ee78d38d8d96
Mark Villacampa <m@markvillacampa.com>
2024-01-02 17:30:53 +0100

    Improve sandbox detector for macOS apps

--- Sources/Misc/SandboxEnvironmentDetector.swift
+++ Sources/Misc/SandboxEnvironmentDetector.swift
@@ -40,14 +40,51 @@ final class BundleSandboxEnvironmentDetector: SandboxEnvironmentDetector {
             return false
         }
 
-        // `true` for either `macOS` or `Catalyst`
-        let isMASReceipt = path.contains("MASReceipt/receipt")
-        if isMASReceipt {
-            return path.contains("Xcode/DerivedData")
-        } else {
+        #if os(macOS) || targetEnvironment(macCatalyst)
+            return !isMacAppStore
+        #else
             return path.contains("sandboxReceipt")
-        }
+        #endif
     }
+
+    /// Returns whether the bundle was signed for Mac App Store distribution by checking
+    /// the existence of a specific extension (marker OID) on the code signing certificate.
+    ///
+    /// This routine is inspired by the source code from ProcInfo, the underlying library
+    /// of the WhatsYourSign code signature checking tool developed by Objective-See. Initially,
+    /// it checked the common name but was changed to an extension check to make it more
+    /// future-proof.
+    ///
+    /// For more information, see the following references:
+    /// - https://github.com/objective-see/ProcInfo/blob/master/procInfo/Signing.m#L184-L247
+    /// - https://gist.github.com/lukaskubanek/cbfcab29c0c93e0e9e0a16ab09586996#gistcomment-3993808
+    #if os(macOS) || targetEnvironment(macCatalyst)
+    var isMacAppStore: Bool {
+        var status = noErr
+
+        var code: SecStaticCode?
+        status = SecStaticCodeCreateWithPath(Bundle.main.bundleURL as CFURL, [], &code)
+
+        guard status == noErr, let code = code else { return false }
+
+        var requirement: SecRequirement?
+        status = SecRequirementCreateWithString(
+            "anchor apple generic and certificate leaf[field.1.2.840.113635.100.6.1.9]" as CFString,
+            [], // default
+            &requirement
+        )
+
+        guard status == noErr, let requirement = requirement else { return false }
+
+        status = SecStaticCodeCheckValidity(
+            code,
+            [], // default
+            requirement
+        )
+
+        return status == errSecSuccess
+    }
+    #endif
 
     #if DEBUG
     // Mutable in tests so it can be overriden
