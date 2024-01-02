990f2639c1fe818588beb3585a1d8dbb33ad99e4
Andy Boedo <andresboedo@gmail.com>
2024-01-02 16:31:45 -0300

    remove useless change

--- RevenueCatUI/UIKit/PaywallFooterViewController.swift
+++ RevenueCatUI/UIKit/PaywallFooterViewController.swift
@@ -51,7 +51,6 @@ public final class PaywallFooterViewController: PaywallViewController {
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
 }
 
 #endif
