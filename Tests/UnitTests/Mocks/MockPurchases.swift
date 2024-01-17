7b34817af47ea696fd528937310b6b9604c56a8a
Mark Villacampa <m@markvillacampa.com>
2024-01-17 23:58:54 +0100

    fix build

--- Tests/UnitTests/Mocks/MockPurchases.swift
+++ Tests/UnitTests/Mocks/MockPurchases.swift
@@ -460,7 +460,7 @@ extension MockPurchases: PurchasesSwiftType {
 
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func handleObserverModeTransaction(
-        purchaseResult: Product.PurchaseResult
+        _ purchaseResult: Product.PurchaseResult
     ) async throws -> RevenueCat.StoreTransaction? {
         self.unimplemented()
     }
