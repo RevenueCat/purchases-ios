43652af87124d5cd00baaf60c66117b71be2f23b
Andy Boedo <andresboedo@gmail.com>
2024-01-16 21:01:36 -0300

    `PurchaseTester`: improved `ReceiptInspector` so it accepts receipts with escape sequences (#3554)
    Receipts in logs have extra escape sequences now (forward slashes), so
    this updates the ReceiptInspector tool to be able to accept them for
    easier usage.
    
    It also accepts extra `"` so that if you copied slightly incorrectly it
    still works.

--- Tests/TestingApps/PurchaseTesterSwiftUI/Shared/ReceiptInspector.swift
+++ Tests/TestingApps/PurchaseTesterSwiftUI/Shared/ReceiptInspector.swift
@@ -73,8 +73,12 @@ struct ReceiptInspectorView: View {
     func inspectReceipt() async {
         do {
             guard !encodedReceipt.isEmpty else { return }
-            parsedReceipt = try PurchasesReceiptParser.default.parse(base64String: encodedReceipt).debugDescription
-            verifyReceiptResult = await ReceiptVerifier().verifyReceipt(base64Encoded: encodedReceipt,
+            // in kibana, receipts get encoded with extra `\`s
+            let receiptWithoutForwardSlashes = encodedReceipt.replacingOccurrences(of: "\\", with: "")
+            // just in case you accidentally copied with extra double-quotations
+            let receiptWithoutQuotations = receiptWithoutForwardSlashes.replacingOccurrences(of: "\"", with: "")
+            parsedReceipt = try PurchasesReceiptParser.default.parse(base64String: receiptWithoutQuotations).debugDescription
+            verifyReceiptResult = await ReceiptVerifier().verifyReceipt(base64Encoded: receiptWithoutQuotations,
                                                                         sharedSecret: sharedSecret)
         } catch {
             parsedReceipt = "Couldn't decode receipt. Error:\n\(error)"
