RevenueCat/purchases-ios

commit b0b7afb632ba25f5fa0d33bb72ce8f954ef81fae

`Paywalls`: improve `PaywallData.config(for:)` disambiguation (#3605)

Tests/UnitTests/Paywalls/PaywallDataTests.swift
@@ -87,6 +87,21 @@ class PaywallDataTests: BaseHTTPResponseTest {
         expect(paywall.config(for: Locale(identifier: "gl_ES"))).to(beNil())
     }
 
+    func testChineseLocalizations() throws {
+        // This logic only works on iOS 16+
+        try AvailabilityChecks.iOS16APIAvailableOrSkipTest()
+
+        let paywall: PaywallData = try self.decodeFixture("PaywallData-chinese")
+
+        let traditional = try XCTUnwrap(paywall.config(for: Locale(identifier: "zh-Hant")))
+        let simplified = try XCTUnwrap(paywall.config(for: Locale(identifier: "zh-Hans")))
+        let taiwan = try XCTUnwrap(paywall.config(for: Locale(identifier: "zh-TW")))
+
+        expect(traditional.title) == "Traditional"
+        expect(simplified.title) == "Simplified"
+        expect(taiwan.title) == "Traditional"
+    }
+
     func testModifyingImages() throws {
         var paywall: PaywallData = try self.decodeFixture("PaywallData-Sample1")
         var expected = paywall.config.images

RevenueCatUI/Resources/zh_TW.lproj/Localizable.strings
@@ -1,19 +1,0 @@
-"OK" = "好的";
-"All subscriptions" = "所有訂閱";
-"Privacy" = "隱私";
-"Privacy policy" = "隱私權政策";
-"Purchases restored successfully!" = "購買成功恢復！";
-"Restore" = "恢復";
-"Restore purchases" = "恢復購買";
-"Terms" = "服務條款";
-"Terms and conditions" = "條款和條件";
-"Annual" = "每年";
-"6 Month" = "6個月";
-"3 Month" = "3個月";
-"2 Month" = "2個月";
-"Monthly" = "每月";
-"Weekly" = "每週";
-"Lifetime" = "終身";
-"%d%% off" = "優惠%d%%";
-"Continue" = "繼續";
-"Default_offer_details_with_intro_offer" = "試用期{{ sub_offer_duration }}，屆滿後{{ total_price_and_per_month }}。";

Sources/Paywalls/PaywallData.swift
@@ -524,7 +524,7 @@ private extension Locale {
 
     func sharesLanguageCode(with other: Locale) -> Bool {
         if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
-            return self.language.languageCode == other.language.languageCode
+            return self.language.isEquivalent(to: other.language)
         } else {
             return self.languageCode == other.languageCode
         }

Tests/TestingApps/PaywallsTester/PaywallsTester.xcodeproj/project.pbxproj
@@ -44,6 +44,8 @@
 		4F0B5EA02A97CD9300DB0FC9 /* de */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = de; path = de.lproj/Localizable.strings; sourceTree = "<group>"; };
 		4F0B5EA12A97CDAC00DB0FC9 /* fr */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = fr; path = fr.lproj/Localizable.strings; sourceTree = "<group>"; };
 		4F102E262A840ECC0059EED6 /* CustomPaywall.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = CustomPaywall.swift; sourceTree = "<group>"; };
+		4F1DBFEA2B6432BE00B0768F /* zh-Hant */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = "zh-Hant"; path = "zh-Hant.lproj/Localizable.strings"; sourceTree = "<group>"; };
+		4F1DBFEB2B6432C100B0768F /* zh-Hans */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = "zh-Hans"; path = "zh-Hans.lproj/Localizable.strings"; sourceTree = "<group>"; };
 		4F34FF622A60AD9A00AADF11 /* AppContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppContentView.swift; sourceTree = "<group>"; };
 		4F34FF642A60ADBD00AADF11 /* Configuration.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Configuration.swift; sourceTree = "<group>"; };
 		4F4557E32A6FFE6A00160521 /* en */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = en; path = en.lproj/Localizable.strings; sourceTree = "<group>"; };
@@ -204,6 +206,8 @@
 				de,
 				fr,
 				ar,
+				"zh-Hant",
+				"zh-Hans",
 			);
 			mainGroup = 4F6BED912A26A64200CD9322;
 			productRefGroup = 4F6BED9B2A26A64200CD9322 /* Products */;
@@ -257,6 +261,8 @@
 				4F0B5EA02A97CD9300DB0FC9 /* de */,
 				4F0B5EA12A97CDAC00DB0FC9 /* fr */,
 				4FFD2A602AA154B4001F4B0C /* ar */,
+				4F1DBFEA2B6432BE00B0768F /* zh-Hant */,
+				4F1DBFEB2B6432C100B0768F /* zh-Hans */,
 			);
 			name = Localizable.strings;
 			sourceTree = "<group>";

Tests/TestingApps/PaywallsTester/PaywallsTester/zh-Hans.lproj/Localizable.strings
@@ -1,0 +1,1 @@
+

Tests/TestingApps/PaywallsTester/PaywallsTester/zh-Hant.lproj/Localizable.strings
@@ -1,0 +1,1 @@
+

commit cf138e904af5be05dd99520ceadcccc05d0a2577

`Paywalls`: fix finding locales with different regions
This was a regression introduced by #3605.

### Example:

Consider a paywall configuration with these locales: `en_US`, `de_DE`.
Running on a device with locale `en_IN` for example.

Before #3605, `PaywallData.localizedConfiguration` would have returned the `en_US` localization. However, that change made it so that it could no longer find a matching localization with a different region.'

This fix improves the locale lookup so that if it doesn't find a matching localization with both language AND region, it then tries to look it up with only the language, before falling back to the default localization.

Sources/FoundationExtensions/Locale+Extensions.swift
@@ -29,4 +29,23 @@ extension Locale {
         #endif
     }
 
+    // swiftlint:disable:next identifier_name
+    var rc_languageCode: String? {
+        #if swift(>=5.9)
+        // `Locale.languageCode` is deprecated
+        if #available(macOS 13, iOS 16, tvOS 16, watchOS 9, visionOS 1.0, *) {
+            return self.language.languageCode?.identifier
+        } else {
+            return self.languageCode
+        }
+        #else
+        return self.languageCode
+        #endif
+    }
+
+    /// - Returns: the same locale as `self` but removing its region.
+    var removingRegion: Self? {
+        return self.rc_languageCode.map(Locale.init(identifier:))
+    }
+
 }

Sources/Paywalls/PaywallData+Localization.swift
@@ -18,13 +18,29 @@ public extension PaywallData {
     /// - Returns: the ``PaywallData/LocalizedConfiguration-swift.struct``  to be used
     /// based on `Locale.current` or `Locale.preferredLocales`.
     var localizedConfiguration: LocalizedConfiguration {
-        let locales: [Locale] = [.current] + Locale.preferredLocales
+        return self.localizedConfiguration(for: Self.localesOrderedByPriority)
+    }
 
+    // Visible for testing
+    internal func localizedConfiguration(for locales: [Locale]) -> LocalizedConfiguration {
         return locales
             .lazy
             .compactMap(self.config(for:))
             .first { _ in true } // See https://github.com/apple/swift/issues/55374
             ?? self.fallbackLocalizedConfiguration
+    }
+
+    // Visible for testing
+    /// - Returns: The list of locales that paywalls should try to search for.
+    /// Includes `Locale.current` and `Locale.preferredLanguages`.
+    internal static var localesOrderedByPriority: [Locale] {
+        var result = [.current] + Locale.preferredLocales
+
+        if let withoutRegion = Locale.current.removingRegion {
+            result.append(withoutRegion)
+        }
+
+        return result
     }
 
     private var fallbackLocalizedConfiguration: LocalizedConfiguration {
@@ -40,9 +56,9 @@ public extension PaywallData {
 
 // MARK: -
 
-private extension Locale {
+extension Locale {
 
-    static var preferredLocales: [Self] {
+    fileprivate static var preferredLocales: [Self] {
         return Self.preferredLanguages.map(Locale.init(identifier:))
     }
 

Tests/UnitTests/FoundationExtensions/LocaleExtensionsTests.swift
@@ -22,8 +22,26 @@ class LocaleExtensionsTests: TestCase {
         expect(Locale(identifier: "en_US").rc_currencyCode) == "USD"
     }
 
-    func testMissingCurrenctCode() {
+    func testMissingCurrencyCode() {
         expect(Locale(identifier: "").rc_currencyCode).to(beNil())
+    }
+
+    func testLanguageCodeCode() {
+        expect(Locale(identifier: "en_US").rc_languageCode) == "en"
+        expect(Locale(identifier: "en-IN").rc_languageCode) == "en"
+        expect(Locale(identifier: "en").rc_languageCode) == "en"
+    }
+
+    func testMissingLanguageCode() {
+        expect(Locale(identifier: "").rc_languageCode).to(beNil())
+    }
+
+    func testRemovingRegion() {
+        expect(Locale(identifier: "en_US").removingRegion?.identifier) == "en"
+        expect(Locale(identifier: "en-IN").removingRegion?.identifier) == "en"
+        expect(Locale(identifier: "en_ES").removingRegion?.identifier) == "en"
+        expect(Locale(identifier: "en").removingRegion?.identifier) == "en"
+        expect(Locale(identifier: "").removingRegion).to(beNil())
     }
 
 }

Tests/UnitTests/Paywalls/PaywallDataTests.swift
@@ -122,6 +122,25 @@ class PaywallDataTests: BaseHTTPResponseTest {
         expect(esConfig.title) == "Tienda"
     }
 
+    func testLocalizedConfigurationFallsBackToLanguageWithDifferentRegion() throws {
+        let paywall: PaywallData = try self.decodeFixture("PaywallData-Sample1")
+
+        let enConfig = try XCTUnwrap(paywall.localizedConfiguration(for: [
+            .init(identifier: "en_IN"),
+            .init(identifier: "en-IN"),
+            .init(identifier: "en-IN").removingRegion
+        ].compactMap { $0 }))
+        expect(enConfig.title) == "Paywall"
+    }
+
+    func testLocalesOrderedByPriority() {
+        expect(PaywallData.localesOrderedByPriority) == [
+            .init(identifier: "en_US"),
+            .init(identifier: "en-US"),
+            .init(identifier: "en")
+        ]
+    }
+
     func testDoesNotFindLocaleWithMissingLanguage() throws {
         let paywall: PaywallData = try self.decodeFixture("PaywallData-Sample1")
 