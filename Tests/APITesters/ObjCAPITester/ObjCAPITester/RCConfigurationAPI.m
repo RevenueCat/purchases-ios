8b1d350b9e5bee673c08eed06b94da8cfce83a4b
NachoSoto <ignaciosoto90@gmail.com>
2024-02-07 11:47:13 -0800

    Fix API tester

--- Tests/APITesters/ObjCAPITester/ObjCAPITester/RCConfigurationAPI.m
+++ Tests/APITesters/ObjCAPITester/ObjCAPITester/RCConfigurationAPI.m
@@ -14,7 +14,7 @@
 + (void)checkAPI {
     RCConfigurationBuilder *builder = [RCConfiguration builderWithAPIKey:@""];
     RCConfiguration *config __unused = [[[[[[[[[[[[[builder withApiKey:@""]
-                                                   withObserverMode:false storeKitVersion:RCStoreKitVersionStoreKit2]
+                                                   withObserverMode:false storeKitVersion:RCStoreKitVersion2]
                                                   withUserDefaults:NSUserDefaults.standardUserDefaults]
                                                  withAppUserID:@""]
                                                 withAppUserID:nil]
@@ -23,7 +23,7 @@
                                              withStoreKit1Timeout: 1]
                                             withPlatformInfo:[[RCPlatformInfo alloc] initWithFlavor:@"" version:@""]]
                                            withUsesStoreKit2IfAvailable:false]
-                                          withStoreKitVersion:RCStoreKitVersionStoreKit2]
+                                          withStoreKitVersion:RCStoreKitVersion2]
                                          withEntitlementVerificationMode:RCEntitlementVerificationModeInformational]
                                         build];
 }
