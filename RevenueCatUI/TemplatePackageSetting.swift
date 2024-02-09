641599fe04498add975c87d26a2dc592509e1284
NachoSoto <NachoSoto@users.noreply.github.com>
2024-02-09 11:32:25 -0800

    `Paywalls`: extract `TemplatePackageSetting` and add `TemplateTierSetting` (#3663)
    This will be used for multi-tier paywalls.

---
+++ RevenueCatUI/TemplatePackageSetting.swift
@@ -0,0 +1,49 @@
+//
+//  TemplatePackageSetting.swift
+//
+//
+//  Created by Nacho Soto on 2/8/24.
+//
+
+import Foundation
+
+/// Whether a template displays 1 or multiple packages.
+enum TemplatePackageSetting: Equatable {
+
+    case single
+    case multiple
+
+}
+
+/// Whether a template displays 1 or multiple tiers.
+enum TemplateTierSetting: Equatable {
+
+    /// Single-tier template.
+    case single
+
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension PaywallTemplate {
+
+    var packageSetting: TemplatePackageSetting {
+        switch self {
+        case .template1: return .single
+        case .template2: return .multiple
+        case .template3: return .single
+        case .template4: return .multiple
+        case .template5: return .multiple
+        }
+    }
+
+}
+
+extension TemplatePackageSetting {
+
+    var tierSetting: TemplateTierSetting {
+        switch self {
+        case .single, .multiple: return .single
+        }
+    }
+
+}
