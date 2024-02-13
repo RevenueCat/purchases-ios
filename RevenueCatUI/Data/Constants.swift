fd6971a88ad162c6f9735556069c0a236f00d87d
NachoSoto <NachoSoto@users.noreply.github.com>
2024-02-13 14:49:09 -0800

    `Paywalls`: initial `Template7View` implementation (#3676)
    ![simulator_screenshot_23BBFF48-D097-48FE-ABD2-E3768F243D1B](https://github.com/RevenueCat/purchases-ios/assets/685609/5a940007-b1ce-4821-a84c-39734c7b7939)

--- RevenueCatUI/Data/Constants.swift
+++ RevenueCatUI/Data/Constants.swift
@@ -31,6 +31,8 @@ enum Constants {
     static let defaultPackageCornerRadius: CGFloat = 16
     static let defaultPackageBorderWidth: CGFloat = 2
 
+    static let defaultPackageVerticalSpacing: CGFloat = 16
+
     static let purchaseInProgressButtonOpacity: CGFloat = 0.4
 
     /// For UI elements that wouldn't make sense to keep scaling up forever
