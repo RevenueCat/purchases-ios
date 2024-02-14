d25ca7e2cbbf9444b9b5428f3e2084d8ec3caa39
Josh Holtz <me@joshholtz.com>
2024-02-14 16:03:26 -0600

    New `Package.presentedOfferingContext` (#3690)
    ### Motivation
    
    Allow for storing of other presented offering context on a `Package`
    
    ### Description
    
    - Replaced `offeringIdentifier: String` on `Package` with new
    `presentedOfferingContext: PresentedOfferingContext`
      - Currently it only has a `offeringIdentifier: String`
    - Eventually this will add new optional properties for targeting and
    placements

---
+++ Tests/APITesters/ObjCAPITester/ObjCAPITester/RCPresentedOfferingContext.m
@@ -0,0 +1,22 @@
+//
+//  RCPresentedOfferingContext.m
+//  ObjCAPITester
+//
+//  Created by Josh Holtz on 2/14/24.
+//
+
+#import "RCPresentedOfferingContext.h"
+
+@import StoreKit;
+@import RevenueCat;
+
+@implementation RCPresentOfferingContextAPI
+
++ (void)checkAPI {
+    RCPresentedOfferingContext *poc = [[RCPresentedOfferingContext alloc] initWithOfferingIdentifier:@""];
+    NSString *oid = poc.offeringIdentifier;
+
+    NSLog(poc, oid);
+}
+
+@end
