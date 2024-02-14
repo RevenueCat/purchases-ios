7ea436dc33d35986f5ed9e0a576ffa64b348d679
NachoSoto <NachoSoto@users.noreply.github.com>
2024-02-14 12:00:38 -0800

    `Paywalls`: template 7 allows color overrides by tier (#3686)
    Depends on #3685.
    
    
    https://github.com/RevenueCat/purchases-ios/assets/685609/f41addda-f893-425f-b1bf-c16fcdcf6fa4

--- RevenueCatUI/Templates/TemplateViewType.swift
+++ RevenueCatUI/Templates/TemplateViewType.swift
@@ -114,6 +114,7 @@ extension PaywallData {
                                       locale: locale),
                 configuration: self.config,
                 colors: self.config.colors.multiScheme,
+                colorsByTier: self.config.multiSchemeColorsByTier,
                 fonts: fonts,
                 assetBaseURL: self.assetBaseURL
             )
