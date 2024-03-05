50cbd8ff20b3853d2dd8a2b2658b293e1c32697e
Josh Holtz <me@joshholtz.com>
2024-03-05 17:16:00 -0600

    Revert "Revert "`Xcode 15.3`: fix warnings on Integration Tests" (#3744)"
    This reverts commit a98d86e32ef6446b95c4bad9f13c068ad6860e13.

--- Tests/BackendIntegrationTests/BaseBackendIntegrationTests.swift
+++ Tests/BackendIntegrationTests/BaseBackendIntegrationTests.swift
@@ -35,7 +35,6 @@ final class TestPurchaseDelegate: NSObject, PurchasesDelegate, Sendable {
 
 }
 
-@MainActor
 class BaseBackendIntegrationTests: TestCase {
 
     private var userDefaults: UserDefaults!
@@ -87,7 +86,6 @@ class BaseBackendIntegrationTests: TestCase {
 
     // MARK: -
 
-    @MainActor
     override func setUp() async throws {
         try await super.setUp()
 
@@ -247,3 +245,6 @@ extension BaseBackendIntegrationTests: InternalDangerousSettingsType {
     final func serverUp() { self.serverIsDown = false }
 
 }
+
+// `InternalDangerousSettingsType` requires this type to be Sendable
+extension BaseBackendIntegrationTests: @unchecked Sendable {}
