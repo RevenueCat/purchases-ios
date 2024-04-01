1a3b79b4b08c81ed3cfa75f291cb22bfa6ef3dc5
Cesar de la Vega <cesarvegaro@gmail.com>
2024-04-01 18:50:10 +0200

    create DiagnosticsEntry

---
+++ Sources/Diagnostics/DiagnosticsEntry.swift
@@ -0,0 +1,70 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  DiagnosticsEntry.swift
+//
+//  Created by Cesar de la Vega on 1/4/24.
+
+import Foundation
+
+typealias DiagnosticsEntries = [String]
+
+protocol DiagnosticsEntry: Codable {
+    var diagnosticType: String { get }
+    var version: Int { get }
+}
+
+extension DiagnosticsEntry {
+    var version: Int { 1 }
+
+    func toJSONString() -> String? {
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        encoder.dateEncodingStrategy = .millisecondsSince1970 // Example date encoding strategy
+
+        guard let jsonData = try? encoder.encode(self) else { return nil }
+        return String(data: jsonData, encoding: .utf8)
+    }
+}
+
+struct DiagnosticsEvent: DiagnosticsEntry {
+    var diagnosticType: String = "event"
+    let name: String
+    let properties: [String: AnyEncodable]
+    let timestamp: Date
+
+    enum CodingKeys: String, CodingKey {
+        case diagnosticType = "type"
+        case name, properties, timestamp
+    }
+}
+
+struct Counter: DiagnosticsEntry {
+    var diagnosticType: String = "counter"
+    let name: String
+    let tags: [String: String]
+    let value: Int
+
+    enum CodingKeys: String, CodingKey {
+        case diagnosticType = "type"
+        case name, tags, value
+    }
+}
+
+struct Histogram: DiagnosticsEntry {
+    var diagnosticType: String = "histogram"
+    let name: String
+    let tags: [String: String]
+    let values: [Double]
+
+    enum CodingKeys: String, CodingKey {
+        case diagnosticType = "type"
+        case name, tags, values
+    }
+}
