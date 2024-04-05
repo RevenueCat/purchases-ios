e5e28b65e84b5bb7c9bb8f9eab22b598ee0b0af5
Josh Holtz <me@joshholtz.com>
2024-04-05 14:50:49 -0500

    Maybe this

--- Tests/UnitTests/Purchasing/Purchases/PurchasesSyncAttributesAndOfferingsIfNeededTests.swift
+++ Tests/UnitTests/Purchasing/Purchases/PurchasesSyncAttributesAndOfferingsIfNeededTests.swift
@@ -38,7 +38,7 @@ class PurchasesSyncAttributesAndOfferingsTests: BasePurchasesTests {
 }
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
-extension PurchasesSyncAttributesAndOfferingsTests {
+class PurchasesSyncAttributesAndOfferingsAsyncTests: BasePurchasesTests {
 
     func testAttributesSyncedAndOfferingsFetchedAsync() async throws {
         self.setupPurchases()
