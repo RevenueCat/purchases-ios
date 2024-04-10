30482f37d04fafa1f6cda34346b217f1c82b61d9
Cesar de la Vega <cesarvegaro@gmail.com>
2024-04-10 12:41:00 +0200

    Create DiagnosticsPostOperation

---
+++ Sources/Diagnostics/Networking/DiagnosticsPostOperation.swift
@@ -0,0 +1,70 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  DiagnosticsPostOperation.swift
+//
+//  Created by Cesar de la Vega on 8/4/24.
+
+import Foundation
+
+typealias DiagnosticsEntries = [String]
+
+final class DiagnosticsPostOperation: NetworkOperation {
+
+    private let configuration: BackendConfiguration
+    private let entries: DiagnosticsEntries
+    private let responseHandler: DiagnosticsAPI.ResponseHandler
+
+    init(
+        configuration: BackendConfiguration,
+        entries: DiagnosticsEntries,
+        responseHandler: @escaping DiagnosticsAPI.ResponseHandler
+    ) {
+        self.configuration = configuration
+        self.entries = entries
+        self.responseHandler = responseHandler
+
+        super.init(configuration: configuration)
+    }
+
+    override func begin(completion: @escaping () -> Void) {
+        self.postDiagnostics(completion: completion)
+    }
+
+    private func postDiagnostics(completion: @escaping () -> Void) {
+        let request = HTTPRequest(method: .post(Body(entries: self.entries)),
+                                  path: .postDiagnostics)
+
+        self.httpClient.perform(request) { (response: VerifiedHTTPResponse<HTTPEmptyResponseBody>.Result) in
+            defer {
+                completion()
+            }
+            self.responseHandler(
+                response
+                    .map { _ in () }
+                    .mapError(BackendError.networkError)
+            )
+        }
+    }
+
+}
+
+extension DiagnosticsPostOperation {
+
+    struct Body: Encodable, HTTPRequestBody {
+
+        let entries: DiagnosticsEntries
+
+        init(entries: DiagnosticsEntries) {
+            self.entries = entries
+        }
+
+    }
+
+}
