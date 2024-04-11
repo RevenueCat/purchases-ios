f813e3217bc648dafee1a8a4b5efc7e5a7a62cce
Cesar de la Vega <cesarvegaro@gmail.com>
2024-04-11 17:12:04 +0200

    Create DiagnosticsPostOperation (#3795)
    Add DiagnosticsPostOperation and DiagnosticsAPI

---
+++ Sources/Diagnostics/Networking/DiagnosticsEventsRequest.swift
@@ -0,0 +1,71 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  DiagnosticsEventsRequest.swift
+//
+//  Created by Cesar de la Vega on 11/4/24.
+
+import Foundation
+
+/// The content of a request to the events endpoints.
+struct DiagnosticsEventsRequest {
+
+    var entries: [Event]
+
+    init(events: [Event]) {
+        self.entries = events
+    }
+
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    init(events: [DiagnosticsEvent]) {
+        self.init(events: events.map { .init(event: $0) })
+    }
+
+}
+
+extension DiagnosticsEventsRequest {
+
+    struct Event {
+
+        let version: Int
+        let name: String
+        let properties: [String: AnyEncodable]
+        let timestamp: String
+
+    }
+
+}
+
+extension DiagnosticsEventsRequest.Event {
+
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    init(event: DiagnosticsEvent) {
+        self.init(
+            version: event.version,
+            name: event.name,
+            properties: event.properties,
+            timestamp: event.timestamp.ISO8601Format()
+        )
+    }
+
+}
+
+// MARK: - Codable
+
+extension DiagnosticsEventsRequest.Event: Encodable {
+
+    enum CodingKeys: String, CodingKey {
+
+        case version, name, properties, timestamp
+
+    }
+
+}
+
+extension DiagnosticsEventsRequest: HTTPRequestBody {}
---
+++ Sources/Diagnostics/Networking/DiagnosticsPostOperation.swift
@@ -0,0 +1,46 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  DiagnosticsPostOperation.swift
+//
+//  Created by Cesar de la Vega on 8/4/24.
+
+import Foundation
+
+final class DiagnosticsPostOperation: NetworkOperation {
+
+    private let configuration: Configuration
+    private let request: DiagnosticsEventsRequest
+    private let responseHandler: CustomerAPI.SimpleResponseHandler?
+
+    init(
+        configuration: Configuration,
+        request: DiagnosticsEventsRequest,
+        responseHandler: CustomerAPI.SimpleResponseHandler?
+    ) {
+        self.configuration = configuration
+        self.request = request
+        self.responseHandler = responseHandler
+
+        super.init(configuration: configuration)
+    }
+
+    override func begin(completion: @escaping () -> Void) {
+        let request = HTTPRequest(method: .post(self.request), path: .postDiagnostics)
+
+        self.httpClient.perform(request) { (response: VerifiedHTTPResponse<HTTPEmptyResponseBody>.Result) in
+            defer {
+                completion()
+            }
+
+            self.responseHandler?(response.error.map(BackendError.networkError))
+        }
+    }
+
+}
