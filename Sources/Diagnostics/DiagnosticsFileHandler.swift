61e47536071238755856f440f14238335cc8842f
Cesar de la Vega <cesarvegaro@gmail.com>
2024-04-11 18:25:02 +0200

    remove AnyObject

--- Sources/Diagnostics/DiagnosticsFileHandler.swift
+++ Sources/Diagnostics/DiagnosticsFileHandler.swift
@@ -14,7 +14,7 @@
 import Foundation
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-protocol DiagnosticsFileHandlerType: AnyObject, Sendable {
+protocol DiagnosticsFileHandlerType: Sendable {
 
     func getEntries() async -> [DiagnosticsEvent]
 
