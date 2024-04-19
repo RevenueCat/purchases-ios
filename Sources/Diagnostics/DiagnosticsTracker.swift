c63cb9b30257a540ebfb7a052d1762c9891e5c21
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-04-19 16:56:41 +0200

    Fix swiftlint

--- Sources/Diagnostics/DiagnosticsTracker.swift
+++ Sources/Diagnostics/DiagnosticsTracker.swift
@@ -14,7 +14,7 @@
 import Foundation
 
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, watchOS 8.0, *)
-protocol DiagnosticsTrackerType : Sendable {
+protocol DiagnosticsTrackerType: Sendable {
 
     func track(_ event: DiagnosticsEvent) async
 
