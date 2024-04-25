6d0ae9673d20e7085b5fe9d2a78eddd9a62a3a23
James Borthwick <borthwick.james@gmail.com>
2024-04-25 16:04:21 -0700

    Universal link code functional

--- Tests/TestingApps/PaywallsTester/PaywallsTester/SimpleApp.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/SimpleApp.swift
@@ -9,27 +9,45 @@ import SwiftUI
 
 @main
 struct SimpleApp: App {
-    
+
+    struct IdentifiableString: Identifiable {
+        let id: String
+    }
+
     @State
     private var application = ApplicationData()
 
     @State
-    private var universalLinkShowing = false
+    private var paywallIDToShow: IdentifiableString?
+
+    func getPaywallIdFrom(incomingURL: URL) -> IdentifiableString? {
+        guard let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
+            return nil
+        }
+
+        guard let params = components.queryItems else { return nil }
+
+        guard let paywallID = params.first(where: { $0.name == "paywall" } )?.value else {
+            return nil
+        }
+
+        return IdentifiableString(id: paywallID)
+    }
 
     var body: some Scene {
         WindowGroup {
             AppContentView()
-                .onOpenURL { URL in
-
-                }
-                .sheet(isPresented: $universalLinkShowing) {
+                .sheet(item: $paywallIDToShow) { paywallID in
                     LoginWall { response in
-                        PaywallForID(apps: response.apps, id: "ofrng71bdfc2037") //ofrngb173f388db
+                        PaywallForID(apps: response.apps, id: paywallID.id)
                     }
                 }
+                .onOpenURL { URL in
+                    paywallIDToShow = getPaywallIdFrom(incomingURL: URL)
+                }
                 .task {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                        universalLinkShowing = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) { //ofrngb173f388db
+                        paywallIDToShow = getPaywallIdFrom(incomingURL: URL(string: "https://app.revenuecat.com/?paywall=ofrng71bdfc2037")!)
                     }
                 }
         }
