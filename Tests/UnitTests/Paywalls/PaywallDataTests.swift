c8cca5d2185b0ddb16a1c2795cfaaf30e5cf2a23
Cesar de la Vega <cesarvegaro@gmail.com>
2024-05-06 19:59:35 +0200

    fix available versions

--- Tests/UnitTests/Paywalls/PaywallDataTests.swift
+++ Tests/UnitTests/Paywalls/PaywallDataTests.swift
@@ -145,7 +145,7 @@ class PaywallDataTests: BaseHTTPResponseTest {
     func testLocalesOrderedByPriority() throws {
         let expected: [String]
 
-        if #available(iOS 17.0, tvOS 17, watchOS 10, *) {
+        if #available(iOS 17.0, tvOS 17.0, watchOS 10.0, *) {
             expected = [
                 "en_US",
                 "en-US"
