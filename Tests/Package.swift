fac5384079e316144f89a9b014bfe5afce369cfd
Andy Boedo <andresboedo@gmail.com>
2024-05-08 17:57:20 -0300

    added Package.swift file to ensure that external apps don't pick up test files

---
+++ Tests/Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.5
+//
+//  Package.swift
+//
+//
+//  Created by Andrés Boedo on 5/7/24.
+//
+// This empty file prevents Xcode from picking up the files in the Tests library
+// when importing through SPM. This is helpful so that users of the SDK
+// don't inadvertently get StoreKit Configuration files added to the project, which
+// can lead to confusion.
+
+import PackageDescription
+
+let package = Package()
