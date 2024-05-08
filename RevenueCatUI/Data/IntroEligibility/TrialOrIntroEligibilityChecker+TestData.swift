896fa1d0dd609a94e9cfb09754832fa36ce49cc0
James Borthwick <borthwick.james@gmail.com>
2024-05-08 13:23:29 -0700

    Mark items with `//@PublicForExternalTesting`

--- RevenueCatUI/Data/IntroEligibility/TrialOrIntroEligibilityChecker+TestData.swift
+++ RevenueCatUI/Data/IntroEligibility/TrialOrIntroEligibilityChecker+TestData.swift
@@ -14,12 +14,12 @@
 import Foundation
 import RevenueCat
 
-#if DEBUG
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, *)
 extension TrialOrIntroEligibilityChecker {
 
     /// Creates a mock `TrialOrIntroEligibilityChecker` with a constant result.
+    //@PublicForExternalTesting
     static func producing(eligibility: @autoclosure @escaping () -> IntroEligibilityStatus) -> Self {
         return .init { packages in
             return Dictionary(
@@ -34,7 +34,7 @@ extension TrialOrIntroEligibilityChecker {
             )
         }
     }
-
+#if DEBUG
     /// Creates a copy of this `TrialOrIntroEligibilityChecker` with a delay.
     func with(delay seconds: TimeInterval) -> Self {
         return .init { [checker = self.checker] in
@@ -43,7 +43,7 @@ extension TrialOrIntroEligibilityChecker {
             return await checker($0)
         }
     }
+#endif
 
 }
 
-#endif
--- RevenueCatUI/PaywallView.swift
+++ RevenueCatUI/PaywallView.swift
@@ -91,6 +91,7 @@ public struct PaywallView: View {
         )
     }
 
+    //@PublicForExternalTesting
     init(configuration: PaywallViewConfiguration) {
         self._introEligibility = .init(wrappedValue: configuration.introEligibility ?? .default())
         self._purchaseHandler = .init(wrappedValue: configuration.purchaseHandler ?? .default())
