6332aa73906df486cddf3731485fe225192536ae
Andy Boedo <andresboedo@gmail.com>
2024-05-10 17:48:28 -0300

    fixed video

--- RevenueCatUI/Views/RemoteVideo.swift
+++ RevenueCatUI/Views/RemoteVideo.swift
@@ -11,14 +11,38 @@ import AVKit
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 struct RemoteVideo: View {
-    let videoURL: URL
-
+    var videoURL: URL
+    @State private var player = AVPlayer()
+    
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: videoURL))
+        VideoPlayer(player: player)
             .onAppear {
-                // Optionally, start playing the video automatically
-                AVPlayer(url: videoURL).play()
+                prepareVideoPlayer()
             }
             .edgesIgnoringSafeArea(.all)
+    }
+    
+    private func prepareVideoPlayer() {
+        let asset = AVURLAsset(url: videoURL)
+        let keys = ["playable", "hasProtectedContent", "preferredTransform"]
+        
+        asset.loadValuesAsynchronously(forKeys: keys) {
+            var error: NSError?
+            for key in keys {
+                let status = asset.statusOfValue(forKey: key, error: &error)
+                if status == .failed {
+                    print("Error loading \(key): \(String(describing: error))")
+                    return
+                }
+            }
+            
+            // Check if the asset is playable
+            if asset.isPlayable {
+                DispatchQueue.main.async {
+                    self.player = AVPlayer(playerItem: AVPlayerItem(asset: asset))
+                    self.player.play()  // Autoplay once the player is set up
+                }
+            }
+        }
     }
 }
