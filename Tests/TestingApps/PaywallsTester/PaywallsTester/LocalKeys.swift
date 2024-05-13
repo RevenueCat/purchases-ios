baaa24ad8ca39781a4606f19a58283ba35d8d0be
James Borthwick <borthwick.james@gmail.com>
2024-05-13 15:36:13 -0700

    Update instructions

--- Tests/TestingApps/PaywallsTester/PaywallsTester/LocalKeys.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/LocalKeys.swift
@@ -8,9 +8,15 @@
 import Foundation
 
 // Add this file to .gitignore and modify to add keys, eg
+// This file is used to store keys for local development
+// DO NOT CHECK IN CHANGES TO THIS FILE
+// Mark this file --skip-worktree:
+// git update-index --skip-worktree LocalKeys.swift
+//
+// Then add local keys:
 // extension Keys {
 //     static var api: String {
-//         "foo"
+//         "appl_FOObar"
 //     }
 // }
  extension Keys {
