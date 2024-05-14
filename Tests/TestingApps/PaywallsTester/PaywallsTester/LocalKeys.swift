2a528473bc51922dfd18f30047370cadb94b0cce
James Borthwick <borthwick.james@gmail.com>
2024-05-14 15:22:09 -0700

    Update instructions

--- Tests/TestingApps/PaywallsTester/PaywallsTester/LocalKeys.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/LocalKeys.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 // This file is used to store keys for local development
+//
 // DO NOT CHECK IN CHANGES TO THIS FILE
-// Mark this file --skip-worktree:
+//
+// To help prevent mistakes, mark this file inn your working directory sith --skip-worktree:
 // git update-index --skip-worktree LocalKeys.swift
+// OR assume unchanged:
+// git update-index --assume-unchanged LocalKeys.swift
+//
+// You will need to unset these when switching branches
+// git update-index --no-skip-worktree LocalKeys.swift
+// git update-index --no-assume-unchanged LocalKeys.swift
 //
 // Then add local keys:
 // extension Keys {
