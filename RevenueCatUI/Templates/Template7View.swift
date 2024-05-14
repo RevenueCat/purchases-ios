6e2c0579be2cf38b9ce0dfb414f6e3574e4cac5c
Josh Holtz <me@joshholtz.com>
2024-05-14 16:35:54 -0500

    Fixed empty badge

--- RevenueCatUI/Templates/Template7View.swift
+++ RevenueCatUI/Templates/Template7View.swift
@@ -366,7 +366,7 @@ struct Template7View: TemplateViewType {
         _ package: TemplateViewConfiguration.Package,
         selected: Bool
     ) -> some View {
-        if let badge = package.localization.offerBadge {
+        if let badge = package.localization.offerBadge, !badge.isEmpty {
             let colors = self.currentColors
 
             Text(badge)
