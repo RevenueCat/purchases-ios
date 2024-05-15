888541c1028ee832082825ba6dc2df208fa325db
James Borthwick <borthwick.james@gmail.com>
2024-05-15 12:04:26 -0700

    PR review items
    - Add a pre-commit hook to avoid keys being checked in
    - Change icon to red RC
    - Don’t make backup file when adding key with CI

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -39,6 +39,7 @@ verify_no_included_apikeys() {
     "${SCRIPT_DIR}/../Examples/MagicWeatherSwiftUI/Shared/Constants.swift"
     "${SCRIPT_DIR}/../Tests/TestingApps/PurchaseTesterSwiftUI/Core/Constants.swift"
     "${SCRIPT_DIR}/../Tests/TestingApps/PurchaseTester/PurchaseTester/Constants.swift"
+    "${SCRIPT_DIR}/../Tests/TestingApps/PaywallsTester/PaywallsTester/Config/LocalConfigItems.swift"
   )
   PATTERN="\"REVENUECAT_API_KEY\""
 
