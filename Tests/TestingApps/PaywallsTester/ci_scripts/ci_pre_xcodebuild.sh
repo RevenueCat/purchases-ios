86f31be35f00dd20bb43acae253ec82556773262
James Borthwick <borthwick.james@gmail.com>
2024-05-15 15:03:05 -0700

    script fix

--- Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Replacing API keys on PaywallsTester"
 
 file="$SCRIPT_DIR/../PaywallsTester/Config/ConfigItem.swift"
-sed -i 's/static var apiKey: String { "" }/static var apiKey: String { "'"$REVENUECAT_XCODE_CLOUD_RC_APP_API_KEY"'" }/' $file
+sed -i '' 's/static var apiKey: String { "" }/static var apiKey: String { "'"$REVENUECAT_XCODE_CLOUD_RC_APP_API_KEY"'" }/' $file
