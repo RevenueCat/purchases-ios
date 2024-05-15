377fcc52d260176eebd0a1a4ba90d60a37817e88
James Borthwick <borthwick.james@gmail.com>
2024-05-15 13:11:24 -0700

    Update CI script for key injection

--- Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Replacing API keys on PaywallsTester"
 
-file="$SCRIPT_DIR/../PaywallsTester/Keys.swift"
-sed -i 's/static var api: String { "" }/static var api: String { "'"$REVENUECAT_XCODE_CLOUD_RC_APP_API_KEY"'" }/' $file
+file="$SCRIPT_DIR/../PaywallsTester/Config/ConfigItem.swift"
+sed -i 's/static var apiKey: String { "" }/static var apiKey: String { "'"$REVENUECAT_XCODE_CLOUD_RC_APP_API_KEY"'" }/' $file
