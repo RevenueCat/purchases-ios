feac687cea5692dacba0161b61d06c3f42b4b5cb
Josh Holtz <me@joshholtz.com>
2024-05-17 07:52:20 -0500

    Undo Void removal

--- Sources/Networking/HTTPClient/HTTPClient.swift
+++ Sources/Networking/HTTPClient/HTTPClient.swift
@@ -487,7 +487,8 @@ private extension HTTPClient {
 
         let requestStartTime = self.dateProvider.now()
 
-        let task = self.session.dataTask(with: urlRequest) { (data, urlResponse, error) in
+        // swiftlint:disable:next redundant_void_return
+        let task = self.session.dataTask(with: urlRequest) { (data, urlResponse, error) -> Void in
             self.handle(urlResponse: urlResponse,
                         request: request,
                         urlRequest: urlRequest,
