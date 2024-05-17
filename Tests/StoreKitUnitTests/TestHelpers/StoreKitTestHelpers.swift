0552370f45c5c180ef5dd624d6cad5a2c921c6df
Josh Holtz <me@joshholtz.com>
2024-05-17 09:41:45 -0500

    Fix for tvos

--- Tests/StoreKitUnitTests/TestHelpers/StoreKitTestHelpers.swift
+++ Tests/StoreKitUnitTests/TestHelpers/StoreKitTestHelpers.swift
@@ -25,7 +25,7 @@ extension XCTestCase {
     }
 
     func setShortestTestSessionTimeRate(_ testSession: SKTestSession) {
-        if #available(iOS 16.4, *) {
+        if #available(iOS 16.4, macOS 13.3, tvOS 16.4, watchOS 9.4, *) {
             testSession.timeRate = .oneRenewalEveryTwoSeconds
         } else {
             testSession.timeRate = SKTestSession.TimeRate.monthlyRenewalEveryThirtySeconds
