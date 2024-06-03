a01061fc5faac00a74792f7ee8655d743ff87c16
Toni Rico <toni.rico.diez@revenuecat.com>
2024-06-03 17:22:49 +0200

    Diagnostics: Make setting public (#3931)
    ### Description
    This actually enables consumers to use diagnostics by making the setting
    public
    
    #### TODO
    - [ ] Write docs
    - [x] Get #3930 merged

--- Tests/APITesters/ObjCAPITester/ObjCAPITester/RCConfigurationAPI.m
+++ Tests/APITesters/ObjCAPITester/ObjCAPITester/RCConfigurationAPI.m
@@ -31,6 +31,10 @@
         RCConfiguration *config __unused = [[builder
                                              withEntitlementVerificationMode:RCEntitlementVerificationModeInformational]                                            build];
     }
+
+    if (@available(iOS 15.0, tvOS 15.0, macOS 12.0, watchOS 8.0, *)) {
+        RCConfiguration *config __unused = [[builder withDiagnosticsEnabled:true] build];
+    }
 }
 
 @end
