3c9308662cd7bb4478d20aeb5742d940aa8e5101
James Borthwick <borthwick.james@gmail.com>
2024-06-06 13:50:26 -0700

    Happy days

---
+++ RevenueCatUI/RevenueCatUI.h
@@ -0,0 +1,24 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  RevenueCatUI.h
+//
+//  Created by James Borthwick on 2024-06-06.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RevenueCatUI.
+FOUNDATION_EXPORT double RevenueCatUIVersionNumber;
+
+//! Project version string for RevenueCatUI.
+FOUNDATION_EXPORT const unsigned char RevenueCatUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RevenueCatUI/PublicHeader.h>
+
+
