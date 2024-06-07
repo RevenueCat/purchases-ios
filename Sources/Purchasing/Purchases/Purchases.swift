c905d61be777d127b050f502b816b62df5d1ef7e
James Borthwick <borthwick.james@gmail.com>
2024-06-07 16:53:45 -0700

    lint

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -1416,8 +1416,6 @@ public extension Purchases {
         )
     }
 
-
-
     @available(*, deprecated, message: """
     The appUserID passed to logIn is a constant string known at compile time.
     This is likely a programmer error. This ID is used to identify the current user.
