87ddb3765c72206622287d9395726c87a10cedb9
James Borthwick <borthwick.james@gmail.com>
2024-06-12 16:41:06 -0700

    `addPurchaseCompletedByMyAppPurchaseResult` -> `didCompletePurchaseWithResult`

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -1159,7 +1159,7 @@ public extension Purchases {
 #endif
 
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-    func addPurchaseCompletedByMyAppPurchaseResult(
+    func didCompletePurchaseWithResult(
         _ purchaseResult: StoreKit.Product.PurchaseResult
     ) async throws -> StoreTransaction? {
         guard self.systemInfo.observerMode else {
