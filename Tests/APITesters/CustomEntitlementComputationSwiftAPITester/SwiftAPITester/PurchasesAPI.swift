30da810e2c6b1365eb3408b11cb60a5bcbf42607
Will Taylor <wtaylor151@gmail.com>
2024-06-24 17:15:27 -0500

    fix tests + linting

--- Tests/APITesters/CustomEntitlementComputationSwiftAPITester/SwiftAPITester/PurchasesAPI.swift
+++ Tests/APITesters/CustomEntitlementComputationSwiftAPITester/SwiftAPITester/PurchasesAPI.swift
@@ -98,7 +98,7 @@ private func checkTypealiases(
                                                   customerInfo: customerInfo,
                                                   userCancelled: userCancelled)
 
-    // swiftlint:disable:next line_length redundant_void_return
+    // swiftlint:disable:next line_length
     let purchaseCompletedBlock: PurchaseCompletedBlock = { (_: StoreTransaction?, _: CustomerInfo?, _: Error?, _: Bool) -> Void in }
 
     let startPurchaseBlock: StartPurchaseBlock = { (_: PurchaseCompletedBlock) in }
