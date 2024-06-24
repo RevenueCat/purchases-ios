fdeb088d275264e88b9bb324b0ccce119a11a9aa
James Borthwick <borthwick.james@gmail.com>
2024-06-24 11:07:43 -0700

    Improve documentation

--- RevenueCatUI/Purchasing/PurchaseHandler.swift
+++ RevenueCatUI/Purchasing/PurchaseHandler.swift
@@ -42,7 +42,7 @@ public final class PurchaseHandler: ObservableObject {
     @Published
     fileprivate(set) var purchased: Bool = false
 
-    /// When `purchased` becomes `true`, this will include the `CustomerInfo` associated to it.
+    /// When `purchased` becomes `true`, this will include the `CustomerInfo` associated to it IF RevenueCat is making the purchase.
     @Published
     fileprivate(set) var purchaseResult: PurchaseResultData?
 
