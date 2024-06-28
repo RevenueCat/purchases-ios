df01780c9bd431796ae22fa656c07f30b88c97ae
Cesar de la Vega <cesarvegaro@gmail.com>
2024-06-28 12:16:54 +0200

    [Customer Center] Create `CustomerCenterView` (#3919)
    Base branch is `integration/customer_support_workflow` so we don't merge
    into `main` yet
    
    Borrows a lot from #3865
    
    Creates a new `CustomerCenterView` that can be used as a customer
    support workflow starting point.
    
    All details can be found in
    https://linear.app/revenuecat/project/sdk-support-workflow-cf7f6a1d5340/overview
    
    ---------
    
    Co-authored-by: Will Taylor <wtaylor151@gmail.com>
    Co-authored-by: James Borthwick <109382862+jamesrb1@users.noreply.github.com>

---
+++ RevenueCatUI/CustomerCenter/Views/WrongPlatformView.swift
@@ -0,0 +1,116 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  WrongPlatformView.swift
+//
+//
+//  Created by Andrés Boedo on 5/3/24.
+//
+
+import Foundation
+import RevenueCat
+import SwiftUI
+
+#if os(iOS)
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+@available(macOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+@available(visionOS, unavailable)
+struct WrongPlatformView: View {
+
+    @State
+    private var store: Store?
+
+    @Environment(\.openURL)
+    private var openURL
+
+    init() {
+    }
+
+    fileprivate init(store: Store) {
+        self._store = State(initialValue: store)
+    }
+
+    var body: some View {
+        VStack {
+
+            switch store {
+            case .appStore, .macAppStore, .playStore, .amazon:
+                let platformName = humanReadablePlatformName(store: store!)
+
+                Text("Your subscription is a \(platformName) subscription.")
+                    .font(.title)
+                    .padding()
+                Text("Go the app settings on \(platformName) to manage your subscription and billing.")
+                    .padding()
+            default:
+                Text("Please contact support to manage your subscription")
+                    .font(.title)
+                    .padding()
+            }
+
+        }
+        .task {
+            if store == nil {
+                if let customerInfo = try? await Purchases.shared.customerInfo(),
+                   let firstEntitlement = customerInfo.entitlements.active.first {
+                    self.store = firstEntitlement.value.store
+                }
+            }
+        }
+    }
+
+    private func humanReadablePlatformName(store: Store) -> String {
+        switch store {
+        case .appStore, .macAppStore:
+            return "Apple App Store"
+        case .playStore:
+            return "Google Play Store"
+        case .stripe,
+                .rcBilling,
+                .external:
+            return "Web"
+        case .promotional:
+            return "Free"
+        case .amazon:
+            return "Amazon Appstore"
+        case .unknownStore:
+            return "Unknown"
+        }
+    }
+
+}
+
+#if DEBUG
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+@available(macOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+@available(visionOS, unavailable)
+struct WrongPlatformView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        Group {
+            WrongPlatformView(store: .appStore)
+                .previewDisplayName("App Store")
+
+            WrongPlatformView(store: .rcBilling)
+                .previewDisplayName("RCBilling")
+        }
+
+    }
+
+}
+
+#endif
+
+#endif
---
+++ Tests/RevenueCatUITests/CustomerCenter/CustomerCenterViewModelTests.swift
@@ -0,0 +1,263 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+// CustomerCenterViewModelTests.swift
+//
+//
+//  Created by Cesar de la Vega on 11/6/24.
+//
+
+import Nimble
+import RevenueCat
+@testable import RevenueCatUI
+import XCTest
+
+#if os(iOS)
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+@available(macOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+@MainActor
+class CustomerCenterViewModelTests: TestCase {
+
+    private let error = TestError(message: "An error occurred")
+
+    private struct TestError: Error, Equatable {
+        let message: String
+        var localizedDescription: String {
+            return message
+        }
+    }
+
+    func testInitialState() {
+        let viewModel = CustomerCenterViewModel()
+
+        expect(viewModel.state) == .notLoaded
+        expect(viewModel.hasSubscriptions) == false
+        expect(viewModel.subscriptionsAreFromApple) == false
+        expect(viewModel.isLoaded) == false
+    }
+
+    func testStateChangeToError() {
+        let viewModel = CustomerCenterViewModel()
+
+        viewModel.state = .error(error)
+
+        switch viewModel.state {
+        case .error(let stateError):
+            expect(stateError as? TestError) == error
+        default:
+            fail("Expected state to be .error")
+        }
+    }
+
+    func testIsLoaded() {
+        let viewModel = CustomerCenterViewModel()
+
+        expect(viewModel.isLoaded) == false
+
+        viewModel.state = .success
+
+        expect(viewModel.isLoaded) == true
+    }
+
+    func testLoadHasSubscriptionsApple() async {
+        let viewModel = CustomerCenterViewModel(customerInfoFetcher: {
+            return CustomerCenterViewModelTests.customerInfoWithAppleSubscriptions
+        })
+
+        await viewModel.loadHasSubscriptions()
+
+        expect(viewModel.hasSubscriptions) == true
+        expect(viewModel.subscriptionsAreFromApple) == true
+        expect(viewModel.state) == .success
+    }
+
+    func testLoadHasSubscriptionsGoogle() async {
+        let viewModel = CustomerCenterViewModel(customerInfoFetcher: {
+            return CustomerCenterViewModelTests.customerInfoWithGoogleSubscriptions
+        })
+
+        await viewModel.loadHasSubscriptions()
+
+        expect(viewModel.hasSubscriptions) == true
+        expect(viewModel.subscriptionsAreFromApple) == false
+        expect(viewModel.state) == .success
+    }
+
+    func testLoadHasSubscriptionsNonActive() async {
+        let viewModel = CustomerCenterViewModel(customerInfoFetcher: {
+            return CustomerCenterViewModelTests.customerInfoWithoutSubscriptions
+        })
+
+        await viewModel.loadHasSubscriptions()
+
+        expect(viewModel.hasSubscriptions) == false
+        expect(viewModel.subscriptionsAreFromApple) == false
+        expect(viewModel.state) == .success
+    }
+
+    func testLoadHasSubscriptionsFailure() async {
+        let viewModel = CustomerCenterViewModel(customerInfoFetcher: {
+            throw TestError(message: "An error occurred")
+        })
+
+        await viewModel.loadHasSubscriptions()
+
+        expect(viewModel.hasSubscriptions) == false
+        expect(viewModel.subscriptionsAreFromApple) == false
+        switch viewModel.state {
+        case .error(let stateError):
+            expect(stateError as? TestError) == error
+        default:
+            fail("Expected state to be .error")
+        }
+    }
+
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+private extension CustomerCenterViewModelTests {
+
+    static let customerInfoWithAppleSubscriptions: CustomerInfo = {
+        return .decode(
+        """
+        {
+            "schema_version": "4",
+            "request_date": "2022-03-08T17:42:58Z",
+            "request_date_ms": 1646761378845,
+            "subscriber": {
+                "first_seen": "2022-03-08T17:42:58Z",
+                "last_seen": "2022-03-08T17:42:58Z",
+                "management_url": "https://apps.apple.com/account/subscriptions",
+                "non_subscriptions": {
+                },
+                "original_app_user_id": "$RCAnonymousID:5b6fdbac3a0c4f879e43d269ecdf9ba1",
+                "original_application_version": "1.0",
+                "original_purchase_date": "2022-04-12T00:03:24Z",
+                "other_purchases": {
+                },
+                "subscriptions": {
+                    "com.revenuecat.product": {
+                        "billing_issues_detected_at": null,
+                        "expires_date": "2062-04-12T00:03:35Z",
+                        "grace_period_expires_date": null,
+                        "is_sandbox": true,
+                        "original_purchase_date": "2022-04-12T00:03:28Z",
+                        "period_type": "intro",
+                        "purchase_date": "2022-04-12T00:03:28Z",
+                        "store": "app_store",
+                        "unsubscribe_detected_at": null
+                    },
+                },
+                "entitlements": {
+                    "premium": {
+                        "expires_date": "2062-04-12T00:03:35Z",
+                        "product_identifier": "com.revenuecat.product",
+                        "purchase_date": "2022-04-12T00:03:28Z"
+                    }
+                }
+            }
+        }
+        """
+        )
+    }()
+
+    static let customerInfoWithGoogleSubscriptions: CustomerInfo = {
+        return .decode(
+        """
+        {
+            "schema_version": "4",
+            "request_date": "2022-03-08T17:42:58Z",
+            "request_date_ms": 1646761378845,
+            "subscriber": {
+                "first_seen": "2022-03-08T17:42:58Z",
+                "last_seen": "2022-03-08T17:42:58Z",
+                "management_url": "https://apps.apple.com/account/subscriptions",
+                "non_subscriptions": {
+                },
+                "original_app_user_id": "$RCAnonymousID:5b6fdbac3a0c4f879e43d269ecdf9ba1",
+                "original_application_version": "1.0",
+                "original_purchase_date": "2022-04-12T00:03:24Z",
+                "other_purchases": {
+                },
+                "subscriptions": {
+                    "com.revenuecat.product": {
+                        "billing_issues_detected_at": null,
+                        "expires_date": "2062-04-12T00:03:35Z",
+                        "grace_period_expires_date": null,
+                        "is_sandbox": true,
+                        "original_purchase_date": "2022-04-12T00:03:28Z",
+                        "period_type": "intro",
+                        "purchase_date": "2022-04-12T00:03:28Z",
+                        "store": "play_store",
+                        "unsubscribe_detected_at": null
+                    },
+                },
+                "entitlements": {
+                    "premium": {
+                        "expires_date": "2062-04-12T00:03:35Z",
+                        "product_identifier": "com.revenuecat.product",
+                        "purchase_date": "2022-04-12T00:03:28Z"
+                    }
+                }
+            }
+        }
+        """
+        )
+    }()
+
+    static let customerInfoWithoutSubscriptions: CustomerInfo = {
+        return .decode(
+        """
+        {
+            "schema_version": "4",
+            "request_date": "2022-03-08T17:42:58Z",
+            "request_date_ms": 1646761378845,
+            "subscriber": {
+                "first_seen": "2022-03-08T17:42:58Z",
+                "last_seen": "2022-03-08T17:42:58Z",
+                "management_url": "https://apps.apple.com/account/subscriptions",
+                "non_subscriptions": {
+                },
+                "original_app_user_id": "$RCAnonymousID:5b6fdbac3a0c4f879e43d269ecdf9ba1",
+                "original_application_version": "1.0",
+                "original_purchase_date": "2022-04-12T00:03:24Z",
+                "other_purchases": {
+                },
+                "subscriptions": {
+                    "com.revenuecat.product": {
+                        "billing_issues_detected_at": null,
+                        "expires_date": "2000-04-12T00:03:35Z",
+                        "grace_period_expires_date": null,
+                        "is_sandbox": true,
+                        "original_purchase_date": "1999-04-12T00:03:28Z",
+                        "period_type": "intro",
+                        "purchase_date": "1999-04-12T00:03:28Z",
+                        "store": "play_store",
+                        "unsubscribe_detected_at": null
+                    },
+                },
+                "entitlements": {
+                    "premium": {
+                        "expires_date": "2000-04-12T00:03:35Z",
+                        "product_identifier": "com.revenuecat.product",
+                        "purchase_date": "1999-04-12T00:03:28Z"
+                    }
+                }
+            }
+        }
+        """
+        )
+    }()
+
+}
+
+#endif
