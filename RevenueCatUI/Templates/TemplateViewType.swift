a076e7e6ff1781583c0d0921c8ebd8291323ec08
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-07-26 11:32:00 +0330

    feat: add an environment variable to control the content max width

--- RevenueCatUI/Templates/TemplateViewType.swift
+++ RevenueCatUI/Templates/TemplateViewType.swift
@@ -238,3 +238,18 @@ private extension TemplateViewConfiguration {
     }
 
 }
+
+// MARK: -
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.2, *)
+private struct ContentMaxWidthKey: EnvironmentKey {
+    static let defaultValue: CGFloat = .infinity
+}
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.2, *)
+extension EnvironmentValues {
+    var contentMaxWidth: CGFloat {
+        get { self[ContentMaxWidthKey.self] }
+        set { self[ContentMaxWidthKey.self] = newValue }
+    }
+}
