7d3a6e89604738183fc5a8a4b9b3f0f2c8f26feb
Andy Boedo <andresboedo@gmail.com>
2024-07-31 17:25:44 -0300

    added all api tests to the xcode project, created a new framework target to replace the old objc api tester

---
+++ Tests/APITesters/AllAPITests/AllAPITests/AllAPITests.h
@@ -0,0 +1,18 @@
+//
+//  AllAPITests.h
+//  AllAPITests
+//
+//  Created by Andrés Boedo on 7/31/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AllAPITests.
+FOUNDATION_EXPORT double AllAPITestsVersionNumber;
+
+//! Project version string for AllAPITests.
+FOUNDATION_EXPORT const unsigned char AllAPITestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AllAPITests/PublicHeader.h>
+
+
