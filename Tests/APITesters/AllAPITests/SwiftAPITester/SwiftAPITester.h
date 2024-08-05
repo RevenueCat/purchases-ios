968a536e61fe8000420407f9830ad25c3ec39943
Andy Boedo <andresboedo@gmail.com>
2024-08-05 11:11:49 -0300

    Clean up API Testers (#4141)
    # API Testers cleanup
    
    - Moves all API Testers into main RevenueCat workspace
    - ensure that they all use the local build, not a hardcoded commit
    - kill all the separate projects, create a single project with a
    framework per test (objc api tests, swift api tests, etc, etc)
    - creates a single scheme that builds all of those targets in one pass
    - update relevant CI (to do)
    
    Here's what this actually looks like: a single scheme in Xcode, which
    you can test using command + U (or just build, since it effectively
    works too)
    
    <img width="515" alt="Screenshot 2024-07-31 at 6 09 33 PM"
    src="https://github.com/user-attachments/assets/53882e38-f34b-4878-a11e-5d9c304e57ec">
    
    <img width="278" alt="Screenshot 2024-07-31 at 6 09 44 PM"
    src="https://github.com/user-attachments/assets/3a7a444c-36da-4c82-ab57-4aeab77adb5d">
    
    *Note:*
    Okay, I'd directly recommend _not_ reviewing the diff, since it's kind
    of insane, and instead checking out the branch to see if things make
    sense.

---
+++ Tests/APITesters/AllAPITests/SwiftAPITester/SwiftAPITester.h
@@ -0,0 +1,18 @@
+//
+//  SwiftAPITester.h
+//  SwiftAPITester
+//
+//  Created by Andrés Boedo on 7/31/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftAPITester.
+FOUNDATION_EXPORT double SwiftAPITesterVersionNumber;
+
+//! Project version string for SwiftAPITester.
+FOUNDATION_EXPORT const unsigned char SwiftAPITesterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAPITester/PublicHeader.h>
+
+
