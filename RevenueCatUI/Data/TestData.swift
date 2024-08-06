34dc733bf0bfa4440d84d3c2513b8e62ab75e2ce
James Borthwick <borthwick.james@gmail.com>
2024-08-06 14:16:17 -0700

    Revert accidental commit of @PublicForExternalTesting change

--- RevenueCatUI/Data/TestData.swift
+++ RevenueCatUI/Data/TestData.swift
@@ -126,43 +126,50 @@ enum TestData {
         subscriptionPeriod: nil,
         locale: Self.locale
     )
-    public static let weeklyPackage = Package(
+    // @PublicForExternalTesting
+    static let weeklyPackage = Package(
         identifier: PackageType.weekly.identifier,
         packageType: .weekly,
         storeProduct: Self.weeklyProduct.toStoreProduct(),
         offeringIdentifier: Self.offeringIdentifier
     )
-    public static let monthlyPackage = Package(
+    // @PublicForExternalTesting
+    static let monthlyPackage = Package(
         identifier: PackageType.monthly.identifier,
         packageType: .monthly,
         storeProduct: Self.monthlyProduct.toStoreProduct(),
         offeringIdentifier: Self.offeringIdentifier
     )
-    public static let threeMonthPackage = Package(
+    // @PublicForExternalTesting
+    static let threeMonthPackage = Package(
         identifier: PackageType.threeMonth.identifier,
         packageType: .threeMonth,
         storeProduct: Self.threeMonthProduct.toStoreProduct(),
         offeringIdentifier: Self.offeringIdentifier
     )
-    public static let sixMonthPackage = Package(
+    // @PublicForExternalTesting
+    static let sixMonthPackage = Package(
         identifier: PackageType.sixMonth.identifier,
         packageType: .sixMonth,
         storeProduct: Self.sixMonthProduct.toStoreProduct(),
         offeringIdentifier: Self.offeringIdentifier
     )
-    public static let annualPackage = Package(
+    // @PublicForExternalTesting
+    static let annualPackage = Package(
         identifier: PackageType.annual.identifier,
         packageType: .annual,
         storeProduct: Self.annualProduct.toStoreProduct(),
         offeringIdentifier: Self.offeringIdentifier
     )
-    public static let annualPackage60 = Package(
+    // @PublicForExternalTesting
+    static let annualPackage60 = Package(
         identifier: PackageType.annual.identifier,
         packageType: .annual,
         storeProduct: Self.annualProduct60.toStoreProduct(),
         offeringIdentifier: Self.offeringIdentifier
     )
-    public static let lifetimePackage = Package(
+    // @PublicForExternalTesting
+    static let lifetimePackage = Package(
         identifier: PackageType.lifetime.identifier,
         packageType: .lifetime,
         storeProduct: Self.lifetimeProduct.toStoreProduct(),
