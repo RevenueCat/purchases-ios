cac09a4f72ecbc49c36b5c750748a365945d57f5
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-08-08 16:05:06 +0200

    Add flag to SPM Package.swift

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
                 resources: [
                     .copy("../Sources/PrivacyInfo.xcprivacy")
                 ],
-                swiftSettings: [visionOSSetting]),
+                swiftSettings: [.define("CUSTOMER_CENTER_ENABLED"), visionOSSetting]),
         .target(name: "RevenueCat_CustomEntitlementComputation",
                 path: "CustomEntitlementComputation",
                 exclude: ["Info.plist", "LocalReceiptParsing/ReceiptParser-only-files"],
@@ -75,7 +75,8 @@ let package = Package(
                     // Note: these have to match the values in RevenueCatUI.podspec
                     .copy("Resources/background.jpg"),
                     .process("Resources/icons.xcassets")
-                ]),
+                ],
+                swiftSettings: [.define("CUSTOMER_CENTER_ENABLED")]),
         .testTarget(name: "RevenueCatUITests",
                     dependencies: [
                         "RevenueCatUI",
