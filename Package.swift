52195fb37a08e8da91569a6af6b1ec1d9e76d16f
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-09 14:30:44 +0200

    Revert "Disable `CustomerCenter` build flags (#4160)"
    This reverts commit 92841076720430ec712fcd9c7f547007f024bd7a.

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
                 resources: [
                     .copy("../Sources/PrivacyInfo.xcprivacy")
                 ],
-                swiftSettings: [visionOSSetting]),
+                swiftSettings: [.define("CUSTOMER_CENTER_ENABLED"), visionOSSetting]),
         .target(name: "RevenueCat_CustomEntitlementComputation",
                 path: "CustomEntitlementComputation",
                 exclude: ["Info.plist", "LocalReceiptParsing/ReceiptParser-only-files"],
@@ -76,7 +76,7 @@ let package = Package(
                     .copy("Resources/background.jpg"),
                     .process("Resources/icons.xcassets")
                 ],
-                swiftSettings: []),
+                swiftSettings: [.define("CUSTOMER_CENTER_ENABLED")]),
         .testTarget(name: "RevenueCatUITests",
                     dependencies: [
                         "RevenueCatUI",
