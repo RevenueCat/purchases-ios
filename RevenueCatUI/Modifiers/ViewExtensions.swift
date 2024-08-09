b8c81e5a524b4b54fe4aa30c7538ae2a56924df6
Mark Villacampa <MarkVillacampa@users.noreply.github.com>
2024-08-09 16:04:21 +0200

    Update RevenueCatUI/Modifiers/ViewExtensions.swift
    Co-authored-by: Toni Rico <toni.rico.diez@revenuecat.com>

--- RevenueCatUI/Modifiers/ViewExtensions.swift
+++ RevenueCatUI/Modifiers/ViewExtensions.swift
@@ -30,7 +30,7 @@ extension View {
 }
 
 private var isIOSVersionWithCrash: Bool = {
-    // There is a bug in iOS 18 beta 5 (as of writing, future versions uncofirmed) that causes a crash.
+    // There is a bug in iOS 18 beta 5 (as of writing, future versions unconfirmed) that causes a crash.
     // This has been reporte to Apple as FB14699941.
     // Until this is fixed, we're rolling back to pre-iOS 16 behavior for this view.
     // More information and discussion here: https://github.com/RevenueCat/purchases-ios/issues/4150
