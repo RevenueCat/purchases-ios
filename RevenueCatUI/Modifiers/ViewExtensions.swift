37f3fb1752ad2052c11fa615d979fe7dfdf773a1
Andy Boedo <andresboedo@gmail.com>
2024-08-15 18:18:45 -0300

    export RevenueCatUI xcframework (#4172)
    We don't currently export RevenueCatUI as an xcframework, which can be a
    source of confusion and a dealbreaker for customers who need a pre-built
    framework.
    This adds RevenueCatUI's xcframework into the `export_xcframeworks`
    lane, and includes the output in the `github_release` lane.
    
    
    Should help address: 
    - https://github.com/RevenueCat/purchases-ios/issues/4168
    - https://github.com/RevenueCat/purchases-ios/issues/4146
    - https://github.com/RevenueCat/purchases-ios/issues/4056

--- RevenueCatUI/Modifiers/ViewExtensions.swift
+++ RevenueCatUI/Modifiers/ViewExtensions.swift
@@ -40,10 +40,13 @@ extension View {
         perform action: @escaping (_ newValue: V) -> Void
     ) -> some View where V: Equatable {
         #if swift(>=5.9)
+        // wrapping with AnyView to type erase is needed because when archiving an xcframework,
+        // the compiler gets confused between the types returned
+        // by the different implementations of self.onChange(of:value).
         if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *) {
-            self.onChange(of: value) { _, newValue in action(newValue) }
+            AnyView(self.onChange(of: value) { _, newValue in action(newValue) })
         } else {
-            self.onChange(of: value) { newValue in action(newValue) }
+            AnyView(self.onChange(of: value) { newValue in action(newValue) })
         }
         #else
         self.onChange(of: value) { newValue in action(newValue) }
