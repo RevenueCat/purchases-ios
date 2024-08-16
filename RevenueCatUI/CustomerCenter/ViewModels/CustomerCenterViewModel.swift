c031f5f24a6c58787050a9f9341c4ef1175a16ed
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-16 12:02:21 +0200

    [Customer Center] CustomerCenterViewModel checks whether the app is the latest version (#4169)

--- RevenueCatUI/CustomerCenter/ViewModels/CustomerCenterViewModel.swift
+++ RevenueCatUI/CustomerCenter/ViewModels/CustomerCenterViewModel.swift
@@ -25,13 +25,20 @@ import RevenueCat
 @available(tvOS, unavailable)
 @available(watchOS, unavailable)
 @MainActor class CustomerCenterViewModel: ObservableObject {
+    // We fail open.
+    private static let defaultAppIsLatestVersion = true
 
     typealias CustomerInfoFetcher = @Sendable () async throws -> CustomerInfo
+    typealias CurrentVersionFetcher = () -> String?
+
+    private lazy var currentAppVersion: String? = currentVersionFetcher()
 
     @Published
     private(set) var hasSubscriptions: Bool = false
     @Published
     private(set) var subscriptionsAreFromApple: Bool = false
+    @Published
+    private(set) var appIsLatestVersion: Bool = defaultAppIsLatestVersion
 
     // @PublicForExternalTesting
     @Published
@@ -43,50 +50,58 @@ import RevenueCat
         }
     }
     @Published
-    var configuration: CustomerCenterConfigData?
+    var configuration: CustomerCenterConfigData? {
+        didSet {
+            guard
+                let currentVersionString = currentAppVersion?.versionString(),
+                let latestVersionString = configuration?.lastPublishedAppVersion?.versionString(),
+                let currentVersion = try? SemanticVersion(currentVersionString),
+                let latestVersion = try? SemanticVersion(latestVersionString)
+            else {
+                self.appIsLatestVersion = Self.defaultAppIsLatestVersion
+                return
+            }
+
+            self.appIsLatestVersion = currentVersion >= latestVersion
+      }
+    }
 
     var isLoaded: Bool {
         return state != .notLoaded && configuration != nil
     }
 
     private var customerInfoFetcher: CustomerInfoFetcher
+    private let currentVersionFetcher: CurrentVersionFetcher
     internal let customerCenterActionHandler: CustomerCenterActionHandler?
 
     private var error: Error?
 
-    convenience init(customerCenterActionHandler: CustomerCenterActionHandler?) {
-        self.init(customerCenterActionHandler: customerCenterActionHandler,
-                  customerInfoFetcher: {
-            guard Purchases.isConfigured else {
-                throw PaywallError.purchasesNotConfigured
-            }
-
+    init(
+        customerCenterActionHandler: CustomerCenterActionHandler?,
+        customerInfoFetcher: @escaping CustomerInfoFetcher = {
+            guard Purchases.isConfigured else { throw PaywallError.purchasesNotConfigured }
             return try await Purchases.shared.customerInfo()
-        })
-    }
-
-    init(customerCenterActionHandler: CustomerCenterActionHandler?,
-         customerInfoFetcher: @escaping CustomerInfoFetcher) {
+        },
+        currentVersionFetcher: @escaping CurrentVersionFetcher = {
+            Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        }
+    ) {
         self.state = .notLoaded
         self.customerInfoFetcher = customerInfoFetcher
+        self.currentVersionFetcher = currentVersionFetcher
         self.customerCenterActionHandler = customerCenterActionHandler
     }
 
     #if DEBUG
 
-    init(hasSubscriptions: Bool = false,
-         areSubscriptionsFromApple: Bool = false) {
+    convenience init(
+        hasSubscriptions: Bool = false,
+        areSubscriptionsFromApple: Bool = false
+    ) {
+        self.init(customerCenterActionHandler: nil)
         self.hasSubscriptions = hasSubscriptions
         self.subscriptionsAreFromApple = areSubscriptionsFromApple
-        self.customerInfoFetcher = {
-            guard Purchases.isConfigured else {
-                throw PaywallError.purchasesNotConfigured
-            }
-
-            return try await Purchases.shared.customerInfo()
-        }
         self.state = .success
-        self.customerCenterActionHandler = nil
     }
 
     #endif
@@ -131,7 +146,21 @@ import RevenueCat
             return .purchasesNotFound
         }
     }
+}
 
+fileprivate extension String {
+    /// Takes the first characters of this string, if they conform to Major.Minor.Patch. Returns nil otherwise.
+    /// Note that Minor and Patch are optional. So if this string starts with a single number, that number is returned.
+    func versionString() -> String? {
+        do {
+            let pattern = #"^(\d+)(?:\.\d+)?(?:\.\d+)?"#
+            let regex = try NSRegularExpression(pattern: pattern)
+            let match = regex.firstMatch(in: self, range: NSRange(self.startIndex..., in: self))
+            return match.map { String(self[Range($0.range, in: self)!]) }
+        } catch {
+            return nil
+        }
+    }
 }
 
 #endif
