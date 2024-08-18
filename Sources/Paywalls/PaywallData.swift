2aee251d87a9d72a80550b5f7208b41709f5ca63
Josh Holtz <me@joshholtz.com>
2024-08-18 14:59:39 -0500

    Fix a lint

--- Sources/Paywalls/PaywallData.swift
+++ Sources/Paywalls/PaywallData.swift
@@ -43,7 +43,7 @@ public struct PaywallData {
     }
 
     internal private(set) var _zeroDecimalPlaceCountries: ZeroDecimalPlaceCountries?
-    
+
     /// The default locale identifier for this paywall.
     public var defaultLocale: String?
 
