b958092dcefe039cb1ec68a336cfd55a8decd80c
Josh Holtz <me@joshholtz.com>
2024-08-19 13:20:35 -0500

    Update comment

--- Tests/UnitTests/FoundationExtensions/LocaleExtensionsTests.swift
+++ Tests/UnitTests/FoundationExtensions/LocaleExtensionsTests.swift
@@ -37,7 +37,7 @@ class LocaleExtensionsTests: TestCase {
         // and iOS 15 tests with Xcode 14
         #if swift(>=5.9)
             if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *) {
-                // Fixed in iOS 16 on Xcode 15
+                // Fixed in iOS 17 on Xcode 15
                 expect(Locale(identifier: "").rc_languageCode).to(beNil())
             } else {
                 // Broken in iOS 16 on Xcode 15
@@ -58,7 +58,7 @@ class LocaleExtensionsTests: TestCase {
         // and iOS 15 tests with Xcode 14
         #if swift(>=5.9)
             if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *) {
-                // Fixed in iOS 16 on Xcode 15
+                // Fixed in iOS 17 on Xcode 15
                 expect(Locale(identifier: "").removingRegion).to(beNil())
             } else {
                 // Broken in iOS 16 on Xcode 15
