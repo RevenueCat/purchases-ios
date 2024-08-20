7b55b6c36fd3b4749659e5fefc636e8864a1df5c
James Borthwick <borthwick.james@gmail.com>
2024-08-19 18:06:14 -0700

    Enable PAYWALL_COMPONENTS

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
                 resources: [
                     .copy("../Sources/PrivacyInfo.xcprivacy")
                 ],
-                swiftSettings: [visionOSSetting]),
+                swiftSettings: [.define("PAYWALL_COMPONENTS"), visionOSSetting]),
         .target(name: "RevenueCat_CustomEntitlementComputation",
                 path: "CustomEntitlementComputation",
                 exclude: ["Info.plist", "LocalReceiptParsing/ReceiptParser-only-files"],
@@ -76,7 +76,7 @@ let package = Package(
                     .copy("Resources/background.jpg"),
                     .process("Resources/icons.xcassets")
                 ],
-                swiftSettings: []),
+                swiftSettings: [.define("PAYWALL_COMPONENTS")]),
         .testTarget(name: "RevenueCatUITests",
                     dependencies: [
                         "RevenueCatUI",
