078a16d1a4bf14454f03739cca1e172d08307602
Andy Boedo <andresboedo@gmail.com>
2024-08-22 19:39:36 -0300

    added tests

--- Tests/UnitTests/Misc/SandboxEnvironmentDetectorTests.swift
+++ Tests/UnitTests/Misc/SandboxEnvironmentDetectorTests.swift
@@ -91,6 +91,66 @@ class SandboxEnvironmentDetectorTests: TestCase {
         ) == true
     }
 
+    func testIsSandboxWhenReceiptEnvironmentIsUnknownDefaultToMacAppStoreDetector() throws {
+        let macAppStore = false
+        var isSandbox = false
+        let macAppStoreDetector = MockMacAppStoreDetector(isMacAppStore: macAppStore)
+        let detector = SystemInfo.with(
+            macAppStore: macAppStore,
+            receiptEnvironment: .unknown,
+            macAppStoreDetector: macAppStoreDetector
+        )
+
+        expect(detector.isSandbox) == isSandbox
+        expect(macAppStoreDetector.isMacAppStoreCalled) == true
+
+        isSandbox = !isSandbox
+
+        expect(detector.isSandbox) == isSandbox
+    }
+
+    func testIsSandboxWhenReceiptIsProductionReturnsProductionAndDoesntHitMacAppStoreDetector() throws {
+        let macAppStoreDetector = MockMacAppStoreDetector(isMacAppStore: false)
+        let detector = SystemInfo.with(
+            macAppStore: false,
+            receiptEnvironment: .production,
+            macAppStoreDetector: macAppStoreDetector
+        )
+
+        expect(detector.isSandbox) == false
+        expect(macAppStoreDetector.isMacAppStoreCalled) == false
+    }
+
+    func testIsSandboxWhenReceiptIsSandboxReturnsSandboxAndDoesntHitMacAppStoreDetector() throws {
+        let macAppStoreDetector = MockMacAppStoreDetector(isMacAppStore: false)
+        let detector = SystemInfo.with(
+            macAppStore: false,
+            receiptEnvironment: .sandbox,
+            macAppStoreDetector: macAppStoreDetector
+        )
+
+        expect(detector.isSandbox) == true
+        expect(macAppStoreDetector.isMacAppStoreCalled) == false
+    }
+
+    func testIsSandboxIfReceiptParsingFailsAndNotInMacAppStore() throws {
+        let macAppStore = false
+        var isSandbox = false
+        let macAppStoreDetector = MockMacAppStoreDetector(isMacAppStore: macAppStore)
+        let detector = SystemInfo.with(
+            macAppStore: macAppStore,
+            failReceiptParsing: true,
+            macAppStoreDetector: macAppStoreDetector
+        )
+
+        expect(detector.isSandbox) == isSandbox
+        expect(macAppStoreDetector.isMacAppStoreCalled) == true
+
+        isSandbox = !isSandbox
+
+        expect(detector.isSandbox) == isSandbox
+    }
+
 }
 
 #endif
@@ -104,7 +164,8 @@ private extension SandboxEnvironmentDetector {
         inSimulator: Bool = false,
         macAppStore: Bool = false,
         receiptEnvironment: AppleReceipt.Environment = .production,
-        failReceiptParsing: Bool = false
+        failReceiptParsing: Bool = false,
+        macAppStoreDetector: MockMacAppStoreDetector? = nil
     ) -> SandboxEnvironmentDetector {
         let bundle = MockBundle()
         bundle.receiptURLResult = result
@@ -126,7 +187,7 @@ private extension SandboxEnvironmentDetector {
             isRunningInSimulator: inSimulator,
             receiptFetcher: MockLocalReceiptFetcher(mockReceipt: mockReceipt,
                                                     failReceiptParsing: failReceiptParsing),
-            macAppStoreDetector: MockMacAppStoreDetector(isMacAppStore: macAppStore)
+            macAppStoreDetector: macAppStoreDetector ?? MockMacAppStoreDetector(isMacAppStore: macAppStore)
         )
     }
 
@@ -151,12 +212,17 @@ private final class MockLocalReceiptFetcher: LocalReceiptFetcherType {
 
 }
 
-private struct MockMacAppStoreDetector: MacAppStoreDetector {
+private final class MockMacAppStoreDetector: MacAppStoreDetector, @unchecked Sendable {
 
-    let isMacAppStore: Bool
+    let isMacAppStoreValue: Bool
+    private(set) var isMacAppStoreCalled = false
 
     init(isMacAppStore: Bool) {
-        self.isMacAppStore = isMacAppStore
+        self.isMacAppStoreValue = isMacAppStore
     }
 
+    var isMacAppStore: Bool {
+        isMacAppStoreCalled = true
+        return isMacAppStoreValue
+    }
 }
