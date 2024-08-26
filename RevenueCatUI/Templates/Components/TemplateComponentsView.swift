6730f0de9f6399cebf93fd9a27e9477453719754
James Borthwick <borthwick.james@gmail.com>
2024-08-26 13:17:27 -0700

    Tidy TemplateComponentsView, ComponentsView

--- RevenueCatUI/Templates/Components/TemplateComponentsView.swift
+++ RevenueCatUI/Templates/Components/TemplateComponentsView.swift
@@ -11,52 +11,31 @@ import SwiftUI
 
 #if PAYWALL_COMPONENTS
 
-@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-private class ComponentPaywallData: ObservableObject {
-//    @Published var selectedPackage: TemplateViewConfiguration.Package
-//
-//    init(selectedPackage: TemplateViewConfiguration.Package) {
-//        self._selectedPackage = .init(initialValue: selectedPackage)
-//    }
-}
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 struct TemplateComponentsView: View {
 
-    let paywallComponentsData: PaywallComponentsData
-
     @StateObject
     private var componentPaywallData: ComponentPaywallData
 
-    @Environment(\.userInterfaceIdiom)
-    var userInterfaceIdiom
     @Environment(\.locale)
     var locale
 
-    #if swift(>=5.9) || (!os(macOS) && !os(watchOS) && !os(tvOS))
-    @Environment(\.verticalSizeClass)
-    var verticalSizeClass
-    #endif
+    let paywallComponentsData: PaywallComponentsData
 
     init(paywallComponentsData: PaywallComponentsData) {
         self.paywallComponentsData = paywallComponentsData
-        self._componentPaywallData = .init(wrappedValue: .init(/*selectedPackage: configuration.packages.default*/))
     }
 
     var body: some View {
         VStack(spacing: 0) {
                 ComponentsView(
                     locale: self.locale,
-                    components: paywallComponentsData.componentsConfig.components,
-                    shouldSplitLandscape: true
+                    components: paywallComponentsData.componentsConfig.components
                 )
                 .environmentObject(self.componentPaywallData)
         }
         .edgesIgnoringSafeArea(.top)
-        .background(
-
-//            try! PaywallColor(stringRepresentation: self.configuration.components!.backgroundColor.light).underlyingColor
-        )
     }
 
 }
@@ -75,17 +54,14 @@ struct ComponentsView: View {
 
     let locale: Locale
     let components: [PaywallComponent]
-    let shouldSplitLandscape: Bool
 
-    init(locale: Locale, components: [PaywallComponent], shouldSplitLandscape: Bool = false) {
+    init(locale: Locale, components: [PaywallComponent]) {
         self.locale = locale
         self.components = components
-        self.shouldSplitLandscape = shouldSplitLandscape
     }
 
     var body: some View {
-            self.layoutComponents(self.components)
-//                .scrollableIfNecessaryWhenAvailable()
+        self.layoutComponents(self.components)
     }
 
     @ViewBuilder
@@ -107,40 +83,6 @@ struct ComponentsView: View {
                 LinkButtonComponentView(locale: locale, component: component)
             }
         }
-    }
-
-    @Environment(\.userInterfaceIdiom)
-    var userInterfaceIdiom
-
-    var defaultHorizontalPaddingLength: CGFloat? {
-        return Constants.defaultHorizontalPaddingLength(self.userInterfaceIdiom)
-    }
-
-    var defaultVerticalPaddingLength: CGFloat? {
-        return Constants.defaultVerticalPaddingLength(self.userInterfaceIdiom)
-    }
-
-    #if swift(>=5.9) || (!os(macOS) && !os(watchOS) && !os(tvOS))
-    @Environment(\.verticalSizeClass)
-    var verticalSizeClass
-    #endif
-
-    var shouldUseLandscapeLayout: Bool {
-        return false
-//        #if os(tvOS)
-//        // tvOS never reports UserInterfaceSizeClass.compact
-//        // but for the purposes of template layouts, we consider landscape
-//        // on tvOS as compact to produce horizontal layouts.
-//        return true
-//        #elseif os(macOS)
-//        return false
-//        #elseif os(watchOS)
-//        return false
-//        #else
-//        // Ignore size class when displaying footer paywalls.
-//        return (self.configuration.mode.isFullScreen &&
-//                self.verticalSizeClass == .compact)
-//        #endif
     }
     
 }
