86a9f364008f978dd48bfc11a341a335d8e2fdaf
James Borthwick <borthwick.james@gmail.com>
2024-08-26 13:35:47 -0700

    Spacing, lint enablement

--- RevenueCatUI/Templates/Components/TemplateComponentsView.swift
+++ RevenueCatUI/Templates/Components/TemplateComponentsView.swift
@@ -7,11 +7,8 @@
 
 import RevenueCat
 import SwiftUI
-// swiftlint:disable all
 
 #if PAYWALL_COMPONENTS
-
-
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 struct TemplateComponentsView: View {
 
