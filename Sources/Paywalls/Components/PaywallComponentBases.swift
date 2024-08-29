8f741130373d8359ad62ee4c583da3ce97ae348c
James Borthwick <borthwick.james@gmail.com>
2024-08-29 14:19:31 -0700

    Missing from last commit

--- Sources/Paywalls/Components/PaywallComponentBases.swift
+++ Sources/Paywalls/Components/PaywallComponentBases.swift
@@ -156,17 +156,6 @@ public extension PaywallComponent {
         case center
         case bottom
 
-        public var stackAlignment: SwiftUI.VerticalAlignment {
-            switch self {
-            case .top:
-                return .top
-            case .center:
-                return .center
-            case .bottom:
-                return .bottom
-            }
-        }
-
     }
 
     enum TwoDimensionAlignment: String, Decodable, Sendable, Hashable, Equatable {
@@ -180,29 +169,6 @@ public extension PaywallComponent {
         case topTrailing
         case bottomLeading
         case bottomTrailing
-
-        public var stackAlignment: SwiftUI.Alignment {
-            switch self {
-            case .center:
-                return .center
-            case .leading:
-                return .leading
-            case .trailing:
-                return .trailing
-            case .top:
-                return .top
-            case .bottom:
-                return .bottom
-            case .topLeading:
-                return .topLeading
-            case .topTrailing:
-                return .topTrailing
-            case .bottomLeading:
-                return .bottomLeading
-            case .bottomTrailing:
-                return .bottomTrailing
-            }
-        }
 
     }
 
