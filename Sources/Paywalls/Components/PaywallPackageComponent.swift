970d1365012fa468473eac707ba1e7a4342a9d8e
James Borthwick <borthwick.james@gmail.com>
2024-09-10 15:21:18 -0700

    PackageComponent as class; no selected component because no visible state

--- Sources/Paywalls/Components/PaywallPackageComponent.swift
+++ Sources/Paywalls/Components/PaywallPackageComponent.swift
@@ -11,18 +11,37 @@ import Foundation
 
 public extension PaywallComponent {
 
-    struct PackageComponent: PaywallComponentBase {
+    final class PackageComponent: PaywallComponentBase {
 
         let type: ComponentType
         public let packageID: String
         public let components: [PaywallComponent]
 
-        public init(type: ComponentType, packageID: String, components: [PaywallComponent]) {
+        public init(type: ComponentType,
+                    packageID: String,
+                    components: [PaywallComponent]
+        ) {
             self.type = .package
             self.packageID = packageID
             self.components = components
         }
 
+    }
+
+}
+
+extension PaywallComponent.PackageComponent: Equatable, Hashable {
+
+    public static func == (lhs: PaywallComponent.PackageComponent, rhs: PaywallComponent.PackageComponent) -> Bool {
+        return lhs.type == rhs.type &&
+               lhs.packageID == rhs.packageID &&
+               lhs.components == rhs.components
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(type)
+        hasher.combine(packageID)
+        hasher.combine(components)
     }
 
 }
