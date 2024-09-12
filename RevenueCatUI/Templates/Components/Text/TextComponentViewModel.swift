b4655cf0ada673b72f491e0ccc778dfc6106e775
James Borthwick <borthwick.james@gmail.com>
2024-09-12 11:55:27 -0700

    Can be private

--- RevenueCatUI/Templates/Components/Text/TextComponentViewModel.swift
+++ RevenueCatUI/Templates/Components/Text/TextComponentViewModel.swift
@@ -21,7 +21,7 @@ import SwiftUI
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 public class TextComponentViewModel {
 
-    let localizedStrings: PaywallComponent.LocalizationDictionary
+    private let localizedStrings: PaywallComponent.LocalizationDictionary
     private let textUnselected: String
     private let textSelected: String?
 
