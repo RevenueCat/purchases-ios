d434ebc10b14e62f04b80bfcd5e5f3e7fa378539
James Borthwick <borthwick.james@gmail.com>
2024-09-12 10:58:15 -0700

    Revert "Merge branch 'customercenter/testing' of https://github.com/RevenueCat/purchases-ios into stateobject-instantiation"
    This reverts commit 56544551d7d46337c3b33f2f4a73a740898e9417, reversing
    changes made to b0dff5bd13228c04dc8cdc3ed44453219b1ca83f.

--- Package.swift
+++ Package.swift
@@ -52,7 +52,7 @@ let package = Package(
                 resources: [
                     .copy("../Sources/PrivacyInfo.xcprivacy")
                 ],
-                swiftSettings: [.define("CUSTOMER_CENTER_ENABLED"), visionOSSetting]),
+                swiftSettings: [visionOSSetting]),
         .target(name: "RevenueCat_CustomEntitlementComputation",
                 path: "CustomEntitlementComputation",
                 exclude: ["Info.plist", "LocalReceiptParsing/ReceiptParser-only-files"],
@@ -78,7 +78,7 @@ let package = Package(
                     .copy("Resources/background.jpg"),
                     .process("Resources/icons.xcassets")
                 ],
-                swiftSettings: [.define("CUSTOMER_CENTER_ENABLED")]),
+                swiftSettings: []),
         .testTarget(name: "RevenueCatUITests",
                     dependencies: [
                         "RevenueCatUI",
