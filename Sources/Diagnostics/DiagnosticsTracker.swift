3edc27ee5eb11d9712af0733452b92482d308cf1
Cesar de la Vega <cesarvegaro@gmail.com>
2024-09-30 10:38:55 +0200

    prevent deadlock

--- Sources/Diagnostics/DiagnosticsTracker.swift
+++ Sources/Diagnostics/DiagnosticsTracker.swift
@@ -159,14 +159,12 @@ private extension DiagnosticsTracker {
     func clearDiagnosticsFileIfTooBig() async {
         if await self.diagnosticsFileHandler.isDiagnosticsFileTooBig() {
             await self.diagnosticsFileHandler.emptyDiagnosticsFile()
-            self.trackMaxEventsStoredLimitReached()
+            await self.clearDiagnosticsFileIfTooBig()
+            let maxEventsStoredEvent = DiagnosticsEvent(eventType: .maxEventsStoredLimitReached,
+                                                        properties: [:],
+                                                        timestamp: self.dateProvider.now())
+            await self.diagnosticsFileHandler.appendEvent(diagnosticsEvent: maxEventsStoredEvent)
         }
-    }
-
-    func trackMaxEventsStoredLimitReached() {
-        self.track(.init(eventType: .maxEventsStoredLimitReached,
-                         properties: [:],
-                         timestamp: self.dateProvider.now()))
     }
 
 }
