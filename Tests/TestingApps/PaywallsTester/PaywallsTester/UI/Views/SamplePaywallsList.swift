507b4c2f5ad133702bd380d5d989e879fb78aeee
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-10-11 15:36:40 +0200

    Adds a PurchaseHandler environmentObject to TemplateComponentsView in SamplePaywallsList.

--- Tests/TestingApps/PaywallsTester/PaywallsTester/UI/Views/SamplePaywallsList.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/UI/Views/SamplePaywallsList.swift
@@ -95,7 +95,7 @@ struct SamplePaywallsList: View {
                 paywallComponentsData: data,
                 offering: Self.loader.offeringWithDefaultPaywall(),
                 onDismiss: { self.display = nil }
-            )
+            ).environmentObject(Self.purchaseHandler)
         #endif
         }
 
@@ -228,6 +228,7 @@ struct SamplePaywallsList: View {
                 }
         )
     }
+    private static let purchaseHandler: PurchaseHandler = .init()
 
 }
 
