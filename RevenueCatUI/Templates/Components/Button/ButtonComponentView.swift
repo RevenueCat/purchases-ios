c3d8356deb907db3f43094edfe83b2e6f0fe45e1
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-10-14 14:19:36 +0200

    Makes openURL private

--- RevenueCatUI/Templates/Components/Button/ButtonComponentView.swift
+++ RevenueCatUI/Templates/Components/Button/ButtonComponentView.swift
@@ -22,7 +22,7 @@ import SwiftUI
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 struct ButtonComponentView: View {
-    @Environment(\.openURL) var openURL
+    @Environment(\.openURL) private var openURL
     @State private var inAppBrowserURL: URL?
 
     private let viewModel: ButtonComponentViewModel
