9cb1b8d962f110d1e6616690378dca29efdb79c2
Will Taylor <wtaylor151@gmail.com>
2024-10-31 14:48:45 -0500

    remove unused import

--- Sources/Purchasing/StoreKitAbstractions/WinBackOffer.swift
+++ Sources/Purchasing/StoreKitAbstractions/WinBackOffer.swift
@@ -12,7 +12,6 @@
 //  Created by Will Taylor on 10/29/24.
 
 import Foundation
-import StoreKit
 
 /// Represents an Apple win-back offer.
 @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
