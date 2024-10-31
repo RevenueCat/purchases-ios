b5aa94839747b7b4896576713c4e194cb7818e14
Josh Holtz <me@joshholtz.com>
2024-10-31 12:25:00 -0500

    Lint

--- RevenueCatUI/Templates/Components/Text/TextComponentViewModel.swift
+++ RevenueCatUI/Templates/Components/Text/TextComponentViewModel.swift
@@ -18,7 +18,7 @@ import SwiftUI
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 class TextComponentViewModel {
-    
+
     private let localizedStrings: PaywallComponent.LocalizationDictionary
     private let component: PaywallComponent.TextComponent
 
