37680b9e18d3baea2562aa154a6f1fa74460cfd5
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-11-07 14:41:00 +0100

    Add docs

--- RevenueCatUI/View+OnRedeemWebPurchaseAttempt.swift
+++ RevenueCatUI/View+OnRedeemWebPurchaseAttempt.swift
@@ -11,12 +11,26 @@
 //
 //  Created by Antonio Rico Diez on 7/11/24.
 
-import SwiftUI
 import RevenueCat
+import SwiftUI
 
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 extension View {
 
+    /// If an URL is found by using onOpenUrl, it will parse the URL and if it's a RevenueCat
+    /// web purchase redemption link, it will perform the redemption automatically
+    /// and return the result in the completion lambda.
+    /// Example:
+    /// ```swift
+    /// var body: some View {
+    ///    YourApp()
+    ///      .onWebPurchaseRedemptionAttempt { result in
+    ///          handleWebPurchaseRedemptionResult(result)
+    ///      }
+    /// }
+    /// ```
+    /// - Note: If the SDK is not configured when the URL is received, the URL will be ignored
+    /// - Note: If you need to control better when to perform the redemption, see our docs for examples.
     public func onWebPurchaseRedemptionAttempt(
         perform completion: @escaping @Sendable (WebPurchaseRedemptionResult) -> Void
     ) -> some View {
