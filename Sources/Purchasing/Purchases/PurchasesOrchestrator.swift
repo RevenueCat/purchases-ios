2ea09f6c6dee243e351b45533ca8f3d6aff04e40
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-11-07 14:33:02 +0100

    Lefover after rebase

--- Sources/Purchasing/Purchases/PurchasesOrchestrator.swift
+++ Sources/Purchasing/Purchases/PurchasesOrchestrator.swift
@@ -168,7 +168,6 @@ final class PurchasesOrchestrator {
             storeMessagesHelper: storeMessagesHelper,
             winBackOfferEligibilityCalculator: winBackOfferEligibilityCalculator,
             paywallEventsManager: paywallEventsManager,
-            deepLinkHandler: deepLinkHandler,
             webPurchaseRedemptionHelper: webPurchaseRedemptionHelper
         )
 
@@ -247,7 +246,6 @@ final class PurchasesOrchestrator {
         self.storeMessagesHelper = storeMessagesHelper
         self.winBackOfferEligibilityCalculator = winBackOfferEligibilityCalculator
         self.paywallEventsManager = paywallEventsManager
-        self.deepLinkHandler = deepLinkHandler
         self.webPurchaseRedemptionHelper = webPurchaseRedemptionHelper
 
         Logger.verbose(Strings.purchase.purchases_orchestrator_init(self))
