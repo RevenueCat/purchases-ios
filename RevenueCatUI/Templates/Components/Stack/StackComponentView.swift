8d5a2e739833b2bdeed0788f62342a7c0b13c6c0
Josh Holtz <me@joshholtz.com>
2024-11-11 05:38:40 -0600

    Update stack size

--- RevenueCatUI/Templates/Components/Stack/StackComponentView.swift
+++ RevenueCatUI/Templates/Components/Stack/StackComponentView.swift
@@ -251,6 +251,7 @@ struct StackComponentView_Previews: PreviewProvider {
         .previewLayout(.sizeThatFits)
         .previewDisplayName("Default - Fill Fit Fixed Fill")
     }
+
 }
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
