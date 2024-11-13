5fc69cce89528ac7275ee298ec9aeec300916765
Josh Holtz <me@joshholtz.com>
2024-11-13 15:27:31 -0600

    Ths is it

--- Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
@@ -10,8 +10,8 @@ sed -i '' 's/static var apiKey: String { "" }/static var apiKey: String { "'"$RE
 # This allow Local.xcconfig to be found by Xcode
 xcconfig_file="$(cd "$SCRIPT_DIR/../../../../" && pwd)/Local.xcconfig"
 
-# echo "Enabling PAYWALL_COMPONENTS compiler flag in $xcconfig_file"
-# echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS\nOTHER_SWIFT_FLAGS = PAYWALL_COMPONENTS" > "$xcconfig_file"
+echo "Enabling PAYWALL_COMPONENTS compiler flag in $xcconfig_file"
+echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS" > "$xcconfig_file"
 
 # Update Package.swift with PAYWALL_COMPONENTS define
 package_file="$(cd "$SCRIPT_DIR/../../../../" && pwd)/Package.swift"
