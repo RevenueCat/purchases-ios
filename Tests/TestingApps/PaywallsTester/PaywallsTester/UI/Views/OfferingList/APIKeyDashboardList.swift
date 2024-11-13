4390e5d6a0f013959a88313bef42f9fa7502c674
Josh Holtz <me@joshholtz.com>
2024-11-13 14:26:00 -0600

    Maybe this

--- Tests/TestingApps/PaywallsTester/PaywallsTester/UI/Views/OfferingList/APIKeyDashboardList.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/UI/Views/OfferingList/APIKeyDashboardList.swift
@@ -200,7 +200,6 @@ extension APIKeyDashboardList.Template: CustomStringConvertible {
 
     var description: String {
         if let name = self.name {
-            #if DEBUG
             if name == "components" {
                 return "V2"
             } else if let template = PaywallTemplate(rawValue: name) {
@@ -208,9 +207,6 @@ extension APIKeyDashboardList.Template: CustomStringConvertible {
             } else {
                 return "Unrecognized template"
             }
-            #else
-            return name
-            #endif
         } else {
             return "No paywall"
         }
--- Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
@@ -7,5 +7,8 @@ echo "Replacing API keys on PaywallsTester"
 file="$SCRIPT_DIR/../PaywallsTester/Config/ConfigItem.swift"
 sed -i '' 's/static var apiKey: String { "" }/static var apiKey: String { "'"$REVENUECAT_XCODE_CLOUD_RC_APP_API_KEY"'" }/' $file
 
-echo "Enabling PAYWALL_COMPONENTS compiler flag"
-echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS" > "$SCRIPT_DIR/../Local.xcconfig"
+# This allow Local.xcconfig to be found by Xcode
+xcconfig_file="$(cd "$SCRIPT_DIR/../../../../" && pwd)/Local.xcconfig"
+
+echo "Enabling PAYWALL_COMPONENTS compiler flag in $xcconfig_file"
+echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS\nOTHER_SWIFT_FLAGS = PAYWALL_COMPONENTS" > "$xcconfig_file"
