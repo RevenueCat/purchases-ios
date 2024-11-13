2b9ad2c90c8250e306053f58a314673ac8bd8340
Josh Holtz <me@joshholtz.com>
2024-11-13 11:08:21 -0600

    Attempt to fix paywall tester but passing another flag

--- Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
@@ -8,4 +8,4 @@ file="$SCRIPT_DIR/../PaywallsTester/Config/ConfigItem.swift"
 sed -i '' 's/static var apiKey: String { "" }/static var apiKey: String { "'"$REVENUECAT_XCODE_CLOUD_RC_APP_API_KEY"'" }/' $file
 
 echo "Enabling PAYWALL_COMPONENTS compiler flag"
-echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS" > "$SCRIPT_DIR/../Local.xcconfig"
+echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS\nOTHER_SWIFT_FLAGS = -DPAYWALL_COMPONENTS" > "$SCRIPT_DIR/../Local.xcconfig"
