68c7e6b99e24ae1ddf32b73da1a327ea58e26033
Josh Holtz <me@joshholtz.com>
2024-11-13 11:53:09 -0600

    Correct path for Local.xcconfig

--- Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
+++ Tests/TestingApps/PaywallsTester/ci_scripts/ci_pre_xcodebuild.sh
@@ -7,5 +7,7 @@ echo "Replacing API keys on PaywallsTester"
 file="$SCRIPT_DIR/../PaywallsTester/Config/ConfigItem.swift"
 sed -i '' 's/static var apiKey: String { "" }/static var apiKey: String { "'"$REVENUECAT_XCODE_CLOUD_RC_APP_API_KEY"'" }/' $file
 
-echo "Enabling PAYWALL_COMPONENTS compiler flag in $SCRIPT_DIR/../Local.xcconfig"
-echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS\nOTHER_SWIFT_FLAGS = PAYWALL_COMPONENTS" > "$SCRIPT_DIR/../Local.xcconfig"
+xcconfig_file="$(cd "$SCRIPT_DIR/../../../../../" && pwd)/Local.xcconfig"
+
+echo "Enabling PAYWALL_COMPONENTS compiler flag in $xcconfig_file"
+echo "SWIFT_ACTIVE_COMPILATION_CONDITIONS = \$(inherited) PAYWALL_COMPONENTS\nOTHER_SWIFT_FLAGS = PAYWALL_COMPONENTS" > "$xcconfig_file"
