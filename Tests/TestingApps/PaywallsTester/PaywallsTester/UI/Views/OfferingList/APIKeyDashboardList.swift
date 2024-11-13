507f84b5c5aa847873fa48e78daf639a7f2ef83d
Josh Holtz <me@joshholtz.com>
2024-11-13 14:02:51 -0600

    Maybe this

--- Tests/TestingApps/PaywallsTester/PaywallsTester/UI/Views/OfferingList/APIKeyDashboardList.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/UI/Views/OfferingList/APIKeyDashboardList.swift
@@ -176,9 +176,10 @@ struct APIKeyDashboardList: View {
                     Text(self.offering.serverDescription)
                     Spacer()
                     #if PAYWALL_COMPONENTS
-                    Image(systemName: "exclamationmark.circle.fill")
-                        .foregroundStyle(Color.red)
-                        .hidden(if: self.offering.paywallComponentsData?.errorInfo?.isEmpty ?? true)
+                    if let errorInfo = self.offering.paywallComponentsData?.errorInfo, !errorInfo.isEmpty {
+                        Image(systemName: "exclamationmark.circle.fill")
+                            .foregroundStyle(Color.red)
+                    }
                     #endif
                 }
             }
