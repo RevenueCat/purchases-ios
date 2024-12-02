4b3d37e73f85f9a93c0e7b7613d0d8bbf15e379c
Noah Martin <noahm444@gmail.com>
2024-12-02 07:57:19 -0800

    Add preview test target (#4405)

---
+++ Tests/TestingApps/PaywallsTester/PaywallsTesterTests/PaywallsTesterTests.swift
@@ -0,0 +1,13 @@
+//
+//  PaywallsTesterTests.swift
+//  PaywallsTesterTests
+//
+//  Created by Noah Martin on 10/24/24.
+//
+
+import XCTest
+import SnapshottingTests
+
+final class PaywallsTesterTests: SnapshotTest {
+
+}
