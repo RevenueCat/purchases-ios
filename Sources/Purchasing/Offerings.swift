774fc1f4d0f30b7825e7d2740fbd805d7a44e1be
Josh Holtz <me@joshholtz.com>
2024-12-20 12:30:47 -0600

    Fix current offering and sticky footer (#4617)

--- Sources/Purchasing/Offerings.swift
+++ Sources/Purchasing/Offerings.swift
@@ -162,11 +162,21 @@ private extension Offering {
             )
         }
 
+        #if PAYWALL_COMPONENTS
+        return Offering(identifier: self.identifier,
+                        serverDescription: self.serverDescription,
+                        metadata: self.metadata,
+                        paywall: self.paywall,
+                        paywallComponentsData: self.paywallComponentsData,
+                        availablePackages: updatedPackages
+        )
+        #else
         return Offering(identifier: self.identifier,
                         serverDescription: self.serverDescription,
                         metadata: self.metadata,
                         paywall: self.paywall,
                         availablePackages: updatedPackages
         )
+        #endif
     }
 }
