069df52867b59dfaeffb6c15dd8fd8ab12499356
Josh Holtz <me@joshholtz.com>
2025-01-09 11:16:20 -0600

    [Paywalls V2] Decode rectangle corners as optional (#4640)
    * Decode rectangle corners as optional
    
    * And fix for shape
    
    * Added tests

---
+++ Tests/UnitTests/Paywalls/Components/Properties/MaskShapeComponentTests.swift
@@ -0,0 +1,82 @@
+import Nimble
+@testable import RevenueCat
+import XCTest
+
+#if PAYWALL_COMPONENTS
+
+class MaskShapeComponentTests: TestCase {
+
+    func testRectangleNoCorners() throws {
+        let json = """
+        {
+            "type": "rectangle",
+        }
+        """
+
+        _ = try JSONDecoder.default.decode(
+            PaywallComponent.MaskShape.self,
+            from: json.data(using: .utf8)!
+        )
+    }
+
+    func testRectangleWithCorners() throws {
+        let json = """
+        {
+            "type": "rectangle",
+            "corners": {
+                "top_leading": 5,
+                "top_trailing": 5,
+                "bottom_leading": 5,
+                "bottom_trailing": 5
+            }
+        }
+        """
+
+        _ = try JSONDecoder.default.decode(
+            PaywallComponent.MaskShape.self,
+            from: json.data(using: .utf8)!
+        )
+    }
+
+    func testPill() throws {
+        let json = """
+        {
+            "type": "pill",
+        }
+        """
+
+        _ = try JSONDecoder.default.decode(
+            PaywallComponent.MaskShape.self,
+            from: json.data(using: .utf8)!
+        )
+    }
+
+    func testConvex() throws {
+        let json = """
+        {
+            "type": "convex",
+        }
+        """
+
+        _ = try JSONDecoder.default.decode(
+            PaywallComponent.MaskShape.self,
+            from: json.data(using: .utf8)!
+        )
+    }
+
+    func testConcave() throws {
+        let json = """
+        {
+            "type": "concave",
+        }
+        """
+
+        _ = try JSONDecoder.default.decode(
+            PaywallComponent.MaskShape.self,
+            from: json.data(using: .utf8)!
+        )
+    }
+
+}
+
+#endif
