24605d7ac89c104a0d515fb3eb7e65bd872ec44c
Josh Holtz <me@joshholtz.com>
2025-01-13 07:47:11 -0600

    More fixes from rebase

--- RevenueCatUI/Templates/V2/Previews/TemplateComponentsViewPreviews/FamilySharingTogglePreview.swift
+++ RevenueCatUI/Templates/V2/Previews/TemplateComponentsViewPreviews/FamilySharingTogglePreview.swift
@@ -42,8 +42,10 @@ private enum FamilySharingTogglePreview {
             )
         ),
         fitMode: .fill,
-        maxHeight: 200,
-        gradientColors: ["#ffffff00", "#ffffff00", "#ffffffff"]
+        colorOverlay: .init(light: .linear(0, [
+            .init(color: "#ffffff", percent: 0),
+            .init(color: "#ffffff00", percent: 40)
+        ]))
     )
 
     static let catFamilyImage = PaywallComponent.ImageComponent(
@@ -57,8 +59,10 @@ private enum FamilySharingTogglePreview {
             )
         ),
         fitMode: .fill,
-        maxHeight: 200,
-        gradientColors: ["#ffffff00", "#ffffff00", "#ffffffff"]
+        colorOverlay: .init(light: .linear(0, [
+            .init(color: "#ffffff", percent: 0),
+            .init(color: "#ffffff00", percent: 40)
+        ]))
     )
 
     static let title = PaywallComponent.TextComponent(
--- RevenueCatUI/Templates/V2/Previews/TemplateComponentsViewPreviews/MultiTierPreview.swift
+++ RevenueCatUI/Templates/V2/Previews/TemplateComponentsViewPreviews/MultiTierPreview.swift
@@ -42,8 +42,10 @@ private enum MultiTierPreview {
             )
         ),
         fitMode: .fill,
-        maxHeight: 200,
-        gradientColors: ["#ffffff00", "#ffffff00", "#ffffffff"]
+        colorOverlay: .init(light: .linear(0, [
+            .init(color: "#ffffff", percent: 0),
+            .init(color: "#ffffff00", percent: 40)
+        ]))
     )
 
     static let title = PaywallComponent.TextComponent(
