50493deb87bcb8537fb98e7d43dea71a684dfb15
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-01-17 17:26:44 +0100

    Add root error info to public error

--- Sources/Error Handling/PurchasesError.swift
+++ Sources/Error Handling/PurchasesError.swift
@@ -12,6 +12,7 @@
 //  Created by Nacho Soto on 8/31/22.
 
 import Foundation
+import StoreKit
 
 /// An error returned by a `RevenueCat` public API.
 public typealias PublicError = NSError
@@ -39,7 +40,29 @@ extension PurchasesError {
     /// let errorCode = error as? ErrorCode
     /// ```
     var asPublicError: PublicError {
-        return NSError(domain: Self.errorDomain, code: self.errorCode, userInfo: self.userInfo)
+        let rootError: Error = self.rootError(from: self)
+        let rootNSError = rootError as NSError
+        var rootErrorInfo = [
+            "code": rootNSError.code,
+            "domain": rootNSError.domain,
+            "localizedDescription": rootNSError.localizedDescription,
+        ] as [String : Any]
+        if #available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *) {
+            if let storeKitErrorInfo = self.getStoreKitErrorInfoIfAny(error: rootError) {
+                rootErrorInfo = rootErrorInfo.merging(["storeKitError": storeKitErrorInfo])
+            }
+        }
+        let userInfoToUse = self.userInfo.merging([ErrorDetails.rootErrorKey: rootErrorInfo])
+        return NSError(domain: Self.errorDomain, code: self.errorCode, userInfo: userInfoToUse)
+    }
+
+    private func rootError(from error: Error) -> Error {
+        let nsError = error as NSError
+        if let underlyingError = nsError.userInfo[NSUnderlyingErrorKey] as? Error {
+            return rootError(from: underlyingError)
+        } else {
+            return error
+        }
     }
 
 }
@@ -62,6 +85,45 @@ extension PurchasesError {
     /// Overload of the default initializer with `NSError.UserInfoKey` as user info key type.
     init(error: ErrorCode, userInfo: [NSError.UserInfoKey: Any]) {
         self.init(error: error, userInfo: userInfo as [String: Any])
+    }
+
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
+private extension PurchasesError {
+
+    func getStoreKitErrorInfoIfAny(error: Error) -> [String: Any]? {
+        if let skError = error as? SKError {
+            return [
+                "skErrorCode": skError.code,
+                "description": skError.code.trackingDescription
+            ]
+        } else if let storeKitError = error as? StoreKitError {
+            let resultMap: [String: Any] = ["description": storeKitError.trackingDescription]
+            switch storeKitError {
+            case .unknown,
+                    .userCancelled,
+                    .notAvailableInStorefront,
+                    .notEntitled:
+                return resultMap
+            case let .networkError(urlError):
+                return resultMap.merging([
+                    "urlErrorCode": urlError.errorCode,
+                    "urlErrorFailingUrl": urlError.failureURLString ?? ""
+                ])
+            case let .systemError(systemError):
+                return resultMap.merging([
+                    "systemErrorDescription": systemError.localizedDescription
+                ])
+
+            @unknown default:
+                return resultMap
+            }
+        } else if let storeKitError = error as? StoreKit.Product.PurchaseError {
+            return ["description": storeKitError.trackingDescription]
+        } else {
+            return nil
+        }
     }
 
 }
