6ec9cd23dbd3fc70d49652ce47096e0d085c3992
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-01-27 16:25:21 +0100

    add new opt-in public API `uiPreviewMode` to `DangerousSettings`

--- Sources/Misc/DangerousSettings.swift
+++ Sources/Misc/DangerousSettings.swift
@@ -59,6 +59,12 @@ import Foundation
     @objc public let autoSyncPurchases: Bool
 
     /**
+     * if `true`, the SDK will return a set of mock products instead of the
+     * products obtained from StoreKit. This is useful for testing or preview purposes. 
+     */
+    @_spi(Internal) public let uiPreviewMode: Bool
+
+    /**
      * A property meant for apps that do their own entitlements computation, separated from RevenueCat.
      * It:
      *   - disables automatic CustomerInfo cache updates
@@ -101,13 +107,24 @@ import Foundation
 
     }
 
-    /// Designated initializer
+    /**
+     * Only use a Dangerous Setting if suggested by RevenueCat support team.
+     *
+     * - Parameter uiPreviewMode: if `true`, the SDK will return a set of mock products instead of the
+     * products obtained from StoreKit. This is useful for testing or preview purposes. 
+     */
+    @_spi(Internal) public init(uiPreviewMode: Bool) {
+        self.init(autoSyncPurchases: false, internalSettings: Internal.default, uiPreviewMode: uiPreviewMode)
+    }
+
     internal init(autoSyncPurchases: Bool,
                   customEntitlementComputation: Bool = false,
-                  internalSettings: InternalDangerousSettingsType) {
+                  internalSettings: InternalDangerousSettingsType,
+                  uiPreviewMode: Bool = false) {
         self.autoSyncPurchases = autoSyncPurchases
         self.internalSettings = internalSettings
         self.customEntitlementComputation = customEntitlementComputation
+        self.uiPreviewMode = uiPreviewMode
     }
 
 }
