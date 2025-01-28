f06e3b1f0a1a2e19c31944cf9d424c8ae289095f
Facundo Menzella <facumenzella@gmail.com>
2025-01-28 17:59:37 +0100

    tweak dark mode

--- RevenueCatUI/CustomerCenter/Views/PurchaseHistory/PurchaseLinkView.swift
+++ RevenueCatUI/CustomerCenter/Views/PurchaseHistory/PurchaseLinkView.swift
@@ -18,6 +18,10 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 struct PurchaseLinkView: View {
+
+    @Environment(\.colorScheme)
+    private var colorScheme
+
     @Environment(\.localization)
     private var localization: CustomerCenterConfigData.Localization
 
@@ -41,14 +45,14 @@ struct PurchaseLinkView: View {
             if let price = purchaseInfo.paidPrice {
                 Text(price)
                     .font(.subheadline)
-                    .foregroundStyle(.tertiary)
+                    .foregroundStyle(.secondary)
             }
 
             Image(systemName: "chevron.forward")
                 .foregroundStyle(.secondary)
         }
         .contentShape(Rectangle())
-        .tint(.black)
+        .tint(colorScheme == .dark ? .white : .black)
         .onAppear {
             Task {
                 guard
