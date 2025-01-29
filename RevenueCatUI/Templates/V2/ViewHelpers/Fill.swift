043b4075495c771886352eba0465d383f0115c21
Facundo Menzella <facumenzella@gmail.com>
2025-01-29 12:54:01 +0100

    use Group as a temporary ix

--- RevenueCatUI/Templates/V2/ViewHelpers/Fill.swift
+++ RevenueCatUI/Templates/V2/ViewHelpers/Fill.swift
@@ -19,40 +19,40 @@ import SwiftUI
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 extension Shape {
 
-    @ViewBuilder
     func fillColorScheme(
         _ color: DisplayableColorScheme,
         colorScheme: ColorScheme
     ) -> some View {
-        let effectiveColor = color.effectiveColor(for: colorScheme)
-        switch effectiveColor {
-        case .hex:
-            // Do not apply a clear text color
-            // Use the default color
-            if color.hasError {
-                return self
-            } else {
-                return self.fill(color.toDynamicColor())
+        Group {
+            let effectiveColor = color.effectiveColor(for: colorScheme)
+            switch effectiveColor {
+            case .hex:
+                // Do not apply a clear text color
+                // Use the default color
+                if color.hasError {
+                    return self
+                } else {
+                    return self.fill(color.toDynamicColor())
+                }
+            case .linear(let degrees, _):
+                return self.fill(
+                    LinearGradient(
+                        gradient: effectiveColor.toGradient(),
+                        startPoint: UnitPoint(angle: Angle(degrees: Double(degrees))),
+                        endPoint: UnitPoint(angle: Angle(degrees: Double(degrees+180)))
+                    )
+                )
+            case .radial:
+                return self.fill(
+                    RadialGradient(
+                        gradient: effectiveColor.toGradient(),
+                        center: .center,
+                        startRadius: 0,
+                        endRadius: 100
+                    )
+                )
             }
-        case .linear(let degrees, _):
-            return self.fill(
-                LinearGradient(
-                    gradient: effectiveColor.toGradient(),
-                    startPoint: UnitPoint(angle: Angle(degrees: Double(degrees))),
-                    endPoint: UnitPoint(angle: Angle(degrees: Double(degrees+180)))
-                )
-            )
-        case .radial:
-            return self.fill(
-                RadialGradient(
-                    gradient: effectiveColor.toGradient(),
-                    center: .center,
-                    startRadius: 0,
-                    endRadius: 100
-                )
-            )
         }
-
     }
 }
 
