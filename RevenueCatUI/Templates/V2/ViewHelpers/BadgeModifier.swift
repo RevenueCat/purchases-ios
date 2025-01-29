1d2ae92e339534ff78561bb454a34085223a2c01
Facundo Menzella <facumenzella@gmail.com>
2025-01-29 11:35:27 +0100

    test rule

--- RevenueCatUI/Templates/V2/ViewHelpers/BadgeModifier.swift
+++ RevenueCatUI/Templates/V2/ViewHelpers/BadgeModifier.swift
@@ -33,7 +33,8 @@ struct BadgeModifier: ViewModifier {
         let uiConfigProvider: UIConfigProvider
 
         var backgroundStyle: BackgroundStyle? {
-            stack.backgroundColor?.asDisplayable(uiConfigProvider: uiConfigProvider).backgroundStyle
+            stack.backgroundColor?
+                .asDisplayable(uiConfigProvider: uiConfigProvider).backgroundStyle
         }
     }
 
