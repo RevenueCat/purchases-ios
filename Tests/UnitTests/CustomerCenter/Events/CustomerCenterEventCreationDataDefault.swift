fa3ea3264d0c24bc4b8f2ef4c88814d9b4707321
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-02-03 12:17:31 +0100

    feat: Add tests for customer center events encoding (#4739)

---
+++ Tests/UnitTests/CustomerCenter/Events/CustomerCenterEventCreationDataDefault.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  CustomerCenterEventCreationDataDefault.swift
+//
+//  Created by Facundo Menzella on 29/1/25.
+
+import Foundation
+@testable import RevenueCat
+import SnapshotTesting
+
+extension CustomerCenterEventCreationData {
+    static var `default`: CustomerCenterEventCreationData {
+        CustomerCenterEventCreationData(
+            id: .init(uuidString: "72164C05-2BDC-4807-8918-A4105F727DEB")!,
+            date: .init(timeIntervalSince1970: 1694029328)
+        )
+    }
+}
