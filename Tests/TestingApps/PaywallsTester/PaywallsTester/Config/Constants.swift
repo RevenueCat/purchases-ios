b077e9e0b9a49e4c51a0aca9a7fbf7012c011ae2
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-02-19 12:41:15 +0100

    Config item rename (#4798)
    * add deeper explanation to `refreshableDisabled()` modifier introduce in #4793
    
    * refactor: rename `ConfigItem` to `Constants` in PaywallsTester app
    
    * fix lint warnings

--- Tests/TestingApps/PaywallsTester/PaywallsTester/Config/Constants.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/Config/Constants.swift
@@ -1,5 +1,5 @@
 //
-//  ConfigItem.swift
+//  Constants.swift
 //  PaywallsTester
 //
 //  Created by James Borthwick on 2024-05-13.
@@ -9,16 +9,20 @@ import Foundation
 
 // DO NOT MODIFY THIS FILE.
 // CI system adds the API key here.
-struct ConfigItem {
+enum Constants {
     /*
      To add your own API key for local development, add it in your local.xcconfig file like this:
      REVENUECAT_API_KEY = your-api-key
      */
-    static var apiKey: String { Bundle.main.object(forInfoDictionaryKey: "REVENUECAT_API_KEY") as? String ?? "" }
+    static let apiKey: String = {
+        Bundle.main.object(forInfoDictionaryKey: "REVENUECAT_API_KEY") as? String ?? ""
+    }()
 
     /*
      To add your own proxyURL for local development, add it in your local.xcconfig file like this:
      REVENUECAT_PROXY_URL = your-api-key
      */
-    static var proxyURL: String? { Bundle.main.object(forInfoDictionaryKey: "REVENUECAT_PROXY_URL") as? String }
+    static let proxyURL: String? = {
+        Bundle.main.object(forInfoDictionaryKey: "REVENUECAT_PROXY_URL") as? String
+    }()
 }
