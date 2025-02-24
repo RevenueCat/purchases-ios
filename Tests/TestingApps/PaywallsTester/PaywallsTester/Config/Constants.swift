f208ea1c30e6626ce383733521acb2f65e0b6148
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-02-24 10:43:57 +0100

    fix: workaround to allow using proxyURL in `Local.xcconfig` (#4810)

--- Tests/TestingApps/PaywallsTester/PaywallsTester/Config/Constants.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTester/Config/Constants.swift
@@ -23,6 +23,14 @@ enum Constants {
      REVENUECAT_PROXY_URL = your-api-key
      */
     static let proxyURL: String? = {
-        Bundle.main.object(forInfoDictionaryKey: "REVENUECAT_PROXY_URL") as? String
+        guard
+            var scheme = Bundle.main.object(forInfoDictionaryKey: "REVENUECAT_PROXY_URL_SCHEME") as? String,
+        let host = Bundle.main.object(forInfoDictionaryKey: "REVENUECAT_PROXY_URL_HOST") as? String else {
+            return nil
+        }
+        if !scheme.hasSuffix(":") {
+            scheme.append(":")
+        }
+        return "\(scheme)//\(host)"
     }()
 }
