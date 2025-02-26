dd54f55c0a44295c805df36de3fbd8434a1eb763
Facundo Menzella <facumenzella@gmail.com>
2025-02-26 11:43:25 +0100

    refund not cancel

--- RevenueCatUI/CustomerCenter/ViewModels/ManageSubscriptions/ManageSubscriptionsViewModel.swift
+++ RevenueCatUI/CustomerCenter/ViewModels/ManageSubscriptions/ManageSubscriptionsViewModel.swift
@@ -264,11 +264,11 @@ private extension Array<CustomerCenterConfigData.HelpPath> {
         }
 
         return filter {
-            // if it's cancel, it cannot be a lifetime subscription nor within trial period
-            ($0.type != .cancel || (!purchaseInformation.isLifetime || !purchaseInformation.isTrial)) &&
+            // if it's cancel, it cannot be a lifetime subscription
+            ($0.type != .cancel || (!purchaseInformation.isLifetime) &&
 
-            // if it's refundRequest, it cannot be free
-            ($0.type != .refundRequest || purchaseInformation.price != .free) &&
+            // if it's refundRequest, it cannot be free  nor within trial period
+            ($0.type != .refundRequest || (purchaseInformation.price != .free || !purchaseInformation.isTrial)) &&
 
             // if it has a refundDuration, check it's still valid
             ($0.refundWindowDuration?.isWithin(purchaseInformation) ?? true)
