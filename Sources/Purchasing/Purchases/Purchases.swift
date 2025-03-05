05fc0a90f3dd7b0461684f4411af66e101329235
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-03-05 18:04:59 +0100

    feat: disable postAttributionData requests when in UI preview mode (#4824)
    * feat: disable postAttributionData requests when in UI preview mode
    
    * tests: add unit test to check `AttributionPoster`'s new `clearPostponedAttributionData` method
    
    * tests: add unit tests to check that `AttributionData` is never posted in UI preview mode
    
    * refactor: simplify implementation of avoiding `AttributionPoster` requests in UI preview mode

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -436,7 +436,8 @@ public typealias StartPurchaseBlock = (@escaping PurchaseCompletedBlock) -> Void
                                                   currentUserProvider: identityManager,
                                                   backend: backend,
                                                   attributionFetcher: attributionFetcher,
-                                                  subscriberAttributesManager: subscriberAttributesManager)
+                                                  subscriberAttributesManager: subscriberAttributesManager,
+                                                  systemInfo: systemInfo)
         let subscriberAttributes = Attribution(subscriberAttributesManager: subscriberAttributesManager,
                                                currentUserProvider: identityManager,
                                                attributionPoster: attributionPoster,
@@ -700,6 +701,7 @@ public typealias StartPurchaseBlock = (@escaping PurchaseCompletedBlock) -> Void
         self.paymentQueueWrapper.sk2Wrapper?.delegate = purchasesOrchestrator
 
         self.subscribeToAppStateNotifications()
+
         self.attributionPoster.postPostponedAttributionDataIfNeeded()
 
         self.customerInfoObservationDisposable = customerInfoManager.monitorChanges { [weak self] old, new in
