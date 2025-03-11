29ce57c0550605787753f2f739dd122f36e5b2d5
Pol Piella Abadia <hi@polpiella.dev>
2025-03-11 10:36:20 +0200

    Initial app creation

---
+++ Examples/SampleCat/SampleCat/SampleCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCatApp.swift
+//  SampleCat
+//
+//  Created by Pol Piella Abadia on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
