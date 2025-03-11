b32ad6a61012254cbdbb2107674cdfe294d93e13
Pol Piella Abadia <hi@polpiella.dev>
2025-03-11 10:49:30 +0200

    Installs and configures the SDK

--- Examples/SampleCat/SampleCat/SampleCatApp.swift
+++ Examples/SampleCat/SampleCat/SampleCatApp.swift
@@ -1,7 +1,12 @@
+import RevenueCat
 import SwiftUI
 
 @main
 struct SampleCatApp: App {
+    init() {
+        Purchases.configure(withAPIKey: Constants.apiKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
