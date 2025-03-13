59f8839e22c195d2859a7f5db2300f70ec9a163a
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-03-13 18:46:09 +0100

    Revert "remove unused internal method"
    This reverts commit 466caa84d8e71034e02c3086971fc8fe6adbaa9e.

--- Sources/Identity/CustomerInfoManager.swift
+++ Sources/Identity/CustomerInfoManager.swift
@@ -369,6 +369,14 @@ class CustomerInfoManager {
 
 extension CustomerInfoManager {
 
+    func fetchAndCacheCustomerInfo(appUserID: String, isAppBackgrounded: Bool) async throws -> CustomerInfo {
+        return try await Async.call { completion in
+            return self.fetchAndCacheCustomerInfo(appUserID: appUserID,
+                                                  isAppBackgrounded: isAppBackgrounded,
+                                                  completion: completion)
+        }
+    }
+
     func customerInfo(
         appUserID: String,
         fetchPolicy: CacheFetchPolicy
