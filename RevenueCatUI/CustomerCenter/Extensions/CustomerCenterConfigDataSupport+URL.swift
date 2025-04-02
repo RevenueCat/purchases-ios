d9a626ae2da7e6a65b5da9a4fa08392c6f53cd5e
Facundo Menzella <facumenzella@gmail.com>
2025-04-02 16:59:00 +0200

    add availability to method not to extension

--- RevenueCatUI/CustomerCenter/Extensions/CustomerCenterConfigDataSupport+URL.swift
+++ RevenueCatUI/CustomerCenter/Extensions/CustomerCenterConfigDataSupport+URL.swift
@@ -14,12 +14,12 @@
 import Foundation
 import RevenueCat
 
-@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-@available(macOS, unavailable)
-@available(tvOS, unavailable)
-@available(watchOS, unavailable)
 extension CustomerCenterConfigData.Support {
 
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    @available(macOS, unavailable)
+    @available(tvOS, unavailable)
+    @available(watchOS, unavailable)
     func supportURL(localization: CustomerCenterConfigData.Localization) -> URL? {
         let subject = localization[.defaultSubject]
         let body = calculateBody(localization)
