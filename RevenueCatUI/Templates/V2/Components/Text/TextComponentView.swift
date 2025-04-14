6961155a52c19f61c340023f756bb65ce6d9bd35
Josh Holtz <me@joshholtz.com>
2025-04-14 11:21:25 -0500

    Fix Paywalls v2 Text component to not localized from bundle but support markdown

--- RevenueCatUI/Templates/V2/Components/Text/TextComponentView.swift
+++ RevenueCatUI/Templates/V2/Components/Text/TextComponentView.swift
@@ -50,9 +50,7 @@ struct TextComponentView: View {
             )
         ) { style in
             if style.visible {
-                Text(verbatim: style.text)
-                    .font(style.font)
-                    .fontWeight(style.fontWeight)
+                NonLocalizedMarkdownText(text: style.text, font: style.font, fontWeight: style.fontWeight)
                     .fixedSize(horizontal: false, vertical: true)
                     .multilineTextAlignment(style.textAlignment)
                     .foregroundColorScheme(style.color)
@@ -65,6 +63,33 @@ struct TextComponentView: View {
         }
     }
 
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+/// Parses markdown using AttributedString and does not use bundle assets for localization
+private struct NonLocalizedMarkdownText: View {
+
+    let text: String
+    let font: Font
+    let fontWeight: Font.Weight
+
+    var markdownText: AttributedString? {
+        return try? AttributedString(markdown: self.text)
+    }
+
+    var body: some View {
+        Group {
+            if let markdownText = self.markdownText {
+                Text(markdownText)
+                    .font(self.font)
+                    .fontWeight(self.fontWeight)
+            } else {
+                Text(verbatim: self.text)
+                    .font(self.font)
+                    .fontWeight(self.fontWeight)
+            }
+        }
+    }
 }
 
 #if DEBUG
@@ -94,6 +119,27 @@ struct TextComponentView_Previews: PreviewProvider {
         .previewRequiredEnvironmentProperties()
         .previewLayout(.sizeThatFits)
         .previewDisplayName("Default")
+
+        // Markdown
+        TextComponentView(
+            // swiftlint:disable:next force_try
+            viewModel: try! .init(
+                localizationProvider: .init(
+                    locale: Locale.current,
+                    localizedStrings: [
+                        "id_1": .string("Hello, world **bold** _italic_ `code` [RevenueCat](https://revenuecat.com)")
+                    ]
+                ),
+                uiConfigProvider: .init(uiConfig: PreviewUIConfig.make()),
+                component: .init(
+                    text: "id_1",
+                    color: .init(light: .hex("#000000"))
+                )
+            )
+        )
+        .previewRequiredEnvironmentProperties()
+        .previewLayout(.sizeThatFits)
+        .previewDisplayName("Markdown")
 
         // Custom Font
         VStack {
