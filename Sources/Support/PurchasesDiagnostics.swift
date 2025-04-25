630d3e6b09a18bf7671c5b959052109998571bae
Pol Piella Abadia <hi@polpiella.dev>
2025-04-25 18:19:26 +0100

    Fixes lint errors

--- Sources/Support/PurchasesDiagnostics.swift
+++ Sources/Support/PurchasesDiagnostics.swift
@@ -201,11 +201,11 @@ extension PurchasesDiagnostics {
         case unhealthy(PurchasesDiagnostics.SDKHealthError)
     }
 
+    /// Perform tests to ensure SDK is configured correctly.
+    /// - `Throws`: ``PurchasesDiagnostics/Error`` if any step fails
     @available(*, deprecated, message: """
     Use the `PurchasesDiagnostics.shared.checkSDKHealth()` method instead.
     """)
-    /// Perform tests to ensure SDK is configured correctly.
-    /// - `Throws`: ``PurchasesDiagnostics/Error`` if any step fails
     @objc(testSDKHealthWithCompletion:)
     public func testSDKHealth() async throws {
         do {
@@ -325,67 +325,6 @@ private extension PurchasesDiagnostics {
 
         default:
             return Error.unknown(error)
-        }
-    }
-
-}
-
-extension PurchasesDiagnostics.SDKHealthError : CustomNSError {
-
-    // swiftlint:disable:next missing_docs
-    public var errorUserInfo: [String: Any] {
-        return [
-            NSUnderlyingErrorKey: self.underlyingError as NSError? ?? NSNull(),
-            NSLocalizedDescriptionKey: self.localizedDescription
-        ]
-    }
-
-    var localizedDescription: String {
-        switch self {
-        case .notAuthorizedToMakePayments: return "The person is not authorized to make payments on this device"
-        case let .unknown(error): return "Unknown error: \(error.localizedDescription)"
-        case .invalidAPIKey: return "API key is not valid"
-        case .noOfferings: return "No offerings configured"
-        case let .offeringConfiguration(payload):
-            guard let offendingOffering = payload.first(where: { $0.status == .failed }) else {
-                return "Default offering is not configured correctly"
-            }
-
-            let offeringIdentifier = offendingOffering.identifier
-            let offendingPackageCount = offendingOffering.packages.filter({ $0.status != .valid }).count
-            let noPackages = "Offering '\(offeringIdentifier)' has no packages"
-            let packagesNotReady = """
-            Offering '\(offeringIdentifier)' uses \(offendingPackageCount) products \
-            that are not ready in App Store Connect
-            """
-
-            return offendingOffering.packages.isEmpty ? noPackages : packagesNotReady
-        case let .invalidBundleId(payload):
-            guard let payload else {
-                return "Bundle ID in your app does not match the Bundle ID in the RevenueCat Website"
-            }
-            let sdkBundleId = payload.sdkBundleId
-            let appBundleId = payload.appBundleId
-            return "Bundle ID in your app '\(sdkBundleId)' does not match the RevenueCat app Bundle ID '\(appBundleId)'"
-        case let .invalidProducts(products):
-            if products.isEmpty {
-                return "Your app has no products"
-            }
-
-            return "You must have at least one product approved in App Store Connect"
-        }
-    }
-
-    private var underlyingError: Swift.Error? {
-        switch self {
-        case let .unknown(error): return error
-        case .invalidAPIKey,
-                .offeringConfiguration,
-                .noOfferings,
-                .invalidBundleId,
-                .invalidProducts,
-                .notAuthorizedToMakePayments:
-            return nil
         }
     }
 
