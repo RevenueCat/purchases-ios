623addd50e1201d55b535007b1552773c3752378
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-05-16 10:34:49 +0200

    Update RevenueCatUI/CustomerCenter/Mocks/PurchaseInformationFixtures.swift

--- RevenueCatUI/CustomerCenter/Mocks/PurchaseInformationFixtures.swift
+++ RevenueCatUI/CustomerCenter/Mocks/PurchaseInformationFixtures.swift
@@ -12,7 +12,7 @@
 //  Created by Cesar de la Vega on 10/25/24.
 
 import Foundation
-@testable import RevenueCat
+import RevenueCat
 import StoreKit
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
