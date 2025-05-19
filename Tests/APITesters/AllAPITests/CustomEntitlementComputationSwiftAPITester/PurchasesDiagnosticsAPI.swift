6fc71c6de20fe340e2e16fe09443f8b9cfffabf9
Pol Piella Abadia <hi@polpiella.dev>
2025-05-19 10:32:14 +0100

    Adds tests for SDK health report in the API tests

--- Tests/APITesters/AllAPITests/CustomEntitlementComputationSwiftAPITester/PurchasesDiagnosticsAPI.swift
+++ Tests/APITesters/AllAPITests/CustomEntitlementComputationSwiftAPITester/PurchasesDiagnosticsAPI.swift
@@ -13,6 +13,11 @@ func checkPurchasesDiagnostics() {
 }
 
 private func checkPurchasesDiagnosticsAsync(_ diagnostics: PurchasesDiagnostics) async {
+    try? await diagnostics.checkSDKHealth()
+}
+
+@available(*, deprecated) // Ignore deprecation warnings
+private func checkDeprecatedPurchasesDiagnosticsAsync(_ diagnostics: PurchasesDiagnostics) async {
     _ = try? await diagnostics.testSDKHealth()
 }
 
@@ -31,6 +36,25 @@ func checkDiagnosticsErrors(_ error: PurchasesDiagnostics.Error) {
         print(error)
 
     case let .unknown(error):
+        print(error)
+    }
+}
+
+func checkHealthReportErrors(_ error: PurchasesDiagnostics.SDKHealthError) {
+    switch error {
+    case .invalidAPIKey:
+        break
+    case .noOfferings:
+        break
+    case .offeringConfiguration(let offerings):
+        print(offerings)
+    case .invalidBundleId(let invalidBundleIdErrorPayload):
+        if let invalidBundleIdErrorPayload { print(invalidBundleIdErrorPayload) }
+    case .invalidProducts(let products):
+        print(products)
+    case .notAuthorizedToMakePayments:
+        break
+    case .unknown(let error):
         print(error)
     }
 }
