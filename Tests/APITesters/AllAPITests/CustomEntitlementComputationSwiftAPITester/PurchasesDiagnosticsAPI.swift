d129050ce19362c6e20388318ab6118800997575
Pol Piella Abadia <hi@polpiella.dev>
2025-05-19 11:37:48 +0100

    Makes the API tests a lot more thorough

--- Tests/APITesters/AllAPITests/CustomEntitlementComputationSwiftAPITester/PurchasesDiagnosticsAPI.swift
+++ Tests/APITesters/AllAPITests/CustomEntitlementComputationSwiftAPITester/PurchasesDiagnosticsAPI.swift
@@ -47,14 +47,56 @@ func checkHealthReportErrors(_ error: PurchasesDiagnostics.SDKHealthError) {
     case .noOfferings:
         break
     case .offeringConfiguration(let offerings):
-        print(offerings)
+        let copy: [PurchasesDiagnostics.OfferingDiagnosticsPayload] = offerings
+
+        copy.forEach {
+            let _: String = $0.identifier
+            let packages: [PurchasesDiagnostics.OfferingPackageDiagnosticsPayload] = $0.packages
+            packages.forEach { package in
+                let _: String = package.identifier
+                let _: String? = package.title
+                let _: PurchasesDiagnostics.ProductStatus = package.status
+                let _: String = package.description
+                let _: String = package.productIdentifier
+                let _: String? = package.productTitle
+            }
+
+            let _: PurchasesDiagnostics.SDKHealthCheckStatus = $0.status
+            switch $0.status {
+            case .passed: break
+            case .failed: break
+            case .warning: break
+            }
+        }
     case .invalidBundleId(let invalidBundleIdErrorPayload):
-        if let invalidBundleIdErrorPayload { print(invalidBundleIdErrorPayload) }
+        guard let copy: PurchasesDiagnostics.InvalidBundleIdErrorPayload = invalidBundleIdErrorPayload else {
+            return
+        }
+
+        let _: String = copy.appBundleId
+        let _: String = copy.sdkBundleId
     case .invalidProducts(let products):
-        print(products)
+        let copy: [PurchasesDiagnostics.ProductDiagnosticsPayload] = products
+
+        products.forEach {
+            let _: String = $0.identifier
+            let _: String? = $0.title
+            let _: PurchasesDiagnostics.ProductStatus = $0.status
+            let _: String = $0.description
+
+            switch $0.status {
+            case .valid,
+                 .couldNotCheck,
+                 .notFound,
+                 .actionInProgress,
+                 .needsAction,
+                 .unknown:
+                break
+            }
+        }
     case .notAuthorizedToMakePayments:
         break
     case .unknown(let error):
-        print(error)
+        let _: Swift.Error = error
     }
 }
