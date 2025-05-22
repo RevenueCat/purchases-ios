366170699c45fe32b07564bfa76afdee602577c3
Mark Villacampa <m@markvillacampa.com>
2025-05-22 17:23:33 +0200

    lint

--- Sources/Paywalls/Components/PaywallStackComponent.swift
+++ Sources/Paywalls/Components/PaywallStackComponent.swift
@@ -20,7 +20,7 @@ public extension PaywallComponent {
 
         public enum Overflow: String, PaywallComponentBase {
             case `default`
-            case scroll = "scrolllable"
+            case scroll = "scrollable"
 
             public init(from decoder: Decoder) throws {
                 let container = try decoder.singleValueContainer()
