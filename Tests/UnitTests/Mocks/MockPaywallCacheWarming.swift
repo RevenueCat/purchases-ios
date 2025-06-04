1291161251e0f612386a438645000a05887995cd
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-06-04 12:00:48 +0200

    Add missing method to `MockPaywallCacheWarming`

--- Tests/UnitTests/Mocks/MockPaywallCacheWarming.swift
+++ Tests/UnitTests/Mocks/MockPaywallCacheWarming.swift
@@ -70,4 +70,25 @@ final class MockPaywallCacheWarming: PaywallCacheWarmingType {
         self.invokedWarmUpPaywallFontsCache = true
         self.invokedWarmUpPaywallFontsCacheOfferings = offerings
     }
+
+#if !os(macOS) && !os(tvOS)
+
+    private let _invokedTriggerFontDownloadIfNeeded: Atomic<Bool> = false
+    private let _invokedTriggerFontDownloadIfNeededFontsConfig: Atomic<UIConfig.FontsConfig?> = nil
+
+    var invokedTriggerFontDownloadIfNeeded: Bool {
+        get { return self._invokedTriggerFontDownloadIfNeeded.value }
+        set { self._invokedTriggerFontDownloadIfNeeded.value = newValue }
+    }
+    var invokedTriggerFontDownloadIfNeededFontsConfig: UIConfig.FontsConfig? {
+        get { return self._invokedTriggerFontDownloadIfNeededFontsConfig.value }
+        set { self._invokedTriggerFontDownloadIfNeededFontsConfig.value = newValue }
+    }
+
+    func triggerFontDownloadIfNeeded(fontsConfig: UIConfig.FontsConfig) async {
+        self.invokedWarmUpEligibilityCache = true
+        self.invokedTriggerFontDownloadIfNeededFontsConfig = fontsConfig
+    }
+
+#endif
 }
