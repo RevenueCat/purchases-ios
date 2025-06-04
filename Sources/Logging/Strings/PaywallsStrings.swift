2022f232ad2dd438586246e6e3068b020c2819d4
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-06-04 11:27:56 +0200

    Further improve error message on missing `hash` in custom web font

--- Sources/Logging/Strings/PaywallsStrings.swift
+++ Sources/Logging/Strings/PaywallsStrings.swift
@@ -76,7 +76,7 @@ extension PaywallsStrings: LogMessage {
             return "Error installing font \(name). Invalid url: \(invalidURLString)"
 
         case let .error_prefetching_font_missing_hash(name):
-            return "Font \(name) does not have a validation hash. " +
+            return "Font \(name) does not have a validation hash. Skipping download. " +
             "Please try to re-upload the font in the RevenueCat dashboard."
 
         case .caching_presented_paywall:
