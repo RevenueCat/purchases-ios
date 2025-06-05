309d8f26ec263813c302935f3af373a5131524bb
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-06-05 12:58:31 +0200

    Update Sources/Logging/Strings/PaywallsStrings.swift

--- Sources/Logging/Strings/PaywallsStrings.swift
+++ Sources/Logging/Strings/PaywallsStrings.swift
@@ -76,7 +76,7 @@ extension PaywallsStrings: LogMessage {
             return "Font '\(fontName)' download already in progress with url: \(fontURL.absoluteString)"
 
         case let .font_downloaded_sucessfully(fontName, fontURL):
-             return "Successfully downloaded and cached font '\(fontName)' from url: \(fontURL.absoluteString)"
+            return "Successfully downloaded and cached font '\(fontName)' from url: \(fontURL.absoluteString)"
 
         case let .triggering_font_download(fontURL):
             return "Downloading remote font from url: \(fontURL.absoluteString)"
