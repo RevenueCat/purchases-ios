1fd78e354ab50dc33a72110db688cde4f585ce5b
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-06-05 18:14:00 +0200

    Warm up caches in parallel (#5240)
    * warm up caches in parallel
    
    * add back line removed by mistake

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -2107,8 +2107,14 @@ private extension Purchases {
             if #available(iOS 15.0, macOS 12.0, watchOS 8.0, tvOS 15.0, *),
                let cache = cache, let offerings = offeringsResultData.value?.offerings {
                 self.operationDispatcher.dispatchOnWorkerThread {
-                    await cache.warmUpEligibilityCache(offerings: offerings)
-                    await cache.warmUpPaywallImagesCache(offerings: offerings)
+                    await withTaskGroup(of: Void.self) { group in
+                        group.addTask {
+                            await cache.warmUpEligibilityCache(offerings: offerings)
+                        }
+                        group.addTask {
+                            await cache.warmUpPaywallImagesCache(offerings: offerings)
+                        }
+                    }
                 }
             }
         }
