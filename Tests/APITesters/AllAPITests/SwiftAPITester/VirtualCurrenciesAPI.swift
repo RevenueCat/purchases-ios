111c0b400acd6a493945e066402149263280cebe
Will Taylor <wtaylor151@gmail.com>
2025-06-10 14:14:39 -0500

    add VirtualCurrencies API tests

---
+++ Tests/APITesters/AllAPITests/SwiftAPITester/VirtualCurrenciesAPI.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualCurrenciesAPI.swift
+//  SwiftAPITester
+//
+//  Created by Will Taylor on 6/10/25.
+//
+
+import Foundation
+import RevenueCat
+
+class VirtualCurrenciesAPI {
+    func checkVirtualCurrencies(_ virtualCurrencies: VirtualCurrencies) {
+        let all: [String: VirtualCurrency] = virtualCurrencies.all
+        let withZeroBalance: [String: VirtualCurrency] = virtualCurrencies.withZeroBalance
+        let withNonZeroBalance: [String: VirtualCurrency] = virtualCurrencies.withNonZeroBalance
+    }
+}
