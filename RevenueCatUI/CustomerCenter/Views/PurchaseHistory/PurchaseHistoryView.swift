def2d1c6f6bc828673a2917d2bc626f8409e3977
Facundo Menzella <facumenzella@gmail.com>
2025-06-13 09:22:16 +0200

    removed

--- RevenueCatUI/CustomerCenter/Views/PurchaseHistory/PurchaseHistoryView.swift
+++ RevenueCatUI/CustomerCenter/Views/PurchaseHistory/PurchaseHistoryView.swift
@@ -103,23 +103,6 @@ struct PurchaseHistoryView: View {
 @available(iOS 15.0, *)
 struct PurchaseHistoryView_Previews: PreviewProvider {
     static var previews: some View {
-        let customerInfo = CustomerInfoFixtures.customerInfo(
-            subscriptions: [
-                CustomerInfoFixtures.Subscription(
-                    id: "id1",
-                    store: "\(Store.appStore.rawValue)",
-                    purchaseDate: "2022-03-08T17:42:58Z",
-                    expirationDate: nil
-                )
-            ],
-            entitlements: [],
-            nonSubscriptions: [
-                CustomerInfoFixtures.NonSubscriptionTransaction(
-                    id: "id2",
-                    store: "\(Store.playStore.rawValue)",
-                    purchaseDate: "2022-03-08T17:42:58Z"
-                )
-            ])
 
         CompatibilityNavigationStack {
             PurchaseHistoryView(
