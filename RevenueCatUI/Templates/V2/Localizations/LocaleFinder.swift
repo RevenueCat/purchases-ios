0451d8ca857995fa0472de374ba953f110910a6d
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-06-23 14:22:52 +0200

    improve algorithm to find the best locale for localization in paywalls components
    The new implementation uses the API `Bundle.preferredLocalizations(from:forPreferences:)`

--- RevenueCatUI/Templates/V2/Localizations/LocaleFinder.swift
+++ RevenueCatUI/Templates/V2/Localizations/LocaleFinder.swift
@@ -15,60 +15,49 @@ import Foundation
 
 extension Dictionary where Key == String {
 
+    /// Finds the best matching value for the provided locale with the restriction that the key
+    /// must match the language of the provided locale.
     func findLocale(_ locale: Locale) -> Value? {
-        let localeIdentifier = locale.identifier
+        let preferredIdentifiers = Self.preferredMatchedLocalesIdentifiers(from: Array(self.keys),
+                                                                           preferredLanguages: [locale.identifier])
 
-        if let exactMatch = self.valueForLocaleString(localeIdentifier) {
-            return exactMatch
-        }
-
-        // For matching unknown locales with full identifier
-        // Ex: `zh_CN` will become `zh_Hans_CN`
-        if let maxIdentifier = locale.rc_lanuageMaxIdentifier {
-            if let exactMatch = self.valueForLocaleString(maxIdentifier) {
-                return exactMatch
+        for localeIdentifier in preferredIdentifiers {
+            if let value = self[localeIdentifier] {
+                return value
             }
-        }
-
-        // For matching language and script without region
-        if let onlyLanguageAndScriptIdentifier = locale.rc_languageAndScript {
-            if let exactMatch = self.valueForLocaleString(onlyLanguageAndScriptIdentifier) {
-                return exactMatch
-            } else if let match = self.valueForLanguageAndScript(onlyLanguageAndScriptIdentifier) {
-                // Matching language and script converting dictionary keys to language and script as well
-                // This covers cases where dictionary keys contain the UN M.49 standard. For example:
-                // If locale = "es_MX" and dictionary key = "es_419" (both have rc_languageAndScript = "es-Latn")
-                return match
-            }
-        }
-
-        // For matching language without script or region
-        if let onlyLanguageIdentifier = locale.rc_languageCode,
-           let exactMatch = self.valueForLocaleString(onlyLanguageIdentifier) {
-            return exactMatch
         }
 
         return nil
     }
 
-    private func valueForLocaleString(_ localeIdentifier: String) -> Value? {
-        if let exactMatch = self[localeIdentifier] {
-            return exactMatch
+    /// Returns the elements in `identifiers` that best match the any language in `preferredLanguages`
+    /// (i.e. same language code), sorted by best match.
+    ///
+    /// This method takes into account the order of `preferredLanguages` to determine the best match.
+    /// E.g.
+    /// if `preferredLanguages` is `["en-US", "es-ES"]`, then the identifier `en-GB`
+    /// will be preferred over `es-ES`.
+    ///
+    /// - important: As specified in the documentation of `Bundle.preferredLocalizations(from:forPreferences:)`
+    /// "_This method doesn’t return all localizations in order of user preference. To get this information,
+    /// you can call this method repeatedly, each time removing the identifiers returned by the previous call._"
+    /// This means that not all matches will be returned, but only the best ones based on `preferredLanguages`.
+    static func preferredMatchedLocalesIdentifiers(from identifiers: [String],
+                                                   preferredLanguages: [String]) -> [String] {
+
+        let preferredLocales = preferredLanguages.map(Locale.init)
+        let identifiersCandidates = identifiers.filter { identifier in
+            let locale = Locale(identifier: identifier)
+            return preferredLocales.contains { $0.matchesLanguage(locale) }
         }
 
-        // For cases like zh-Hans and zh-Hant
-        let underscoreLocaleIdentifier = localeIdentifier.replacingOccurrences(of: "-", with: "_")
-        return self[underscoreLocaleIdentifier]
-    }
-
-    private func valueForLanguageAndScript(_ languageAndScript: String) -> Value? {
-        guard let matchKey = self.keys.lazy.first(where: {
-            let keyLocale = Locale(identifier: $0)
-            return keyLocale.rc_languageAndScript == languageAndScript
-        }) else {
-            return nil
+        guard !identifiersCandidates.isEmpty else {
+            return []
         }
-        return self[matchKey]
+
+        let identifiers = Bundle.preferredLocalizations(from: identifiersCandidates,
+                                                        forPreferences: preferredLanguages)
+        return identifiers
     }
 
 }
