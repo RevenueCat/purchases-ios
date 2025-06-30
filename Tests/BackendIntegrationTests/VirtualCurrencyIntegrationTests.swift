5137e12cf90a4778f370f6a7df0491f933df87b0
Will Taylor <wtaylor151@gmail.com>
2025-06-29 21:11:24 -0500

    finish integration tests

--- Tests/BackendIntegrationTests/VirtualCurrencyIntegrationTests.swift
+++ Tests/BackendIntegrationTests/VirtualCurrencyIntegrationTests.swift
@@ -35,8 +35,7 @@ class VirtualCurrencyStoreKit1IntegrationTests: BaseStoreKitIntegrationTests {
         try self.purchases.invalidateVirtualCurrenciesCache()
         let virtualCurrenciesBeforePurchase = try await self.purchases.virtualCurrencies()
 
-        // TODO: We might need to check for this to be 0 depending on the outcome from a discussion with
-        // the team
+        // TODO: We might need to check for this to be 0 depending on the outcome from a discussion with the team
         expect(virtualCurrenciesBeforePurchase["TEST"]?.balance).to(beNil())
         expect(virtualCurrenciesBeforePurchase["TEST2"]?.balance).to(beNil())
 
@@ -47,6 +46,42 @@ class VirtualCurrencyStoreKit1IntegrationTests: BaseStoreKitIntegrationTests {
         let virtualCurrenciesAfterPurchase = try await self.purchases.virtualCurrencies()
 
         expect(virtualCurrenciesAfterPurchase["TEST"]?.balance).to(equal(1))
-        expect(virtualCurrenciesAfterPurchase["TEST2"]?.balance).to(equal(0))
+        expect(virtualCurrenciesAfterPurchase["TEST"]?.code).to(equal("TEST"))
+        expect(virtualCurrenciesAfterPurchase["TEST"]?.name).to(equal("Test Currency"))
+        expect(virtualCurrenciesAfterPurchase["TEST"]?.serverDescription).to(equal("This is a test currency"))
+
+        // TODO: We might need to check for this to be 0 depending on the outcome from a discussion with the team
+        expect(virtualCurrenciesAfterPurchase["TEST2"]?.balance).to(beNil())
+
+        // Ensure that this purchase didn't grant VCs to other subscribers
+        _ = try await self.purchases.logIn(userID2)
+        let virtualCurrenciesForOtherUser = try await self.purchases.virtualCurrencies()
+
+        // TODO: We might need to check for this to be 0 depending on the outcome from a discussion with the team
+        expect(virtualCurrenciesForOtherUser["TEST"]?.balance).to(beNil())
+        expect(virtualCurrenciesForOtherUser["TEST2"]?.balance).to(beNil())
+    }
+
+    func testDoesntGrantVCForProductWithoutVCGrant() async throws {
+        let userID1 = "vc_user_\(UUID().uuidString)"
+
+        _ = try await self.purchases.logIn(userID1)
+
+        try self.purchases.invalidateVirtualCurrenciesCache()
+        let virtualCurrenciesBeforePurchase = try await self.purchases.virtualCurrencies()
+
+        // TODO: We might need to check for this to be 0 depending on the outcome from a discussion with the team
+        expect(virtualCurrenciesBeforePurchase["TEST"]?.balance).to(beNil())
+        expect(virtualCurrenciesBeforePurchase["TEST2"]?.balance).to(beNil())
+
+        try await self.purchaseNonRenewingSubscriptionPackage()
+
+        try self.purchases.invalidateVirtualCurrenciesCache()
+
+        let virtualCurrenciesAfterPurchase = try await self.purchases.virtualCurrencies()
+
+        // TODO: We might need to check for this to be 0 depending on the outcome from a discussion with the team
+        expect(virtualCurrenciesAfterPurchase["TEST"]?.balance).to(beNil())
+        expect(virtualCurrenciesAfterPurchase["TEST2"]?.balance).to(beNil())
     }
 }
