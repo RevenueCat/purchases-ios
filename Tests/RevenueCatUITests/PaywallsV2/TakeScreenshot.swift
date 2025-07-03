197ee4c02cd6334afbac2831153ba6f5ea259772
Chris Vasselli <clindsay@gmail.com>
2025-07-03 19:59:52 +0900

    Switch from CALayer.render(in:) to UIView.drawHierarchy(in:afterScreenUpdates:) for rendering paywall screenshots.
    Before this change, AppKit views like buttons were simply not rendered on Mac Catalyst builds because they are not backed by CALayers.

--- Tests/RevenueCatUITests/PaywallsV2/TakeScreenshot.swift
+++ Tests/RevenueCatUITests/PaywallsV2/TakeScreenshot.swift
@@ -256,7 +256,7 @@ extension UIView {
   func asImage() -> UIImage {
     let renderer = UIGraphicsImageRenderer(bounds: bounds)
     return renderer.image { rendererContext in
-      layer.render(in: rendererContext.cgContext)
+        drawHierarchy(in: bounds, afterScreenUpdates: true)
     }
   }
 }
