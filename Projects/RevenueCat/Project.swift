17d9e629ab4e3500afa01fdc9f9c2e9ab61c4eee
Facundo Menzella <facumenzella@gmail.com>
2025-07-04 12:20:49 +0200

    remove run from RevneueCat and add xctestplans to workspace

--- Projects/RevenueCat/Project.swift
+++ Projects/RevenueCat/Project.swift
@@ -302,14 +302,6 @@ let project = Project(
                     .relativeToRoot("Tests/TestPlans/AllTests.xctestplan")
                 ]
             ),
-            runAction: .runAction(
-                executable: "RevenueCat",
-                options: .options(
-                    storeKitConfigurationPath: .relativeToRoot(
-                        "Tests/StoreKitUnitTests/UnitTestsConfiguration.storekit"
-                    )
-                )
-            ),
             archiveAction: .archiveAction(configuration: "Release"),
             profileAction: .profileAction(configuration: "Release"),
             analyzeAction: .analyzeAction(configuration: "Debug")
@@ -345,5 +337,5 @@ let project = Project(
             ]),
             runAction: .runAction(configuration: "Debug")
         )
-    ],
+    ]
 )
