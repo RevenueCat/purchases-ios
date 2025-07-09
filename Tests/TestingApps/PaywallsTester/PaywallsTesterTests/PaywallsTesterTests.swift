0eeadcd3fc101103a7a882fc5368debc6d7489fa
Chris Lindsay <clindsay@gmail.com>
2025-07-09 11:42:46 +0900

    Update comments about exclusions restricting screenshots only to our frameworks.

--- Tests/TestingApps/PaywallsTester/PaywallsTesterTests/PaywallsTesterTests.swift
+++ Tests/TestingApps/PaywallsTester/PaywallsTesterTests/PaywallsTesterTests.swift
@@ -12,6 +12,8 @@ final class PaywallsTesterTests: SnapshotTest {
     override class func snapshotPreviews() -> [String]? {
         // Gets around an issue that was causing StoreKit previews to be included when running in Catalyst mode.
         // Should be fixed in EmergeTools v0.10.23+, and this won't be necessary anymore.
+        // This method only applies when running these tests locally. When running on EmergeTools' server,
+        // we apply the same exclusion by specifying it in emerge_config.yaml.
         // PR: https://github.com/EmergeTools/SnapshotPreviews/pull/239
         return ["PaywallsTester.*", "RevenueCatUI.*"]
     }
