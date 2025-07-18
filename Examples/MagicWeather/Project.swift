9ba8327432bc4a67ef4981052bcf5fdac90bfbdd
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-07-18 09:03:57 +0200

    tuist: rename tuist-generated Xcode project for MagicWeather to avoid name clashing with existing Xcode project

--- Examples/MagicWeather/Project.swift
+++ Examples/MagicWeather/Project.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project(
-    name: "MagicWeather",
+    name: "MagicWeatherApp",
     organizationName: .revenueCatOrgName,
     settings: .appProject,
     targets: [
