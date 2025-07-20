df0d990d4ac22829c76d4ad8a1f0e359ff233e73
Chris Lindsay <clindsay@gmail.com>
2025-07-20 17:45:32 -0400

    Migrate PaywallValidationTester to Tuist.

---
+++ Projects/PaywallValidationTester/Project.swift
@@ -0,0 +1,63 @@
+import Foundation
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let allDestinations: Destinations = [
+    .iPhone,
+    .iPad,
+    .mac,
+    .macWithiPadDesign,
+    .macCatalyst,
+    .appleVision,
+    .appleVisionWithiPadDesign
+]
+
+let allDeploymentTargets: DeploymentTargets = .multiplatform(
+    iOS: "18.5",
+    macOS: "15.5",
+    visionOS: "2.5"
+)
+
+let project = Project(
+    name: "PaywallValidationTester",
+    organizationName: .revenueCatOrgName,
+    settings: .settings(
+        base: [:].automaticCodeSigning(devTeam: .revenueCatTeamID),
+        configurations: .xcconfigFileConfigurations,
+        defaultSettings: .recommended
+    ),
+    targets: [
+        .target(
+            name: "PaywallValidationTester",
+            destinations: allDestinations,
+            product: .app,
+            bundleId: "com.revenuecat.PaywallValidationTester",
+            deploymentTargets: allDeploymentTargets,
+            infoPlist: "../../Tests/TestingApps/PaywallValidationTester/Info.plist",
+            sources: [
+                "../../Tests/TestingApps/PaywallValidationTester/**/*.swift",
+                "../../Tests/RevenueCatUITests/PaywallsV2/PaywallPreviewResourcesLoader.swift"
+            ],
+            resources: [
+                .folderReference(path:"../../Tests/paywall-preview-resources"),
+                "../../Tests/RevenueCatUITests/Resources/header.heic",
+                "../../Tests/RevenueCatUITests/Resources/background.heic"
+            ],
+            dependencies: [
+                .revenueCat,
+                .revenueCatUI
+            ]
+        )
+    ],
+    schemes: [
+        .scheme(
+            name: "PaywallValidationTester",
+            shared: true,
+            buildAction: .buildAction(targets: ["PaywallValidationTester"]),
+            runAction: .runAction(
+                configuration: "Debug",
+                executable: "PaywallValidationTester"
+            )
+        ),
+    ]
+)
