f26fc38cedcba8a496732ff05868fceeebcee527
Jacob Rakidzich <Jacob@JacobZivanDesign.com>
2025-08-15 08:04:38 -0500

    setting up test suite

---
+++ Tests/UnitTests/Paywalls/Components/ImageComponentTests.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  ImageComponentTests.swift
+//
+//  Created by Jacob Zivan Rakidzich on 8/14/25.
+
+import Foundation
+@testable import RevenueCat
+import XCTest
+
+class ImageComponentTests: TestCase {
+
+    func testCodable() throws {
+        let jsonData = try JsonLoader.data(for: "ImageComponent")
+
+        // Validate decoding works
+        let image = try JSONDecoder.default
+            .decode(PaywallComponent.ImageComponent.self, from: jsonData)
+
+        // validate encoding
+        let image2 = try image.encodeAndDecode()
+
+        // Validate some data
+        XCTAssertEqual(
+            image.source.light.original.absoluteString,
+            "https://assets.pawwalls.com/1151049_1732039548.png"
+        )
+        XCTAssertNil(image.source.dark)
+        XCTAssertEqual(image, image2)
+
+    }
+}
---
+++ Tests/UnitTests/Paywalls/Components/JsonLoader.swift
@@ -0,0 +1,37 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  JsonLoader.swift
+//
+//  Created by Jacob Zivan Rakidzich on 8/14/25.
+
+import Foundation
+import XCTest
+
+enum JsonLoader {
+    private class BundleToken { }
+
+    static func data(
+        for fileName: String,
+        in subDirectory: String = "JSON",
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws -> Data {
+        let url = try XCTUnwrap(
+            URL(fileURLWithPath: String(describing: file))
+                        .deletingLastPathComponent()
+                        .appendingPathComponent(subDirectory)
+                        .appendingPathComponent(fileName + ".json"),
+            "Could not find file with name: '\(fileName).json'",
+            file: file,
+            line: line
+        )
+        return try XCTUnwrap(Data(contentsOf: url), file: file, line: line)
+    }
+}
