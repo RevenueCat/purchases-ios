90eb6fcecebbc9d1056f9aeb4ff9f9f717ca4277
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-10-06 07:40:04 -0700

    Add formatted price to ProductPaidPrice (#5623)
    * add missing test
    
    * Update Tests/UnitTests/Identity/ProductPaidPriceTests.swift
    
    * nits
    
    * swiftlint

---
+++ Tests/APITesters/AllAPITests/SwiftAPITester/ProductPaidPriceAPI.swift
@@ -0,0 +1,22 @@
+//
+//  ProductPaidPriceAPI.swift
+//  SwiftAPITester
+//
+//  Created by Facundo Menzella on 3/10/25.
+//
+
+import RevenueCat
+
+var productPaidPrice: ProductPaidPrice!
+
+func checkProductPaidPriceAPI() {
+    _ = ProductPaidPrice(currency: "USD", amount: 4.99)
+    
+    _ = ProductPaidPrice(currency: "USD", amount: 4.99, formatted: "$4.99")
+    
+    _ = ProductPaidPrice(currency: "USD", amount: 4.99, locale: .current)
+    
+    let _: String = productPaidPrice.currency
+    let _: Double = productPaidPrice.amount
+    let _: String = productPaidPrice.formatted
+}
