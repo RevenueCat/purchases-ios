d82e0926f451e75fcffaaf386676a421526500cf
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-10-17 13:38:11 +0200

    Have `DiagnosticsFileHandler` accept any `FileHandlerType` (for testing)

--- Sources/Diagnostics/DiagnosticsFileHandler.swift
+++ Sources/Diagnostics/DiagnosticsFileHandler.swift
@@ -43,18 +43,18 @@ actor DiagnosticsFileHandler: DiagnosticsFileHandlerType {
 
     private weak var delegate: DiagnosticsFileHandlerDelegate?
 
-    private let fileHandler: FileHandler
+    private let fileHandler: FileHandlerType
 
     init?() {
         do {
-            self.fileHandler = try .init(Self.diagnosticsFile)
+            self.fileHandler = try FileHandler(Self.diagnosticsFile)
         } catch {
             Logger.error("Initialization error: \(error.localizedDescription)")
             return nil
         }
     }
 
-    init(_ fileHandler: FileHandler) {
+    init(_ fileHandler: FileHandlerType) {
         self.fileHandler = fileHandler
     }
 
