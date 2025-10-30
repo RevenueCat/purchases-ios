697bcb0f6b42f9557de4bd0df44d5c0787f43cea
Rick <rick.vanderlinden@revenuecat.com>
2025-10-30 11:32:14 +0100

    Allow setting force_server_error_strategy from env variable, using it in the fallback e2e tests.

--- Examples/rc-maestro/rc-maestro/Sources/rc-maestro/RcMaestroApp.swift
+++ Examples/rc-maestro/rc-maestro/Sources/rc-maestro/RcMaestroApp.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-import RevenueCat
+@testable import RevenueCat
 
 @main
 struct RcMaestroApp: App {
@@ -7,7 +7,23 @@ struct RcMaestroApp: App {
     init() {
         Purchases.logLevel = .verbose
         Purchases.proxyURL = Constants.proxyURL.flatMap { URL(string: $0) }
-        Purchases.configure(withAPIKey: Constants.apiKey)
+        Purchases.configure(
+            with: .builder(withAPIKey: Constants.apiKey)
+                .with(dangerousSettings: .init(
+                    autoSyncPurchases: true,
+                    internalSettings: DangerousSettings.Internal(
+                        forceServerErrorStrategy: .init { request in
+                            switch Constants.forceServerErrorStrategy {
+                            case .never:
+                                return false
+                            case .primaryDomainDown:
+                                return request.fallbackHostIndex == nil
+                            }
+                        }
+                    )
+                ))
+                .build()
+        )
     }
 
     var body: some Scene {
