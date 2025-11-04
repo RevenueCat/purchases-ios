4d3db5c27b90d02b42619cd0540bc598a9f3299c
Rick <rick.vanderlinden@revenuecat.com>
2025-11-04 17:19:07 +0100

    Added support for setting the API base URL in the backend integration tests

--- Tests/BackendIntegrationTests/Constants.swift
+++ Tests/BackendIntegrationTests/Constants.swift
@@ -13,6 +13,9 @@ enum Constants {
     static let apiKey = "REVENUECAT_API_KEY"
     static let loadShedderApiKey = "REVENUECAT_LOAD_SHEDDER_API_KEY"
     static let customEntitlementComputationApiKey = "REVENUECAT_CUSTOM_ENTITLEMENT_COMPUTATION_API_KEY"
+    
+    // The API Base URL for the tests. Configures the main backend in the SDK while still using the fallback backend logic as opposed to the proxyURL
+    static let apiBaseURL = "REVENUECAT_API_BASE_URL"
 
     // Server URL for the tests. If set to empty string, we'll use the default URL.
     static let proxyURL = "REVENUECAT_PROXY_URL"
