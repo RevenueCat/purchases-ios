991f754b86b08296fec105a1d18742ff250e8b5d
Rick <rick.vanderlinden@revenuecat.com>
2025-11-05 14:35:35 +0100

    Fixed swiftlint warning

--- Tests/BackendIntegrationTests/LoadShedderIntegrationTests.swift
+++ Tests/BackendIntegrationTests/LoadShedderIntegrationTests.swift
@@ -85,7 +85,7 @@ class BaseLoadShedderStoreKitIntegrationTests: BaseStoreKitIntegrationTests {
         SystemInfo.apiBaseURL = self.backend.apiBaseURL
         try await super.setUp()
     }
-    
+
     override func tearDown() async throws {
         SystemInfo.apiBaseURL = SystemInfo.defaultApiBaseURL
         try await super.tearDown()
