2640dac7e1ddc1eb6575eebc3ae1d6be00cbbfc6
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-11-17 09:25:08 +0100

    Prevent duplicate post receipt requests (#5795)
    * Prevent duplicate post receipt requests caused by timestamp-only attribute changes in the `consentStatus` SubscriberAttribute
    
    * unit tests
    
    * More tests
    
    * Simplified MockDateProvider
    
    * Add unit test covering edge case
    
    * Fix lint
    # Conflicts:
    #	RevenueCat.xcodeproj/project.pbxproj
    #	Tests/UnitTests/Mocks/MockDateProvider.swift

--- Tests/UnitTests/Mocks/MockDateProvider.swift
+++ Tests/UnitTests/Mocks/MockDateProvider.swift
@@ -6,17 +6,31 @@
 @testable import RevenueCat
 
 class MockDateProvider: DateProvider {
-    var invokedNow = false
-    var invokedNowCount = 0
-    var stubbedNowResult: Date!
+
+    private var dates: [Date]
+    private var currentIndex = 0
+
+    var invokedNowCount: Int {
+        return currentIndex
+    }
+    var invokedNow: Bool {
+        return invokedNowCount > 0
+    }
+
+    init(stubbedNow: Date, subsequentNows: Date...) {
+        self.dates = [stubbedNow] + subsequentNows
+    }
+
+    init(stubbedNow: Date, subsequentNows: [Date]) {
+        self.dates = [stubbedNow] + subsequentNows
+    }
 
     init(stubbedNow: Date) {
         self.stubbedNowResult = stubbedNow
     }
 
     override func now() -> Date {
-        invokedNow = true
-        invokedNowCount += 1
-        return stubbedNowResult
+        defer { currentIndex += 1 }
+        return dates[min(currentIndex, dates.count - 1)]
     }
 }
