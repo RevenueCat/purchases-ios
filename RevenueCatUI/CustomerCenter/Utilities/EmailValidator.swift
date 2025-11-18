acd71f9b3e0d5c478192e697952c432134aad36f
Rosie Watson <rosie.watson@revenuecat.com>
2025-11-18 11:11:37 +0000

    chore: refine email checks

--- RevenueCatUI/CustomerCenter/Utilities/EmailValidator.swift
+++ RevenueCatUI/CustomerCenter/Utilities/EmailValidator.swift
@@ -16,7 +16,13 @@ import Foundation
 enum EmailValidator {
 
     static func isValid(_ email: String) -> Bool {
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        // Basic checks
+        guard !email.isEmpty else { return false }
+        guard !email.contains("..") else { return false }
+        guard email.filter({ $0 == "@" }).count == 1 else { return false }
+
+        // Regex validation
+        let emailRegex = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailPredicate.evaluate(with: email)
     }
