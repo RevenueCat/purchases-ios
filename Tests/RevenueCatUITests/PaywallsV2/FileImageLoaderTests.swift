8d953e963ef63b6af1f92fcb39fae33881a22ce1
Facundo Menzella <facumenzella@gmail.com>
2026-01-19 10:20:31 +0100

    extends test coverage

--- Tests/RevenueCatUITests/PaywallsV2/FileImageLoaderTests.swift
+++ Tests/RevenueCatUITests/PaywallsV2/FileImageLoaderTests.swift
@@ -70,6 +70,90 @@ final class FileImageLoaderTests: TestCase {
         expect(originalImageData) == updatedImageData
     }
 
+    func testUpdateURLToNilClearsResult() throws {
+        let fileRepository = self.makeFileRepository()
+        let url = Self.makeLocalURL(filename: "test-image-4.png")
+
+        let data = try Self.makeImageData(variant: .red)
+        let cachedURL = try XCTUnwrap(fileRepository.generateLocalFilesystemURL(forRemoteURL: url, withChecksum: nil))
+        try Self.writeImageData(data, to: cachedURL)
+
+        let loader = FileImageLoader(fileRepository: fileRepository, url: url)
+        expect(loader.result).toNot(beNil())
+        expect(loader.url) == url
+
+        loader.updateURL(nil)
+
+        expect(loader.result).to(beNil())
+        expect(loader.url).to(beNil())
+    }
+
+    func testUpdateURLToNonCachedURLClearsResult() throws {
+        let fileRepository = self.makeFileRepository()
+        let cachedURL = Self.makeLocalURL(filename: "test-image-7.png")
+        let nonCachedURL = Self.makeLocalURL(filename: "test-image-non-cached.png")
+
+        let data = try Self.makeImageData(variant: .green)
+        let cachedFileURL = try XCTUnwrap(
+            fileRepository.generateLocalFilesystemURL(forRemoteURL: cachedURL, withChecksum: nil)
+        )
+        try Self.writeImageData(data, to: cachedFileURL)
+
+        let loader = FileImageLoader(fileRepository: fileRepository, url: cachedURL)
+        expect(loader.result).toNot(beNil())
+        expect(loader.wasLoadedFromCache) == true
+
+        loader.updateURL(nonCachedURL)
+
+        expect(loader.result).to(beNil())
+        expect(loader.wasLoadedFromCache) == false
+    }
+
+    func testSequentialURLUpdatesLoadCorrectImages() throws {
+        guard #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) else {
+            throw XCTSkip("API only available on iOS 16")
+        }
+
+        let fileRepository = self.makeFileRepository()
+        let url1 = Self.makeLocalURL(filename: "test-seq-1.png")
+        let url2 = Self.makeLocalURL(filename: "test-seq-2.png")
+        let url3 = Self.makeLocalURL(filename: "test-seq-3.png")
+
+        let data1 = try Self.makeImageData(variant: .red)
+        let data2 = try Self.makeImageData(variant: .blue)
+        let data3 = try Self.makeImageData(variant: .green)
+
+        let cachedURL1 = try XCTUnwrap(fileRepository.generateLocalFilesystemURL(forRemoteURL: url1, withChecksum: nil))
+        let cachedURL2 = try XCTUnwrap(fileRepository.generateLocalFilesystemURL(forRemoteURL: url2, withChecksum: nil))
+        let cachedURL3 = try XCTUnwrap(fileRepository.generateLocalFilesystemURL(forRemoteURL: url3, withChecksum: nil))
+
+        try Self.writeImageData(data1, to: cachedURL1)
+        try Self.writeImageData(data2, to: cachedURL2)
+        try Self.writeImageData(data3, to: cachedURL3)
+
+        let loader = FileImageLoader(fileRepository: fileRepository, url: url1)
+        let firstImageData = try XCTUnwrap(loader.result?.image.platformPNGData())
+
+        // Simulate rapid package selection changes
+        loader.updateURL(url2)
+        let secondImageData = try XCTUnwrap(loader.result?.image.platformPNGData())
+
+        loader.updateURL(url3)
+        let thirdImageData = try XCTUnwrap(loader.result?.image.platformPNGData())
+
+        // Return to original
+        loader.updateURL(url1)
+        let backToFirstImageData = try XCTUnwrap(loader.result?.image.platformPNGData())
+
+        // Verify all images are different from each other
+        expect(firstImageData).toNot(equal(secondImageData))
+        expect(secondImageData).toNot(equal(thirdImageData))
+        expect(firstImageData).toNot(equal(thirdImageData))
+
+        // Verify returning to url1 gives the same image as initially
+        expect(backToFirstImageData) == firstImageData
+    }
+
     // MARK: - Helpers
 
     private func makeFileRepository() -> FileRepository {
