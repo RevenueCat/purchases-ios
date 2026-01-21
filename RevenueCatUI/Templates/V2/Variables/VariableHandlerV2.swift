535854d14b6aa02383d1a413bfdce703c5f905f6
Josh Holtz <me@joshholtz.com>
2026-01-21 13:42:14 -0600

    Remove debug logging to fix test failures

--- RevenueCatUI/Templates/V2/Variables/VariableHandlerV2.swift
+++ RevenueCatUI/Templates/V2/Variables/VariableHandlerV2.swift
@@ -462,7 +462,6 @@ extension VariablesV2 {
             return self.countdownValue(countdownTime: countdownTime, format: .secondsWithoutZero)
         }
 
-        Logger.debug("Variable '\(self.rawValue)' returned empty string due to missing package context")
         return ""
     }
 
