4028059099f70c8219f460c55ba9eb15270e52d6
Eren Kulaksız <erenkulaksz@gmail.com>
2026-01-23 11:55:49 +0300

    feat: add warning log if audioSession.setCategory fails, store previous value of AVAudioSession to restore it

--- RevenueCatUI/Templates/V2/Components/Video/VideoPlayerViewUIView.swift
+++ RevenueCatUI/Templates/V2/Components/Video/VideoPlayerViewUIView.swift
@@ -12,6 +12,7 @@
 //  Created by Jacob Zivan Rakidzich on 8/18/25.
 
 import AVKit
+import RevenueCat
 import SwiftUI
 
 #if canImport(UIKit) && !os(watchOS)
@@ -53,12 +54,6 @@ struct VideoPlayerUIView: UIViewControllerRepresentable {
         avPlayer.preventsDisplaySleepDuringVideoPlayback = false
         avPlayer.allowsExternalPlayback = false
 
-        try? AVAudioSession.sharedInstance().setCategory(
-            .ambient,
-            mode: .default,
-            options: [.mixWithOthers]
-        )
-
         self.player = avPlayer
 
         if shouldAutoPlay {
@@ -66,7 +61,26 @@ struct VideoPlayerUIView: UIViewControllerRepresentable {
         }
     }
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
     func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let audioSession = AVAudioSession.sharedInstance()
+        context.coordinator.previousCategory = audioSession.category
+        context.coordinator.previousMode = audioSession.mode
+        context.coordinator.previousOptions = audioSession.categoryOptions
+
+        do {
+            try audioSession.setCategory(
+                .ambient,
+                mode: .default,
+                options: [.mixWithOthers]
+            )
+        } catch {
+            Logger.warning(Strings.video_failed_to_set_audio_session_category(error))
+        }
+
         let controller = AVPlayerViewController()
         controller.player = player
         controller.view.backgroundColor = .clear
@@ -84,5 +98,29 @@ struct VideoPlayerUIView: UIViewControllerRepresentable {
     }
 
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) { }
+
+    class Coordinator {
+        var previousCategory: AVAudioSession.Category?
+        var previousMode: AVAudioSession.Mode?
+        var previousOptions: AVAudioSession.CategoryOptions?
+
+        deinit {
+            guard let category = previousCategory,
+                  let mode = previousMode,
+                  let options = previousOptions else {
+                return
+            }
+
+            do {
+                try AVAudioSession.sharedInstance().setCategory(
+                    category,
+                    mode: mode,
+                    options: options
+                )
+            } catch {
+                Logger.warning(Strings.video_failed_to_set_audio_session_category(error))
+            }
+        }
+    }
 }
 #endif
