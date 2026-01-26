3c7c1edfec43691993db3c6148dcb9228e736be2
Drago Crnjac <drago@revenuecat.com>
2026-01-26 22:59:12 +0100

    Fix price_per_period for non-subscription products (PW-69)

--- RevenueCatUI/Templates/V2/Variables/VariableHandlerV2.swift
+++ RevenueCatUI/Templates/V2/Variables/VariableHandlerV2.swift
@@ -484,8 +484,12 @@ extension VariablesV2 {
         showZeroDecimalPlacePrices: Bool
     ) -> String {
         let price = package.localizedPrice(showZeroDecimalPlacePrices: showZeroDecimalPlacePrices)
-        let period = self.productPeriod(package: package, localizations: localizations)
 
+        guard package.storeProduct.subscriptionPeriod != nil else {
+            return price
+        }
+
+        let period = self.productPeriod(package: package, localizations: localizations)
         return "\(price)/\(period)"
     }
 
@@ -495,8 +499,12 @@ extension VariablesV2 {
         showZeroDecimalPlacePrices: Bool
     ) -> String {
         let price = package.localizedPrice(showZeroDecimalPlacePrices: showZeroDecimalPlacePrices)
-        let periodAbbreviated = self.productPeriodAbbreviated(package: package, localizations: localizations)
 
+        guard package.storeProduct.subscriptionPeriod != nil else {
+            return price
+        }
+
+        let periodAbbreviated = self.productPeriodAbbreviated(package: package, localizations: localizations)
         return "\(price)/\(periodAbbreviated)"
     }
 
