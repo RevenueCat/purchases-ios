1c730951f6fa7d68b972d4aa4a5f8d5bdf36b6de
Antonio Pallares <antonio.pallares@revenuecat.com>
2026-01-30 10:50:09 +0100

    Now with AppTransaction.shared

--- Sources/Misc/SandboxEnvironmentDetector.swift
+++ Sources/Misc/SandboxEnvironmentDetector.swift
@@ -117,8 +117,7 @@ private extension SandboxEnvironmentDetector {
     func prefetchAppTransactionEnvironmentIfAvailable() {
         Task.detached {
             if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
-                try! await Task.sleep(for: .seconds(20))
-//                let environment = try? await AppTransaction.shared.verifiedAppTransaction?.environment
+                let environment = try? await AppTransaction.shared.verifiedAppTransaction?.environment
                 self.cachedAppTransactionEnvironment = .sandbox
             }
         }
