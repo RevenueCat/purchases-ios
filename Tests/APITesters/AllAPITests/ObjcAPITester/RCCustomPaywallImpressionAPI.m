e816eac89e44336e01cc8a7186a6488e86a8a01b
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-03-16 12:33:20 +0100

    Revert "Revert "Revert "Remove ObjC API tester for experimental custom paywall impression API"""
    This reverts commit c2a164f0ca414106cec522bb0867b0dc6f49e7ec.

---
+++ Tests/APITesters/AllAPITests/ObjcAPITester/RCCustomPaywallImpressionAPI.m
@@ -0,0 +1,33 @@
+//
+//  RCCustomPaywallImpressionAPI.m
+//  ObjCAPITester
+//
+
+#import "RCCustomPaywallImpressionAPI.h"
+
+@import RevenueCat;
+
+@implementation RCCustomPaywallImpressionAPI
+
++ (void)checkAPI {
+    if (@available(iOS 15.0, tvOS 15.0, macOS 12.0, watchOS 8.0, *)) {
+        // CustomPaywallImpressionParams API
+        RCCustomPaywallImpressionParams *paramsDefault __unused = [[RCCustomPaywallImpressionParams alloc] initWithPaywallId:nil];
+        RCCustomPaywallImpressionParams *paramsWithId __unused = [[RCCustomPaywallImpressionParams alloc] initWithPaywallId:@"my-paywall"];
+        RCCustomPaywallImpressionParams *paramsWithNil __unused = [[RCCustomPaywallImpressionParams alloc] initWithPaywallId:nil];
+        RCCustomPaywallImpressionParams *paramsWithOffering __unused = [[RCCustomPaywallImpressionParams alloc] initWithPaywallId:@"my-paywall" offeringId:@"my-offering"];
+        RCCustomPaywallImpressionParams *paramsOfferingOnly __unused = [[RCCustomPaywallImpressionParams alloc] initWithPaywallId:nil offeringId:@"my-offering"];
+
+        // CustomPaywallImpressionParams properties
+        NSString *paywallId __unused = paramsWithId.paywallId;
+        NSString *offeringId __unused = paramsWithOffering.offeringId;
+
+        // trackCustomPaywallImpression API
+        RCPurchases *purchases = RCPurchases.sharedPurchases;
+        [purchases trackCustomPaywallImpression:paramsDefault];
+        [purchases trackCustomPaywallImpression:paramsWithId];
+        [purchases trackCustomPaywallImpression];
+    }
+}
+
+@end
