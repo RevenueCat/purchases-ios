ea8b5d175fb777232aa9ccd6fc71b4a4019a1c7e
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-03-16 12:09:15 +0100

    Remove experimental annotation from trackCustomPaywallImpression API

--- Tests/UnitTests/Paywalls/Events/PurchasesCustomPaywallEventsTests.swift
+++ Tests/UnitTests/Paywalls/Events/PurchasesCustomPaywallEventsTests.swift
@@ -15,7 +15,7 @@ import Nimble
 import StoreKit
 import XCTest
 
-@_spi(Experimental) @testable import RevenueCat
+@testable import RevenueCat
 
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, watchOS 8.0, *)
 class PurchasesCustomPaywallEventsTests: BasePurchasesTests {
