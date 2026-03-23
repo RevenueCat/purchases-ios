20b204c8fb3226357629065bc608d4593309270c
JayShortway <29483617+JayShortway@users.noreply.github.com>
2026-03-23 21:45:11 +0100

    Adds ObjC inits for Paywalls-only in UIKit (#6507)

--- Tests/APITesters/AllAPITests/ObjcAPITester/RCPaywallViewControllerAPI.m
+++ Tests/APITesters/AllAPITests/ObjcAPITester/RCPaywallViewControllerAPI.m
@@ -9,6 +9,22 @@
 
 @import RevenueCatUI;
 
+@interface TestObjCPurchaseHandler : NSObject <RCPaywallPurchaseHandler>
+@end
+
+@implementation TestObjCPurchaseHandler
+
+- (void)performPurchaseFor:(RCPackage *)package
+                completion:(void (^)(BOOL, NSError * _Nullable))completion {
+    completion(NO, nil);
+}
+
+- (void)performRestoreWithCompletion:(void (^)(BOOL, NSError * _Nullable))completion {
+    completion(YES, nil);
+}
+
+@end
+
 @implementation RCPaywallViewControllerAPI
 
 + (void)checkAPI {
@@ -17,6 +33,31 @@
 
         // Custom Variables ObjC API
         [controller setCustomVariable:@"John" forKey:@"player_name"];
+
+        // PaywallPurchaseHandler-based init
+        TestObjCPurchaseHandler *handler = [[TestObjCPurchaseHandler alloc] init];
+        RCPaywallViewController *__unused controllerWithHandler =
+            [[RCPaywallViewController alloc] initWithOffering:nil
+                                          displayCloseButton:NO
+                                     shouldBlockTouchEvents:NO
+                                            purchaseHandler:handler
+                                    dismissRequestedHandler:nil];
+        RCPaywallViewController *__unused controllerWithNilHandler =
+            [[RCPaywallViewController alloc] initWithOffering:nil
+                                          displayCloseButton:NO
+                                     shouldBlockTouchEvents:NO
+                                            purchaseHandler:nil
+                                    dismissRequestedHandler:nil];
+
+        // PaywallFooterViewController with PaywallPurchaseHandler
+        RCPaywallFooterViewController *__unused footerWithHandler =
+            [[RCPaywallFooterViewController alloc] initWithOffering:nil
+                                                    purchaseHandler:handler
+                                            dismissRequestedHandler:nil];
+        RCPaywallFooterViewController *__unused footerWithNilHandler =
+            [[RCPaywallFooterViewController alloc] initWithOffering:nil
+                                                    purchaseHandler:nil
+                                            dismissRequestedHandler:nil];
     }
 }
 
