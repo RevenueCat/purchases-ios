57a7525ccabfcd42bca5550b80bff8a060133c29
Monika Mateska <monika.mateska@revenuecat.com>
2026-03-27 20:02:43 +0100

    Add CodingKeys for componentUrl in control interaction events (PWENG-9)

--- Sources/Paywalls/Events/PaywallEvent.swift
+++ Sources/Paywalls/Events/PaywallEvent.swift
@@ -300,6 +300,13 @@ extension PaywallEvent {
         /// Destination URL for URL-based controls (e.g. terms, privacy, generic links), when applicable.
         public var componentURL: URL?
 
+        private enum CodingKeys: String, CodingKey {
+            case componentType
+            case componentName
+            case componentValue
+            case componentURL = "componentUrl"
+        }
+
         public init(
             componentType: ControlType,
             componentName: String? = nil,
