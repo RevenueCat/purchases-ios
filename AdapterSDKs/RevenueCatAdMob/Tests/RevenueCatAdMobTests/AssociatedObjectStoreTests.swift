30d913c66bb44fb02cba5dfe20b798ab7d0ac099
Peter Porfy <peter.porfy@revenuecat.com>
2026-04-28 09:43:03 +0200

    chore(ads): add AssociatedObjectStore tests

---
+++ AdapterSDKs/RevenueCatAdMob/Tests/RevenueCatAdMobTests/AssociatedObjectStoreTests.swift
@@ -0,0 +1,51 @@
+import XCTest
+
+#if os(iOS) && canImport(GoogleMobileAds)
+@_spi(Experimental) @testable import RevenueCatAdMob
+
+@available(iOS 15.0, *)
+final class AssociatedObjectStoreTests: AdapterTestCase {
+
+    func testRetainThenRetrieveReturnsSameInstance() {
+        let store = Tracking.AssociatedObjectStore<NSObject>()
+        let owner = NSObject()
+        let value = NSObject()
+
+        store.retain(value, for: owner)
+
+        XCTAssertIdentical(store.retrieve(for: owner), value)
+    }
+
+    func testRetrieveOnUnknownOwnerReturnsNil() {
+        let store = Tracking.AssociatedObjectStore<NSObject>()
+        let owner = NSObject()
+
+        XCTAssertNil(store.retrieve(for: owner))
+    }
+
+    func testTwoStoresOfSameTypeOnSameOwnerDontCollide() {
+        let storeA = Tracking.AssociatedObjectStore<NSObject>()
+        let storeB = Tracking.AssociatedObjectStore<NSObject>()
+        let owner = NSObject()
+        let valueA = NSObject()
+        let valueB = NSObject()
+
+        storeA.retain(valueA, for: owner)
+        storeB.retain(valueB, for: owner)
+
+        XCTAssertIdentical(storeA.retrieve(for: owner), valueA)
+        XCTAssertIdentical(storeB.retrieve(for: owner), valueB)
+    }
+
+    func testRetainNilClearsPreviousValue() {
+        let store = Tracking.AssociatedObjectStore<NSObject>()
+        let owner = NSObject()
+
+        store.retain(NSObject(), for: owner)
+        store.retain(nil, for: owner)
+
+        XCTAssertNil(store.retrieve(for: owner))
+    }
+
+}
+#endif
