c82eeb7bd26f95f913e975939a1724cc558a9ba7
Will Taylor <wtaylor151@gmail.com>
2026-05-14 14:33:22 -0500

    update product caching cache key

--- Tests/UnitTests/Purchasing/CachingProductsManagerTests.swift
+++ Tests/UnitTests/Purchasing/CachingProductsManagerTests.swift
@@ -97,6 +97,48 @@ class CachingProductsManagerTests: TestCase {
         self.expectProductsWereFetched(times: 1, for: Self.productID)
     }
 
+    func testReturnsCachedProductUsingStoreProductID() async throws {
+        try AvailabilityChecks.iOS26APIAvailableOrSkipTest()
+
+        let product = self.createTestProduct(
+            productIdentifier: "com.revenuecat.product_id",
+            installmentsInfo: Self.installmentsInfo(billingPlanType: .monthly)
+        )
+        self.mockManager.stubbedProductsCompletionResult = .success([product])
+
+        _ = try await self.cachingManager.products(withIdentifiers: [product.id])
+        let result = try await self.cachingManager.products(withIdentifiers: [product.id])
+
+        expect(result.onlyElement) === product
+        self.expectProductsWereFetched(times: 1, for: product.id)
+    }
+
+    func testFetchesUncachedProductsByStoreProductID() async throws {
+        try AvailabilityChecks.iOS26APIAvailableOrSkipTest()
+
+        let cachedProduct = self.createTestProduct(
+            productIdentifier: "com.revenuecat.product_id",
+            installmentsInfo: Self.installmentsInfo(billingPlanType: .monthly)
+        )
+        let uncachedProduct = self.createTestProduct(
+            productIdentifier: "com.revenuecat.lifetime",
+            installmentsInfo: nil
+        )
+
+        self.mockManager.stubbedProductsCompletionResult = .success([cachedProduct])
+        _ = try await self.cachingManager.products(withIdentifiers: [cachedProduct.id])
+
+        self.mockManager.stubbedProductsCompletionResult = .success([uncachedProduct])
+        let result = try await self.cachingManager.products(withIdentifiers: [cachedProduct.id, uncachedProduct.id])
+
+        expect(result) == [cachedProduct, uncachedProduct]
+        expect(self.mockManager.invokedProductsCount) == 2
+        expect(self.mockManager.invokedProductsParametersList) == [
+            Set([cachedProduct.id]),
+            Set([uncachedProduct.id])
+        ]
+    }
+
     func testRefetchesAfterClearingCache() async throws {
         let product = self.createMockProduct()
         self.mockManager.stubbedProductsCompletionResult = .success([product])
@@ -142,6 +184,21 @@ private extension CachingProductsManagerTests {
 
     static let productID = "com.revenuecat.product_id"
 
+    static func installmentsInfo(
+        billingPlanType: BillingPlanType
+    ) -> InstallmentsInfo {
+        return InstallmentsInfo(
+            commitmentInstallmentsCount: 3,
+            commitmentInstallmentPeriod: SubscriptionPeriod(value: 1, unit: .month),
+            installmentBillingPrice: 3.99,
+            installmentBillingDisplayPrice: "$3.99",
+            commitmentTotalPeriod: SubscriptionPeriod(value: 3, unit: .month),
+            commitmentTotalPrice: 11.97,
+            commitmentTotalDisplayPrice: "$11.97",
+            billingPlanType: billingPlanType
+        )
+    }
+
     func expectProductsWereFetched(
         times: Int,
         for identifiers: String...,
@@ -157,6 +214,24 @@ private extension CachingProductsManagerTests {
         // Using SK1 products because they can be mocked, but `CachingProductsManager
         // works with generic `StoreProduct`s regardless of what they contain
         return StoreProduct(sk1Product: MockSK1Product(mockProductIdentifier: identifier))
+    }
+
+    func createTestProduct(
+        productIdentifier: String = CachingProductsManagerTests.productID,
+        installmentsInfo: InstallmentsInfo?
+    ) -> StoreProduct {
+        return TestStoreProduct(
+            localizedTitle: "product",
+            price: 11.97,
+            currencyCode: "USD",
+            localizedPriceString: "$11.97",
+            productIdentifier: productIdentifier,
+            productType: .autoRenewableSubscription,
+            localizedDescription: "description",
+            subscriptionPeriod: SubscriptionPeriod(value: 1, unit: .month),
+            locale: Locale(identifier: "en_US"),
+            installmentsInfo: installmentsInfo
+        ).toStoreProduct()
     }
 
 }
