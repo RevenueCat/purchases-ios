47f3c9949a199fc26ae07a7247a1871ebefc1f4f
Will Taylor <wtaylor151@gmail.com>
2026-05-15 17:06:21 -0500

    log fix

--- Tests/StoreKitUnitTests/ProductsManagerTests.swift
+++ Tests/StoreKitUnitTests/ProductsManagerTests.swift
@@ -132,18 +132,34 @@ class ProductsManagerTests: StoreKitConfigTestCase {
         let unwrappedProducts = try XCTUnwrap(receivedProducts?.get())
         expect(unwrappedProducts).to(beEmpty())
         expect(productsRequestFactory.invokedRequest) == false
-        self.logger.verifyMessageWasLogged(
-            Strings.storeKit.sk2_billing_plans_are_unavailable_on_this_os_version(
-                compoundProductIdentifier: compoundProductIdentifier
-            ),
-            level: .warn
-        )
-        expect(self.logger.messages).toNot(containElementSatisfying { message in
-            message.level == .warn
-                && message.message == Strings.storeKit.sk1_does_not_support_billing_plans(
-                    compoundProductIdentifier: compoundProductIdentifier
-                ).description
-        })
+        if #available(iOS 16.0, *) {
+            self.logger.verifyMessageWasLogged(
+                Strings.storeKit.sk2_billing_plans_are_unavailable_on_this_os_version(
+                    compoundProductIdentifier: compoundIdentifier
+                ),
+                level: .warn
+            )
+            expect(self.logger.messages).toNot(containElementSatisfying { message in
+                message.level == .warn
+                    && message.message == Strings.storeKit.sk1_does_not_support_billing_plans(
+                        compoundProductIdentifier: compoundIdentifier
+                    ).description
+            })
+        } else {
+            // On iOS 14/15, the StoreKit 1 check happens first, so they'll see the SK1 warning log instead
+            self.logger.verifyMessageWasLogged(
+                Strings.storeKit.sk1_does_not_support_billing_plans(
+                    compoundProductIdentifier: compoundIdentifier
+                ).description,
+                level: .warn
+            )
+            expect(self.logger.messages).toNot(containElementSatisfying { message in
+                message.level == .warn
+                    && message.message == Strings.storeKit.sk2_billing_plans_are_unavailable_on_this_os_version(
+                        compoundProductIdentifier: compoundIdentifier
+                    ).description
+            })
+        }
     }
 
     func testFetchProductsWithCompoundIdentifierWithBillingPlanDoesNotRequestProductWithStoreKit1() throws {
