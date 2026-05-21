daf8fe5173b0268bd20836f6a82a8874119c1d24
Dave DeLong <dave.delong@revenuecat.com>
2026-05-21 13:49:07 -0600

    Appease the linter gods

--- RevenueCatUI/Templates/V2/ViewHelpers/GlassModifier.swift
+++ RevenueCatUI/Templates/V2/ViewHelpers/GlassModifier.swift
@@ -9,26 +9,26 @@ import SwiftUI
 @_spi(Internal) import RevenueCat
 
 extension View {
-    
+
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func withGlassEffect(from stackViewModel: StackComponentViewModel) -> some View {
         modifier(GlassEffectModifier(effect: stackViewModel.component.glassEffect,
                                      shape: stackViewModel.component.shape,
                                      uiConfigProvider: stackViewModel.uiConfigProvider))
     }
-    
+
 }
 
 // this availability matches that of UIConfigProvider, etc
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 private struct GlassEffectModifier: ViewModifier {
-    
+
     var effect: PaywallComponent.GlassEffect?
     var shape: PaywallComponent.Shape?
     var uiConfigProvider: UIConfigProvider
-    
+
     @Environment(\.colorScheme) var colorScheme
-    
+
     @available(macOS 26.0, iOS 26.0, tvOS 26.0, watchOS 26.0, *)
     var resolvedGlassEffect: Glass {
         var glass: Glass
@@ -37,21 +37,21 @@ private struct GlassEffectModifier: ViewModifier {
             case .clear: glass = .clear
             case .glassy: glass = .regular
         }
-        
+
         if effect != nil {
             if let tint = effect?.tint, let resolved = try? DisplayableColorScheme.from(colorScheme: tint, uiConfigProvider: uiConfigProvider) {
                 let color = resolved.toDynamicColor(with: colorScheme)
                 glass = glass.tint(color)
             }
-            
+
             if let interactive = effect?.interactive {
                 glass = glass.interactive(interactive)
             }
         }
-        
+
         return glass
     }
-    
+
     @available(macOS 26.0, iOS 26.0, tvOS 26.0, watchOS 26.0, *)
     var resolvedShape: AnyShape {
         switch shape {
@@ -65,14 +65,14 @@ private struct GlassEffectModifier: ViewModifier {
                                                         bottomLeadingRadius: radii.bottomLeading ?? 0,
                                                         bottomTrailingRadius: radii.bottomTrailing ?? 0,
                                                         topTrailingRadius: radii.topTrailing ?? 0)
-                    
+
                     return AnyShape(uneven)
                 } else {
                     return AnyShape(.rect)
                 }
         }
     }
-    
+
     func body(content: Content) -> some View {
         if #available(macOS 26.0, iOS 26.0, tvOS 26.0, watchOS 26.0, *) {
             content
