871ec28be903e2cc20d3d926ba66ab0488ed7c66
Will Taylor <wtaylor151@gmail.com>
2026-05-26 09:15:21 -0500

    move populateSK2CompoundProductsIfSupported to ProductsFetcherSK2

--- Tests/StoreKitUnitTests/ProductsManagerTests.swift
+++ Tests/StoreKitUnitTests/ProductsManagerTests.swift
@@ -244,31 +244,6 @@ class ProductsManagerTests: StoreKitConfigTestCase {
         })
     }
 
-    func testBaseSK2ProductIsRemovedWhenOnlyCompoundIdentifierIsRequested() throws {
-        let storeKitProductIdentifier = "com.revenuecat.subscription"
-        let requestedIdentifiers: Set<CompoundProductIdentifier> = [
-            try XCTUnwrap(CompoundProductIdentifier(compoundProductIdentifier: "\(storeKitProductIdentifier):monthly"))
-        ]
-
-        expect(ProductsManager.shouldRemoveBaseSK2Product(
-            productIdentifier: storeKitProductIdentifier,
-            requestedIdentifiers: requestedIdentifiers
-        )) == true
-    }
-
-    func testBaseSK2ProductIsKeptWhenBaseAndCompoundIdentifiersAreRequested() throws {
-        let storeKitProductIdentifier = "com.revenuecat.subscription"
-        let requestedIdentifiers: Set<CompoundProductIdentifier> = [
-            try XCTUnwrap(CompoundProductIdentifier(compoundProductIdentifier: storeKitProductIdentifier)),
-            try XCTUnwrap(CompoundProductIdentifier(compoundProductIdentifier: "\(storeKitProductIdentifier):monthly"))
-        ]
-
-        expect(ProductsManager.shouldRemoveBaseSK2Product(
-            productIdentifier: storeKitProductIdentifier,
-            requestedIdentifiers: requestedIdentifiers
-        )) == false
-    }
-
     func testClearCacheAfterStorefrontChangesSK1() async throws {
         let manager = self.createManager(storeKitVersion: .storeKit1)
 
