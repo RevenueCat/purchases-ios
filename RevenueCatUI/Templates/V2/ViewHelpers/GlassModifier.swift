d2f26a6f07b7120d4ac2c347abf2db8a1b3d92ed
Dave DeLong <dave.delong@revenuecat.com>
2026-05-26 15:43:03 -0600

    more availability checks

--- RevenueCatUI/Templates/V2/ViewHelpers/GlassModifier.swift
+++ RevenueCatUI/Templates/V2/ViewHelpers/GlassModifier.swift
@@ -14,12 +14,20 @@ extension View {
 
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func withGlassEffect(from stackViewModel: StackComponentViewModel) -> some View {
+        #if swift(>=6.2)
         modifier(GlassEffectModifier(effect: stackViewModel.component.glassEffect,
                                      shape: stackViewModel.component.shape,
                                      uiConfigProvider: stackViewModel.uiConfigProvider))
+        #else
+        self
+        #endif
     }
 
 }
+
+// The following will not compile when using an Xcode from before 26.0, because it will not understand
+// the "Glass" type, even if it would be available on the system running the code.
+#if swift(>=6.2)
 
 // this availability matches that of UIConfigProvider, etc
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
@@ -32,6 +40,7 @@ private struct GlassEffectModifier: ViewModifier {
     @Environment(\.colorScheme) var colorScheme
 
     @available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, *)
+    @available(visionOS, unavailable)
     var resolvedGlassEffect: Glass {
         var glass: Glass
         switch effect?.effect {
@@ -56,6 +65,7 @@ private struct GlassEffectModifier: ViewModifier {
     }
 
     @available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, *)
+    @available(visionOS, unavailable)
     var resolvedShape: AnyShape {
         switch shape {
         case .none:
@@ -85,5 +95,7 @@ private struct GlassEffectModifier: ViewModifier {
         }
     }
 }
+
+#endif
 
 #endif
