fc2a76d1ece5c99238fa05cfb4f8aeddd3de8fc5
Will Taylor <wtaylor151@gmail.com>
2026-06-03 10:49:50 -0500

    use correct id

--- Tests/StoreKitUnitTests/TestHelpers/StoreKitConfigTestCase+Extensions.swift
+++ Tests/StoreKitUnitTests/TestHelpers/StoreKitConfigTestCase+Extensions.swift
@@ -130,7 +130,7 @@ extension StoreKitConfigTestCase {
     static let consumableProductId = "com.revenuecat.consumable"
     static let nonConsumableProductId = "lifetime"
     static let nonRenewableProductId = "com.revenuecat.non_renewable"
-    static let productIDWithBillingPlans = "com.revenuecat.annual_with_commitment"
+    static let productIDWithBillingPlans = "com.revenuecat.sampleapp.monthly.12mocommitment"
 
 }
 
