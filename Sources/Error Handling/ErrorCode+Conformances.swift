b7312d99bb28d9b3b789b6ded5a63ccec0d36e13
Álvaro Brey <alvaro.brey@revenuecat.com>
2026-06-10 13:50:59 +0200

    Adopt generated ErrorCode

---
+++ Sources/Error Handling/ErrorCode+Conformances.swift
@@ -0,0 +1,55 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  ErrorCode+Conformances.swift
+//
+//  Created by Álvaro Brey on 6/10/26.
+//
+
+import Foundation
+
+extension ErrorCode: DescribableError {}
+
+extension ErrorCode: CustomNSError {
+
+    // swiftlint:disable missing_docs
+    public var errorUserInfo: [String: Any] {
+        return [
+            NSDebugDescriptionErrorKey: self.description,
+            "rc_code_name": self.codeName
+        ]
+    }
+
+}
+
+// MARK: - PurchasesErrorConvertible
+
+/// An `Error` that can be converted into a `PurchasesError`
+protocol PurchasesErrorConvertible: Swift.Error {
+
+    /// Convert the receiver into a `PurchasesError` with all the necessary context.
+    ///
+    /// ### Related symbols:
+    /// - ``ErrorUtils``
+    /// - ``ErrorCode``
+    var asPurchasesError: PurchasesError { get }
+
+}
+
+extension PurchasesErrorConvertible {
+
+    var asPublicError: PublicError {
+        return self.asPurchasesError.asPublicError
+    }
+
+    var description: String {
+        return self.asPurchasesError.localizedDescription
+    }
+
+}
