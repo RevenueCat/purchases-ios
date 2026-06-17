ab2084e305d859d0e453f322aee027346ca62ad2
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-06-17 17:08:13 +0000

    Version bump for 5.79.0

--- Sources/Misc/SystemInfo.swift
+++ Sources/Misc/SystemInfo.swift
@@ -112,7 +112,7 @@ class SystemInfo {
     }
 
     static var frameworkVersion: String {
-        return "5.79.0-SNAPSHOT"
+        return "5.79.0"
     }
 
     static var installationMethod: String {
