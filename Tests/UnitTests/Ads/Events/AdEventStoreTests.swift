448c8e28946edc476089dfb56afaefd0556a23b3
Pol Miro <polmiro@gmail.com>
2026-06-18 09:35:33 +0200

    fix: import @_spi(Internal) in AdEventStoreTests for captureMethod

--- Tests/UnitTests/Ads/Events/AdEventStoreTests.swift
+++ Tests/UnitTests/Ads/Events/AdEventStoreTests.swift
@@ -13,7 +13,7 @@
 
 import Foundation
 import Nimble
-@_spi(Experimental) @testable import RevenueCat
+@_spi(Internal) @_spi(Experimental) @testable import RevenueCat
 import XCTest
 
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, watchOS 8.0, *)
