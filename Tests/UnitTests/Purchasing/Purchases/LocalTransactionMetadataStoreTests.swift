eba544694d5c6fa3e2aa14ef89f8d8197e94f732
Mark Villacampa <MarkVillacampa@users.noreply.github.com>
2026-06-22 17:45:34 -0700

    improve tests

--- Tests/UnitTests/Purchasing/Purchases/LocalTransactionMetadataStoreTests.swift
+++ Tests/UnitTests/Purchasing/Purchases/LocalTransactionMetadataStoreTests.swift
@@ -512,11 +512,7 @@ class LocalTransactionMetadataStoreE2ETests: TestCase {
         expect(retrieved?.transactionData.metadata) == originalMetadata.transactionData.metadata
         expect(retrieved?.transactionData.aadAttributionToken) == originalMetadata.transactionData.aadAttributionToken
         expect(retrieved?.transactionData.storeCountry) == originalMetadata.transactionData.storeCountry
-        if let expectedTransferBehavior = originalMetadata.transactionData.transferBehavior?.rawValue {
-            expect(retrieved?.transactionData.transferBehavior?.rawValue) == expectedTransferBehavior
-        } else {
-            expect(retrieved?.transactionData.transferBehavior?.rawValue).to(beNil())
-        }
+        expect(retrieved?.transactionData.transferBehavior?.rawValue) == "keep_with_original_app_user_id"
 
         // Verify encoded receipt
         expect(retrieved?.encodedAppleReceipt) == originalMetadata.encodedAppleReceipt
@@ -540,6 +536,7 @@ class LocalTransactionMetadataStoreE2ETests: TestCase {
         expect(retrieved).toNot(beNil())
         expect(retrieved?.transactionId) == transactionId
         expect(retrieved?.productData).to(beNil())
+        expect(retrieved?.transactionData.transferBehavior).to(beNil())
         expect(retrieved?.originalPurchasesAreCompletedBy) == .myApp
         expect(retrieved?.sdkOriginated) == false
     }
@@ -1022,7 +1019,8 @@ class LocalTransactionMetadataStoreE2ETests: TestCase {
             unsyncedAttributes: nil,
             metadata: ["integration_key": "integration_value", "another_key": "another_value"],
             aadAttributionToken: "integration_attribution_token",
-            storeCountry: "US"
+            storeCountry: "US",
+            transferBehavior: TransferBehavior(rawValue: "keep_with_original_app_user_id")
         )
 
         return LocalTransactionMetadata(
