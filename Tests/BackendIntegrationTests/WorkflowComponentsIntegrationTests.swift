b1b9596e6e3f9dcf1e285243ad0f64ad23c856b8
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-07-16 18:28:45 +0200

    Fix integration test lint

--- Tests/BackendIntegrationTests/WorkflowComponentsIntegrationTests.swift
+++ Tests/BackendIntegrationTests/WorkflowComponentsIntegrationTests.swift
@@ -1,5 +1,5 @@
 //
-//  ProductionWorkflowsPaywallComponentsIntegrationTests.swift
+//  WorkflowComponentsIntegrationTests.swift
 //  BackendIntegrationTests
 //
 //  Created by Rick van der Linden on 7/15/26.
@@ -17,7 +17,7 @@ import XCTest
 #endif
 
 @MainActor
-final class ProductionWorkflowsPaywallComponentsIntegrationTests: BaseStoreKitIntegrationTests {
+final class WorkflowComponentsIntegrationTests: BaseStoreKitIntegrationTests {
 
     override class var storeKitVersion: StoreKitVersion { .storeKit2 }
     override class var useWorkflows: Bool { true }
