cf2a9da1c30e49276f60cefe0dbd5c01aea00bef
Peter Porfy <peter.porfy@revenuecat.com>
2026-07-30 18:39:17 +0200

    feat(ads): add optional tracking metadata to pollRewardVerification

---
+++ Sources/Ads/RewardVerification/RewardedAdTrackingMetadata.swift
@@ -0,0 +1,46 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  RewardedAdTrackingMetadata.swift
+//
+
+import Foundation
+
+/// Ad metadata for a rewarded ad events while polling reward verification.
+///
+/// Pass this to ``pollRewardVerification(clientTransactionID:trackingMetadata:)`` to have the
+/// SDK automatically track those events.
+@_spi(Experimental) public struct RewardedAdTrackingMetadata: Sendable {
+
+    // swiftlint:disable missing_docs
+    public let networkName: String?
+    public let mediatorName: MediatorName
+    public let adFormat: AdFormat
+    public let placement: String?
+    public let adUnitId: String
+    public let impressionId: String
+
+    public init(
+        networkName: String?,
+        mediatorName: MediatorName,
+        adFormat: AdFormat,
+        placement: String?,
+        adUnitId: String,
+        impressionId: String
+    ) {
+        self.networkName = networkName
+        self.mediatorName = mediatorName
+        self.adFormat = adFormat
+        self.placement = placement
+        self.adUnitId = adUnitId
+        self.impressionId = impressionId
+    }
+    // swiftlint:enable missing_docs
+
+}
