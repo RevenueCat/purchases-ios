df3be9c5d50e1134dd586beb9359e76c4aa3d7da
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-08-04 14:39:47 +0200

    Adopt checkpoint paywall outcome naming

--- Examples/CheckpointTester/CheckpointTester/Sources/Demo/DemoWorkflowLoader.swift
+++ Examples/CheckpointTester/CheckpointTester/Sources/Demo/DemoWorkflowLoader.swift
@@ -1,3 +1,17 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  DemoWorkflowLoader.swift
+//
+//  Created by Rick van der Linden.
+//
+
 import Foundation
 
 /// Demo-only workflow loading. Production workflows are fetched from RevenueCat's server.
