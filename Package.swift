68196e8494e0343b2fa4f2dbcaa3aecff404ab79
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-08-07 14:56:14 +0200

    fix: gate checkpoint UI tests behind feature flag

--- Package.swift
+++ Package.swift
@@ -143,6 +143,7 @@ let package = Package(
                         .copy("Resources/header.heic"),
                         .copy("Resources/background.heic"),
                         .copy("PaywallsV2/__PreviewResources__")
-                    ])
+                    ],
+                    swiftSettings: additionalCompilerFlags)
     ]
 )
