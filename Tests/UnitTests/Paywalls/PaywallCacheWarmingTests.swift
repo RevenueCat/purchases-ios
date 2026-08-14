f9d2829849eaf1035cfc5dcfec950ac5b6460020
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-08-14 15:06:47 +0200

    Remove environment-dependent font lookup test

--- Tests/UnitTests/Paywalls/PaywallCacheWarmingTests.swift
+++ Tests/UnitTests/Paywalls/PaywallCacheWarmingTests.swift
@@ -698,15 +698,6 @@ final class PaywallCacheWarmingTests: TestCase {
                                                         fontFamily: otherFamily)).to(beTrue())
     }
 
-    func testPlatformFontLookupIsNotCaseSensitive() throws {
-        let font = try Self.anyInstalledFont()
-        let uppercased = font.name.uppercased()
-        try XCTSkipIf(uppercased == font.name)
-
-        expect(Self.platformResolvesFont(named: font.name)).to(beTrue())
-        expect(Self.platformResolvesFont(named: uppercased)).to(beTrue())
-    }
-
     func testFontIsAlreadyInstalled_AgreesWithPlatformFontLookupOnCasing() throws {
         let font = try Self.anyInstalledFont()
         let miscased = font.name.uppercased()
