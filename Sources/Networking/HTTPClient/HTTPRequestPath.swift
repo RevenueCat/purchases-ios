43db23e48a734b30c1224983c350a516ab738a94
Dave DeLong <dave.delong@revenuecat.com>
2026-08-18 12:11:11 -0600

    slightly more graceful

--- Sources/Networking/HTTPClient/HTTPRequestPath.swift
+++ Sources/Networking/HTTPClient/HTTPRequestPath.swift
@@ -471,7 +471,7 @@ extension HTTPRequest.Path: HTTPRequestPath {
         case .getIntroEligibility:
             return "customer/intro_eligibility"
         case .logIn:
-            precondition(false, "The .login endpoint is not allowed when IAM is enabled")
+            assertionFailure("The .login endpoint is not allowed when IAM is enabled")
             return self.pathComponent
         case .postAttributionData:
             return "customer/attribution"
