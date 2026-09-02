e313187a5c725373e5bad0b98cba73c5db259ee2
Jacob Rakidzich <Jacob@JacobZivanDesign.com>
2026-09-02 08:09:45 -0500

    ensure stack sizing allows it to layout the distribution correctly

--- RevenueCatUI/Templates/V2/Components/Stack/StackComponentViewModel.swift
+++ RevenueCatUI/Templates/V2/Components/Stack/StackComponentViewModel.swift
@@ -227,7 +227,7 @@ extension PresentedStackPartial: PresentedPartial {
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 struct StackComponentStyle {
 
-    enum StackStrategy {
+    enum StackStrategy: Equatable {
         case normal, flex
     }
 
@@ -294,19 +294,7 @@ struct StackComponentStyle {
             return .normal
         }
 
-        switch distribution {
-        case .start, .center, .end:
-            return .normal
-        case .spaceBetween, .spaceAround, .spaceEvenly:
-            // We dont want to use a flex stack if its axis is set to fit.
-            // Otherwise we would be adding Spacer()'s which would make the stack act as fill.
-            switch self.size.height {
-            case .fit:
-                return .normal
-            default:
-                return .flex
-            }
-        }
+        return Self.strategy(for: distribution, sizeConstraint: self.size.height)
     }
 
     var hstackStrategy: StackStrategy {
@@ -315,14 +303,21 @@ struct StackComponentStyle {
             return .normal
         }
 
+        return Self.strategy(for: distribution, sizeConstraint: self.size.width)
+    }
+
+    static func strategy(
+        for distribution: PaywallComponent.FlexDistribution,
+        sizeConstraint: PaywallComponent.SizeConstraint
+    ) -> StackStrategy {
         switch distribution {
         case .start, .center, .end:
             return .normal
         case .spaceBetween, .spaceAround, .spaceEvenly:
-            // We dont want to use a flex stack if its axis is set to fit.
-            // Otherwise we would be adding Spacer()'s which would make the stack act as fill.
-            switch self.size.width {
-            case .fit:
+            // An unconstrained fit stack must not contain flexible spacers because that would make
+            // it act like fill. A positive minimum creates space that flex distribution must use.
+            switch sizeConstraint {
+            case let .fit(_, minMax) where minMax.min == nil || minMax.min == 0:
                 return .normal
             default:
                 return .flex
