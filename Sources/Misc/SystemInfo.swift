80d2d301828ff833026cb8ab03bf6435c434e942
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-09-03 06:15:19 +0000

    Preparing for next version

--- Sources/Misc/SystemInfo.swift
+++ Sources/Misc/SystemInfo.swift
@@ -117,7 +117,7 @@ class SystemInfo {
     }
 
     static var frameworkVersion: String {
-        return "5.88.0"
+        return "5.89.0-SNAPSHOT"
     }
 
     static var installationMethod: String {
