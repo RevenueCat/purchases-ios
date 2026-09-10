5f13432f03ebfe85ebe204061282a574d8c6f313
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-09-10 13:58:50 +0200

    test: preserve checkpoint result API coverage

--- Tests/APITesters/AllAPITests/RevenueCatUISwiftAPITester/CheckpointAPI.swift
+++ Tests/APITesters/AllAPITests/RevenueCatUISwiftAPITester/CheckpointAPI.swift
@@ -50,3 +50,29 @@ func checkCheckpointAPI(_ purchases: Purchases) {
     let _: CheckpointNoActionReason = .unknownCheckpoint
     let _: CheckpointNoActionReason = .invalidCheckpointIdentifier
 }
+
+private func checkCheckpointResultAPI(_ result: CheckpointResult) {
+    let _: String = result.description
+
+    if let presented = result as? CheckpointResult.PaywallPresented {
+        let outcome: CheckpointPaywallOutcome = presented.paywallOutcome
+        let _: String = outcome.description
+
+        if let purchased = outcome as? CheckpointPaywallOutcome.Purchased {
+            let _: StoreTransaction? = purchased.transaction
+            let _: CustomerInfo = purchased.customerInfo
+        } else if let restored = outcome as? CheckpointPaywallOutcome.Restored {
+            let _: CustomerInfo = restored.customerInfo
+        } else if let failed = outcome as? CheckpointPaywallOutcome.Error {
+            let _: PublicError = failed.error
+        } else if outcome is CheckpointPaywallOutcome.WebCheckoutOpened {
+            let _: Bool = outcome is CheckpointPaywallOutcome.WebCheckoutOpened
+        } else {
+            let _: Bool = outcome is CheckpointPaywallOutcome.Dismissed
+        }
+    } else if let receivedOffering = result as? CheckpointResult.ReceivedOffering {
+        let _: Offering = receivedOffering.offering
+    } else if let noAction = result as? CheckpointResult.NoAction {
+        let _: CheckpointNoActionReason = noAction.reason
+    }
+}
