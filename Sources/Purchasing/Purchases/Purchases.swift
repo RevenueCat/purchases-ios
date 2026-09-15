e952dc6a959da134dc2fc1a3ca577c0c1cb53394
Josh Holtz <me@joshholtz.com>
2026-09-15 16:02:37 -0500

    Trace Customer Center event delivery in CI validation

--- Sources/Purchasing/Purchases/Purchases.swift
+++ Sources/Purchasing/Purchases/Purchases.swift
@@ -1871,10 +1871,17 @@ public extension Purchases {
 
     /// Used by `RevenueCatUI` to keep track of ``CustomerCenterEvent``s.
     @_spi(Internal) func track(customerCenterEvent: any CustomerCenterEventType) {
+        Logger.debug("RC7726 Customer Center track submitted")
         operationDispatcher.dispatchOnWorkerThread {
+            Logger.debug("RC7726 Customer Center worker started")
             // If we make CustomerCenterEventType implement FeatureEvent, we have to make FeatureEvent public
-            guard let event = customerCenterEvent as? FeatureEvent else { return }
+            guard let event = customerCenterEvent as? FeatureEvent else {
+                Logger.debug("RC7726 Customer Center event cast failed")
+                return
+            }
+            Logger.debug("RC7726 Customer Center manager present: \(self.eventsManager != nil)")
             await self.eventsManager?.track(featureEvent: event)
+            Logger.debug("RC7726 Customer Center track completed")
         }
     }
 
