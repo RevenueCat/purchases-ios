68f6cf82ac63e067c24c2f2c118d58b63ed13bde
Josh Holtz <me@joshholtz.com>
2026-09-15 14:01:19 -0500

    Wait for StoreKit cleanup before observer-mode initialization

--- Tests/BackendIntegrationTests/StoreKitObserverModeIntegrationTests.swift
+++ Tests/BackendIntegrationTests/StoreKitObserverModeIntegrationTests.swift
@@ -152,7 +152,7 @@ class StoreKit1ObserverModeWithExistingPurchasesTests: BaseStoreKitObserverModeI
     func testDoesNotSyncExistingSK1Purchases() async throws {
         // 1. Purchase product directly from StoreKit
         try await self.manager.purchaseProductFromStoreKit1()
-        try await self.waitUntilNoUnfinishedTransactions()
+        try await self.waitUntilNoUnfinishedTransactions(timeout: .seconds(30))
 
         // 2. Configure SDK
         try await super.setUp()
@@ -165,7 +165,7 @@ class StoreKit1ObserverModeWithExistingPurchasesTests: BaseStoreKitObserverModeI
     func testDoesNotSyncExistingSK2Purchases() async throws {
         // 1. Purchase product directly from StoreKit
         try await self.manager.purchaseProductFromStoreKit2()
-        try await self.waitUntilNoUnfinishedTransactions()
+        try await self.waitUntilNoUnfinishedTransactions(timeout: .seconds(30))
 
         // 2. Configure SDK
         try await super.setUp()
