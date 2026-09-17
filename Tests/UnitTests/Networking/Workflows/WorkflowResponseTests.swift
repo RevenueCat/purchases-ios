3b43197ea8b1e662643f4d4343fcfdad55cf5d6c
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-09-17 10:51:51 +0200

    Fix required workflow step type tests

--- Tests/UnitTests/Networking/Workflows/WorkflowResponseTests.swift
+++ Tests/UnitTests/Networking/Workflows/WorkflowResponseTests.swift
@@ -518,16 +518,19 @@ class WorkflowResponseTests: TestCase {
         expect(step.metadata).to(beNil())
     }
 
-    func testDecodeWorkflowStepWithoutType() throws {
-        let step = try JSONDecoder.default.decode(WorkflowStep.self, from: Data(#"{ "id": "step_1" }"#.utf8))
-
-        expect(step.type).to(beNil())
+    func testDecodeWorkflowStepWithoutTypeThrows() {
+        XCTAssertThrowsError(
+            try JSONDecoder.default.decode(WorkflowStep.self, from: Data(#"{ "id": "step_1" }"#.utf8))
+        )
     }
 
     func testDecodeWorkflowStepOfferingIdentifier() throws {
         let step = try JSONDecoder.default.decode(
             WorkflowStep.self,
-            from: Data(#"{ "id": "step_1", "param_values": { "offering": { "identifier": "default" } } }"#.utf8)
+            from: Data((
+                #"{ "id": "step_1", "type": "offering", "param_values": "#
+                + #"{ "offering": { "identifier": "default" } } }"#
+            ).utf8)
         )
 
         expect(step.offeringIdentifier) == "default"
@@ -536,7 +539,10 @@ class WorkflowResponseTests: TestCase {
     func testDecodeWorkflowStepOfferingIdentifierFallsBackToFlatValue() throws {
         let step = try JSONDecoder.default.decode(
             WorkflowStep.self,
-            from: Data(#"{ "id": "step_1", "param_values": { "offering_identifier": "default" } }"#.utf8)
+            from: Data((
+                #"{ "id": "step_1", "type": "offering", "param_values": "#
+                + #"{ "offering_identifier": "default" } }"#
+            ).utf8)
         )
 
         expect(step.offeringIdentifier) == "default"
@@ -550,7 +556,7 @@ class WorkflowResponseTests: TestCase {
         ]
 
         for paramValues in values {
-            let json = #"{ "id": "step_1", "param_values": "# + paramValues + " }"
+            let json = #"{ "id": "step_1", "type": "offering", "param_values": "# + paramValues + " }"
             let step = try JSONDecoder.default.decode(
                 WorkflowStep.self,
                 from: Data(json.utf8)
